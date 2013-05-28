7e0bf876ee0d4c08bdb1ed03a3833df9fb8f4463
Rogozhkin, Dmitry V <dmitry.v.rogozhkin@intel.com>
2013-05-28 14:30:50 +0000

    Linux compilation fix.

--- _studio/shared/include/mfx_dxva2_device.h
+++ _studio/shared/include/mfx_dxva2_device.h
@@ -4,7 +4,7 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2007-2012 Intel Corporation. All Rights Reserved.
+Copyright(c) 2007-2013 Intel Corporation. All Rights Reserved.
 
 File Name: mfx_dxva2_device.h
 
@@ -67,8 +67,10 @@ protected:
     // Free DLL module
     void UnloadDLLModule(void);
 
+#if defined(MFX_VA_WIN)
     // Handle to the DLL library
     HMODULE m_hModule;
+#endif    
 
     // Number of adapters available
     mfxU32 m_numAdapters;
