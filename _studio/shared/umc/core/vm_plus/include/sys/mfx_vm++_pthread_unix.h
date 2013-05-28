e62405bca634673ae6f40f4ac1397398f02deb2f
Rogozhkin, Dmitry V <dmitry.v.rogozhkin@intel.com>
2013-05-28 09:07:42 +0000

    Forgotten files for vm_plus.

---
+++ _studio/shared/umc/core/vm_plus/include/sys/mfx_vm++_pthread_unix.h
@@ -0,0 +1,25 @@
+/* ****************************************************************************** *\ 
+ 
+ INTEL CORPORATION PROPRIETARY INFORMATION
+ This software is supplied under the terms of a license agreement or nondisclosure
+ agreement with Intel Corporation and may not be copied or disclosed except in
+ accordance with the terms of that agreement
+ Copyright(c) 2012-2013 Intel Corporation. All Rights Reserved.
+  
+\* ****************************************************************************** */
+
+#ifndef __MFX_VMPLUSPLUS_PTHREAD_UNIX_H__
+#define __MFX_VMPLUSPLUS_PTHREAD_UNIX_H__
+
+#if !defined(_WIN32) && !defined(_WIN64)
+
+#include <pthread.h>
+
+struct MfxMutexHandle
+{
+    pthread_mutex_t m_mutex;
+};
+
+#endif // #if defined(_WIN32) || defined(_WIN64)
+
+#endif //__MFX_VMPLUSPLUS_PTHREAD_UNIX_H__
---
+++ _studio/shared/umc/core/vm_plus/include/sys/mfx_vm++_pthread_windows.h
@@ -0,0 +1,26 @@
+/* ****************************************************************************** *\ 
+ 
+ INTEL CORPORATION PROPRIETARY INFORMATION
+ This software is supplied under the terms of a license agreement or nondisclosure
+ agreement with Intel Corporation and may not be copied or disclosed except in
+ accordance with the terms of that agreement
+ Copyright(c) 2012-2013 Intel Corporation. All Rights Reserved.
+  
+\* ****************************************************************************** */
+
+#ifndef __MFX_VMPLUSPLUS_PTHREAD_WINDOWS_H__
+#define __MFX_VMPLUSPLUS_PTHREAD_WINDOWS_H__
+
+#if defined(_WIN32) || defined(_WIN64)
+
+#include <windows.h>
+#include <process.h>
+
+struct MfxMutexHandle
+{
+    CRITICAL_SECTION m_CritSec;
+};
+
+#endif // #if defined(_WIN32) || defined(_WIN64)
+
+#endif //__MFX_VMPLUSPLUS_PTHREAD_WINDOWS_H__
