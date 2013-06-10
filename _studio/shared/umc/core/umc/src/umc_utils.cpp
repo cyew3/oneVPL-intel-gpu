dc4388c4fdadc95cc3fec0e0c6167e3420bf2f43
Baygulov, Aleksei <aleksei.baygulov@intel.com>
2013-06-10 09:03:20 +0000

    hevc string added

--- _studio/shared/umc/core/umc/src/umc_utils.cpp
+++ _studio/shared/umc/core/umc/src/umc_utils.cpp
@@ -153,6 +153,7 @@ namespace UMC
     { UMC::VC1_VIDEO,           VM_STRING("VC1") }, // for umc_video_enc_con
     { UMC::AVS_VIDEO,           VM_STRING("AVS") },
     { UMC::VP8_VIDEO,           VM_STRING("VP8") },
+    { UMC::HEVC_VIDEO,          VM_STRING("HEVC") },
   };
 
   static CodeStringTable StringsOfVideoSubType[] = {
