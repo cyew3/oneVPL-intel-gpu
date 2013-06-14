545bf3b8e19abda12be80cbe9a0ca0e257cf6d80
Shaporenko, Artem <artem.shaporenko@intel.com>
2013-06-14 09:26:11 +0000

    fix build after previous commit

--- _studio/mfx_lib/shared/src/mfx_h264_encode_d3d9.cpp
+++ _studio/mfx_lib/shared/src/mfx_h264_encode_d3d9.cpp
@@ -1216,7 +1216,7 @@ mfxStatus D3D9Encoder::QueryStatus(
     //WA for d3d9 runtime or driver bug in case of TDR:
     // set default error status, in normal case driver will update it to right status
     // in case if TDR occur driver device failed, runtime always return S_OK and doesn't change anything in fitback structures
-    for(int i=0; i< m_feedbackUpdate.size(); i++)
+    for(int i=0; i< (int)m_feedbackUpdate.size(); i++)
         m_feedbackUpdate[i].bStatus = ENCODE_ERROR;
     // if task is not in cache then query its status
     if (feedback == 0 || feedback->bStatus != ENCODE_OK)
