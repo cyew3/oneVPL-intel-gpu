91e3b5cd18b722e9d214a23d3313ff31aabc2816
Guschina, Elena <elena.guschina@intel.com>
2013-06-19 08:00:39 +0000

    AUDIO: libmfxsw_audio_decode.cpp libmfxsw_audio_encode.cpp added

---
+++ _studio/mfx_lib/shared/src/libmfxsw_audio_decode.cpp
@@ -0,0 +1,375 @@
+/* ****************************************************************************** *\
+
+INTEL CORPORATION PROPRIETARY INFORMATION
+This software is supplied under the terms of a license agreement or nondisclosure
+agreement with Intel Corporation and may not be copied or disclosed except in
+accordance with the terms of that agreement
+Copyright(c) 2008-2013 Intel Corporation. All Rights Reserved.
+
+File Name: libmfxsw_decode.cpp
+
+\* ****************************************************************************** */
+
+#include <mfxaudio.h>
+
+#include <mfx_session.h>
+#include <mfx_tools.h>
+#include <mfx_common.h>
+
+// scheduling and threading stuff
+#include <mfx_task.h>
+
+#include <libmfx_core.h>
+
+#if defined (MFX_ENABLE_AAC_AUDIO_DECODE)
+#include "mfx_aac_dec_decode.h"
+#endif
+
+#if defined (MFX_ENABLE_MP3_AUDIO_DECODE)
+#include "mfx_mp3_dec_decode.h"
+#endif
+
+
+
+AudioDECODE *CreateAudioDECODESpecificClass(mfxU32 CodecId, AudioCORE *core, mfxSession session)
+{
+    AudioDECODE *pDECODE = (AudioDECODE *) 0;
+    mfxStatus mfxRes = MFX_ERR_MEMORY_ALLOC;
+
+    // touch unreferenced parameter
+    session = session;
+
+    // create a codec instance
+    switch (CodecId)
+    {
+#if defined (MFX_ENABLE_AAC_AUDIO_DECODE)
+    case MFX_CODEC_AAC:
+        pDECODE = new AudioDECODEAAC(core, &mfxRes);
+        break;
+#endif
+
+#if defined (MFX_ENABLE_MP3_AUDIO_DECODE)
+    case MFX_CODEC_MP3:
+        pDECODE = new AudioDECODEMP3(core, &mfxRes);
+        break;
+#endif
+
+    default:
+        break;
+    }
+
+    // check error(s)
+    if (MFX_ERR_NONE != mfxRes)
+    {
+        delete pDECODE;
+        pDECODE = (AudioDECODE *) 0;
+    }
+
+    return pDECODE;
+
+} // AudioDECODE *CreateDECODESpecificClass(mfxU32 CodecId, AudioCORE *core)
+
+mfxStatus MFXAudioDECODE_Query(mfxSession session, mfxAudioParam *in, mfxAudioParam *out)
+{
+    MFX_CHECK(session, MFX_ERR_INVALID_HANDLE);
+    MFX_CHECK(out, MFX_ERR_NULL_PTR);
+
+    MFX_AUTO_LTRACE_FUNC(MFX_TRACE_LEVEL_API);
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, in);
+
+    mfxStatus mfxRes;
+
+    try
+    {
+        switch (out->mfx.CodecId)
+        {
+#ifdef MFX_ENABLE_AAC_AUDIO_DECODE
+        case MFX_CODEC_AAC:
+            mfxRes = AudioDECODEAAC::Query(session->m_pAudioCORE.get(), in, out);
+            break;
+#endif
+
+#ifdef MFX_ENABLE_MP3_AUDIO_DECODE
+        case MFX_CODEC_MP3:
+            mfxRes = AudioDECODEMP3::Query(session->m_pAudioCORE.get(), in, out);
+            break;
+#endif
+
+        default:
+            mfxRes = MFX_ERR_UNSUPPORTED;
+        }
+    }
+    // handle error(s)
+    catch(MFX_CORE_CATCH_TYPE)
+    {
+        mfxRes = MFX_ERR_UNKNOWN;
+    }
+
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, out);
+    MFX_LTRACE_I(MFX_TRACE_LEVEL_API, mfxRes);
+    return mfxRes;
+}
+
+mfxStatus MFXAudioDECODE_QueryIOSize(mfxSession session, mfxAudioParam *par, mfxAudioAllocRequest *request)
+{
+    MFX_CHECK(session, MFX_ERR_INVALID_HANDLE);
+    MFX_CHECK(par, MFX_ERR_NULL_PTR);
+    MFX_CHECK(request, MFX_ERR_NULL_PTR);
+
+    MFX_AUTO_LTRACE_FUNC(MFX_TRACE_LEVEL_API);
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, par);
+
+
+
+    mfxStatus mfxRes;
+    try
+    {
+        switch (par->mfx.CodecId)
+        {
+#ifdef MFX_ENABLE_AAC_AUDIO_DECODE
+        case MFX_CODEC_AAC:
+            mfxRes = AudioDECODEAAC::QueryIOSize(session->m_pAudioCORE.get(), par, request);
+            break;
+#endif
+
+#ifdef MFX_ENABLE_MP3_AUDIO_DECODE
+        case MFX_CODEC_MP3:
+            mfxRes = AudioDECODEMP3::QueryIOSize(session->m_pAudioCORE.get(), par, request);
+            break;
+#endif
+
+        default:
+            mfxRes = MFX_ERR_UNSUPPORTED;
+        }
+    }
+    // handle error(s)
+    catch(MFX_CORE_CATCH_TYPE)
+    {
+        mfxRes = MFX_ERR_UNKNOWN;
+    }
+
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, request);
+    MFX_LTRACE_I(MFX_TRACE_LEVEL_API, mfxRes);
+    return mfxRes;
+}
+
+mfxStatus MFXAudioDECODE_DecodeHeader(mfxSession session, mfxBitstream *bs, mfxAudioParam *par)
+{
+    MFX_CHECK(session, MFX_ERR_INVALID_HANDLE);
+    MFX_CHECK(bs, MFX_ERR_NULL_PTR);
+    MFX_CHECK(par, MFX_ERR_NULL_PTR);
+
+    MFX_AUTO_LTRACE_FUNC(MFX_TRACE_LEVEL_API);
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, bs);
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, par);
+
+    mfxStatus mfxRes;
+    try
+    {
+        switch (par->mfx.CodecId)
+        {
+#ifdef MFX_ENABLE_AAC_AUDIO_DECODE
+        case MFX_CODEC_AAC:
+            mfxRes = AudioDECODEAAC::DecodeHeader(session->m_pAudioCORE.get(), bs, par);
+            break;
+#endif
+
+#ifdef MFX_ENABLE_MP3_AUDIO_DECODE
+        case MFX_CODEC_MP3:
+            mfxRes = AudioDECODEMP3::DecodeHeader(session->m_pAudioCORE.get(), bs, par);
+            break;
+#endif
+        default:
+            mfxRes = MFX_ERR_UNSUPPORTED;
+        }
+    }
+    // handle error(s)
+    catch(MFX_CORE_CATCH_TYPE)
+    {
+        mfxRes = MFX_ERR_UNKNOWN;
+        if (0 == session)
+        {
+            return MFX_ERR_INVALID_HANDLE;
+        }
+    }
+
+    MFX_LTRACE_I(MFX_TRACE_LEVEL_API, mfxRes);
+    return mfxRes;
+}
+
+mfxStatus MFXAudioDECODE_Init(mfxSession session, mfxAudioParam *par)
+{
+    mfxStatus mfxRes;
+    MFX_CHECK(par, MFX_ERR_NULL_PTR);
+
+    MFX_AUTO_LTRACE_FUNC(MFX_TRACE_LEVEL_API);
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, par);
+
+    try
+    {
+        // check existence of component
+        if (!session->m_pAudioDECODE.get())
+        {
+            // create a new instance
+            session->m_pAudioDECODE.reset(CreateAudioDECODESpecificClass(par->mfx.CodecId, session->m_pAudioCORE.get(), session));
+        }
+        
+        mfxRes = session->m_pAudioDECODE->Init(par);
+
+    }
+    // handle error(s)
+    catch(MFX_CORE_CATCH_TYPE)
+    {
+        // set the default error value
+        mfxRes = MFX_ERR_UNKNOWN;
+        if (0 == session)
+        {
+            mfxRes = MFX_ERR_INVALID_HANDLE;
+        }
+        else if (0 == session->m_pAudioDECODE.get())
+        {
+            mfxRes = MFX_ERR_INVALID_AUDIO_PARAM;
+        }
+        else if (0 == par)
+        {
+            mfxRes = MFX_ERR_NULL_PTR;
+        }
+    }
+
+    MFX_LTRACE_I(MFX_TRACE_LEVEL_API, mfxRes);
+    return mfxRes;
+
+} // mfxStatus MFXAudioDECODE_Init(mfxSession session, mfxVideoParam *par)
+
+mfxStatus MFXAudioDECODE_Close(mfxSession session)
+{
+    mfxStatus mfxRes = MFX_ERR_NONE;
+
+    MFX_AUTO_LTRACE_FUNC(MFX_TRACE_LEVEL_API);
+
+    try
+    {
+        if (!session->m_pAudioDECODE.get())
+        {
+            return MFX_ERR_NOT_INITIALIZED;
+        }
+
+        // wait until all tasks are processed
+        session->m_pScheduler->WaitForTaskCompletion(session->m_pDECODE.get());
+
+        mfxRes = session->m_pAudioDECODE->Close();
+        // delete the codec's instance
+        session->m_pAudioDECODE.reset((AudioDECODE *) 0);
+    }
+    // handle error(s)
+    catch(MFX_CORE_CATCH_TYPE)
+    {
+        // set the default error value
+        mfxRes = MFX_ERR_UNKNOWN;
+        if (0 == session)
+        {
+            mfxRes = MFX_ERR_INVALID_HANDLE;
+        }
+    }
+
+    MFX_LTRACE_I(MFX_TRACE_LEVEL_API, mfxRes);
+    return mfxRes;
+
+} // mfxStatus MFXAudioDECODE_Close(mfxSession session)
+
+mfxStatus MFXAudioDECODE_DecodeFrameAsync(mfxSession session, mfxBitstream *bs ,mfxBitstream *buffer_out, mfxSyncPoint *syncp)
+{
+    mfxStatus mfxRes;
+
+#ifdef MFX_TRACE_ENABLE
+    MFX_AUTO_LTRACE_WITHID(MFX_TRACE_LEVEL_API, "MFX_DecodeAudioFrameAsync");
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, bs);
+#endif
+
+    try
+    {
+        mfxSyncPoint syncPoint = NULL;
+        MFX_TASK task;
+
+        // Wait for the bit stream
+        mfxRes = session->m_pScheduler->WaitForDependencyResolved(bs);
+        if (MFX_ERR_NONE != mfxRes)
+        {
+            return mfxRes;
+        }
+
+        // reset the sync point
+        *syncp = NULL;
+
+        memset(&task, 0, sizeof(MFX_TASK));
+        mfxRes = session->m_pAudioDECODE->DecodeFrameCheck(bs, buffer_out, &task.entryPoint);
+        // source data is OK, go forward
+        if (task.entryPoint.pRoutine)
+        {
+            mfxStatus mfxAddRes;
+
+            task.pOwner = session->m_pAudioDECODE.get();
+            task.priority = session->m_priority;
+            task.threadingPolicy = session->m_pAudioDECODE->GetThreadingPolicy();
+            // fill dependencies
+            task.pDst[0] = buffer_out;
+
+#ifdef MFX_TRACE_ENABLE
+            task.nParentId = MFX_AUTO_TRACE_GETID();
+            task.nTaskId = MFX::CreateUniqId() + MFX_TRACE_ID_DECODE;
+#endif
+
+            // register input and call the task
+            mfxAddRes = session->m_pScheduler->AddTask(task, &syncPoint);
+            if (MFX_ERR_NONE != mfxAddRes)
+            {
+                return mfxAddRes;
+            }
+        }
+
+        // return pointer to synchronization point
+        if (MFX_ERR_NONE == mfxRes)
+        {
+            *syncp = syncPoint;
+        }
+    }
+    // handle error(s)
+    catch(MFX_CORE_CATCH_TYPE)
+    {
+        // set the default error value
+        mfxRes = MFX_ERR_UNKNOWN;
+        if (0 == session)
+        {
+            return MFX_ERR_INVALID_HANDLE;
+        }
+        else if (0 == session->m_pDECODE.get())
+        {
+            return MFX_ERR_NOT_INITIALIZED;
+        }
+        else if (0 == syncp)
+        {
+            return MFX_ERR_NULL_PTR;
+        }
+    }
+
+    if (mfxRes == MFX_ERR_NONE)
+    {
+        if (syncp)
+        {
+            MFX_LTRACE_P(MFX_TRACE_LEVEL_API, *syncp);
+        }
+    }
+    MFX_LTRACE_I(MFX_TRACE_LEVEL_API, mfxRes);
+
+    return mfxRes;
+
+} // mfxStatus MFXAudioDECODE_DecodeFrameAsync(mfxSession session, mfxBitstream *bs ,mfxBitstream *buffer_out, mfxSyncPoint *syncp)
+
+//
+// THE OTHER DECODE FUNCTIONS HAVE IMPLICIT IMPLEMENTATION
+//
+
+FUNCTION_AUDIO_RESET_IMPL(DECODE, Reset, (mfxSession session, mfxAudioParam *par), (par))
+
+FUNCTION_AUDIO_IMPL(DECODE, GetAudioParam, (mfxSession session, mfxAudioParam *par), (par))
+
---
+++ _studio/mfx_lib/shared/src/libmfxsw_audio_encode.cpp
@@ -0,0 +1,312 @@
+/* ****************************************************************************** *\
+
+INTEL CORPORATION PROPRIETARY INFORMATION
+This software is supplied under the terms of a license agreement or nondisclosure
+agreement with Intel Corporation and may not be copied or disclosed except in
+accordance with the terms of that agreement
+Copyright(c) 2008-2013 Intel Corporation. All Rights Reserved.
+
+File Name: libmfxsw_audio_encode.cpp
+
+\* ****************************************************************************** */
+
+#include <mfxaudio.h>
+
+#include <mfx_session.h>
+#include <mfx_tools.h>
+#include <mfx_common.h>
+
+// scheduling and threading stuff
+#include <mfx_task.h>
+
+#include <libmfx_core.h>
+
+#if defined (MFX_ENABLE_AAC_AUDIO_ENCODE)
+#include "mfx_aac_encode.h"
+#endif
+
+
+AudioENCODE *CreateAudioENCODESpecificClass(mfxU32 CodecId, AudioCORE *core, mfxSession session)
+{
+    AudioENCODE *pENCODE = (AudioENCODE *) 0;
+    mfxStatus mfxRes = MFX_ERR_MEMORY_ALLOC;
+
+    // touch unreferenced parameter
+    session = session;
+    // create a codec instance
+    switch (CodecId)
+    {
+#if defined(MFX_ENABLE_AAC_AUDIO_ENCODE)
+    case MFX_CODEC_AAC:
+        pENCODE = new AudioENCODEAAC(core, &mfxRes);
+        break;
+#endif // MFX_ENABLE_AAC_AUDIO_ENCODE
+
+    default:
+        break;
+    }
+
+    // check error(s)
+    if (MFX_ERR_NONE != mfxRes)
+    {
+        delete pENCODE;
+        pENCODE = (AudioENCODE *) 0;
+    }
+
+    return pENCODE;
+
+} // AudioENCODE *CreateENCODESpecificClass(mfxU32 CodecId, AudioCORE *core, mfxSession session)
+
+mfxStatus MFXAudioENCODE_Query(mfxSession session, mfxAudioParam *in, mfxAudioParam *out)
+{
+    MFX_CHECK(session, MFX_ERR_INVALID_HANDLE);
+    MFX_CHECK(out, MFX_ERR_NULL_PTR);
+
+    mfxStatus mfxRes;
+    MFX_AUTO_LTRACE_FUNC(MFX_TRACE_LEVEL_API);
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, in);
+
+    try
+    {
+        switch (out->mfx.CodecId)
+        {
+#ifdef MFX_ENABLE_AAC_AUDIO_ENCODE
+        case MFX_CODEC_AAC:
+            mfxRes = AudioENCODEAAC::Query(session->m_pAudioCORE.get(), in, out);
+            break;
+#endif
+
+        default:
+            mfxRes = MFX_ERR_UNSUPPORTED;
+        }
+    }
+    // handle error(s)
+    catch(MFX_CORE_CATCH_TYPE)
+    {
+        mfxRes = MFX_ERR_NULL_PTR;
+    }
+    
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, out);
+    MFX_LTRACE_I(MFX_TRACE_LEVEL_API, mfxRes);
+    return mfxRes;
+}
+
+mfxStatus MFXAudioENCODE_QueryIOSize(mfxSession session, mfxAudioParam *par, mfxAudioAllocRequest *request)
+{
+    MFX_CHECK(session, MFX_ERR_INVALID_HANDLE);
+    MFX_CHECK(par, MFX_ERR_NULL_PTR);
+    MFX_CHECK(request, MFX_ERR_NULL_PTR);
+
+    mfxStatus mfxRes;
+    MFX_AUTO_LTRACE_FUNC(MFX_TRACE_LEVEL_API);
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, par);
+
+    try
+    {
+        switch (par->mfx.CodecId)
+        {
+#ifdef MFX_ENABLE_AAC_AUDIO_ENCODE
+        case MFX_CODEC_AAC:
+            mfxRes = AudioENCODEAAC::QueryIOSize(session->m_pAudioCORE.get(), par, request);
+            break;
+#endif // MFX_ENABLE_AAC_AUDIO_ENCODE
+
+
+
+        default:
+            mfxRes = MFX_ERR_UNSUPPORTED;
+        }
+    }
+    // handle error(s)
+    catch(MFX_CORE_CATCH_TYPE)
+    {
+        mfxRes = MFX_ERR_NULL_PTR;
+    }
+
+
+
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, request);
+    MFX_LTRACE_I(MFX_TRACE_LEVEL_API, mfxRes);
+    return mfxRes;
+}
+
+mfxStatus MFXAudioENCODE_Init(mfxSession session, mfxAudioParam *par)
+{
+    mfxStatus mfxRes;
+    MFX_CHECK(par, MFX_ERR_NULL_PTR);
+
+    MFX_AUTO_LTRACE_FUNC(MFX_TRACE_LEVEL_API);
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, par);
+
+    try
+    {
+        // check existence of component
+        if (!session->m_pAudioENCODE.get())
+        {
+            // create a new instance
+            session->m_bIsHWENCSupport = true;
+            session->m_pAudioENCODE.reset(CreateAudioENCODESpecificClass(par->mfx.CodecId, session->m_pAudioCORE.get(), session));
+        }
+
+        mfxRes = session->m_pAudioENCODE->Init(par);
+
+    }
+    // handle error(s)
+    catch(MFX_CORE_CATCH_TYPE)
+    {
+        // set the default error value
+        mfxRes = MFX_ERR_UNKNOWN;
+        if (0 == session)
+        {
+            mfxRes = MFX_ERR_INVALID_HANDLE;
+        }
+
+        if (0 == session->m_pAudioENCODE.get())
+        {
+            mfxRes = MFX_ERR_INVALID_VIDEO_PARAM;
+        }
+
+        if (0 == par)
+        {
+            mfxRes = MFX_ERR_NULL_PTR;
+        }
+    }
+
+    MFX_LTRACE_I(MFX_TRACE_LEVEL_API, mfxRes);
+    return mfxRes;
+
+} // mfxStatus MFXVideoENCODE_Init(mfxSession session, mfxVideoParam *par)
+
+mfxStatus MFXAudioENCODE_Close(mfxSession session)
+{
+    mfxStatus mfxRes = MFX_ERR_NONE;
+
+    MFX_AUTO_LTRACE_FUNC(MFX_TRACE_LEVEL_API);
+
+    try
+    {
+        if (!session->m_pAudioENCODE.get())
+        {
+            return MFX_ERR_NOT_INITIALIZED;
+        }
+
+        // wait until all tasks are processed
+        session->m_pScheduler->WaitForTaskCompletion(session->m_pAudioENCODE.get());
+
+        mfxRes = session->m_pAudioENCODE->Close();
+        // delete the codec's instance
+        session->m_pAudioENCODE.reset((AudioENCODE *) 0);
+    }
+    // handle error(s)
+    catch(MFX_CORE_CATCH_TYPE)
+    {
+        // set the default error value
+        mfxRes = MFX_ERR_UNKNOWN;
+        if (0 == session)
+        {
+            mfxRes = MFX_ERR_INVALID_HANDLE;
+        }
+    }
+
+    MFX_LTRACE_I(MFX_TRACE_LEVEL_API, mfxRes);
+    return mfxRes;
+
+} // mfxStatus MFXAudioENCODE_Close(mfxSession session)
+
+
+mfxStatus MFXAudioENCODE_EncodeFrameAsync(mfxSession session, mfxBitstream *bs, mfxBitstream *buffer_out, mfxSyncPoint *syncp)
+{
+    mfxStatus mfxRes;
+
+#ifdef MFX_TRACE_ENABLE
+    MFX_AUTO_LTRACE_WITHID(MFX_TRACE_LEVEL_API, "MFX_EncodeAudioFrameAsync");
+    MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, bs);
+#endif
+
+    try
+    {
+        mfxSyncPoint syncPoint = NULL;
+        MFX_TASK task;
+
+        // Wait for the bit stream
+        mfxRes = session->m_pScheduler->WaitForDependencyResolved(bs);
+        if (MFX_ERR_NONE != mfxRes)
+        {
+            return mfxRes;
+        }
+
+        // reset the sync point
+        *syncp = NULL;
+
+        memset(&task, 0, sizeof(MFX_TASK));
+        mfxRes = session->m_pAudioENCODE->EncodeFrameCheck(bs, buffer_out, &task.entryPoint);
+        // source data is OK, go forward
+        if (task.entryPoint.pRoutine)
+        {
+            mfxStatus mfxAddRes;
+
+            task.pOwner = session->m_pAudioDECODE.get();
+            task.priority = session->m_priority;
+            task.threadingPolicy = session->m_pAudioENCODE->GetThreadingPolicy();
+            // fill dependencies
+            task.pDst[0] = buffer_out;
+
+#ifdef MFX_TRACE_ENABLE
+            task.nParentId = MFX_AUTO_TRACE_GETID();
+            task.nTaskId = MFX::CreateUniqId() + MFX_TRACE_ID_DECODE;
+#endif
+
+            // register input and call the task
+            mfxAddRes = session->m_pScheduler->AddTask(task, &syncPoint);
+            if (MFX_ERR_NONE != mfxAddRes)
+            {
+                return mfxAddRes;
+            }
+        }
+
+        // return pointer to synchronization point
+        if (MFX_ERR_NONE == mfxRes)
+        {
+            *syncp = syncPoint;
+        }
+    }
+    // handle error(s)
+    catch(MFX_CORE_CATCH_TYPE)
+    {
+        // set the default error value
+        mfxRes = MFX_ERR_UNKNOWN;
+        if (0 == session)
+        {
+            return MFX_ERR_INVALID_HANDLE;
+        }
+        else if (0 == session->m_pDECODE.get())
+        {
+            return MFX_ERR_NOT_INITIALIZED;
+        }
+        else if (0 == syncp)
+        {
+            return MFX_ERR_NULL_PTR;
+        }
+    }
+
+    if (mfxRes == MFX_ERR_NONE)
+    {
+        if (syncp)
+        {
+            MFX_LTRACE_P(MFX_TRACE_LEVEL_API, *syncp);
+        }
+    }
+    MFX_LTRACE_I(MFX_TRACE_LEVEL_API, mfxRes);
+
+    return mfxRes;
+
+} // mfxStatus MFXAudioENCODE_EncodeFrameAsync(mfxSession session, mfxBitstream *bs, mfxBitstream *buffer_out, mfxSyncPoint *syncp)
+
+//
+// THE OTHER ENCODE FUNCTIONS HAVE IMPLICIT IMPLEMENTATION
+//
+
+FUNCTION_AUDIO_RESET_IMPL(ENCODE, Reset, (mfxSession session, mfxAudioParam *par), (par))
+
+FUNCTION_AUDIO_IMPL(ENCODE, GetAudioParam, (mfxSession session, mfxAudioParam *par), (par))
+
