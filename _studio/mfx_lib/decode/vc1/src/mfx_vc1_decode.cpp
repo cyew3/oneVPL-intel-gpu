f543f48fb13412007e2847dd2fc3fcbf345478a0
Nabiullin, Oleg <oleg.nabiullin@intel.com>
2013-07-04 17:07:59 +0000

    vc1 decode: fix for decoding corrupted streams.

--- _studio/mfx_lib/decode/vc1/src/mfx_vc1_decode.cpp
+++ _studio/mfx_lib/decode/vc1/src/mfx_vc1_decode.cpp
@@ -1356,6 +1356,8 @@ mfxStatus MFXVideoDECODEVC1::SelfConstructFrame(mfxBitstream *bs)
         }
 
         Ipp32u dataSize = ((bs->DataLength) > (m_FrameSize - ReadDataSize))?(m_FrameSize - ReadDataSize):(bs->DataLength);
+        Ipp32u remainedBytes = m_FrameConstrData.GetBufferSize() - m_FrameConstrData.GetDataSize();
+        dataSize = (dataSize > remainedBytes) ? remainedBytes : dataSize;
         ippsCopy_8u(bs->Data + bs->DataOffset,
                    (Ipp8u*)m_FrameConstrData.GetBufferPointer() + m_FrameConstrData.GetDataSize(),
                     dataSize);
