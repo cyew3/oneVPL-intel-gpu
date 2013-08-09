43359103648e986dc593f02edead17aae79ab4bb
Shellunts, Artur <artur.shellunts@intel.com>
2013-08-09 12:18:01 +0000

    Fixed a bug in vm_sys_info_get_program_path: missing ending NULL in result C string.

--- _studio/shared/umc/core/vm/src/vm_sys_info_win32.c
+++ _studio/shared/umc/core/vm/src/vm_sys_info_win32.c
@@ -4,7 +4,7 @@
 //     This software is supplied under the terms of a license agreement or
 //     nondisclosure agreement with Intel Corporation and may not be copied
 //     or disclosed except in accordance with the terms of that agreement.
-//       Copyright(c) 2003-2011 Intel Corporation. All Rights Reserved.
+//       Copyright(c) 2003-2013 Intel Corporation. All Rights Reserved.
 //
 */
 
@@ -252,6 +252,7 @@ void vm_sys_info_get_program_path(vm_char *program_path)
     GetModuleFileName(NULL, tmp, _MAX_LEN);
     i = (Ipp32s) (vm_string_strrchr(tmp, (vm_char)('\\')) - tmp + 1);
     vm_string_strncpy(program_path, tmp, i - 1);
+    program_path[i - 1] = 0;
 
 } /* void vm_sys_info_get_program_path(vm_char *program_path) */
 
