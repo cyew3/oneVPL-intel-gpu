b5c5d8ec3cf7566abdd563bafd05df163efeb52c
Shellunts, Artur <artur.shellunts@intel.com>
2013-08-09 16:20:29 +0000

    Fixed race condition issue in OperatorCore

--- _studio/shared/include/libmfx_core_operation.h
+++ _studio/shared/include/libmfx_core_operation.h
@@ -4,9 +4,9 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2007-2011 Intel Corporation. All Rights Reserved.
+Copyright(c) 2007-2013 Intel Corporation. All Rights Reserved.
 
-File Name: libmfx_core_aperation.h
+File Name: libmfx_core_operation.h
 
 \* ****************************************************************************** */
 #ifndef __LIBMFX_CORE_OPERATOR_H__
@@ -14,6 +14,8 @@ File Name: libmfx_core_aperation.h
 
 #include <vector>
 #include <vm_interlocked.h>
+#include <umc_mutex.h>
+#include <umc_automatic_mutex.h>
 
 class VideoCORE;
 
@@ -22,17 +24,20 @@ class OperatorCORE
 public:
     OperatorCORE(VideoCORE* pCore):m_refCounter(1)
     {
+        UMC::AutomaticUMCMutex guard(m_guard);
         m_Cores.push_back(pCore);
         pCore->SetCoreId(0);
     };
 
     void AddCore(VideoCORE* pCore)
     {
+        UMC::AutomaticUMCMutex guard(m_guard);
         m_Cores.push_back(pCore);
         pCore->SetCoreId((mfxU32)m_Cores.size()-1);
     }
     void RemoveCore(VideoCORE* pCore)
     {
+        UMC::AutomaticUMCMutex guard(m_guard);
         std::vector<VideoCORE*>::iterator it = m_Cores.begin();
         for (;it != m_Cores.end();it++)
         {
@@ -47,6 +52,7 @@ public:
     // functor to run fuction from child cores
     bool  IsOpaqSurfacesAlreadyMapped(mfxFrameSurface1 **pOpaqueSurface, mfxU32 NumOpaqueSurface, mfxFrameAllocResponse *response)
     {
+        UMC::AutomaticUMCMutex guard(m_guard);
         bool sts;
         std::vector<VideoCORE*>::iterator it = m_Cores.begin();
 
@@ -62,6 +68,7 @@ public:
     // functor to run fuction from child cores
     bool CheckOpaqRequest(mfxFrameAllocRequest *request, mfxFrameSurface1 **pOpaqueSurface, mfxU32 NumOpaqueSurface)
     {
+        UMC::AutomaticUMCMutex guard(m_guard);
         bool sts;
         std::vector<VideoCORE*>::iterator it = m_Cores.begin();
 
@@ -78,6 +85,7 @@ public:
     template <typename func, typename arg, typename arg2>
     mfxStatus DoFrameOperation(func functor, arg par, arg2 out)
     {
+        UMC::AutomaticUMCMutex guard(m_guard);
         mfxStatus sts;
         std::vector<VideoCORE*>::iterator it = m_Cores.begin();
 
@@ -95,6 +103,7 @@ public:
     template <typename func, typename arg>
     mfxStatus DoCoreOperation(func functor, arg par)
     {
+        UMC::AutomaticUMCMutex guard(m_guard);
         mfxStatus sts;
         std::vector<VideoCORE*>::iterator it = m_Cores.begin();
 
@@ -112,6 +121,7 @@ public:
     template <typename func, typename arg>
     mfxFrameSurface1* GetSurface(func functor, arg par)
     {
+        UMC::AutomaticUMCMutex guard(m_guard);
         mfxFrameSurface1* pSurf;
         std::vector<VideoCORE*>::iterator it = m_Cores.begin();
         for (;it != m_Cores.end();it++)
@@ -128,7 +138,8 @@ public:
     virtual
     void AddRef(void)
     {
-        vm_interlocked_inc32(&m_refCounter);
+        UMC::AutomaticUMCMutex guard(m_guard);
+        m_refCounter++;
     };
     // Decrement reference counter of the object.
     // If the counter is equal to zero, destructor is called and
@@ -136,7 +147,8 @@ public:
     virtual
     void Release(void)
     {
-        vm_interlocked_dec32(&m_refCounter);
+        UMC::AutomaticUMCMutex guard(m_guard);
+        m_refCounter--;
 
         if (0 == m_refCounter)
         {
@@ -147,12 +159,14 @@ public:
     virtual
     mfxU32 GetNumRef(void)
     {
+        UMC::AutomaticUMCMutex guard(m_guard);
         return m_refCounter;
     };
 private:
 
     virtual ~OperatorCORE()
     {
+        UMC::AutomaticUMCMutex guard(m_guard);
         m_Cores.clear();
     };
     // self and child cores
@@ -160,6 +174,8 @@ private:
 
     // Reference counters
     mfxU32 m_refCounter;
+
+    UMC::Mutex m_guard;
 
     // Forbid the assignment operator
     OperatorCORE & operator = (const OperatorCORE &);
