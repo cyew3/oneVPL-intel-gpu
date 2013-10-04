71d0a568f512389fd3583d289df2c29b6a35f921
Sosunova, Maria <maria.sosunova@intel.com>
2013-10-04 13:57:35 +0000

    what changed - fix behavior aac_sbr_ds_off option in audio_dec_con

--- _studio/mfx_lib/audio_decode/aac/src/mfx_aac_dec_decode.cpp
+++ _studio/mfx_lib/audio_decode/aac/src/mfx_aac_dec_decode.cpp
@@ -96,10 +96,10 @@ mfxStatus AudioDECODEAAC::Init(mfxAudioParam *par)
     }
 
     switch(par->mfx.ModeDwnsmplHEAACprofile)  {
-case MFX_AUDIO_AAC_HE_DWNSMPL_OFF:
+case MFX_AUDIO_AAC_HE_DWNSMPL_ON:
     params.ModeDwnsmplHEAACprofile = HEAAC_DWNSMPL_ON;
     break;
-case MFX_AUDIO_AAC_HE_DWNSMPL_ON:
+case MFX_AUDIO_AAC_HE_DWNSMPL_OFF:
     params.ModeDwnsmplHEAACprofile = HEAAC_DWNSMPL_OFF;
     break;
 default:
@@ -764,14 +764,7 @@ mfxStatus MFX_AAC_Utility::Query(AudioCORE *core, mfxAudioParam *in, mfxAudioPar
         }
         else
         {
-            if(modeDwnsmplHEAACprofile == 0)
-            {
-                out->mfx.ModeDwnsmplHEAACprofile = MFX_AUDIO_AAC_HE_DWNSMPL_OFF;
-            }
-            else
-            {
-                sts = MFX_ERR_UNSUPPORTED;
-            }
+            sts = MFX_ERR_UNSUPPORTED;
         }
 
         mfxU32 flagPSSupportLev = in->mfx.FlagPSSupportLev;
