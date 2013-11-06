f4e4e186668064c726687536f9325b9586dc2df6
Smirnov, Eugene <eugene.smirnov@intel.com>
2013-11-06 19:14:40 +0000

    mfx_get_dll_handle

--- opensource/mfx_dispatch/include/mfx_load_dll.h
+++ opensource/mfx_dispatch/include/mfx_load_dll.h
@@ -46,6 +46,8 @@ namespace MFX
     mfxStatus mfx_get_default_audio_dll_name(msdk_disp_char *pPath, size_t pathSize, eMfxImplType implType);
 
     mfxModuleHandle mfx_dll_load(const msdk_disp_char *file_name);
+    //increments reference counter
+    mfxModuleHandle mfx_get_dll_handle(const msdk_disp_char *file_name);
     mfxFunctionPointer mfx_dll_get_addr(mfxModuleHandle handle, const char *func_name);
     bool mfx_dll_free(mfxModuleHandle handle);
 
