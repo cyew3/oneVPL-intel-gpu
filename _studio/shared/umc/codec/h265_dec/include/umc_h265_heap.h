b828892813541ca9faa4a1a7e736bcc3a6052241
Nabiullin, Oleg <oleg.nabiullin@intel.com>
2014-02-06 16:16:26 +0000

    HEVC Decoder: fixed UB, which lead to memory leak on Linux.

--- _studio/shared/umc/codec/h265_dec/include/umc_h265_heap.h
+++ _studio/shared/umc/codec/h265_dec/include/umc_h265_heap.h
@@ -4,7 +4,7 @@
 //  This software is supplied under the terms of a license  agreement or
 //  nondisclosure agreement with Intel Corporation and may not be copied
 //  or disclosed except in  accordance  with the terms of that agreement.
-//        Copyright (c) 2012-2013 Intel Corporation. All Rights Reserved.
+//        Copyright (c) 2012-2014 Intel Corporation. All Rights Reserved.
 //
 //
 */
@@ -23,7 +23,7 @@ namespace UMC_HEVC_DECODER
 {
 
 Ipp8u * CumulativeArraysAllocation(int n, int align, ...);
-void CumulativeFree(void * ptr);
+void CumulativeFree(Ipp8u * ptr);
 
 //*********************************************************************************************/
 //
