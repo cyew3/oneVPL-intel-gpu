69aab51387ba69841465bfe97e484f3c14b8c6eb
Frolov, Pavel V <pavel.v.frolov@intel.com>
2014-02-18 09:57:41 +0000

    Recompile with CM2.4 - fixed issue with CM3.0 performance.

--- _studio/mfx_lib/genx/h264_encode/include/genx_hsw_simple_me_isa.h
+++ _studio/mfx_lib/genx/h264_encode/include/genx_hsw_simple_me_isa.h
@@ -3,11 +3,11 @@
 //     This software is supplied under the terms of a license agreement or
 //     nondisclosure agreement with Intel Corporation and may not be copied
 //     or disclosed except in accordance with the terms of that agreement.
-//          Copyright(c) 2012-2013 Intel Corporation. All Rights Reserved.
+//          Copyright(c) 2012-2014 Intel Corporation. All Rights Reserved.
 //
 */
 
 #ifndef __genx_hsw_simple_me_isa__
 #define __genx_hsw_simple_me_isa__
-extern const unsigned char genx_hsw_simple_me[259259];
+extern const unsigned char genx_hsw_simple_me[294454];
 #endif
