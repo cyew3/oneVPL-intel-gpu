97942a7f1c65f3c62d5102a91bf596d01ef874e6
Shaporenko, Artem <artem.shaporenko@intel.com>
2014-03-04 11:06:44 +0000

    fixed year in RC files

--- _studio/mfx_lib/plugin/resource.h
+++ _studio/mfx_lib/plugin/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by libmfxhw_plugin_hevcd.rc
+// Used by libmfxsw_plugin_hevce.rc
 //
 
 // Next default values for new objects
