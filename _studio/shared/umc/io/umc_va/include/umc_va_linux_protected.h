8886f0a0aa95af1823a9e3cfe01d6535bcb8d278
Brazhkin, Dmitry <dmitry.brazhkin@intel.com>
2014-03-07 15:54:08 +0000

    Build fix: add missing files

---
+++ _studio/shared/umc/io/umc_va/include/umc_va_linux_protected.h
@@ -0,0 +1,68 @@
+/* /////////////////////////////////////////////////////////////////////////////
+//
+//                  INTEL CORPORATION PROPRIETARY INFORMATION
+//     This software is supplied under the terms of a license agreement or
+//     nondisclosure agreement with Intel Corporation and may not be copied
+//     or disclosed except in accordance with the terms of that agreement.
+//          Copyright(c) 2014 Intel Corporation. All Rights Reserved.
+*/
+
+#pragma once
+
+#include "umc_va_base.h"
+
+#ifdef UMC_VA_LINUX
+
+#ifndef __UMC_VA_LINUX_PROTECTED_H_
+#define __UMC_VA_LINUX_PROTECTED_H_
+
+#include "mfxpcp.h"
+#include "mfx_utils.h"
+#include "vaapi_ext_interface.h"
+
+namespace UMC
+{
+
+class ProtectedVA
+{
+public:
+
+    ProtectedVA(mfxU16 p = 0);
+    virtual ~ProtectedVA()
+    {
+    }
+
+    virtual mfxU16 GetProtected() const;
+
+    void SetProtected(mfxU16 p);
+
+    Status SetModes(mfxVideoParam * params);
+
+    Ipp32s GetEncryptionMode() const;
+
+    Ipp32s GetCounterMode() const;
+
+    void SetBitstream(mfxBitstream *bs);
+
+    mfxBitstream * GetBitstream();
+
+    mfxU32 GetBSCurrentEncrypt() const;
+    void MoveBSCurrentEncrypt(mfxI32 count);
+    void ResetBSCurrentEncrypt();
+
+protected:
+    mfxU16 m_protected;
+    mfxBitstream *m_bs;
+
+    Ipp32s m_counterMode;
+    Ipp32s m_encryptionType;
+
+    Ipp32u m_encryptBegin;
+    Ipp32u m_encryptCount;
+};
+
+}
+
+#endif // __UMC_VA_LINUX_PROTECTED_H_
+
+#endif // UMC_VA_LINUX
---
+++ _studio/shared/umc/io/umc_va/src/umc_va_linux_protected.cpp
@@ -0,0 +1,132 @@
+/* /////////////////////////////////////////////////////////////////////////////
+//
+//                  INTEL CORPORATION PROPRIETARY INFORMATION
+//     This software is supplied under the terms of a license agreement or
+//     nondisclosure agreement with Intel Corporation and may not be copied
+//     or disclosed except in accordance with the terms of that agreement.
+//          Copyright(c) 2014 Intel Corporation. All Rights Reserved.
+*/
+
+
+#include "umc_va_base.h"
+
+#ifdef UMC_VA_LINUX
+
+#include "umc_va_linux_protected.h"
+#include "umc_va_linux.h"
+
+using namespace UMC;
+
+static mfxExtBuffer* GetExtBuffer(mfxExtBuffer** extBuf, mfxU32 numExtBuf, mfxU32 id)
+{
+    if (extBuf != 0)
+    {
+        for (mfxU16 i = 0; i < numExtBuf; i++)
+        {
+            if (extBuf[i] != 0 && extBuf[i]->BufferId == id) // assuming aligned buffers
+                return (extBuf[i]);
+        }
+    }
+
+    return 0;
+}
+
+/////////////////////////////////////////////////
+ProtectedVA::ProtectedVA(mfxU16 p)
+    : m_protected(p)
+    , m_bs(0)
+    , m_counterMode(0)
+    , m_encryptionType(0)
+    , m_encryptBegin(0)
+    , m_encryptCount(0)
+{
+}
+
+mfxU16 ProtectedVA::GetProtected() const
+{
+    return m_protected;
+}
+
+void ProtectedVA::SetProtected(mfxU16 p)
+{
+    m_protected = p;
+}
+
+Status ProtectedVA::SetModes(mfxVideoParam * params)
+{
+    if (IS_PROTECTION_PAVP_ANY(m_protected))
+    {
+        mfxExtPAVPOption const * extPAVP = (mfxExtPAVPOption*)GetExtBuffer(params->ExtParam, params->NumExtParam, MFX_EXTBUFF_PAVP_OPTION);
+
+        if (!extPAVP)
+            return UMC_ERR_FAILED;
+
+        m_encryptionType = extPAVP->EncryptionType;
+
+        m_counterMode = extPAVP->CounterType;
+    }
+    else
+    {
+        m_encryptionType = MFX_PAVP_AES128_CTR;
+        m_counterMode = MFX_PAVP_CTR_TYPE_C;
+    }
+
+    return UMC_OK;
+}
+
+Ipp32s ProtectedVA::GetEncryptionMode() const
+{
+    return m_encryptionType;
+}
+
+Ipp32s ProtectedVA::GetCounterMode() const
+{
+    return m_counterMode;
+}
+
+void ProtectedVA::SetBitstream(mfxBitstream *bs)
+{
+    if (m_encryptBegin && m_bs == bs)
+        return;
+
+    m_bs = bs;
+    m_encryptCount = 0;
+    m_encryptBegin = 0;
+
+    if (!m_bs || !m_bs->EncryptedData)
+        return;
+
+    mfxEncryptedData * temp = bs->EncryptedData;
+    while (temp)
+    {
+        m_encryptCount++;
+        temp = temp->Next;
+    }
+}
+
+mfxBitstream * ProtectedVA::GetBitstream()
+{
+    return m_bs;
+}
+
+mfxU32 ProtectedVA::GetBSCurrentEncrypt() const
+{
+    return m_encryptBegin;
+}
+
+void ProtectedVA::ResetBSCurrentEncrypt()
+{
+    m_encryptBegin = 0;
+}
+
+void ProtectedVA::MoveBSCurrentEncrypt(mfxI32 count)
+{
+    m_encryptBegin += count;
+
+    if (m_encryptBegin >= m_encryptCount)
+    {
+        ResetBSCurrentEncrypt();
+    }
+}
+
+#endif // UMC_VA_LINUX
