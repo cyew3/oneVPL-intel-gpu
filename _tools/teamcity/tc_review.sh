5da3557dbdcd73ebffb8503ef4e5dd64635b7e97
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2014-03-15 14:03:08 +0400

    TeamCity script to automate code review, script to strips tabs
    Change-Id: I1e62ddf1c193e0483d2ab65d195f3e874201e268
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

---
+++ _tools/teamcity/tc_review.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Script usage:
+#  tc_review <username> <branch>
+# Call the script from the repo root folder.
+
+__STATUS=0
+
+function is_gerrit_branch
+{
+  if echo $1 | grep -q -E refs/changes/[0-9]{2}/[0-9]{4}/[0-9]+$ ; then
+    return 0;
+  else
+    return 1;
+  fi
+}
+
+function is_first_patchset
+{
+  if echo $1 | grep -q -E refs/changes/[0-9]{2}/[1-9]{4}/1$ ; then
+    return 0;
+  else
+    return 1;
+  fi
+}
+
+function set_status
+{
+  if [ $1 -ne 0 ] ; then
+    __STATUS=$1
+  fi
+}
+
+_USERNAME=$1
+_GIT_BRANCH=$2
+
+echo "info: _USERNAME=$_USERNAME"
+echo "info: _GIT_BRANCH=$_GIT_BRANCH"
+
+# Here we set reviewers. We are doing that for the first patchset only.
+if is_first_patchset $_GIT_BRANCH; then
+  echo "Setting reviewers for the $_GIT_BRANCH"
+  ./_tools/set_reviewers.sh $_USERNAME `git rev-parse HEAD`
+  set_status $?
+else
+  echo "That's not the first patch set - no need to set reviewers"
+fi
+
+# Here we check for the coding guidelines. We simply apply them and check whether anything
+# have changed in the repo.
+./_tools/strip_tabs.sh
+./_tools/strip_tailing_spaces.sh
+
+__CHANGES=$(git status --porcelain -uno)
+__REVIEW=
+__VOTE=
+if echo "$__CHANGES" | grep -q . ; then
+  echo "error: the change violates coding guidelines in the following files:"
+  echo "$__CHANGES"
+  echo "The diff looks like:"
+  git --no-pager diff
+  __DIFF=$(git diff | head)
+  __REVIEW="
+The change violates the coding guidelines in the following files:
+$__CHANGES
+
+The head of the diff looks like:
+$__DIFF
+
+Consider to run and reupload the change:
+  ./_tools/tc_review.sh <username> HEAD
+  git commit --amend .
+"
+  __VOTE=-1
+  set_status -1
+else
+  echo "The change follows coding guidelines"
+  __REVIEW="The change follows coding guidelines."
+  __VOTE=+1
+fi
+
+if is_gerrit_branch $_GIT_BRANCH ; then
+  ssh -p 29418 $_USERNAME@git-ccr-1.devtools.intel.com gerrit review \
+    -p mdp_msdk-samples \
+    --code-review $__VOTE \
+    \'--message="$__REVIEW"\' \
+    `git rev-parse HEAD`
+
+  set_status $?
+fi
+
+exit $__STATUS
