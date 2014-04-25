b63f378d6b4c10375926abaec3753d1dbc586c07
Efremov, Stanislav <stanislav.efremov@intel.com>
2014-04-25 11:18:49 +0000

    New status reporting DX9 interface for VP8 hybrid encoder

--- _studio/mfx_lib/encode_hw/vp8/src/mfx_vp8_encode_ddi_hw.cpp
+++ _studio/mfx_lib/encode_hw/vp8/src/mfx_vp8_encode_ddi_hw.cpp
@@ -658,7 +658,13 @@ mfxStatus D3D9Encoder::QueryStatus(Task & task)
     // As we won't get all bitstreams we need to cache all other statuses. 
 
     // first check cache.
-    const ENCODE_QUERY_STATUS_PARAMS* feedback = m_feedbackCached.Hit(task.m_frameOrder);
+    const ENCODE_QUERY_STATUS_PARAMS* feedback = m_feedbackCached.Hit(task.m_frameOrder + 1);
+
+#ifdef NEW_STATUS_REPORTING_DDI_0915
+    ENCODE_QUERY_STATUS_PARAMS_DESCR feedbackDescr;
+    feedbackDescr.SizeOfStatusParamStruct = sizeof(m_feedbackUpdate[0]);
+    feedbackDescr.StatusParamType = QUERY_STATUS_PARAM_FRAME;
+#endif // NEW_STATUS_REPORTING_DDI_0915
 
     // if task is not in cache then query its status
     if (feedback == 0 || feedback->bStatus != ENCODE_OK)
@@ -667,8 +673,13 @@ mfxStatus D3D9Encoder::QueryStatus(Task & task)
         {
             HRESULT hr = m_auxDevice->Execute(
                 ENCODE_QUERY_STATUS_ID,
+#ifdef NEW_STATUS_REPORTING_DDI_0915
+                (void *)&feedbackDescr,
+                sizeof(feedbackDescr),
+#else // NEW_STATUS_REPORTING_DDI_0915
                 (void *)0,
                 0,
+#endif // NEW_STATUS_REPORTING_DDI_0915
                 &m_feedbackUpdate[0],
                 (mfxU32)m_feedbackUpdate.size() * sizeof(m_feedbackUpdate[0]));
             MFX_CHECK(hr != D3DERR_WASSTILLDRAWING, MFX_WRN_DEVICE_BUSY);
