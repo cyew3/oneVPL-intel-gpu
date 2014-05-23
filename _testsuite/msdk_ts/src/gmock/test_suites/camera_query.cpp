37a88ea298cc69ba099280e7dba1404b0d9ec9e2
Ryabinin, Sergey <sergey.ryabinin@intel.com>
2014-05-23 15:03:07 +0400

    beh tests for camera (2)

---
+++ _testsuite/msdk_ts/src/gmock/test_suites/camera_query.cpp
@@ -0,0 +1,256 @@
+#include "ts_vpp.h"
+#include "ts_struct.h"
+#include "ts_alloc.h"
+
+#define EXT_BUF_PAR(eb) tsExtBufTypeToId<eb>::id, sizeof(eb)
+
+namespace camera_query
+{
+
+typedef void (*callback)(tsExtBufType<mfxVideoParam>&, mfxU32, mfxU32);
+
+void ext_buf(tsExtBufType<mfxVideoParam>& par, mfxU32 id, mfxU32 size)
+{
+    par.AddExtBuffer(id, size); 
+}
+
+class TestSuite : tsVideoVPP
+{
+public:
+    TestSuite() : tsVideoVPP(true, MFX_MAKEFOURCC('C','A','M','R')) {}
+    ~TestSuite() {}
+    int RunTest(unsigned int id);
+    static const unsigned int n_cases;
+
+private:
+    static const mfxU32 n_par = 6;
+
+    enum QueryMode
+    {
+        NULL_SESSION  =   0x1 ,  //Query(0,p_in,p_out)
+        NULL_IN       =   0x2 ,  //Query(ses,0,p_out)
+        NULL_OUT      =   0x4 ,  //Query(ses,p_in,0)
+        ZERO_UID      =   0x8 ,  //Zero plugin GUID
+        DIFFERENT_IO  =   0x10,  //Query(ses,p_in,p_out), p_in and p_out are different structures with different extented buffers
+        SAME_IO       =   0x20,  //Query(ses,p_in,p_out), p_in and p_out are different structures with the same extented buffers
+        INPLACE       =   0x40,  //Query(ses,p_in,p_in)
+        OPAQUE        =   0x80,
+        ALLOC_LESS    = 0x1000,
+        ALLOC_MORE    = 0x2000,
+        SET_HANDLE_BEFOR =  0x100,
+        SET_HANDLE_AFTER =  0x200
+
+    };
+
+    struct tc_struct
+    {
+        mfxStatus sts;
+        mfxU32 mode;
+        struct f_pair 
+        {
+            const  tsStruct::Field* f;
+            mfxU32 v;
+        } set_par[n_par];
+        struct 
+        {
+            callback func;
+            mfxU32 buf;
+            mfxU32 size;
+        } set_buf[2];
+    };
+
+    static const tc_struct test_case[];
+};
+
+const TestSuite::tc_struct TestSuite::test_case[] = 
+{
+    {/*00*/  MFX_ERR_INVALID_HANDLE, NULL_SESSION},
+    {/*01*/  MFX_ERR_NONE,           NULL_IN},
+    {/*02*/  MFX_ERR_NULL_PTR,       NULL_OUT},
+    {/*03*/  MFX_ERR_NULL_PTR,       NULL_IN|NULL_OUT},
+    {/*04*/  MFX_ERR_NONE,           SAME_IO},
+    {/*05*/  MFX_ERR_NONE,           INPLACE},
+    {/*06*/  MFX_ERR_NONE,           OPAQUE},
+    {/*07*/  MFX_ERR_UNSUPPORTED,    OPAQUE|ALLOC_LESS},
+    {/*08*/  MFX_ERR_UNSUPPORTED,    OPAQUE|ALLOC_MORE},
+    {/*09*/  MFX_ERR_NONE,           SET_HANDLE_AFTER},
+    {/*10*/  MFX_ERR_UNSUPPORTED, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtVPPDoNotUse            )}}, // ext buffers
+    {/*11*/  MFX_ERR_UNSUPPORTED, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtVppAuxData             )}},
+    {/*12*/  MFX_ERR_UNSUPPORTED, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtVPPDenoise             )}},
+    {/*13*/  MFX_ERR_UNSUPPORTED, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtVPPProcAmp             )}},
+    {/*14*/  MFX_ERR_UNSUPPORTED, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtVPPDetail              )}},
+    {/*15*/  MFX_ERR_UNSUPPORTED, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtVideoSignalInfo        )}},
+    {/*16*/  MFX_ERR_UNDEFINED_BEHAVIOR, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtVPPDoUse               )}},
+    {/*17*/  MFX_ERR_UNSUPPORTED, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtAVCRefListCtrl         )}},
+    {/*18*/  MFX_ERR_UNSUPPORTED, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtVPPFrameRateConversion )}},
+    {/*19*/  MFX_ERR_UNSUPPORTED, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtVPPImageStab           )}},
+    {/*20*/  MFX_ERR_UNSUPPORTED, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtVPPComposite           )}},
+    {/*21*/  MFX_ERR_UNSUPPORTED, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtVPPVideoSignalInfo     )}},
+    {/*22*/  MFX_ERR_UNSUPPORTED, SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtVPPDeinterlacing       )}},
+    {/*23*/  MFX_ERR_NONE,        SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamGammaCorrection      )}},  //Camera buffers
+    {/*24*/  MFX_ERR_NONE,        SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamWhiteBalance         )}},
+    {/*25*/  MFX_ERR_NONE,        SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamHotPixelRemoval      )}},
+    {/*26*/  MFX_ERR_NONE,        SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamBlackLevelCorrection )}},
+    {/*27*/  MFX_ERR_NONE,        SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamVignetteCorrection   )}},
+    {/*28*/  MFX_ERR_NONE,        SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamBayerDenoise         )}},
+    {/*29*/  MFX_ERR_NONE,        SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamColorCorrection3x3   )}},
+    {/*30*/  MFX_ERR_NONE,        SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamPadding              )}},
+    {/*31*/  MFX_ERR_NONE,        SAME_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamPipeControl          )}},
+    {/*32*/  MFX_ERR_NONE,        INPLACE, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamGammaCorrection      )}},
+    {/*33*/  MFX_ERR_NONE,        INPLACE, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamWhiteBalance         )}},
+    {/*34*/  MFX_ERR_NONE,        INPLACE, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamHotPixelRemoval      )}},
+    {/*35*/  MFX_ERR_NONE,        INPLACE, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamBlackLevelCorrection )}},
+    {/*36*/  MFX_ERR_NONE,        INPLACE, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamVignetteCorrection   )}},
+    {/*37*/  MFX_ERR_NONE,        INPLACE, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamBayerDenoise         )}},
+    {/*38*/  MFX_ERR_NONE,        INPLACE, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamColorCorrection3x3   )}},
+    {/*39*/  MFX_ERR_NONE,        INPLACE, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamPadding              )}},
+    {/*40*/  MFX_ERR_NONE,        INPLACE, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamPipeControl          )}},
+    {/*41*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamGammaCorrection      )}},
+    {/*42*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamWhiteBalance         )}},
+    {/*43*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamHotPixelRemoval      )}},
+    {/*44*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamBlackLevelCorrection )}},
+    {/*45*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamVignetteCorrection   )}},
+    {/*46*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamBayerDenoise         )}},
+    {/*47*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamColorCorrection3x3   )}},
+    {/*48*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamPadding              )}},
+    {/*49*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {ext_buf, EXT_BUF_PAR(mfxExtCamPipeControl          )}},
+    {/*50*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {{ext_buf, EXT_BUF_PAR(mfxExtCamGammaCorrection      )},{ext_buf, EXT_BUF_PAR(mfxExtCamWhiteBalance         )} }},
+    {/*51*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {{ext_buf, EXT_BUF_PAR(mfxExtCamWhiteBalance         )},{ext_buf, EXT_BUF_PAR(mfxExtCamHotPixelRemoval      )} }},
+    {/*52*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {{ext_buf, EXT_BUF_PAR(mfxExtCamHotPixelRemoval      )},{ext_buf, EXT_BUF_PAR(mfxExtCamBlackLevelCorrection )} }},
+    {/*53*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {{ext_buf, EXT_BUF_PAR(mfxExtCamBlackLevelCorrection )},{ext_buf, EXT_BUF_PAR(mfxExtCamVignetteCorrection   )} }},
+    {/*54*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {{ext_buf, EXT_BUF_PAR(mfxExtCamVignetteCorrection   )},{ext_buf, EXT_BUF_PAR(mfxExtCamBayerDenoise         )} }},
+    {/*55*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {{ext_buf, EXT_BUF_PAR(mfxExtCamBayerDenoise         )},{ext_buf, EXT_BUF_PAR(mfxExtCamColorCorrection3x3   )} }},
+    {/*56*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {{ext_buf, EXT_BUF_PAR(mfxExtCamColorCorrection3x3   )},{ext_buf, EXT_BUF_PAR(mfxExtCamPadding              )} }},
+    {/*57*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {{ext_buf, EXT_BUF_PAR(mfxExtCamPadding              )},{ext_buf, EXT_BUF_PAR(mfxExtCamPipeControl          )} }},
+    {/*58*/  MFX_ERR_UNDEFINED_BEHAVIOR,        DIFFERENT_IO, {}, {{ext_buf, EXT_BUF_PAR(mfxExtCamPipeControl          )},{ext_buf, EXT_BUF_PAR(mfxExtCamGammaCorrection      )} }},
+};
+
+const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case)/sizeof(TestSuite::tc_struct);
+
+int TestSuite::RunTest(unsigned int id)
+{
+    TS_START;
+    const tc_struct& tc = test_case[id];
+
+    if(tc.mode & ZERO_UID)
+    {
+        m_uid = 0;
+    }
+
+    if(!(tc.mode & NULL_SESSION))
+    {
+        MFXInit();
+        if(m_uid)
+        {
+            Load();
+        }
+    }
+
+    if (tc.mode & NULL_IN)
+        m_pPar = 0;
+
+    // set up parameters for case
+    for(mfxU32 i = 0; i < n_par; i++)
+    {
+        if(tc.set_par[i].f)
+        {
+            tsStruct::set(m_pPar, *tc.set_par[i].f, tc.set_par[i].v);
+        }
+    }
+
+    m_spool_in.UseDefaultAllocator(!!(m_par.IOPattern & MFX_IOPATTERN_OUT_SYSTEM_MEMORY));
+    if (!(tc.mode & NULL_SESSION))
+    {
+        SetFrameAllocator(m_session, m_spool_in.GetAllocator());
+        m_spool_out.SetAllocator(m_spool_in.GetAllocator(), true);
+    }
+
+    if (tc.mode & OPAQUE)
+    {
+        m_par.vpp.In.PicStruct = MFX_PICSTRUCT_FIELD_TFF;
+        mfxExtOpaqueSurfaceAlloc& osa = m_par;
+        QueryIOSurf();
+        if (tc.mode & ALLOC_LESS)
+        {
+            m_request[0].NumFrameSuggested = m_request[0].NumFrameMin = m_request[0].NumFrameMin - 1;
+            m_request[1].NumFrameSuggested = m_request[1].NumFrameMin = m_request[1].NumFrameMin - 1;
+        }
+        else if (tc.mode & ALLOC_MORE)
+        {
+            m_request[0].NumFrameSuggested = m_request[0].NumFrameMin = m_request[0].NumFrameMin + 1;
+            m_request[1].NumFrameSuggested = m_request[1].NumFrameMin = m_request[1].NumFrameMin + 1;
+        }
+        if (m_par.IOPattern & MFX_IOPATTERN_IN_OPAQUE_MEMORY)
+            m_spool_in.AllocOpaque(m_request[0], osa);
+        if (m_par.IOPattern & MFX_IOPATTERN_OUT_OPAQUE_MEMORY)
+            m_spool_out.AllocOpaque(m_request[1], osa);
+    }
+
+    tsExtBufType<mfxVideoParam> parOut;
+    parOut.IOPattern = m_par.IOPattern;
+    mfxVideoParam* pout = &parOut;
+    if(tc.mode & INPLACE)
+        pout = m_pPar;
+    else if(tc.mode & NULL_OUT)
+        pout = 0;
+
+    if(*tc.set_buf[0].func)
+    {
+        (*tc.set_buf[0].func)(this->m_par, tc.set_buf[0].buf, tc.set_buf[0].size);
+        if(tc.mode & SAME_IO)
+            (*tc.set_buf[0].func)(parOut,  tc.set_buf[0].buf, tc.set_buf[0].size);
+        if((tc.mode & DIFFERENT_IO) && *tc.set_buf[1].func)
+            (*tc.set_buf[1].func)(parOut,  tc.set_buf[1].buf, tc.set_buf[1].size);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    g_tsStatus.expect(tc.sts);
+    Query(m_session, m_pPar, pout);
+
+    if(tc.mode & SET_HANDLE_AFTER)
+    {
+        mfxHDL hdl;
+        mfxHandleType type; 
+        m_spool_in.GetAllocator()->get_hdl(type, hdl); 
+        g_tsStatus.expect(MFX_ERR_UNDEFINED_BEHAVIOR); //API 1.4 behavior is not acceptable since 1.6 API
+        SetHandle(m_session, type, hdl);
+    }
+
+    if (tc.sts == MFX_ERR_UNSUPPORTED || tc.sts == MFX_WRN_INCOMPATIBLE_VIDEO_PARAM)
+    {
+        for(mfxU32 i = 0; i < n_par; i++)
+        {
+            if(tc.set_par[i].f)
+            {
+                if (tc.sts == MFX_ERR_UNSUPPORTED)
+                    tsStruct::check_eq(pout, *tc.set_par[i].f, 0);
+                else if (tc.sts == MFX_WRN_INCOMPATIBLE_VIDEO_PARAM)
+                    tsStruct::check_ne(pout, *tc.set_par[i].f, tc.set_par[i].v);
+            }
+        }
+    }
+
+    if (tc.mode == NULL_IN)
+    {
+        EXPECT_NE(0, pout->IOPattern);
+        EXPECT_NE(0, pout->vpp.In.ChromaFormat);
+        EXPECT_NE(0, pout->vpp.Out.ChromaFormat);
+        EXPECT_NE(0, pout->vpp.In.FourCC);
+        EXPECT_NE(0, pout->vpp.Out.FourCC);
+        EXPECT_NE(0, pout->vpp.In.Width);
+        EXPECT_NE(0, pout->vpp.Out.Width);
+        EXPECT_NE(0, pout->vpp.In.Height);
+        EXPECT_NE(0, pout->vpp.Out.Height);
+        EXPECT_NE(0, pout->vpp.In.FrameRateExtN);
+        EXPECT_NE(0, pout->vpp.In.FrameRateExtD);
+        EXPECT_NE(0, pout->vpp.Out.FrameRateExtN);
+        EXPECT_NE(0, pout->vpp.Out.FrameRateExtD);
+    }
+
+    TS_END;
+    return 0;
+}
+
+TS_REG_TEST_SUITE_CLASS(camera_query);
+
+}
---
+++ _testsuite/msdk_ts/src/gmock/test_suites/camera_query_io_surf.cpp
@@ -0,0 +1,221 @@
+#include "ts_vpp.h"
+
+namespace camera_query_io_surf
+{
+
+typedef struct
+{
+    mfxSession            session;
+    mfxVideoParam*        pPar;
+    mfxFrameAllocRequest* pRequest;
+}QIOSpar;
+
+typedef void (*callback)(QIOSpar&);
+void wrong_resol(QIOSpar& qiospar)
+{
+    qiospar.pPar->vpp.In.Width  = qiospar.pPar->vpp.Out.Width  = 721;
+    qiospar.pPar->vpp.In.Height = qiospar.pPar->vpp.Out.Height = 481;
+    qiospar.pPar->vpp.In.CropW  = qiospar.pPar->vpp.Out.CropW  = 741;
+    qiospar.pPar->vpp.In.CropH  = qiospar.pPar->vpp.Out.CropH  = 582;
+}
+void wrong_ARs(QIOSpar& qiospar)
+{
+    qiospar.pPar->vpp.In.AspectRatioW  = qiospar.pPar->vpp.Out.AspectRatioW  = 666;
+    qiospar.pPar->vpp.In.AspectRatioH = qiospar.pPar->vpp.Out.AspectRatioH = 0;
+}
+
+void zero_session   (QIOSpar& p) { p.session = 0; }
+void zero_param     (QIOSpar& p) { p.pPar = 0; }
+void zero_request   (QIOSpar& p) { p.pRequest = 0; }
+
+typedef struct
+{
+    callback    set_par;
+    mfxU32      IOPattern;
+    mfxU32      AsyncDepth;
+    mfxStatus   sts;
+} tc_struct;
+
+tc_struct test_case[] =
+{
+    // zero values
+    {/*0 */ zero_session, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 0, MFX_ERR_INVALID_HANDLE },
+    {/*1 */ zero_param,   MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 0, MFX_ERR_NULL_PTR },
+    {/*2 */ zero_request, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 0, MFX_ERR_NULL_PTR },
+    // negative (IN only)
+    {/*3 */ 0, MFX_IOPATTERN_IN_SYSTEM_MEMORY, 0, MFX_ERR_INVALID_VIDEO_PARAM },
+    {/*4 */ 0, MFX_IOPATTERN_IN_VIDEO_MEMORY,  0, MFX_ERR_INVALID_VIDEO_PARAM },
+    {/*5 */ 0, MFX_IOPATTERN_IN_OPAQUE_MEMORY, 0, MFX_ERR_INVALID_VIDEO_PARAM },
+    // negative (OUT only)
+    {/*6 */ 0, MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 0, MFX_ERR_INVALID_VIDEO_PARAM },
+    {/*7 */ 0, MFX_IOPATTERN_OUT_VIDEO_MEMORY,  0, MFX_ERR_INVALID_VIDEO_PARAM },
+    {/*8 */ 0, MFX_IOPATTERN_OUT_OPAQUE_MEMORY, 0, MFX_ERR_INVALID_VIDEO_PARAM },
+        //AsyncDepth = 0
+    // IN = SYSTEM
+    {/*9 */ 0, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 0, MFX_ERR_NONE },
+    {/*10*/ 0, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_VIDEO_MEMORY,  0, MFX_ERR_NONE },
+    {/*11*/ 0, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY, 0, MFX_ERR_NONE },
+    // IN = VIDEO
+    {/*12*/ 0, MFX_IOPATTERN_IN_VIDEO_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY,  0, MFX_ERR_NONE },
+    {/*13*/ 0, MFX_IOPATTERN_IN_VIDEO_MEMORY|MFX_IOPATTERN_OUT_VIDEO_MEMORY,   0, MFX_ERR_NONE },
+    {/*14*/ 0, MFX_IOPATTERN_IN_VIDEO_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY,  0, MFX_ERR_NONE },
+    // IN = OPAQ
+    {/*15*/ 0, MFX_IOPATTERN_IN_OPAQUE_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 0, MFX_ERR_NONE },
+    {/*16*/ 0, MFX_IOPATTERN_IN_OPAQUE_MEMORY|MFX_IOPATTERN_OUT_VIDEO_MEMORY,  0, MFX_ERR_NONE },
+    {/*17*/ 0, MFX_IOPATTERN_IN_OPAQUE_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY, 0, MFX_ERR_NONE },
+        //AsyncDepth = 3
+    // IN = SYSTEM
+    {/*18*/ 0, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 3, MFX_ERR_NONE },
+    {/*19*/ 0, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_VIDEO_MEMORY,  3, MFX_ERR_NONE },
+    {/*20*/ 0, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY, 3, MFX_ERR_NONE },
+    // IN = VIDEO
+    {/*21*/ 0, MFX_IOPATTERN_IN_VIDEO_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY,  3, MFX_ERR_NONE },
+    {/*22*/ 0, MFX_IOPATTERN_IN_VIDEO_MEMORY|MFX_IOPATTERN_OUT_VIDEO_MEMORY,   3, MFX_ERR_NONE },
+    {/*23*/ 0, MFX_IOPATTERN_IN_VIDEO_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY,  3, MFX_ERR_NONE },
+    // IN = OPAQ
+    {/*24*/ 0, MFX_IOPATTERN_IN_OPAQUE_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 3, MFX_ERR_NONE },
+    {/*25*/ 0, MFX_IOPATTERN_IN_OPAQUE_MEMORY|MFX_IOPATTERN_OUT_VIDEO_MEMORY,  3, MFX_ERR_NONE },
+    {/*26*/ 0, MFX_IOPATTERN_IN_OPAQUE_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY, 3, MFX_ERR_NONE },
+        //AsyncDepth = 5
+    // IN = SYSTEM
+    {/*27*/ 0, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 5, MFX_ERR_NONE },
+    {/*28*/ 0, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_VIDEO_MEMORY,  5, MFX_ERR_NONE },
+    {/*29*/ 0, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY, 5, MFX_ERR_NONE },
+    // IN = VIDEO
+    {/*30*/ 0, MFX_IOPATTERN_IN_VIDEO_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY,  5, MFX_ERR_NONE },
+    {/*31*/ 0, MFX_IOPATTERN_IN_VIDEO_MEMORY|MFX_IOPATTERN_OUT_VIDEO_MEMORY,   5, MFX_ERR_NONE },
+    {/*32*/ 0, MFX_IOPATTERN_IN_VIDEO_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY,  5, MFX_ERR_NONE },
+    // IN = OPAQ
+    {/*33*/ 0, MFX_IOPATTERN_IN_OPAQUE_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 5, MFX_ERR_NONE },
+    {/*34*/ 0, MFX_IOPATTERN_IN_OPAQUE_MEMORY|MFX_IOPATTERN_OUT_VIDEO_MEMORY,  5, MFX_ERR_NONE },
+    {/*35*/ 0, MFX_IOPATTERN_IN_OPAQUE_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY, 5, MFX_ERR_NONE },
+
+    /* This function does not validate I/O parameters except those used in calculating the number of input and output surfaces.*/
+    {/*36*/ wrong_resol, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 5, MFX_ERR_NONE },
+    {/*37*/ wrong_resol, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_VIDEO_MEMORY,  5, MFX_ERR_NONE },
+    {/*38*/ wrong_resol, MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY, 5, MFX_ERR_NONE },
+    {/*39*/ wrong_ARs,   MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 5, MFX_ERR_NONE },
+};
+
+enum
+{
+    IOP_IN  = (MFX_IOPATTERN_IN_VIDEO_MEMORY|MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_IN_OPAQUE_MEMORY),
+    IOP_OUT = (MFX_IOPATTERN_OUT_VIDEO_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY),
+    IOP_NUM = (1<<(3+3)),
+};
+
+
+tc_struct GetTestCase(unsigned int id)
+{
+    if(id < sizeof(test_case)/sizeof(tc_struct))
+        return test_case[id];
+
+    //all possible combinations of IOPattern
+    tc_struct tc = {};
+    id -= sizeof(test_case)/sizeof(tc_struct); // exclude pre-defined cases
+
+    // get IOPattern IN from test-case id
+    tc.IOPattern = (id & IOP_IN);
+
+    // get IOPattern OUT from rest of test-case id
+    id &= ~IOP_IN;
+    while(id && (id & (~IOP_OUT)))
+        id <<= 1;
+    tc.IOPattern |= id;
+
+    // pure IOPatterns only
+    if((tc.IOPattern == (MFX_IOPATTERN_IN_VIDEO_MEMORY |MFX_IOPATTERN_OUT_VIDEO_MEMORY )) ||
+       (tc.IOPattern == (MFX_IOPATTERN_IN_VIDEO_MEMORY |MFX_IOPATTERN_OUT_SYSTEM_MEMORY)) ||
+       (tc.IOPattern == (MFX_IOPATTERN_IN_VIDEO_MEMORY |MFX_IOPATTERN_OUT_OPAQUE_MEMORY)) ||
+       (tc.IOPattern == (MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_VIDEO_MEMORY )) ||
+       (tc.IOPattern == (MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY)) ||
+       (tc.IOPattern == (MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY)) ||
+       (tc.IOPattern == (MFX_IOPATTERN_IN_OPAQUE_MEMORY|MFX_IOPATTERN_OUT_VIDEO_MEMORY )) ||
+       (tc.IOPattern == (MFX_IOPATTERN_IN_OPAQUE_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY)) ||
+       (tc.IOPattern == (MFX_IOPATTERN_IN_OPAQUE_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY)))
+       tc.sts = MFX_ERR_NONE;
+    else
+        tc.sts = MFX_ERR_INVALID_VIDEO_PARAM;
+
+    return tc;
+}
+
+
+int test(unsigned int id)
+{
+    TS_START;
+    tsVideoVPP vpp(true, MFX_MAKEFOURCC('C', 'A', 'M', 'R'));
+    tc_struct tc = GetTestCase(id);
+
+    vpp.MFXInit();
+    vpp.Load();
+
+    QIOSpar par = {vpp.m_session, &vpp.m_par, vpp.m_request};
+    vpp.m_par.IOPattern  = tc.IOPattern;
+    vpp.m_par.AsyncDepth = 1;
+
+    if(tc.set_par)
+    {
+        (*tc.set_par)(par);
+    }
+
+    g_tsStatus.expect(tc.sts);
+    mfxStatus sts = vpp.QueryIOSurf(par.session, par.pPar, par.pRequest);
+
+    if(sts >= MFX_ERR_NONE)
+    {
+        mfxU32 expectedType[] = {MFX_MEMTYPE_FROM_VPPIN|MFX_MEMTYPE_EXTERNAL_FRAME,
+                                 MFX_MEMTYPE_FROM_VPPOUT|MFX_MEMTYPE_EXTERNAL_FRAME};
+
+        if (vpp.m_par.IOPattern & MFX_IOPATTERN_IN_SYSTEM_MEMORY)
+            expectedType[0] |= MFX_MEMTYPE_SYSTEM_MEMORY;
+        else if (vpp.m_par.IOPattern & MFX_IOPATTERN_IN_VIDEO_MEMORY)
+            expectedType[0] |= MFX_MEMTYPE_VIDEO_MEMORY_PROCESSOR_TARGET;
+        else
+            expectedType[0] |= MFX_MEMTYPE_OPAQUE_FRAME;
+
+        if (vpp.m_par.IOPattern & MFX_IOPATTERN_OUT_SYSTEM_MEMORY)
+            expectedType[1] |= MFX_MEMTYPE_SYSTEM_MEMORY;
+        else if (vpp.m_par.IOPattern & MFX_IOPATTERN_OUT_VIDEO_MEMORY)
+            expectedType[1] |= MFX_MEMTYPE_VIDEO_MEMORY_PROCESSOR_TARGET;
+        else
+            expectedType[1] |= MFX_MEMTYPE_OPAQUE_FRAME;
+
+        if (vpp.m_par.IOPattern & MFX_IOPATTERN_IN_OPAQUE_MEMORY)
+            EXPECT_EQ( vpp.m_request[0].Type & (MFX_MEMTYPE_OPAQUE_FRAME|MFX_MEMTYPE_FROM_VPPIN),
+                                               (MFX_MEMTYPE_OPAQUE_FRAME|MFX_MEMTYPE_FROM_VPPIN) );
+        else
+            EXPECT_EQ( expectedType[0], vpp.m_request[0].Type );
+        EXPECT_EQ( vpp.m_par.vpp.In, vpp.m_request[0].Info );
+        EXPECT_GT( vpp.m_request[0].NumFrameMin, 0 );
+        EXPECT_GE( vpp.m_request[0].NumFrameSuggested, vpp.m_request[0].NumFrameMin );
+
+        if (vpp.m_par.IOPattern & MFX_IOPATTERN_OUT_OPAQUE_MEMORY)
+            EXPECT_EQ( vpp.m_request[1].Type & (MFX_MEMTYPE_OPAQUE_FRAME|MFX_MEMTYPE_FROM_VPPOUT),
+                                               (MFX_MEMTYPE_OPAQUE_FRAME|MFX_MEMTYPE_FROM_VPPOUT) );
+        else
+            EXPECT_EQ( expectedType[1], vpp.m_request[1].Type );
+        EXPECT_EQ( vpp.m_par.vpp.Out, vpp.m_request[1].Info );
+        EXPECT_GT( vpp.m_request[1].NumFrameMin, 0 );
+        EXPECT_GE( vpp.m_request[1].NumFrameSuggested, vpp.m_request[1].NumFrameMin );
+
+        if(0 != tc.AsyncDepth)
+        {
+            mfxFrameAllocRequest request_for_async_depth_1[2];
+            par.pPar->AsyncDepth = 1;
+            mfxStatus sts = vpp.QueryIOSurf(par.session, par.pPar, request_for_async_depth_1);
+
+            EXPECT_GE( vpp.m_request[0].NumFrameSuggested, request_for_async_depth_1[0].NumFrameSuggested );
+            EXPECT_GE( vpp.m_request[1].NumFrameSuggested, request_for_async_depth_1[1].NumFrameSuggested );
+            EXPECT_GE( vpp.m_request[0].NumFrameMin, request_for_async_depth_1[0].NumFrameMin );
+            EXPECT_GE( vpp.m_request[1].NumFrameMin, request_for_async_depth_1[1].NumFrameMin );
+        }
+
+    }
+
+    TS_END;
+    return 0;
+}
+
+TS_REG_TEST_SUITE(camera_query_io_surf, test, sizeof(test_case)/sizeof(tc_struct) + IOP_NUM);
+}
