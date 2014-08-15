dd09ffa8d1c7a510e202be4fa9b1fe365c6a76a9
Guschina, Elena <Elena.Guschina@intel.com>
2014-08-15 14:32:36 +0400

    Revert "Added audio plugins support to plugins loader."
    This reverts commit 9d7ccffa51d1298e20b0731cf8c0db2cae75a992.
    
    Change-Id: Idbf67df1b70e9c525ad3707983db1a6699432184
    Signed-off-by: Guschina, Elena <Elena.Guschina@intel.com>

--- samples/sample_user_modules/plugin_api/plugin_loader.h
+++ samples/sample_user_modules/plugin_api/plugin_loader.h
@@ -85,26 +85,6 @@ inline void intrusive_ptr_release(MFXGenericPlugin * pResource)
     pResource->Release();
 }
 
-/* for plugin API*/
-inline void intrusive_ptr_addref(MFXAudioDecoderPlugin *)
-{
-}
-inline void intrusive_ptr_release(MFXAudioDecoderPlugin * pResource)
-{
-    //fully destroy plugin resource
-    pResource->Release();
-}
-
-/* for plugin API*/
-inline void intrusive_ptr_addref(MFXAudioEncoderPlugin *)
-{
-}
-inline void intrusive_ptr_release(MFXAudioEncoderPlugin * pResource)
-{
-    //fully destroy plugin resource
-    pResource->Release();
-}
-
 template<class T>
 struct PluginCreateTrait
 {
@@ -116,8 +96,9 @@ struct PluginCreateTrait<MFXDecoderPlugin>
     static const char* Name(){
         return "mfxCreateDecoderPlugin";
     }
-    static const mfxPluginType type = MFX_PLUGINTYPE_VIDEO_DECODE;
-
+    enum PluginType {
+        type=MFX_PLUGINTYPE_VIDEO_DECODE
+    };
     typedef PluginModuleTemplate::fncCreateDecoderPlugin TCreator;
 };
 
@@ -127,31 +108,10 @@ struct PluginCreateTrait<MFXEncoderPlugin>
     static const char* Name() {
         return "mfxCreateEncoderPlugin";
     }
-    static const mfxPluginType type = MFX_PLUGINTYPE_VIDEO_ENCODE;
-
+    enum PluginType {
+        type=MFX_PLUGINTYPE_VIDEO_ENCODE
+    };
     typedef PluginModuleTemplate::fncCreateEncoderPlugin TCreator;
-};
-
-template<>
-struct PluginCreateTrait<MFXAudioDecoderPlugin>
-{
-    static const char* Name() {
-        return "mfxCreateAudioDecoderPlugin";
-    }
-    static const mfxPluginType type = MFX_PLUGINTYPE_AUDIO_DECODE;
-
-    typedef PluginModuleTemplate::fncCreateAudioDecoderPlugin TCreator;
-};
-
-template<>
-struct PluginCreateTrait<MFXAudioEncoderPlugin>
-{
-    static const char* Name() {
-        return "mfxCreateAudioEncoderPlugin";
-    }
-    static const mfxPluginType type = MFX_PLUGINTYPE_AUDIO_ENCODE;
-
-    typedef PluginModuleTemplate::fncCreateAudioEncoderPlugin TCreator;
 };
 
 template<>
@@ -160,8 +120,9 @@ struct PluginCreateTrait<MFXGenericPlugin>
     static const char* Name(){
         return "mfxCreateGenericPlugin";
     }
-    static const mfxPluginType type = MFX_PLUGINTYPE_VIDEO_GENERAL;
-
+    enum PluginType {
+        type=MFX_PLUGINTYPE_VIDEO_GENERAL
+    };
     typedef PluginModuleTemplate::fncCreateGenericPlugin TCreator;
 };
 
@@ -174,10 +135,11 @@ class PluginLoader : public MFXPlugin
 protected:
     MsdkSoModule m_PluginModule;
     intrusive_ptr<T> m_plugin;
-    MFXBaseUSER *m_userModule;
+    MFXVideoUSER *m_userModule;
     MFXPluginAdapter<T> m_adapter;
-    mfxPluginType ePluginType;
-
+    enum {
+        ePluginType = PluginCreateTrait<T>::type
+    };
     typedef typename PluginCreateTrait<T>::TCreator TCreator;
 
     mfxSession        m_session;
@@ -201,12 +163,11 @@ private:
     }
 
 public:
-    PluginLoader(MFXBaseUSER *userModule, const msdk_string & pluginName)
+    PluginLoader(MFXVideoUSER *userModule, const msdk_string & pluginName)
         : m_PluginModule(pluginName)
         , m_userModule(NULL)
-        , m_plugin(NULL)
     {
-        ePluginType = PluginCreateTrait<T>::type;
+
         plugin_load_type = MFX_PLUGINLOAD_TYPE_FILE;
 
         TCreator pCreateFunc = m_PluginModule.GetAddr<TCreator>(PluginCreateTrait<T>::Name());
@@ -226,7 +187,7 @@ public:
         mfxStatus sts = userModule->Register(ePluginType, &plg);
         if (MFX_ERR_NONE != sts) {
             MSDK_TRACE_ERROR(MSDK_STRING("Failed to register plugin: ") << pluginName
-                << MSDK_STRING(", MFXBaseUSER::Register(type=") << ePluginType << MSDK_STRING("), sts=") << sts);
+                << MSDK_STRING(", MFXVideoUSER::Register(type=") << ePluginType << MSDK_STRING("), sts=") << sts);
             return;
         }
         MSDK_TRACE_INFO(MSDK_STRING("Plugin(type=")<< ePluginType <<MSDK_STRING("): loaded from: ")<< pluginName );
@@ -236,12 +197,8 @@ public:
     PluginLoader(mfxSession session, const mfxPluginUID & uid, mfxU32 version)
         : m_session()
         , m_uid()
-        , m_plugin(NULL)
     {
-        mfxStatus sts = MFX_ERR_NONE;
-        ePluginType = PluginCreateTrait<T>::type;
         msdk_stringstream strStream;
-
         plugin_load_type = MFX_PLUGINLOAD_TYPE_GUID;
 
         MSDK_MEMCPY(&m_uid, &uid, sizeof(mfxPluginUID));
@@ -251,36 +208,26 @@ public:
             if (i != (sizeof(mfxPluginUID)-1)) strStream << MSDK_STRING(", ");
         }
 
-        if ((ePluginType == MFX_PLUGINTYPE_AUDIO_DECODE) ||
-            (ePluginType == MFX_PLUGINTYPE_AUDIO_ENCODE))
-        {
-            sts = MFXAudioUSER_Load(session, &m_uid, version);
-        }
-        else
-        {
-            sts = MFXVideoUSER_Load(session, &m_uid, version);
-        }
-
+        mfxStatus sts = MFXVideoUSER_Load(session, &m_uid, version);
         if (MFX_ERR_NONE != sts)
         {
             MSDK_TRACE_ERROR(MSDK_STRING("Failed to load plugin from GUID, sts=") << sts << MSDK_STRING(": { ") << strStream.str().c_str() << MSDK_STRING(" } (") << msdkGetPluginName(m_uid) << MSDK_STRING(")"));
         }
         else
         {
-            MSDK_TRACE_INFO(MSDK_STRING("Plugin was loaded from GUID"));
+            MSDK_TRACE_INFO(MSDK_STRING("Plugin was loaded from GUID: { ") << strStream.str().c_str() << MSDK_STRING(" } (") << msdkGetPluginName(m_uid) << MSDK_STRING(")"));
             m_session = session;
         }
     }
 
     virtual ~PluginLoader()
     {
-        mfxStatus sts = MFX_ERR_NONE;
         if (plugin_load_type == MFX_PLUGINLOAD_TYPE_FILE)
         {
             if (m_userModule) {
                 mfxStatus sts = m_userModule->Unregister(ePluginType);
                 if (sts != MFX_ERR_NONE) {
-                    MSDK_TRACE_INFO(MSDK_STRING("MFXBaseUSER::Unregister(type=") << ePluginType << MSDK_STRING("), sts=") << sts);
+                    MSDK_TRACE_INFO(MSDK_STRING("MFXVideoUSER::Unregister(type=") << ePluginType << MSDK_STRING("), sts=") << sts);
                 }
             }
         }
@@ -288,84 +235,54 @@ public:
         {
             if (m_session)
             {
-                if ((ePluginType == MFX_PLUGINTYPE_AUDIO_DECODE) ||
-                    (ePluginType == MFX_PLUGINTYPE_AUDIO_ENCODE))
-                {
-                    sts = MFXAudioUSER_UnLoad(m_session, &m_uid);
-                }
-                else
-                {
-                    sts = MFXVideoUSER_UnLoad(m_session, &m_uid);
-                }
-
+                mfxStatus sts = MFXVideoUSER_UnLoad(m_session, &m_uid);
                 if (sts != MFX_ERR_NONE)
                 {
-                    MSDK_TRACE_ERROR(MSDK_STRING("Failed to unload plugin from GUID, sts=") << sts);
-                }
-                else
-                {
-                    MSDK_TRACE_INFO(MSDK_STRING("MFXBaseUSER_UnLoad(session=0x") << m_session << MSDK_STRING("), sts=") << sts);
+                     MSDK_TRACE_INFO(MSDK_STRING("MFXVideoUSER_UnLoad(session=0x") << m_session << MSDK_STRING("), sts=") << sts);
                 }
             }
         }
     }
-
     bool IsOk() {
         if (plugin_load_type == MFX_PLUGINLOAD_TYPE_FILE)
             return m_userModule != 0;
         else
             return m_session != 0;
     }
+    virtual mfxStatus Init( mfxVideoParam *par ) {
+        return m_plugin->Init(par);
+    }
     virtual mfxStatus PluginInit( mfxCoreInterface *core ) {
-        if (m_plugin.get())
-            return m_plugin->PluginInit(core);
-        else
-            return MFX_ERR_NULL_PTR;
+        return m_plugin->PluginInit(core);
     }
     virtual mfxStatus PluginClose() {
-        if (m_plugin.get())
-            return m_plugin->PluginClose();
-        else
-            return MFX_ERR_NULL_PTR;
+        return m_plugin->PluginClose();
     }
     virtual mfxStatus GetPluginParam( mfxPluginParam *par ) {
-        if (m_plugin.get())
-            return m_plugin->GetPluginParam(par);
-        else
-            return MFX_ERR_NULL_PTR;
+        return m_plugin->GetPluginParam(par);
     }
     virtual mfxStatus Execute( mfxThreadTask task, mfxU32 uid_p, mfxU32 uid_a ) {
-        if (m_plugin.get())
-            return m_plugin->Execute(task, uid_p, uid_a);
-        else
-            return MFX_ERR_NULL_PTR;
+        return m_plugin->Execute(task, uid_p, uid_a);
     }
     virtual mfxStatus FreeResources( mfxThreadTask task, mfxStatus sts ) {
-        if (m_plugin.get())
-            return m_plugin->FreeResources(task, sts);
-        else
-            return MFX_ERR_NULL_PTR;
+        return m_plugin->FreeResources(task, sts);
+    }
+    virtual mfxStatus QueryIOSurf( mfxVideoParam *par, mfxFrameAllocRequest *in , mfxFrameAllocRequest *out ) {
+        return m_plugin->QueryIOSurf(par, in, out);
     }
     virtual void Release() {
-        if (m_plugin.get())
-            m_plugin->Release();
+        return m_plugin->Release();
     }
     virtual mfxStatus Close() {
-        if (m_plugin.get())
-            return m_plugin->Close();
-        else
-            return MFX_ERR_NULL_PTR;
+        return m_plugin->Close();
     }
     virtual mfxStatus SetAuxParams( void* auxParam, int auxParamSize ) {
-        if (m_plugin.get())
-            return m_plugin->SetAuxParams(auxParam, auxParamSize);
-        else
-            return MFX_ERR_NULL_PTR;
+        return m_plugin->SetAuxParams(auxParam, auxParamSize);
     }
 };
 
 template <class T>
-MFXPlugin * LoadPluginByType(MFXBaseUSER *userModule, const msdk_string & pluginFullPath) {
+MFXPlugin * LoadPluginByType(MFXVideoUSER *userModule, const msdk_string & pluginFullPath) {
     std::auto_ptr<PluginLoader<T> > plg(new PluginLoader<T> (userModule, pluginFullPath));
     return plg->IsOk() ? plg.release() : NULL;
 }
@@ -385,10 +302,6 @@ inline MFXPlugin * LoadPlugin(mfxPluginType type, MFXVideoUSER *userModule, cons
         return LoadPluginByType<MFXDecoderPlugin>(userModule, pluginFullPath);
     case MFX_PLUGINTYPE_VIDEO_ENCODE:
         return LoadPluginByType<MFXEncoderPlugin>(userModule, pluginFullPath);
-    case MFX_PLUGINTYPE_AUDIO_DECODE:
-        return LoadPluginByType<MFXAudioDecoderPlugin>(userModule, pluginFullPath);
-    case MFX_PLUGINTYPE_AUDIO_ENCODE:
-        return LoadPluginByType<MFXAudioEncoderPlugin>(userModule, pluginFullPath);
     default:
         MSDK_TRACE_ERROR(MSDK_STRING("Unsupported plugin type : ")<< type << MSDK_STRING(", for plugin: ")<< pluginFullPath);
         return NULL;
@@ -404,10 +317,6 @@ inline MFXPlugin * LoadPlugin(mfxPluginType type, mfxSession session, const mfxP
         return LoadPluginByGUID<MFXDecoderPlugin>(session, uid, version);
     case MFX_PLUGINTYPE_VIDEO_ENCODE:
         return LoadPluginByGUID<MFXEncoderPlugin>(session, uid, version);
-    case MFX_PLUGINTYPE_AUDIO_DECODE:
-        return LoadPluginByGUID<MFXAudioDecoderPlugin>(session, uid, version);
-    case MFX_PLUGINTYPE_AUDIO_ENCODE:
-        return LoadPluginByGUID<MFXAudioEncoderPlugin>(session, uid, version);
     default:
         MSDK_TRACE_ERROR(MSDK_STRING("Unsupported plugin type : ")<< type);
         return NULL;
