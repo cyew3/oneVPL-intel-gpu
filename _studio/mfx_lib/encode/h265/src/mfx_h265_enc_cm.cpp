11e702aa723fdd3e4f8ed893a43806cc1597bcfc
Sergey Osipov <sergey.a.osipov@intel.com>
2014-10-09 18:42:09 +0400

    fixed KW for Linux

--- _studio/mfx_lib/encode/h265/src/mfx_h265_enc_cm.cpp
+++ _studio/mfx_lib/encode/h265/src/mfx_h265_enc_cm.cpp
@@ -323,7 +323,7 @@ CmDevice * CmContext::TryCreateCmDevicePtr(VideoCORE * core, mfxU32 * version)
     }
     else if (core->GetVAType() == MFX_HW_VAAPI)
     {
-        //throw std::logic_error("GetDeviceManager not implemented on Linux for Look Ahead");
+        //throw std::logic_error("GetDeviceManager not implemented on Linux");
 #if defined(MFX_VA_LINUX)
         VADisplay display;
         mfxStatus res = core->GetHandle(MFX_HANDLE_VA_DISPLAY, &display); // == MFX_HANDLE_RESERVED2
@@ -334,11 +334,6 @@ CmDevice * CmContext::TryCreateCmDevicePtr(VideoCORE * core, mfxU32 * version)
             return 0;
 #endif
     }
-#if defined(LINUX32) || defined(LINUX64)
-        VADisplay display = 0;
-        if ((result = ::CreateCmDevice(device, *version, display)) != CM_SUCCESS)
-            return 0;
-#endif // #if defined(LINUX32) || defined(LINUX64)
 
     return device;
 }
