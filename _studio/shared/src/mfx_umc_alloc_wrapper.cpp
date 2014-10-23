41680ca6e32f5171169637ebb6349c660d0d351b
Dmitry Ermilov <dmitry.ermilov@intel.com>
2014-10-23 11:40:43 -0400

    We can't overwrite width/height of source surface when copy to output memory
    It causes crash when real outut surface is bigger when internal one.
    
    Change-Id: I8ff900e7f76e823a393163d72146cf1e143438ff
    Signed-off-by: Dmitry Ermilov <dmitry.ermilov@intel.com>

--- _studio/shared/src/mfx_umc_alloc_wrapper.cpp
+++ _studio/shared/src/mfx_umc_alloc_wrapper.cpp
@@ -878,12 +878,6 @@ mfxStatus mfx_UMC_FrameAllocator::PrepareToOutput(mfxFrameSurface1 *surface_work
         return MFX_ERR_UNSUPPORTED;
     }
 
-    MFX_INTERNAL_CPY(&m_surface.Info, &surface_work->Info, sizeof(surface_work->Info));
-
-    mfxFrameSurface1 dstSurface;
-    MFX_INTERNAL_CPY(&dstSurface.Info, &surface_work->Info, sizeof(surface_work->Info));
-    MFX_INTERNAL_CPY(&dstSurface.Data, &surface_work->Data, sizeof(surface_work->Data));
-
     sts = m_pCore->DoFastCopyWrapper(surface_work,
                                      MFX_MEMTYPE_EXTERNAL_FRAME | MFX_MEMTYPE_DXVA2_DECODER_TARGET,
                                      &m_surface,
