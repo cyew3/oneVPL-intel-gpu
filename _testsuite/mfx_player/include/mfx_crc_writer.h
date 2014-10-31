72e32573d285f1a65e1aa91998ad4c1d49bb29a2
Dmitry Sharipo <dmitry.sharipo@intel.com>
2014-10-31 07:38:15 -0700

    md5 support in player
    Change-Id: I2527c47522cb16dca5788adc458ed87cbd4b5d3f

--- _testsuite/mfx_player/include/mfx_crc_writer.h
+++ _testsuite/mfx_player/include/mfx_crc_writer.h
@@ -15,6 +15,7 @@ File Name: .h
 #include "mfx_ifile.h"
 #include "mfx_pipeline_utils.h"
 #include "ippdc.h"
+#include "ippcp.h"
 #include "vm_file.h"
 
 //wrapping to file writer render provided crc calc+file writing
@@ -31,22 +32,46 @@ public:
         , m_crc32()
         , m_crcFile(sCrcFile)
     {
+        int size;
+        ippsMD5GetSize(&size);
+        ctx = (IppsMD5State *)malloc(size);
+        if ( ctx )
+            ippsMD5Init(ctx);
     }
     ~CRCFileWriter()
     {
+        if (ctx)
+            ippsMD5Final(md5_digest, ctx);
+
+        vm_char md5_result[33] = {0};
+        for(int i = 0; i < 16; i++)
+        {
+            vm_string_sprintf(&md5_result[i*2], VM_STRING("%02x"), md5_digest[i]);
+        }
+        PrintInfo(VM_STRING("MD5"), md5_result);
+
         PrintInfo(VM_STRING("CRC"), VM_STRING("%08X"), m_crc32 );
         WriteCrcToFile();
+
+        if (ctx)
+            free(ctx);
     }
     virtual mfxStatus Write(mfxU8* p, mfxU32 size)
     {
         if (NULL != p)
+        {
             ippsCRC32_8u(p, size, &m_crc32);
+            if (ctx) ippsMD5Update(p, size, ctx);
+        }
         return base::Write(p, size);
     }
     virtual mfxStatus Write(mfxBitstream * pBs)
     {
         if (NULL != pBs)
+        {
             ippsCRC32_8u(pBs->DataOffset + pBs->Data, pBs->DataLength, &m_crc32);
+            if (ctx) ippsMD5Update(pBs->DataOffset + pBs->Data, pBs->DataLength, ctx);
+        }
         return base::Write(pBs);
     }
 
@@ -55,6 +80,11 @@ protected:
         : base((const base&)that)
         , m_crc32()
     {
+        int size;
+        ippsMD5GetSize(&size);
+        ctx = (IppsMD5State *)malloc(size);
+        if ( ctx )
+            ippsMD5Init(ctx);
     }
     mfxStatus WriteCrcToFile()
     {
@@ -68,4 +98,7 @@ protected:
         }
         return MFX_ERR_NONE;
     }
+
+    IppsMD5State *ctx;
+    mfxU8 md5_digest[MD5_DIGEST_LENGTH];
 };
