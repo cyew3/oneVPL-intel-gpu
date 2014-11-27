e4d12c17d9474441b2e13a96edddce4d33f0ad53
Lyalyushkin, Nikolay <nikolay.lyalyushkin@intel.com>
2014-11-27 15:26:55 +0300

    VP8 encode and decode plugins were added in the msdkGetPluginName method
    Change-Id: I52d02e8a14e70a55d4f381ba2c4e55d5c05823c2
    Signed-off-by: Lyalyushkin, Nikolay <nikolay.lyalyushkin@intel.com>

--- samples/sample_user_modules/plugin_api/plugin_loader.h
+++ samples/sample_user_modules/plugin_api/plugin_loader.h
@@ -154,8 +154,12 @@ private:
             return MSDK_STRING("Intel (R) Media SDK plugin for HEVC DECODE");
         else if(AreGuidsEqual(guid, MFX_PLUGINID_HEVCD_HW))
             return MSDK_STRING("Intel (R) Media SDK HW plugin for HEVC DECODE");
+        else if(AreGuidsEqual(guid, MFX_PLUGINID_VP8D_HW))
+            return MSDK_STRING("Intel (R) Media SDK HW plugin for VP8 DECODE");
         else if(AreGuidsEqual(guid, MFX_PLUGINID_HEVCE_SW))
             return MSDK_STRING("Intel (R) Media SDK plugin for HEVC ENCODE");
+        else if(AreGuidsEqual(guid, MFX_PLUGINID_VP8E_HW))
+            return MSDK_STRING("Intel (R) Media SDK HW plugin for VP8 ENCODE");
         else if(AreGuidsEqual(guid, MFX_PLUGINID_H264LA_HW))
             return MSDK_STRING("Intel (R) Media SDK plugin for LA ENC");
         else
