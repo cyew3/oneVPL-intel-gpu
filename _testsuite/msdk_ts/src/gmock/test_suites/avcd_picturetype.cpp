bf366c0b1451fdb51a17c8806438ce976e97891b
Tsiruleva, Olga <olga.tsiruleva@intel.com>
2015-02-04 11:18:01 +0300

    Extended avcd_picturetype suite
    Added interlace cases, extbuffer doesn't attach to all surfaces

--- _testsuite/msdk_ts/src/gmock/test_suites/avcd_picturetype.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/avcd_picturetype.cpp
@@ -26,7 +26,8 @@ private:
     enum
     {
         DISPORDER = 1,
-        DECORDER,
+        DECORDER  = 1 << 1,
+        RAND      = 1 << 2,
     };
 
     struct tc_struct
@@ -49,51 +50,80 @@ private:
 
 const TestSuite::tc_struct TestSuite::test_case[] = 
 {
-    {/*00*/ MFX_ERR_NONE, DECORDER,  "forBehaviorTest/picturetype/salesman_176x144_f15p_refdist6_gop12.h264"           , 0},
-    {/*00*/ MFX_ERR_NONE, DISPORDER, "forBehaviorTest/picturetype/salesman_176x144_f5p_refdist1_gop4.h264"             , 1},
-    {/*00*/ MFX_ERR_NONE, DISPORDER, "forBehaviorTest/picturetype/salesman_176x144_f5p_refdist1_gop1_idrinterval4.h264", 2},
+    {/*01*/ MFX_ERR_NONE, DECORDER      , "forBehaviorTest/picturetype/salesman_176x144_f15p_refdist6_gop12.h264"           , 0},
+    {/*02*/ MFX_ERR_NONE, DISPORDER     , "forBehaviorTest/picturetype/salesman_176x144_f5p_refdist1_gop4.h264"             , 1},
+    {/*03*/ MFX_ERR_NONE, DISPORDER     , "forBehaviorTest/picturetype/salesman_176x144_f5p_refdist1_gop1_idrInterval4.h264", 2},
+    {/*04*/ MFX_ERR_NONE, DISPORDER     , "forBehaviorTest/picturetype/salesman_176x144_f15p_refdist6_gop12.h264"           , 3},
+    {/*05*/ MFX_ERR_NONE, DECORDER|RAND , "forBehaviorTest/picturetype/salesman_176x144_f15p_refdist6_gop12.h264"           , 0},
+    {/*06*/ MFX_ERR_NONE, DISPORDER|RAND, "forBehaviorTest/picturetype/salesman_176x144_f15p_refdist6_gop12.h264"           , 3},
+    {/*07*/ MFX_ERR_NONE, DISPORDER     , "forBehaviorTest/picturetype/salesman_176x144_f15_refdist5_gop9_bff.h264"         , 4},
+    {/*08*/ MFX_ERR_NONE, DISPORDER     , "forBehaviorTest/picturetype/salesman_176x144_f15_refdist5_gop9_tff.h264"         , 4},
+    {/*09*/ MFX_ERR_NONE, DISPORDER|RAND, "forBehaviorTest/picturetype/salesman_176x144_f15_refdist5_gop9_tff.h264"         , 4},
+    {/*10*/ MFX_ERR_NONE, DECORDER      , "forBehaviorTest/picturetype/salesman_176x144_f15_refdist5_gop9_bff.h264"         , 5},
 
 };
 
 const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case)/sizeof(TestSuite::tc_struct);
 
 //Table of stream's structure aligned with 3 frames
+// for interlace cases aligned with 2 frames
 const TestSuite::stream_struct TestSuite::pictype_case[] =
 {
     {0, 15, { MFX_FRAMETYPE_IDR|MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF, MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF,
               MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_B                                    , MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF,
               MFX_FRAMETYPE_B                                    , MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF,
               MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_B                                    , MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF,
-              MFX_FRAMETYPE_IDR|MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF, MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_B                  ,}},
+              MFX_FRAMETYPE_IDR|MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF, MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_B                   }},
 
     {1, 5,  { MFX_FRAMETYPE_IDR|MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF, MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF,
               MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_IDR|MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF,                                   }},
 
     {2, 5,  { MFX_FRAMETYPE_IDR|MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF, MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF,
-              MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF                  ,                                   }}
+              MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF                  ,                                   }},
+
+    {3, 15, { MFX_FRAMETYPE_IDR|MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF, MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_B,
+              MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_B,
+              MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_B,
+              MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF                  , MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF,
+              MFX_FRAMETYPE_IDR|MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF, MFX_FRAMETYPE_B                                    , MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF }},
+
+    {4, 10, { MFX_FRAMETYPE_IDR|MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xP|MFX_FRAMETYPE_xREF, MFX_FRAMETYPE_B|MFX_FRAMETYPE_xB,
+              MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xB|MFX_FRAMETYPE_xREF                  , MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xB|MFX_FRAMETYPE_xREF,
+              MFX_FRAMETYPE_B|MFX_FRAMETYPE_xB                                                       , MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xP|MFX_FRAMETYPE_xREF,
+              MFX_FRAMETYPE_B|MFX_FRAMETYPE_xB                                                       , MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xB|MFX_FRAMETYPE_xREF,
+              MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xP|MFX_FRAMETYPE_xREF                  , MFX_FRAMETYPE_IDR|MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xP|MFX_FRAMETYPE_xREF,}},
+
+    {5, 10, { MFX_FRAMETYPE_IDR|MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xP|MFX_FRAMETYPE_xREF, MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xP|MFX_FRAMETYPE_xREF,
+              MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xB|MFX_FRAMETYPE_xREF                  , MFX_FRAMETYPE_B|MFX_FRAMETYPE_xB,
+              MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xB|MFX_FRAMETYPE_xREF                  , MFX_FRAMETYPE_P|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xP|MFX_FRAMETYPE_xREF,
+              MFX_FRAMETYPE_B|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xB|MFX_FRAMETYPE_xREF                  , MFX_FRAMETYPE_B|MFX_FRAMETYPE_xB,
+              MFX_FRAMETYPE_B|MFX_FRAMETYPE_xB                                                       , MFX_FRAMETYPE_IDR|MFX_FRAMETYPE_I|MFX_FRAMETYPE_REF|MFX_FRAMETYPE_xP|MFX_FRAMETYPE_xREF,}},
+
 };
 
 class Verifier : public tsSurfaceProcessor
 {
 public:
     std::list <mfxU16> expected_pictype;
+    mfxU16 frame;
     Verifier(const mfxU16* type, const mfxU16 N)
-        : expected_pictype(type, type + N) {}
+        : expected_pictype(type, type + N), frame(0) {}
     ~Verifier() {}
 
     mfxStatus ProcessSurface(mfxFrameSurface1& s)
     {
         if (s.Data.NumExtParam != 1)
         {
-            g_tsLog << "ERROR: expected number of external buffers is not equal 1\n";
-            return MFX_ERR_ABORTED;
+            expected_pictype.pop_front();
+            frame++;
+            return MFX_ERR_NONE;
         }
         if (s.Data.ExtParam[0] == NULL)
         {
             g_tsLog << "ERROR: null pointer ExtParam\n";
             return MFX_ERR_NULL_PTR;
         }
-        if (s.Data.ExtParam[0]->BufferId != MFX_EXTBUFF_DECODED_FRAME_INFO || 
+        if (s.Data.ExtParam[0]->BufferId != MFX_EXTBUFF_DECODED_FRAME_INFO ||
             s.Data.ExtParam[0]->BufferSz == 0)
         {
             g_tsLog << "ERROR: Wrong external buffer or zero size buffer\n";
@@ -104,10 +134,11 @@ public:
             return MFX_ERR_NONE;
         if (pictype != expected_pictype.front())
         {
-            g_tsLog << "ERROR: Real picture type = " << pictype  << " is not equal expected = " << expected_pictype.front() << "\n";
+            g_tsLog << "ERROR: Frame#" << frame << " real picture type = " << pictype  << " is not equal expected = " << expected_pictype.front() << "\n";
             return MFX_ERR_INCOMPATIBLE_VIDEO_PARAM;
         }
         expected_pictype.pop_front();
+        frame++;
 
         return MFX_ERR_NONE;
     }
@@ -130,7 +161,8 @@ int TestSuite::RunTest(unsigned int id)
         m_par.mfx.DecodedOrder = 1;
     Init();
     AllocSurfaces();
-    for (mfxU32 i = 0; i < PoolSize(); i++)
+    mfxU32 step =  (tc.mode & RAND) ? 2 : 1;
+    for (mfxU32 i = 0; i < PoolSize(); i+=step)
     {
         mfxFrameSurface1* p = GetSurface(i);
         p->Data.ExtParam = new mfxExtBuffer*[1];
