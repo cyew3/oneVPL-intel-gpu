1827bbbf80b6e03b8ae4614bb5660e805d82719a
Andrey Larionov <andrey.larionov@intel.com>
2015-02-13 19:09:32 +0300

    Removed MFXVideoPAK declaration (moved into mfxvideo++.h) (master branch)
    Change-Id: Ief33bb84cdde3bac64a6791e575a14ddf810cc32
    Signed-off-by: Andrey Larionov <andrey.larionov@intel.com>

--- _studio/shared/include/mfxvideopro++.h
+++ _studio/shared/include/mfxvideopro++.h
@@ -19,48 +19,7 @@ File Name: mfxvideopro++.h
 
 // MFXVideoENC was moved in mfxvideo++.h
 
-class MFXVideoPAK
-{
-public:
+// MFXVideoPAK was moved in mfxvideo++.h
 
-    MFXVideoPAK(mfxSession session) { m_session = session; }
-    virtual ~MFXVideoPAK(void) { Close(); }
-
-    mfxStatus Query(mfxVideoParam *in, mfxVideoParam *out) { return MFXVideoPAK_Query(m_session, in, out); }
-    mfxStatus QueryIOSurf(mfxVideoParam *par, mfxFrameAllocRequest *request) { return MFXVideoPAK_QueryIOSurf(m_session, par, request); }
-    mfxStatus Init(mfxVideoParam *par) { return MFXVideoPAK_Init(m_session, par); }
-    mfxStatus Reset(mfxVideoParam *par) { return MFXVideoPAK_Reset(m_session, par); }
-    mfxStatus Close(void) { return MFXVideoPAK_Close(m_session); }
-
-    mfxStatus GetVideoParam(mfxVideoParam *par) { return MFXVideoPAK_GetVideoParam(m_session, par); }
-    mfxStatus GetFrameParam(mfxFrameParam *par) { return MFXVideoPAK_GetFrameParam(m_session, par); }
-    mfxStatus RunSeqHeader(mfxFrameCUC *cuc) { return MFXVideoPAK_RunSeqHeader(m_session, cuc); }
-    mfxStatus RunFramePAKAsync(mfxFrameCUC *cuc, mfxSyncPoint *syncp) { return MFXVideoPAK_RunFramePAKAsync(m_session, cuc, syncp); }
-
-protected:
-
-    mfxSession m_session;                                       // (mfxSession) handle to the owning session
-};
-
-class MFXVideoBRC
-{
-public:
-
-    MFXVideoBRC(mfxSession session) { m_session = session; }
-    virtual ~MFXVideoBRC(void) { Close(); }
-
-    mfxStatus Query(mfxVideoParam *in, mfxVideoParam *out) { return MFXVideoBRC_Query(m_session, in, out); }
-    mfxStatus Init(mfxVideoParam *par) { return MFXVideoBRC_Init(m_session, par); }
-    mfxStatus Reset(mfxVideoParam *par) { return MFXVideoBRC_Reset(m_session, par); }
-    mfxStatus Close(void) { return MFXVideoBRC_Close(m_session); }
-
-    mfxStatus FrameENCUpdate(mfxFrameCUC *cuc) { return MFXVideoBRC_FrameENCUpdate(m_session, cuc); }
-    mfxStatus FramePAKRefine(mfxFrameCUC *cuc) { return MFXVideoBRC_FramePAKRefine(m_session, cuc); }
-    mfxStatus FramePAKRecord(mfxFrameCUC *cuc) { return MFXVideoBRC_FramePAKRecord(m_session, cuc); }
-
-protected:
-
-    mfxSession m_session;                                       // (mfxSession) handle to the owning session
-};
 
 #endif // __MFXVIDEOPLUSPLUS_H
