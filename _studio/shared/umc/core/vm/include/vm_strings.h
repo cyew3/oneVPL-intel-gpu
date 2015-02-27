8d49e52d8a9a817e2eb2851de46d53da501cf8c7
Nabiullin, Oleg <oleg.nabiullin@intel.com>
2015-02-27 17:56:24 +0300

    Remove SafeLib for Android.
    Change-Id: I495c7d63823f584553b9aca03eda2063f049e50e

--- _studio/shared/umc/core/vm/include/vm_strings.h
+++ _studio/shared/umc/core/vm/include/vm_strings.h
@@ -26,6 +26,8 @@
 # include <errno.h>
 # include <ipps.h>
 
+#if !defined(ANDROID)
+
 #ifdef __cplusplus
 extern "C"
 {
@@ -36,6 +38,8 @@ extern "C"
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
+
+#endif // !defined(ANDROID)
 
 typedef char vm_char;
 
@@ -54,6 +58,9 @@ typedef char vm_char;
 #define vm_string_vsprintf  vsprintf
 #define vm_string_vsnprintf vsnprintf
 
+
+#if !defined(ANDROID)
+
 #define vm_string_strcat    strcat
 #define vm_string_strcat_s  strcat_s
 #define vm_string_strncat   strncat_s
@@ -70,6 +77,27 @@ typedef char vm_char;
 #define vm_string_stricmp   strcmp
 #define vm_string_strnicmp  strncmp
 #define vm_string_strrchr   strrchr
+
+#else // ANDROID
+
+#define vm_string_strcat    strcat
+#define vm_string_strcat_s  (dest, size, src)  (strncat((dest), (src), (size)),0)
+#define vm_string_strncat   strncat
+#define vm_string_strcpy    strcpy
+#define vm_string_strcpy_s  (dest, size, src)  (strncpy((dest), (src), (size)),0)
+#define vm_string_strncpy   strncpy
+#define vm_string_strncpy_s (dst, dst_size, src, n) (strncpy(dst,src,n))
+#define vm_string_strcspn   strcspn
+#define vm_string_strspn    strspn
+
+#define vm_string_strlen    strlen
+#define vm_string_strcmp    strcmp
+#define vm_string_strncmp   strncmp
+#define vm_string_stricmp   strcasecmp
+#define vm_string_strnicmp  strncasecmp
+#define vm_string_strrchr   strrchr
+
+#endif // ANDROID
 
 #define vm_string_atol      atol
 #define vm_string_atoi      atoi
@@ -191,7 +219,9 @@ Ipp32s vm_string_findnext(vm_findptr handle, vm_finddata_t* fileinfo);
 
 #if !defined(_WIN32) && !defined(_WIN64)
 typedef int error_t;
-/*
+
+#if defined(ANDROID)
+
 static inline
 error_t memcpy_s(void* pDst, size_t nDstSize, const void* pSrc, size_t nCount)
 {
@@ -210,7 +240,8 @@ error_t memcpy_s(void* pDst, size_t nDstSize, const void* pSrc, size_t nCount)
     ippsZero_8u((Ipp8u*)pDst, nDstSize);
     return ERANGE;
 }
-*/
+
+#endif //  defined(ANDROID)
 
 #endif 
 
