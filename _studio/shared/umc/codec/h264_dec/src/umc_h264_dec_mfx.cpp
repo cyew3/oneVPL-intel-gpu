2e103e03db57c7699faf96883017d0e0509b1230
Baygulov, Aleksei <aleksei.baygulov@intel.com>
2015-05-13 14:47:37 +0300

    maxDecPicBuffering fix

--- _studio/shared/umc/codec/h264_dec/src/umc_h264_dec_mfx.cpp
+++ _studio/shared/umc/codec/h264_dec/src/umc_h264_dec_mfx.cpp
@@ -185,7 +185,7 @@ Status FillVideoParam(const UMC_H264_DECODER::H264SeqParamSet * seq, mfxVideoPar
 
     par->mfx.CodecProfile = seq->profile_idc;
     par->mfx.CodecLevel = seq->level_idc;
-    par->mfx.MaxDecFrameBuffering = seq->vui.max_dec_frame_buffering;
+    par->mfx.MaxDecFrameBuffering =  seq->vui_parameters_present_flag ? seq->vui.max_dec_frame_buffering : 0;
 
     // video signal section
     mfxExtVideoSignalInfo * videoSignal = (mfxExtVideoSignalInfo *)GetExtendedBuffer(par->ExtParam, par->NumExtParam, MFX_EXTBUFF_VIDEO_SIGNAL_INFO);
