42532cdba50cfa27fec67e4504eb77e439c21ffb
Nabiullin, Oleg <oleg.nabiullin@intel.com>
2015-06-08 14:42:20 +0300

    Multi-target Linux build system.
    Change-Id: If2d7c95f2ce54ef4ca964b59600a9a28ae131cae

--- _studio/shared/include/mfx_common_linux_bdw.h
+++ _studio/shared/include/mfx_common_linux_bdw.h
@@ -10,83 +10,8 @@
 //
 //
 */
-#ifndef _MFX_COMMON_H_
-#define _MFX_COMMON_H_
-
-#include "mfxvideo.h"
-#include "mfxvideo++int.h"
-#include "ippdefs.h"
-#include "mfx_utils.h"
-#include <stdio.h>
-#include <string.h>
-
-#include <string>
-#include <stdexcept> /* for std exceptions on Linux/Android */
-
-#if defined(_WIN32) || defined(_WIN64)
-    #include <windows.h>
-#else
-    #include <stddef.h>
-#endif
-
-#ifdef UMC_VA_LINUX
-    #undef  MFX_VA
-    #define MFX_VA
-#endif // UMC_VA_LINUX
-
-#ifdef MFX_VA
-    #if defined(LINUX32) || defined(LINUX64)
-        #undef  MFX_VA_LINUX
-        #define MFX_VA_LINUX
-
-        #undef  UMC_VA_LINUX
-        #define UMC_VA_LINUX
-
-        /* Android and Linux uses one video acceleration library: LibVA, but
-         * it is possible that their versions are different (especially during
-         * development). To simplify code development MFX_VA_ANDROID macro is introduced.
-         */
-        #if defined(ANDROID)
-            #define MFX_VA_ANDROID
-        #endif // #if defined(ANDROID)
-
-    #elif defined(_WIN32) || defined(_WIN64)
-
-        #undef  MFX_VA_WIN
-        #define MFX_VA_WIN
-
-        #undef  UMC_VA_DXVA
-        #define UMC_VA_DXVA
-
-    #elif defined(__APPLE__)
-        #undef  MFX_VA_OSX
-        #define MFX_VA_OSX
-
-        #undef  UMC_VA_OSX
-        #define UMC_VA_OSX
-    #endif // #if defined(LINUX32) || defined(LINUX64)
-#endif // MFX_VA
-
-#if defined(AS_HEVCD_PLUGIN) || defined(AS_HEVCE_PLUGIN)
-    #if defined(HEVCE_EVALUATION)
-        //#define MFX_ENABLE_WATERMARK
-        #define MFX_MAX_ENCODE_FRAMES 1000
-    #endif
-    #if defined(HEVCD_EVALUATION)
-        #define MFX_MAX_DECODE_FRAMES 1000
-    #endif
-#endif
-
-//#define MFX_ENABLE_C2CPP_DEBUG          // debug the C to C++ layer only.
-#ifndef MFX_ENABLE_C2CPP_DEBUG
-#include "umc_structures.h"
-
-#define MFX_BIT_IN_KB 8*1000
-#endif
-
-#if !defined(LINUX_TARGET_PLATFORM)
-
-#if !defined(ANDROID)
+#ifndef _MFX_COMMON_LINUX_BDW_H_
+#define _MFX_COMMON_LINUX_BDW_H_
 
 // h264
 #define MFX_ENABLE_H264_VIDEO_DECODE
@@ -167,14 +92,53 @@
 
 //mp3
 #define MFX_ENABLE_MP3_AUDIO_DECODE
+// linux support
 
-#else // #if !defined(ANDROID)
-    #include "mfx_android_defs.h"
+//#undef MFX_ENABLE_MP3_AUDIO_DECODE
+// HW limitation
+#if defined (MFX_VA)
+    // h265
+    //#undef MFX_ENABLE_H265_VIDEO_DECODE
+    //#undef MFX_ENABLE_H265_VIDEO_ENCODE
+    // h264
+    //#undef MFX_ENABLE_H264_VIDEO_ENCODE
+    #undef MFX_ENABLE_MVC_VIDEO_ENCODE
+    //#undef MFX_ENABLE_H264_VIDEO_PAK
+    //#undef MFX_ENABLE_H264_VIDEO_BRC
 
-    #define SYNCHRONIZATION_BY_VA_SYNC_SURFACE
+    //#undef MFX_ENABLE_H264_VIDEO_ENCODE_HW
+    //#undef MFX_ENABLE_MVC_VIDEO_ENCODE_HW
 
-#endif // #if !defined(ANDROID)
+    // mpeg2
+    //#undef MFX_ENABLE_MPEG2_VIDEO_DECODE
+    //#undef MFX_ENABLE_MPEG2_VIDEO_ENCODE
+    //#undef MFX_ENABLE_MPEG2_VIDEO_PAK
+    //#undef MFX_ENABLE_MPEG2_VIDEO_ENC
+    //#undef MFX_ENABLE_MPEG2_VIDEO_BRC
 
+    // mjpeg
+    //#undef MFX_ENABLE_MJPEG_VIDEO_DECODE
+    //#undef MFX_ENABLE_MJPEG_VIDEO_ENCODE
+
+    // vpp
+    //#undef MFX_ENABLE_DENOISE_VIDEO_VPP
+    //#undef MFX_ENABLE_VPP
+
+    // vc1
+    //#undef MFX_ENABLE_VC1_VIDEO_DECODE
+
+    // vp8
+    //#undef MFX_ENABLE_VP8_VIDEO_DECODE
+    //#undef MFX_ENABLE_VP8_VIDEO_DECODE_HW
+
+    //#undef MFX_ENABLE_H265_VIDEO_ENCODE
+// SW limitation
+#else // #if defined (MFX_VA)
+    //#undef MFX_ENABLE_VP8_VIDEO_DECODE
+    //#undef MFX_ENABLE_VP8_VIDEO_DECODE_HW
+#endif // #if defined (MFX_VA)
+
+#define SYNCHRONIZATION_BY_VA_SYNC_SURFACE
 #if defined (MFX_VA_LINUX)
     //#define SYNCHRONIZATION_BY_VA_SYNC_SURFACE
     #if defined (SYNCHRONIZATION_BY_VA_SYNC_SURFACE)
@@ -196,7 +160,7 @@
 #endif
 
 #if defined(AS_HEVCD_PLUGIN) || defined(AS_HEVCE_PLUGIN) || defined(AS_VP8D_PLUGIN) || defined(AS_VP8E_PLUGIN) || defined(AS_VP9D_PLUGIN) || defined(AS_CAMERA_PLUGIN) || defined (MFX_RT)
-    #undef MFX_ENABLE_H265_VIDEO_DECODE
+	#undef MFX_ENABLE_H265_VIDEO_DECODE
     #undef MFX_ENABLE_H265_VIDEO_ENCODE
     #undef MFX_ENABLE_H264_VIDEO_DECODE
     #undef MFX_ENABLE_H264_VIDEO_ENCODE
@@ -263,50 +227,4 @@
 #endif
 
 
-#else // LINUX_TARGET_PLATFORM
-
-#if defined(LINUX_TARGET_PLATFORM_BSW)
-
-#include "mfx_common_linux_bsw.h"
-
-#elif defined(LINUX_TARGET_PLATFORM_BDW)
-
-#include "mfx_common_linux_bdw.h"
-
-#elif defined(LINUX_TARGET_PLATFORM_TBD)
-
-#include "mfx_common_lnx_tbd.h"
-
-#else
-#error "Target platform should be specified!"
-#endif
-
-#endif // LINUX_TARGET_PLATFORM
-class MfxException
-{
-public:
-    MfxException (mfxStatus st, const char *msg = NULL)
-        : m_message (msg)
-        , m_status (st)
-    {
-    }
-
-    const char * GetMessage() const
-    {
-        return m_message;
-    }
-
-    mfxStatus GetStatus() const
-    {
-        return m_status;
-    }
-
-protected:
-    const char *m_message;
-    mfxStatus m_status;
-};
-
-#define MFX_AUTO_ASYNC_DEPTH_VALUE  5
-#define MFX_MAX_ASYNC_DEPTH_VALUE   15
-
-#endif //_MFX_COMMON_H_
+#endif //_MFX_COMMON_LINUX_BDW_H_
