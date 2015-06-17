7a1d7c49d8ce117a6920f15051ad851bc9401fb1
Dmitry Sharipo <dmitry.sharipo@intel.com>
2015-06-17 06:04:01 -0700

    Missed argb16 allocator for dx11
    Change-Id: I1f58eac75dcc1451b2cf647bbac148c2e905dd1a

--- _studio/shared/src/libmfx_allocator_d3d11.cpp
+++ _studio/shared/src/libmfx_allocator_d3d11.cpp
@@ -83,6 +83,8 @@ DXGI_FORMAT mfxDefaultAllocatorD3D11::MFXtoDXGI(mfxU32 format)
     case MFX_FOURCC_R16_GRBG:
     case MFX_FOURCC_R16_GBRG:
         return DXGI_FORMAT_R16_TYPELESS;
+    case MFX_FOURCC_ARGB16:
+        return DXGI_FORMAT_R16G16B16A16_UINT;
     }
     return DXGI_FORMAT_UNKNOWN;
 
@@ -115,6 +117,7 @@ mfxStatus mfxDefaultAllocatorD3D11::AllocFramesHW(mfxHDL pthis, mfxFrameAllocReq
     case MFX_FOURCC_R16_BGGR:
     case MFX_FOURCC_R16_GBRG:
     case MFX_FOURCC_R16_GRBG:
+    case MFX_FOURCC_ARGB16:
         break;
     default:
         return MFX_ERR_UNSUPPORTED;
