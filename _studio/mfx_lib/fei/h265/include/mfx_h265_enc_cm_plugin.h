be1fcfd191a36656e9158268de345f23db1578e4
Jon Recker <jon.recker@intel.com>
2015-07-30 16:40:05 -0700

    Updated GPU Assist plugin to stay in sync with latest Gacc

--- _studio/mfx_lib/fei/h265/include/mfx_h265_enc_cm_plugin.h
+++ _studio/mfx_lib/fei/h265/include/mfx_h265_enc_cm_plugin.h
@@ -14,8 +14,148 @@
 
 #ifdef AS_H265FEI_PLUGIN
 
+// map current ENC interface to new "repipelined" Gacc code
+// will be replaced when new ENC API is finalized (e.g. adding new functions to alloc/free CM surfaces, etc.)
+#define ALT_GAA_PLUGIN_API
+
 #include "mfx_enc_ext.h"
+
+#ifdef ALT_GAA_PLUGIN_API
+
+#include "mfxfeih265.h"     /* inherit public API - this file should only contain non-public definitions */
+
+#pragma warning(disable: 4351)  // VS2012: disable default initialization warning
+
+typedef void *mfxFEIH265;
+typedef void *mfxFEISyncPoint;
+
+// NOTE - these definitions are changed in new API - must match corresponding values in mfx_h265_fei.h
+// use different names ("new") to avoid conflicts with public definitions (mfxfeih265.h)
+// calling code needs to map old->new before ProcessFrameAsync()
+typedef struct
+{
+    mfxExtBuffer       Header;
+
+    mfxHDL             SurfIntraMode[4];
+    mfxHDL             SurfIntraDist;
+    mfxHDL             SurfInterDist[64];
+    mfxHDL             SurfInterMV[64];
+    mfxHDL             SurfInterp[3];
+
+    mfxU16             reserved[24];
+} mfxFEIH265OutputNew;
+
+enum
+{
+    MFX_FEI_H265_OP_NEW_NOP          = 0x00,
+    MFX_FEI_H265_OP_NEW_GPU_COPY_SRC = 0x01,
+    MFX_FEI_H265_OP_NEW_GPU_COPY_REF = 0x02,
+    MFX_FEI_H265_OP_NEW_INTRA_MODE   = 0x04,
+    MFX_FEI_H265_OP_NEW_INTRA_DIST   = 0x08,
+    MFX_FEI_H265_OP_NEW_INTER_ME     = 0x10,
+    MFX_FEI_H265_OP_NEW_INTERPOLATE  = 0x20,
+};
+// END - new definitions
+
+// NOTE - these definitions are the same in the new API
+// these are duplicates so should be removed from this file (will get pulled in automatically once mfxfeih265.h is updated)
+
+/* input parameters - set once at init */
+typedef struct
+{
+    mfxU32 Width;
+    mfxU32 Height;
+    mfxU32 MaxCUSize;
+    mfxU32 MPMode;
+    mfxU32 NumRefFrames;
+    mfxU32 NumIntraModes;
+    mfxU32 BitDepth;
+    mfxU32 TargetUsage;
+} mfxFEIH265Param;
+
+/* basic info for current and reference frames */
+typedef struct
+{
+    void  *YPlane;
+    mfxU32 YPitch;
+    mfxU32 YBitDepth;
+    mfxU32 PicOrder;
+    mfxU32 EncOrder;
+
+    mfxU32 copyFrame;
+    mfxHDL surfIn;
+} mfxFEIH265Frame;
+
+/* FEI input - update before each call to ProcessFrameAsync */
+typedef struct
+{
+    mfxU32 FEIOp;
+    mfxU32 FrameType;
+    mfxU32 SaveSyncPoint;
+
+    mfxFEIH265Frame FEIFrameIn;
+    mfxFEIH265Frame FEIFrameRef;
+
+} mfxFEIH265Input;
+
+enum UnsupportedBlockSizes {
+    /* public API allocates 64 slots so this could be increased later (update SyncCurrent()) */
+    MFX_FEI_H265_BLK_MAX      = 12,
+};
+
+/* required buffer sizes - returned from call to Query() */
+typedef struct
+{
+    mfxU32 size;
+    mfxU32 pitch;
+    mfxU32 align;
+    mfxU32 numBytesInRow;
+    mfxU32 numRows;
+   
+    mfxU32 reserved[3];     /* do not modify */
+} mfxSurfInfoENC;
+
+typedef struct
+{
+    mfxExtBuffer       Header;
+
+    mfxSurfInfoENC     IntraMode[4];    /* 4 = intra block sizes (square 4,8,16,32) */
+    mfxSurfInfoENC     IntraDist;
+    mfxSurfInfoENC     InterDist[64];   /* 64 = max number of inter block sizes */
+    mfxSurfInfoENC     InterMV[64];
+    mfxSurfInfoENC     Interpolate[3];  /* 3 = half-pel planes (H,V,D) */
+
+    mfxU16             reserved[32];
+} mfxExtFEIH265Alloc;
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+/* FEI functions - LIB interface */
+// VideoCore* is needed for multisession mode to have its own CmDevice in each session (number of Cm surfases is restricted)
+mfxStatus H265FEI_Init(mfxFEIH265 *feih265, mfxFEIH265Param *param, void *core = NULL);
+mfxStatus H265FEI_Close(mfxFEIH265 feih265);
+mfxStatus H265FEI_ProcessFrameAsync(mfxFEIH265 feih265, mfxFEIH265Input *in, void *out, mfxFEISyncPoint *syncp);  // NOTE void * to avoid name conflicts (must point to NEW mfxExtFEIH265Output)
+mfxStatus H265FEI_SyncOperation(mfxFEIH265 feih265, mfxFEISyncPoint syncp, mfxU32 wait);
+mfxStatus H265FEI_DestroySavedSyncPoint(mfxFEIH265 feih265, mfxFEISyncPoint syncp);
+
+mfxStatus H265FEI_GetSurfaceDimensions(void *core, mfxU32 width, mfxU32 height, mfxExtFEIH265Alloc *extAlloc);
+mfxStatus H265FEI_AllocateInputSurface(mfxFEIH265 feih265, mfxHDL *pInSurf);
+mfxStatus H265FEI_AllocateOutputSurface(mfxFEIH265 feih265, mfxU8 *sysMem, mfxSurfInfoENC *surfInfo, mfxHDL *pOutSurf);
+mfxStatus H265FEI_FreeSurface(mfxFEIH265 feih265, mfxHDL pSurf);
+
+// END - duplicate definitions
+
+#ifdef __cplusplus
+}
+#endif
+
+#else
 #include "mfx_h265_fei.h"
+#endif
+
 #include "cmrt_cross_platform.h"
 
 #include <memory>
@@ -26,7 +166,12 @@
 typedef struct
 {
     mfxFEIH265Input      feiH265In;
+#ifdef ALT_GAA_PLUGIN_API
+    mfxFEIH265OutputNew *feiH265OutNew;
+    mfxFEIH265Output    *feiH265Out;
+#else
     mfxExtFEIH265Output  feiH265Out;
+#endif
     mfxFEISyncPoint      feiH265SyncPoint;
 
 } sAsyncParams;
@@ -76,6 +221,41 @@ public:
     mfxFEIH265Param         m_feiH265Param;
     mfxFEIH265Input         m_feiH265In;
     mfxFEIH265Output        m_feiH265Out;
+
+#ifdef ALT_GAA_PLUGIN_API
+    mfxStatus               AllocateAllSurfaces(void);
+    mfxStatus               FreeAllSurfaces(void);
+    mfxStatus               GetInputSurface(mfxFEIH265Frame *inFrame, mfxHDL *surfIn, mfxI32 *surfInMap, mfxI32 numSurfaces);
+    mfxU32                  MapOP(mfxU32 FEIOpPublic);
+
+    mfxExtFEIH265Alloc      m_feiAlloc;
+    mfxFEIH265OutputNew     m_feiH265OutNew[2][MFX_FEI_H265_MAX_NUM_REF_FRAMES];
+    mfxU32                  m_feiInIdx;
+    mfxU32                  m_feiInEncOrderLast;
+
+    /* handles to GPU input/output surfaces */
+    mfxHDL                  m_surfInSource[2];
+    mfxHDL                  m_surfInRecon[MFX_FEI_H265_MAX_NUM_REF_FRAMES+1];
+
+    /* mapping of input frames to GPU surfaces (encOrder) */
+    mfxI32                  m_numSourceSurfaces;
+    mfxI32                  m_numReconSurfaces;
+    mfxI32                  m_surfInSourceMap[2];
+    mfxI32                  m_surfInReconMap[MFX_FEI_H265_MAX_NUM_REF_FRAMES+1];
+
+    mfxHDL                  m_surfIntraMode[2][4];
+    mfxHDL                  m_surfIntraDist[2];
+    mfxHDL                  m_surfInterDist[2][MFX_FEI_H265_MAX_NUM_REF_FRAMES][MFX_FEI_H265_BLK_MAX];
+    mfxHDL                  m_surfInterMV[2][MFX_FEI_H265_MAX_NUM_REF_FRAMES][MFX_FEI_H265_BLK_MAX];
+    mfxHDL                  m_surfInterp[2][MFX_FEI_H265_MAX_NUM_REF_FRAMES+1][3];
+
+    /* pointers to user-allocated memory */
+    mfxU32                * m_IntraMode[2][4];
+    mfxFEIH265IntraDist   * m_IntraDist[2];
+    mfxU32                * m_InterDist[2][MFX_FEI_H265_MAX_NUM_REF_FRAMES][MFX_FEI_H265_BLK_MAX];
+    mfxI16Pair            * m_InterMV[2][MFX_FEI_H265_MAX_NUM_REF_FRAMES][MFX_FEI_H265_BLK_MAX];
+    mfxU8                 * m_Interp[2][MFX_FEI_H265_MAX_NUM_REF_FRAMES+1][3];
+#endif
 
 private:
     bool                    m_bInit;
--- _studio/mfx_lib/fei/h265/src/mfx_h265_enc_cm_plugin.cpp
+++ _studio/mfx_lib/fei/h265/src/mfx_h265_enc_cm_plugin.cpp
@@ -24,6 +24,33 @@ VideoENC_H265FEI::VideoENC_H265FEI(VideoCORE *core,  mfxStatus * sts) :
     m_feiH265Out(),
     m_bInit(false),
     m_core(core)
+#ifdef ALT_GAA_PLUGIN_API
+    ,
+    m_feiAlloc(),
+    m_feiH265OutNew(),
+    m_feiInIdx(),
+    m_feiInEncOrderLast(),
+
+    m_surfInSource(),
+    m_surfInRecon(),
+    
+    m_numSourceSurfaces(),
+    m_numReconSurfaces(),
+    m_surfInSourceMap(),
+    m_surfInReconMap(),
+
+    m_surfIntraMode(),
+    m_surfIntraDist(),
+    m_surfInterDist(),
+    m_surfInterMV(),
+    m_surfInterp(),
+
+    m_IntraMode(),
+    m_IntraDist(),
+    m_InterDist(),
+    m_InterMV(),
+    m_Interp()
+#endif
 {
     *sts = MFX_ERR_NONE;
 } 
@@ -32,6 +59,160 @@ VideoENC_H265FEI::~VideoENC_H265FEI()
 {
     Close();
 }
+
+#ifdef ALT_GAA_PLUGIN_API
+mfxStatus VideoENC_H265FEI::AllocateAllSurfaces(void)
+{
+    mfxStatus err = MFX_ERR_NONE;
+
+    /* fixed for now - these can be changed for deeper lookahead */
+    m_numSourceSurfaces = 2;
+    m_numReconSurfaces = (mfxI32)m_feiH265Param.NumRefFrames + 1;
+
+    /* allocate 2 source input surfaces for lookahead */
+    for (Ipp32s i = 0; i < m_numSourceSurfaces; i++) {
+        err = AllocateInputSurface(&m_surfInSource[i]);
+        m_surfInSourceMap[i] = -1;
+        MFX_CHECK_STS(err);
+    }
+
+    /* allocate numRefFrames + 1 recon input surfaces  */
+    for (Ipp32s i = 0; i < m_numReconSurfaces; i++) {
+        err = AllocateInputSurface(&m_surfInRecon[i]);
+        m_surfInReconMap[i] = -1;
+        MFX_CHECK_STS(err);
+    }
+
+    /* allocate output surfaces */
+    for (Ipp32s i = 0; i < 2; i++) {
+        /* intra modes: 4x4, 8x8, 16x16, 32x32 */
+        for (Ipp32s j = 0; j < 4; j++) {
+            m_IntraMode[i][j] = (mfxU32 *)CM_ALIGNED_MALLOC(m_feiAlloc.IntraMode[j].size, m_feiAlloc.IntraMode[j].align);
+            err = AllocateOutputSurface((mfxU8 *)m_IntraMode[i][j], &m_feiAlloc.IntraMode[j], &m_surfIntraMode[i][j]);
+            MFX_CHECK_STS(err);
+        }
+
+        /* intra distortion */
+        m_IntraDist[i] = (mfxFEIH265IntraDist *)CM_ALIGNED_MALLOC(m_feiAlloc.IntraDist.size, m_feiAlloc.IntraDist.align);
+        err = AllocateOutputSurface((mfxU8 *)m_IntraDist[i], &m_feiAlloc.IntraDist, &m_surfIntraDist[i]);
+        MFX_CHECK_STS(err);
+
+        /* inter distortion and MV's */
+        for (Ipp32s j = 0; j < (Ipp32s)m_feiH265Param.NumRefFrames; j++) {
+            for (Ipp32s k = MFX_FEI_H265_BLK_32x32; k <= MFX_FEI_H265_BLK_8x8; k++) {
+                m_InterDist[i][j][k] = (mfxU32 *)CM_ALIGNED_MALLOC(m_feiAlloc.InterDist[k].size, m_feiAlloc.InterDist[k].align);
+                err = AllocateOutputSurface((mfxU8 *)m_InterDist[i][j][k], &m_feiAlloc.InterDist[k], &m_surfInterDist[i][j][k]);
+                MFX_CHECK_STS(err);
+
+                m_InterMV[i][j][k] = (mfxI16Pair *)CM_ALIGNED_MALLOC(m_feiAlloc.InterMV[k].size, m_feiAlloc.InterMV[k].align);
+                err = AllocateOutputSurface((mfxU8 *)m_InterMV[i][j][k], &m_feiAlloc.InterMV[k], &m_surfInterMV[i][j][k]);
+                MFX_CHECK_STS(err);
+            }
+        }
+    }
+
+    /* half-pel interpolated frames */
+    for (Ipp32s i = 0; i < 2; i++) {
+        for (Ipp32s j = 0; j < (Ipp32s)m_feiH265Param.NumRefFrames; j++) {
+            for (Ipp32s k = 0; k < 3; k++) {
+                m_Interp[i][j][k] = (mfxU8 *)CM_ALIGNED_MALLOC(m_feiAlloc.Interpolate[k].size, m_feiAlloc.Interpolate[k].align);
+                err = AllocateOutputSurface((mfxU8 *)m_Interp[i][j][k], &m_feiAlloc.Interpolate[k], &m_surfInterp[i][j][k]);
+                MFX_CHECK_STS(err);
+            }
+        }
+    }
+
+    return MFX_ERR_NONE;
+}
+
+mfxStatus VideoENC_H265FEI::FreeAllSurfaces(void)
+{
+    /* destroy input surfaces */
+    for (Ipp32s i = 0; i < m_numSourceSurfaces; i++) {
+        FreeSurface(m_surfInSource[i]);
+    }
+
+    for (Ipp32s i = 0; i < m_numReconSurfaces; i++) {
+        FreeSurface(m_surfInRecon[i]);
+    }
+
+    /* destroy output surfaces and user memory */
+    for (Ipp32s i = 0; i < 2; i++) {
+        for (Ipp32s j = 0; j < 4; j++) {
+            FreeSurface(m_surfIntraMode[i][j]);
+            CM_ALIGNED_FREE(m_IntraMode[i][j]);
+        }
+
+        FreeSurface(m_surfIntraDist[i]);
+        CM_ALIGNED_FREE(m_IntraDist[i]);
+
+        for (Ipp32s j = 0; j < (Ipp32s)m_feiH265Param.NumRefFrames; j++) {
+            for (Ipp32s k = MFX_FEI_H265_BLK_32x32; k <= MFX_FEI_H265_BLK_8x8; k++) {
+                FreeSurface(m_surfInterDist[i][j][k]);
+                FreeSurface(m_surfInterMV[i][j][k]);
+
+                CM_ALIGNED_FREE(m_InterDist[i][j][k]);
+                CM_ALIGNED_FREE(m_InterMV[i][j][k]);
+            }
+        }
+    }
+
+    for (Ipp32s i = 0; i < 2; i++) {
+        for (Ipp32s j = 0; j < (Ipp32s)m_feiH265Param.NumRefFrames; j++) {
+            for (Ipp32s k = 0; k < 3; k++) {
+                FreeSurface(m_surfInterp[i][j][k]);
+                CM_ALIGNED_FREE(m_Interp[i][j][k]);
+            }
+        }
+    }
+
+    return MFX_ERR_NONE;
+}
+
+mfxU32 VideoENC_H265FEI::MapOP(mfxU32 FEIOpPublic)
+{
+    mfxU32 FEIOpNew = 0;
+
+    if (FEIOpPublic & MFX_FEI_H265_OP_NOP)          FEIOpNew |= MFX_FEI_H265_OP_NEW_NOP;
+    if (FEIOpPublic & MFX_FEI_H265_OP_INTRA_MODE)   FEIOpNew |= MFX_FEI_H265_OP_NEW_INTRA_MODE;
+    if (FEIOpPublic & MFX_FEI_H265_OP_INTRA_DIST)   FEIOpNew |= MFX_FEI_H265_OP_NEW_INTRA_DIST;
+    if (FEIOpPublic & MFX_FEI_H265_OP_INTER_ME)     FEIOpNew |= MFX_FEI_H265_OP_NEW_INTER_ME;
+    if (FEIOpPublic & MFX_FEI_H265_OP_INTERPOLATE)  FEIOpNew |= MFX_FEI_H265_OP_NEW_INTERPOLATE;
+    
+    return FEIOpNew;
+}
+    
+mfxStatus VideoENC_H265FEI::GetInputSurface(mfxFEIH265Frame *inFrame, mfxHDL *surfIn, mfxI32 *surfInMap, mfxI32 numSurfaces)
+{
+    mfxI32 i, frameIdxOldest;
+
+    for (i = 0; i < numSurfaces; i++) {
+        if (surfInMap[i] == (mfxI32)inFrame->EncOrder)
+            break;
+    }
+
+    /* frame already copied - set handle to surface */
+    if (i < numSurfaces) {
+        inFrame->copyFrame = 0;
+        inFrame->surfIn = surfIn[i];
+        return MFX_ERR_NONE;
+    }
+            
+    /* frame not copied - replace oldest input frame in queue */
+    frameIdxOldest = 0;
+    for (i = 1; i < numSurfaces; i++) {
+        if (surfInMap[i] < surfInMap[frameIdxOldest])
+            frameIdxOldest = i;
+    }
+    surfInMap[frameIdxOldest] = inFrame->EncOrder;
+
+    inFrame->copyFrame = 1;
+    inFrame->surfIn = surfIn[frameIdxOldest];
+
+    return MFX_ERR_NONE;
+}
+
+#endif
 
 mfxStatus VideoENC_H265FEI::Init(mfxVideoParam *par)
 { 
@@ -51,15 +232,23 @@ mfxStatus VideoENC_H265FEI::Init(mfxVideoParam *par)
     m_feiH265Param.MPMode        = pParams->MPMode;
     m_feiH265Param.NumIntraModes = pParams->NumIntraModes;
 
+#ifdef ALT_GAA_PLUGIN_API
+    m_feiH265Param.BitDepth      = 8;
+    m_feiH265Param.TargetUsage   = 0;
+
+    memset(&m_feiAlloc, 0, sizeof(mfxExtFEIH265Alloc));
+    H265FEI_GetSurfaceDimensions(m_core, m_feiH265Param.Width, m_feiH265Param.Height, &m_feiAlloc);
+#else
     // TODO - validate bounds
     m_feiH265Param.BitDepth      = pParams->BitDepth;
     m_feiH265Param.TargetUsage   = pParams->TargetUsage;
+#endif
 
     /* validate parameters - update these limits as necessary in future versions */
     MFX_CHECK ((m_feiH265Param.Width > 0)         && (m_feiH265Param.Height > 0),     MFX_ERR_INVALID_VIDEO_PARAM);
     MFX_CHECK ((m_feiH265Param.Width <= 4088)     && (m_feiH265Param.Height) <= 4088, MFX_ERR_INVALID_VIDEO_PARAM);
     MFX_CHECK ((m_feiH265Param.Width & 0x0f) == 0 && (m_feiH265Param.Height & 0x0f) == 0, MFX_ERR_INVALID_VIDEO_PARAM);
-    MFX_CHECK ((m_feiH265Param.NumRefFrames >= 0) && (m_feiH265Param.NumRefFrames <= MFX_FEI_H265_MAX_NUM_REF_FRAMES), MFX_ERR_INVALID_VIDEO_PARAM);
+    MFX_CHECK ((m_feiH265Param.NumRefFrames <= MFX_FEI_H265_MAX_NUM_REF_FRAMES), MFX_ERR_INVALID_VIDEO_PARAM);
     MFX_CHECK ((m_feiH265Param.MaxCUSize == 16)   || (m_feiH265Param.MaxCUSize == 32), MFX_ERR_INVALID_VIDEO_PARAM);
     MFX_CHECK ((m_feiH265Param.MPMode >= 1)       && (m_feiH265Param.MPMode <= 3), MFX_ERR_INVALID_VIDEO_PARAM);
     MFX_CHECK ((m_feiH265Param.NumIntraModes == 1), MFX_ERR_INVALID_VIDEO_PARAM);
@@ -72,6 +261,11 @@ mfxStatus VideoENC_H265FEI::Init(mfxVideoParam *par)
     }
     memset(&m_feiH265In,  0, sizeof(m_feiH265In));
     memset(&m_feiH265Out, 0, sizeof(m_feiH265Out));
+
+#ifdef ALT_GAA_PLUGIN_API
+    err = AllocateAllSurfaces();
+    m_feiInIdx = 0;
+#endif
 
     return err;
 }
@@ -87,8 +281,13 @@ mfxStatus VideoENC_H265FEI::Close()
 {
     mfxStatus err = MFX_ERR_NONE;
 
-    if (m_feiH265)
-        err = H265FEI_Close(m_feiH265);
+    if (!m_feiH265)
+        return err;
+
+#ifdef ALT_GAA_PLUGIN_API
+    FreeAllSurfaces();
+#endif
+    err = H265FEI_Close(m_feiH265);
     m_feiH265 = 0;
 
     return err;
@@ -157,7 +356,11 @@ static mfxStatus SubmitFrameH265FEIRoutine(void *pState, void *pParam, mfxU32 /*
     VideoENC_H265FEI *pFEI = (VideoENC_H265FEI *)pState;
     sAsyncParams *out = (sAsyncParams *)pParam;
  
+#ifdef ALT_GAA_PLUGIN_API
+    H265FEI_ProcessFrameAsync(pFEI->m_feiH265, &(out->feiH265In), out->feiH265OutNew, &(out->feiH265SyncPoint));
+#else
     H265FEI_ProcessFrameAsync(pFEI->m_feiH265, &(out->feiH265In), &(out->feiH265Out), &(out->feiH265SyncPoint));
+#endif
 
     return tskRes;
 
@@ -180,7 +383,11 @@ static mfxStatus CompleteFrameH265FEIRoutine(void *pState, void * pParam , mfxSt
     VideoENC_H265FEI *pFEI = (VideoENC_H265FEI *)pState;
     sAsyncParams *out = (sAsyncParams *)pParam;
 
-    H265FEI_SyncOperation(pFEI->m_feiH265, (out->feiH265SyncPoint), 0xFFFFFFFF);
+    mfxStatus tskRes = H265FEI_SyncOperation(pFEI->m_feiH265, (out->feiH265SyncPoint), 0xFFFFFFFF);
+    if (tskRes != MFX_ERR_NONE)
+        return tskRes;
+
+    H265FEI_DestroySavedSyncPoint(pFEI->m_feiH265, (out->feiH265SyncPoint));
 
     if (pParam)
     {
@@ -212,13 +419,21 @@ mfxStatus VideoENC_H265FEI::RunFrameVmeENCCheck(mfxENCInput *input, mfxENCOutput
     memset(pAsyncParams.get(), 0, sizeof(sAsyncParams));
     sp = pAsyncParams.get();
 
+    sp->feiH265In.SaveSyncPoint = 1;    // automatically destroy sync points
+
     /* copy frame info into params struct */
     if (input->InSurface) {
         sp->feiH265In.FEIFrameIn.EncOrder = input->InSurface->Data.FrameOrder;
         sp->feiH265In.FEIFrameIn.PicOrder = input->InSurface->Data.FrameOrder;      // unused in FEI core currently, but leave field if needed later
+#ifdef ALT_GAA_PLUGIN_API
+        sp->feiH265In.FEIFrameIn.YPitch   = input->InSurface->Data.Pitch;
+        sp->feiH265In.FEIFrameIn.YPlane   = input->InSurface->Data.Y;
+        sp->feiH265In.FEIFrameIn.YBitDepth = 8;
+#else
         sp->feiH265In.FEIFrameIn.YPitch   = input->InSurface->Data.PitchLow + ((mfxU32)(input->InSurface->Data.PitchHigh) << 16);
         sp->feiH265In.FEIFrameIn.YPlane   = input->InSurface->Data.Y;
         sp->feiH265In.FEIFrameIn.YBitDepth = input->InSurface->Info.BitDepthLuma;
+#endif
         MFX_CHECK_NULL_PTR1(sp->feiH265In.FEIFrameIn.YPlane);
     } 
 
@@ -226,9 +441,15 @@ mfxStatus VideoENC_H265FEI::RunFrameVmeENCCheck(mfxENCInput *input, mfxENCOutput
         MFX_CHECK_NULL_PTR1(input->L0Surface[0]);
         sp->feiH265In.FEIFrameRef.EncOrder = input->L0Surface[0]->Data.FrameOrder;
         sp->feiH265In.FEIFrameRef.PicOrder = input->L0Surface[0]->Data.FrameOrder;
+#ifdef ALT_GAA_PLUGIN_API
+        sp->feiH265In.FEIFrameRef.YPitch   = input->L0Surface[0]->Data.Pitch;
+        sp->feiH265In.FEIFrameRef.YPlane   = input->L0Surface[0]->Data.Y;
+        sp->feiH265In.FEIFrameRef.YBitDepth = 8;
+#else
         sp->feiH265In.FEIFrameRef.YPitch   = input->L0Surface[0]->Data.PitchLow + ((mfxU32)(input->L0Surface[0]->Data.PitchHigh) << 16);
         sp->feiH265In.FEIFrameRef.YPlane   = input->L0Surface[0]->Data.Y;
         sp->feiH265In.FEIFrameRef.YBitDepth = input->L0Surface[0]->Info.BitDepthLuma;
+#endif
         MFX_CHECK_NULL_PTR1(sp->feiH265In.FEIFrameRef.YPlane);
     }
 
@@ -239,6 +460,84 @@ mfxStatus VideoENC_H265FEI::RunFrameVmeENCCheck(mfxENCInput *input, mfxENCOutput
     MFX_CHECK_NULL_PTR1(pIn);
     MFX_CHECK_NULL_PTR1(pOut);
 
+#ifdef ALT_GAA_PLUGIN_API
+    mfxU32 k;
+
+    sp->feiH265In.FEIOp     = MapOP(pIn->FEIOp);
+    sp->feiH265In.FrameType = pIn->FrameType;
+
+    /* when input frame is updated, flip double buffer (max 2 frames in flight) */
+    if (sp->feiH265In.FEIFrameIn.EncOrder > m_feiInEncOrderLast) {
+        m_feiInIdx = 1 - m_feiInIdx;
+        m_feiInEncOrderLast = sp->feiH265In.FEIFrameIn.EncOrder;
+    }
+
+    //fprintf(stderr, "Inframe % 4d, feiInIdx = %d\n", sp->feiH265In.FEIFrameIn.EncOrder, m_feiInIdx);
+    
+    if (sp->feiH265In.FEIOp & (MFX_FEI_H265_OP_NEW_INTRA_MODE | MFX_FEI_H265_OP_NEW_INTRA_DIST | MFX_FEI_H265_OP_NEW_INTER_ME)) {
+        /* get source frame surface, uploading to GPU if necessary */
+        GetInputSurface(&sp->feiH265In.FEIFrameIn, m_surfInSource, m_surfInSourceMap, m_numSourceSurfaces);
+        if (sp->feiH265In.FEIFrameIn.copyFrame)    
+            sp->feiH265In.FEIOp |= MFX_FEI_H265_OP_NEW_GPU_COPY_SRC;
+    }
+
+    if (sp->feiH265In.FEIOp & (MFX_FEI_H265_OP_NEW_INTER_ME | MFX_FEI_H265_OP_NEW_INTERPOLATE)) {
+        /* get recon frame surface, uploading to GPU if necessary */
+        GetInputSurface(&sp->feiH265In.FEIFrameRef, m_surfInRecon,  m_surfInReconMap,  m_numReconSurfaces);
+        if (sp->feiH265In.FEIFrameRef.copyFrame)    
+            sp->feiH265In.FEIOp |= MFX_FEI_H265_OP_NEW_GPU_COPY_REF;
+    }
+
+    /* pass pointers to output surfaces */
+    for (k = 0; k < 4; k++) {
+        m_feiH265OutNew[m_feiInIdx][pIn->RefIdx].SurfIntraMode[k] = m_surfIntraMode[m_feiInIdx][k];              
+    }
+    m_feiH265OutNew[m_feiInIdx][pIn->RefIdx].SurfIntraDist = m_surfIntraDist[m_feiInIdx];
+
+    /* pass pointers to output surfaces */
+    for (k = MFX_FEI_H265_BLK_32x32; k <= MFX_FEI_H265_BLK_8x8; k++) {
+        m_feiH265OutNew[m_feiInIdx][pIn->RefIdx].SurfInterDist[k] = m_surfInterDist[m_feiInIdx][pIn->RefIdx][k];
+        m_feiH265OutNew[m_feiInIdx][pIn->RefIdx].SurfInterMV[k]   = m_surfInterMV[m_feiInIdx][pIn->RefIdx][k];
+    }
+
+    // TODO - proper queue for interp ref frames (no need to process same frame more than once) - this would be handled by caller in new API
+    for (k = 0; k < 3; k++) {
+        m_feiH265OutNew[m_feiInIdx][pIn->RefIdx].SurfInterp[k]    = m_surfInterp[m_feiInIdx][pIn->RefIdx][k];
+    }
+
+    /* repeat, filling in pointers contained in caller's struct */
+    pOut->feiOut->IntraDist = m_IntraDist[m_feiInIdx];
+
+    pOut->feiOut->IntraModes4x4   = m_IntraMode[m_feiInIdx][0];     pOut->feiOut->IntraPitch4x4   = m_feiAlloc.IntraMode[0].pitch / sizeof(mfxU32);
+    pOut->feiOut->IntraModes8x8   = m_IntraMode[m_feiInIdx][1];     pOut->feiOut->IntraPitch8x8   = m_feiAlloc.IntraMode[1].pitch / sizeof(mfxU32);
+    pOut->feiOut->IntraModes16x16 = m_IntraMode[m_feiInIdx][2];     pOut->feiOut->IntraPitch16x16 = m_feiAlloc.IntraMode[2].pitch / sizeof(mfxU32);
+    pOut->feiOut->IntraModes32x32 = m_IntraMode[m_feiInIdx][3];     pOut->feiOut->IntraPitch32x32 = m_feiAlloc.IntraMode[3].pitch / sizeof(mfxU32);
+    pOut->feiOut->IntraMaxModes   = m_feiH265Param.NumIntraModes;
+
+    pOut->feiOut->IntraDist       = m_IntraDist[m_feiInIdx];        pOut->feiOut->IntraPitch      = m_feiAlloc.IntraDist.pitch / sizeof(mfxFEIH265IntraDist);
+
+    for (Ipp32s k = MFX_FEI_H265_BLK_32x32; k <= MFX_FEI_H265_BLK_8x8; k++) {
+        pOut->feiOut->Dist[pIn->RefIdx][k] = m_InterDist[m_feiInIdx][pIn->RefIdx][k];
+        pOut->feiOut->MV[pIn->RefIdx][k]   = m_InterMV[m_feiInIdx][pIn->RefIdx][k];
+
+        pOut->feiOut->PitchMV[k]   = m_feiAlloc.InterMV[k].pitch / sizeof(mfxI16Pair);
+        pOut->feiOut->PitchDist[k] = m_feiAlloc.InterDist[k].pitch / sizeof(mfxU32);
+    }
+
+    for (Ipp32s k = 0; k < 3; k++) {
+        pOut->feiOut->Interp[pIn->RefIdx][k] = m_Interp[m_feiInIdx][pIn->RefIdx][k];
+        pOut->feiOut->InterpolatePitch  = m_feiAlloc.Interpolate[k].pitch;
+    }
+
+    pOut->feiOut->PaddedWidth =  (m_feiH265Param.Width + 15) & ~0x0f;
+    pOut->feiOut->PaddedHeight = (m_feiH265Param.Height + 15) & ~0x0f;
+
+    pOut->feiOut->InterpolateWidth =  pOut->feiOut->PaddedWidth + 2*MFX_FEI_H265_INTERP_BORDER;
+    pOut->feiOut->InterpolateHeight = pOut->feiOut->PaddedHeight + 2*MFX_FEI_H265_INTERP_BORDER;
+
+    sp->feiH265OutNew = &m_feiH265OutNew[m_feiInIdx][pIn->RefIdx];
+    sp->feiH265Out = pOut->feiOut;
+#else
     sp->feiH265In.FEIOp     = pIn->FEIOp;
     sp->feiH265In.FrameType = pIn->FrameType;
 
@@ -247,6 +546,7 @@ mfxStatus VideoENC_H265FEI::RunFrameVmeENCCheck(mfxENCInput *input, mfxENCOutput
 
     /* save a local copy of the mfxExtFEIH265Output struct allocated by the user (need copy since this operates asynchronously with FEI core) */
     sp->feiH265Out = *pOut;
+#endif
 
     /* basic error checking */
     if (sp->feiH265In.FrameType != MFX_FRAMETYPE_I && sp->feiH265In.FrameType != MFX_FRAMETYPE_P && sp->feiH265In.FrameType != MFX_FRAMETYPE_B)
@@ -304,8 +604,10 @@ mfxStatus VideoENC_H265FEI::Query(VideoCORE* pCore, mfxVideoParam *in, mfxVideoP
         pParams->MPMode = 1;
         pParams->NumIntraModes = 1;
 
+#ifndef ALT_GAA_PLUGIN_API
         pParams->BitDepth = 1;
         pParams->TargetUsage = 1;
+#endif
     }
     else
     {
@@ -340,13 +642,16 @@ mfxStatus VideoENC_H265FEI::Query(VideoCORE* pCore, mfxVideoParam *in, mfxVideoP
         pParams_out->MPMode        = pParams_in->MPMode;
         pParams_out->NumIntraModes = pParams_in->NumIntraModes;
 
+#ifndef ALT_GAA_PLUGIN_API
         pParams_out->BitDepth      = pParams_in->BitDepth;
         pParams_out->TargetUsage   = pParams_in->TargetUsage;
+#endif
 
 
         MFX_CHECK ((pParams_out->MPMode >= 1) && (pParams_out->MPMode <= 3), MFX_ERR_INVALID_VIDEO_PARAM);
         MFX_CHECK ((pParams_out->NumIntraModes == 1), MFX_ERR_INVALID_VIDEO_PARAM);
 
+#ifndef ALT_GAA_PLUGIN_API
         /* fill out extBuf with required surface dimensions so user can allocate memory */
         mfxExtFEIH265Alloc *pAlloc_in  = (mfxExtFEIH265Alloc *)GetExtBuffer(in->ExtParam,  in->NumExtParam, MFX_EXTBUFF_FEI_H265_ALLOC);
         mfxExtFEIH265Alloc *pAlloc_out = (mfxExtFEIH265Alloc *)GetExtBuffer(out->ExtParam, out->NumExtParam, MFX_EXTBUFF_FEI_H265_ALLOC);
@@ -355,6 +660,7 @@ mfxStatus VideoENC_H265FEI::Query(VideoCORE* pCore, mfxVideoParam *in, mfxVideoP
         MFX_CHECK_NULL_PTR1(pAlloc_out);
 
         H265FEI_GetSurfaceDimensions(pCore, out->mfx.FrameInfo.Width, out->mfx.FrameInfo.Height, pAlloc_out);
+#endif
     }
 
     return MFX_ERR_NONE;
