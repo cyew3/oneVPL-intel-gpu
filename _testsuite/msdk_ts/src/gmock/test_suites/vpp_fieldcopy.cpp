36bd5c983b5d5999e3ffe9f40fb44f646e092900
Matveeva, Svetlana <svetlana.matveeva@intel.com>
2015-09-14 18:35:48 +0000

    Added DoUse cases and checked compatibility with other filters for VPPFieldProcessing
    Change-Id: I6d3f1b0d85dca8c63591798e9065f654c19bda1b

--- _testsuite/msdk_ts/src/gmock/test_suites/vpp_fieldcopy.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/vpp_fieldcopy.cpp
@@ -27,6 +27,7 @@ private:
 
     tsNoiseFiller m_noise;
     mfxExtVPPFieldProcessing m_vpp_fp;
+    mfxExtVPPDoUse vpp_du;
 
     enum
     {
@@ -36,7 +37,13 @@ private:
     enum
     {
         MFX_PAR = 1,
-        EXT_VPP
+        EXT_VPP,
+        EXT_VPP_DENOISE,
+        EXT_VPP_DETAIL,
+        EXT_VPP_FRC,
+        EXT_VPP_IS,
+        EXT_VPP_PD,
+        EXT_VPP_PROCAMP
     };
 
     struct tc_struct
@@ -118,16 +125,59 @@ const TestSuite::tc_struct TestSuite::test_case[] =
         {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.Mode, MFX_VPP_COPY_FRAME},
         {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.OutField, MFX_PICTYPE_UNKNOWN}}
     },
-
     {/*09*/ MFX_ERR_UNSUPPORTED, 0, {   // feature is not supported
         {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.Mode, MFX_VPP_SWAP_FIELDS}}
     },
-
     {/*10*/ MFX_ERR_NONE, DO_USE, {
         {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.Mode, MFX_VPP_COPY_FIELD},
         {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.InField, MFX_PICTYPE_BOTTOMFIELD},
         {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.OutField, MFX_PICTYPE_TOPFIELD}}
-    }
+    },
+    {/*11*/ MFX_ERR_NONE, DO_USE, {
+        {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.Mode, MFX_VPP_COPY_FIELD},
+        {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.InField, MFX_PICTYPE_TOPFIELD},
+        {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.OutField, MFX_PICTYPE_BOTTOMFIELD}}
+    },
+    {/*12*/ MFX_ERR_NONE, DO_USE, {
+        {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.Mode, MFX_VPP_COPY_FIELD},
+        {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.InField, MFX_PICTYPE_TOPFIELD},
+        {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.OutField, MFX_PICTYPE_TOPFIELD}}
+    },
+    {/*13*/ MFX_ERR_NONE, DO_USE, {
+        {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.Mode, MFX_VPP_COPY_FIELD},
+        {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.InField, MFX_PICTYPE_BOTTOMFIELD},
+        {EXT_VPP, &tsStruct::mfxExtVPPFieldProcessing.OutField, MFX_PICTYPE_BOTTOMFIELD}}
+    },
+    {/*14*/ MFX_ERR_NONE, DO_USE, {
+        {EXT_VPP_DENOISE, &tsStruct::mfxExtVPPFieldProcessing.Mode, MFX_VPP_COPY_FIELD},
+        {EXT_VPP_DENOISE, &tsStruct::mfxExtVPPFieldProcessing.InField, MFX_PICTYPE_BOTTOMFIELD},
+        {EXT_VPP_DENOISE, &tsStruct::mfxExtVPPFieldProcessing.OutField, MFX_PICTYPE_BOTTOMFIELD}}
+    },
+    {/*15*/ MFX_ERR_NONE, DO_USE, {
+        {EXT_VPP_DETAIL, &tsStruct::mfxExtVPPFieldProcessing.Mode, MFX_VPP_COPY_FIELD},
+        {EXT_VPP_DETAIL, &tsStruct::mfxExtVPPFieldProcessing.InField, MFX_PICTYPE_BOTTOMFIELD},
+        {EXT_VPP_DETAIL, &tsStruct::mfxExtVPPFieldProcessing.OutField, MFX_PICTYPE_BOTTOMFIELD}}
+    },
+    {/*16*/ MFX_ERR_NONE, DO_USE, {
+        {EXT_VPP_FRC, &tsStruct::mfxExtVPPFieldProcessing.Mode, MFX_VPP_COPY_FIELD},
+        {EXT_VPP_FRC, &tsStruct::mfxExtVPPFieldProcessing.InField, MFX_PICTYPE_BOTTOMFIELD},
+        {EXT_VPP_FRC, &tsStruct::mfxExtVPPFieldProcessing.OutField, MFX_PICTYPE_BOTTOMFIELD}}
+    },
+    {/*17*/ MFX_ERR_NONE, DO_USE, {
+        {EXT_VPP_IS, &tsStruct::mfxExtVPPFieldProcessing.Mode, MFX_VPP_COPY_FIELD},
+        {EXT_VPP_IS, &tsStruct::mfxExtVPPFieldProcessing.InField, MFX_PICTYPE_BOTTOMFIELD},
+        {EXT_VPP_IS, &tsStruct::mfxExtVPPFieldProcessing.OutField, MFX_PICTYPE_BOTTOMFIELD}}
+    },
+    {/*18*/ MFX_ERR_NONE, DO_USE, {
+        {EXT_VPP_PD, &tsStruct::mfxExtVPPFieldProcessing.Mode, MFX_VPP_COPY_FIELD},
+        {EXT_VPP_PD, &tsStruct::mfxExtVPPFieldProcessing.InField, MFX_PICTYPE_BOTTOMFIELD},
+        {EXT_VPP_PD, &tsStruct::mfxExtVPPFieldProcessing.OutField, MFX_PICTYPE_BOTTOMFIELD}}
+    },
+    {/*19*/ MFX_ERR_NONE, DO_USE, {
+        {EXT_VPP_PROCAMP, &tsStruct::mfxExtVPPFieldProcessing.Mode, MFX_VPP_COPY_FIELD},
+        {EXT_VPP_PROCAMP, &tsStruct::mfxExtVPPFieldProcessing.InField, MFX_PICTYPE_BOTTOMFIELD},
+        {EXT_VPP_PROCAMP, &tsStruct::mfxExtVPPFieldProcessing.OutField, MFX_PICTYPE_BOTTOMFIELD}}
+    },
 };
 
 const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case)/sizeof(TestSuite::tc_struct);
@@ -142,17 +192,45 @@ int TestSuite::RunTest(unsigned int id)
     m_par.IOPattern = MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY;
     m_par.AsyncDepth = 1;
 
-    SETPARS(&m_vpp_fp, EXT_VPP);
     SETPARS(m_pPar, MFX_PAR);
 
     if (tc.mode == DO_USE)
     {
-        mfxExtVPPDoUse vpp_du = {0};
-        vpp_du.NumAlg = 1;
-        vpp_du.AlgList = new mfxU32[1];
+        vpp_du.Header.BufferId = MFX_EXTBUFF_VPP_DOUSE;
+        vpp_du.Header.BufferSz = sizeof(mfxExtVPPDoUse);
+
+        if (tc.set_par[0].ext_type == EXT_VPP)
+        {
+            vpp_du.NumAlg = 1;
+            vpp_du.AlgList = new mfxU32[1];
+        }
+        else
+        {
+            vpp_du.NumAlg = 2;
+            vpp_du.AlgList = new mfxU32[2];
+        }
+
+        SETPARS(&m_vpp_fp, EXT_VPP);
         vpp_du.AlgList[0] = MFX_EXTBUFF_VPP_FIELD_PROCESSING;
 
+        if (tc.set_par[0].ext_type == EXT_VPP_DENOISE)
+            vpp_du.AlgList[1] = MFX_EXTBUFF_VPP_DENOISE;
+        else if (tc.set_par[0].ext_type == EXT_VPP_DETAIL)
+            vpp_du.AlgList[1] = MFX_EXTBUFF_VPP_DETAIL;
+        else if (tc.set_par[0].ext_type == EXT_VPP_FRC)
+            vpp_du.AlgList[1] = MFX_EXTBUFF_VPP_FRAME_RATE_CONVERSION;
+        else if (tc.set_par[0].ext_type == EXT_VPP_IS)
+            vpp_du.AlgList[1] = MFX_EXTBUFF_VPP_IMAGE_STABILIZATION;
+        else if (tc.set_par[0].ext_type == EXT_VPP_PD)
+            vpp_du.AlgList[1] = MFX_EXTBUFF_VPP_PICSTRUCT_DETECTION;
+        else if (tc.set_par[0].ext_type == EXT_VPP_PROCAMP)
+            vpp_du.AlgList[1] = MFX_EXTBUFF_VPP_PROCAMP;
+
         attach2frame = true;
+
+        m_par.ExtParam = new mfxExtBuffer*[1];
+        m_par.ExtParam[0] = (mfxExtBuffer*)&vpp_du;
+        m_par.NumExtParam = 1;
     } else
     {
         m_par.ExtParam = new mfxExtBuffer*[1];
@@ -168,7 +246,57 @@ int TestSuite::RunTest(unsigned int id)
     AllocSurfaces();
 
     g_tsStatus.expect(tc.sts);
+
+    if (tc.set_par[0].ext_type == EXT_VPP_IS)
+        g_tsStatus.expect(MFX_WRN_FILTER_SKIPPED);
+    if (tc.set_par[0].ext_type == EXT_VPP_PD && g_tsOSFamily != MFX_OS_FAMILY_WINDOWS)
+        g_tsStatus.expect(MFX_WRN_FILTER_SKIPPED);
+
     mfxStatus sts = Init(m_session, m_pPar);
+
+    if (tc.mode == DO_USE)
+    {
+        mfxVideoParam par2 = {0};
+        mfxExtVPPDoUse vpp_du2;
+        vpp_du2.Header.BufferId = MFX_EXTBUFF_VPP_DOUSE;
+        vpp_du2.Header.BufferSz = sizeof(mfxExtVPPDoUse);
+
+       if (tc.set_par[0].ext_type == EXT_VPP)
+       {
+           vpp_du2.NumAlg = 1;
+           vpp_du2.AlgList = new mfxU32[1];
+       }
+       else
+       {
+           vpp_du2.NumAlg = 2;
+           vpp_du2.AlgList = new mfxU32[2];
+       }
+
+        par2.NumExtParam = 1;
+        par2.ExtParam = new mfxExtBuffer*[1];
+        par2.ExtParam[0] = (mfxExtBuffer*)&vpp_du2;
+
+        MFXVideoVPP_GetVideoParam(m_session, &par2);
+
+        if (vpp_du2.NumAlg != vpp_du.NumAlg)
+        {
+            g_tsStatus.check(MFX_ERR_ABORTED);
+            g_tsLog << "ERROR: Number of algorithms in specified DoUse buffer is not equal to number of algorithms in buffer returned by GetVideoParam() \n";
+        }
+        for (mfxU32 i = 0; i< vpp_du.NumAlg; i++)
+        {
+            sts = MFX_ERR_ABORTED;
+            for (mfxU32 j = 0; j< vpp_du2.NumAlg; j++)
+            {
+                if (vpp_du.AlgList[i]==vpp_du2.AlgList[j])
+                    sts = MFX_ERR_NONE;
+            }
+            if (sts == MFX_ERR_ABORTED)
+                g_tsLog << "ERROR: List of algorithms in specified DoUse buffer is not equal to list of algorithms in buffer returned by GetVideoParam() \n";
+            g_tsStatus.check(sts);
+        }
+    }
+
     /*if ((sts != tc.sts) && (sts = MFX_WRN_FILTER_SKIPPED))
     {
         g_tsLog << "ERROR: Init() returned MFX_WRN_FILTER_SKIPPED\n";
