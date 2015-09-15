44a845b16766e5230a191dfd463cbd8d4abc46e6
Kokoshnikov, Alexey S <alexey.s.kokoshnikov@intel.com>
2015-09-15 21:37:18 +0300

    VCSD100023285: beh_hevcd_decode_frame_async
    Affect tests: beh_hevcd_decode_frame_async-plugin_hevcdhw
    
    If decoder get invalid values (Data.Y, Dtata.Pitch) from LockFrame then
    DecodeFrameAsync returns ERR_NONE, SyncOperation - ABORTED.
    DecodeFrameAsync returns ERR_LOCK_MEMORY when LockFrame throw exemption.
    
    DecodeHeader must be called after SetHandle

--- _testsuite/msdk_ts/src/gmock/test_suites/hevcd_decode_frame_async.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/hevcd_decode_frame_async.cpp
@@ -68,11 +68,11 @@ private:
             case MEMID     : m_use_memid = !!c.par[0];    break;
             case ALLOCATOR : SetAllocator(
                                  new frame_allocator(
-                                    (frame_allocator::AllocatorType)    c.par[0], 
-                                    (frame_allocator::AllocMode)        c.par[1], 
-                                    (frame_allocator::LockMode)         c.par[2], 
+                                    (frame_allocator::AllocatorType)    c.par[0],
+                                    (frame_allocator::AllocMode)        c.par[1],
+                                    (frame_allocator::LockMode)         c.par[2],
                                     (frame_allocator::OpaqueAllocMode)  c.par[3]
-                                 ), 
+                                 ),
                                  false
                              ); break;
             case CLOSE_DEC : Close(); break;
@@ -90,7 +90,7 @@ private:
     }
 };
 
-const TestSuite::tc_struct TestSuite::test_case[] = 
+const TestSuite::tc_struct TestSuite::test_case[] =
 {
     {/* 0*/ MFX_ERR_NONE, },
     {/* 1*/ MFX_ERR_INVALID_HANDLE, {SESSION}},
@@ -100,30 +100,30 @@ const TestSuite::tc_struct TestSuite::test_case[] =
     {/* 5*/ MFX_ERR_NULL_PTR, {SYNCP}},
     {/* 6*/ MFX_ERR_MORE_DATA, {BITSTREAM, &tsStruct::mfxBitstream.DataLength, {10}}},
     {/* 7*/ MFX_ERR_NOT_INITIALIZED, {CLOSE_DEC}},
-    {/* 8*/ MFX_ERR_INCOMPATIBLE_VIDEO_PARAM, 
+    {/* 8*/ MFX_ERR_INCOMPATIBLE_VIDEO_PARAM,
        {{INIT|MFXVPAR, &tsStruct::mfxVideoParam.mfx.FrameInfo.Width, {320}},
         {INIT|MFXVPAR, &tsStruct::mfxVideoParam.mfx.FrameInfo.CropW, {320}}}
     },
-    {/* 9*/ MFX_ERR_INCOMPATIBLE_VIDEO_PARAM, 
+    {/* 9*/ MFX_ERR_INCOMPATIBLE_VIDEO_PARAM,
        {{INIT|MFXVPAR, &tsStruct::mfxVideoParam.mfx.FrameInfo.Height, {208}},
         {INIT|MFXVPAR, &tsStruct::mfxVideoParam.mfx.FrameInfo.CropH, {208}}}
     },
     {/*10*/ MFX_ERR_NONE, {BITSTREAM, &tsStruct::mfxBitstream.DataFlag, {MFX_BITSTREAM_COMPLETE_FRAME}}},
-    {/*11*/ MFX_ERR_LOCK_MEMORY, 
-       {{INIT|ALLOCATOR, 0, {frame_allocator::SOFTWARE, frame_allocator::ALLOC_MAX, frame_allocator::LARGE_PITCH_LOCK}}, 
+    {/*11*/ MFX_ERR_ABORTED,
+       {{INIT|ALLOCATOR, 0, {frame_allocator::SOFTWARE, frame_allocator::ALLOC_MAX, frame_allocator::LARGE_PITCH_LOCK}},
         {INIT|MEMID, 0, {1}}}
     },
     {/*12*/ MFX_ERR_UNDEFINED_BEHAVIOR, {BITSTREAM, &tsStruct::mfxBitstream.DataOffset, {100001}}},
     {/*13*/ MFX_ERR_MORE_SURFACE, {SURF_WORK, &tsStruct::mfxFrameSurface1.Data.Locked, {1}}},
-    {/*14*/ MFX_ERR_LOCK_MEMORY, 
-        {{INIT|ALLOCATOR, 0, {frame_allocator::SOFTWARE, frame_allocator::ALLOC_MAX, frame_allocator::ZERO_LUMA_LOCK}}, 
+    {/*14*/ MFX_ERR_ABORTED,
+        {{INIT|ALLOCATOR, 0, {frame_allocator::SOFTWARE, frame_allocator::ALLOC_MAX, frame_allocator::ZERO_LUMA_LOCK}},
         {INIT|MEMID, 0, {1}}}
     },
     {/*15*/ MFX_ERR_NONE, {INIT|MFXVPAR, &tsStruct::mfxVideoParam.IOPattern, {MFX_IOPATTERN_OUT_VIDEO_MEMORY}}},
     {/*16*/ MFX_ERR_UNDEFINED_BEHAVIOR, {SURF_WORK, &tsStruct::mfxFrameSurface1.Data.PitchHigh, {0x8000}}},
     {/*17*/ MFX_ERR_UNDEFINED_BEHAVIOR, {SURF_WORK, &tsStruct::mfxFrameSurface1.Data.Y, {0}}},
     {/*18*/ MFX_ERR_NONE, {INIT|MFXVPAR, &tsStruct::mfxVideoParam.IOPattern, {MFX_IOPATTERN_OUT_OPAQUE_MEMORY}}},
-    {/*19*/ MFX_ERR_UNDEFINED_BEHAVIOR, 
+    {/*19*/ MFX_ERR_UNDEFINED_BEHAVIOR,
        {{INIT|MFXVPAR, &tsStruct::mfxVideoParam.IOPattern, {MFX_IOPATTERN_OUT_OPAQUE_MEMORY}},
         {SURF_WORK, &tsStruct::mfxFrameSurface1.Data.Y, {1}}}
     },
@@ -140,8 +140,9 @@ int TestSuite::RunTest(unsigned int id)
     m_bs_processor = &reader;
     auto tc = test_case[id];
 
-    DecodeHeader();
-    
+    if (tc.sts == MFX_ERR_INCOMPATIBLE_VIDEO_PARAM)
+        DecodeHeader();
+
     apply_par(tc, INIT);
     Init();
 
@@ -158,8 +159,22 @@ int TestSuite::RunTest(unsigned int id)
     else
     {
         g_tsStatus.expect(tc.sts);
-        DecodeFrameAsync(m_session, m_pBitstream, m_pSurf, m_ppSurfOut, m_pSyncPoint);
-        g_tsStatus.check();
+        TRACE_FUNC5(MFXVideoDECODE_DecodeFrameAsync, m_session, m_pBitstream, m_pSurf, m_ppSurfOut, m_pSyncPoint);
+        mfxStatus mfxRes = MFXVideoDECODE_DecodeFrameAsync(m_session, m_pBitstream, m_pSurf, m_ppSurfOut, m_pSyncPoint);
+        TS_TRACE(mfxRes);
+        TS_TRACE(m_pBitstream);
+        TS_TRACE(m_ppSurfOut);
+        TS_TRACE(m_pSyncPoint);
+        if (mfxRes == MFX_ERR_NONE)
+        {
+            SyncOperation(*m_pSyncPoint);
+        }
+        else
+        {
+            g_tsStatus.m_status = mfxRes;
+            g_tsStatus.check();
+        }
+
     }
 
     TS_END;
