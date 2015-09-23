1ea091eb06708fb9db10a2685ce3fcc6b5eee0c7
Dmitry Sharipo <dmitry.sharipo@intel.com>
2015-09-23 16:10:31 +0400

    Fix SysToVideo copy for argb4.
    Change-Id: Ifdad12413d889ee6ae4563df14786aed87e9d3ed

--- _studio/shared/src/libmfx_core_vaapi.cpp
+++ _studio/shared/src/libmfx_core_vaapi.cpp
@@ -1274,7 +1274,7 @@ VAAPIVideoCORE::DoFastCopyExtended(
             MFX_CHECK_STS(sts);
         }
         else if(m_bCmCopy == true && CM_ALIGNED(pSrc->Data.Pitch) && pSrc->Info.FourCC == MFX_FOURCC_RGB4 && CM_ALIGNED(IPP_MIN(IPP_MIN(pSrc->Data.R,pSrc->Data.G),pSrc->Data.B)) && CM_SUPPORTED_COPY_SIZE(roi)){
-            sts = pCmCopy->CopySystemToVideoMemoryAPI(pDst->Data.MemId, 0, IPP_MIN(IPP_MIN(pSrc->Data.R,pSrc->Data.G),pSrc->Data.B), pSrc->Data.Pitch, (mfxU32)verticalPitch, roi);
+            sts = pCmCopy->CopySystemToVideoMemoryAPI(pDst->Data.MemId, 0, IPP_MIN(IPP_MIN(pSrc->Data.R,pSrc->Data.G),pSrc->Data.B), pSrc->Data.Pitch, (mfxU32)pSrc->Info.Height, roi);
             MFX_CHECK_STS(sts);
         }
         else if(m_bCmCopy == true && CM_ALIGNED(pSrc->Data.Pitch) && pSrc->Info.FourCC != MFX_FOURCC_UYVY && pSrc->Info.FourCC != MFX_FOURCC_YV12 && pSrc->Info.FourCC != MFX_FOURCC_NV12 && CM_ALIGNED(pSrc->Data.Y) && CM_SUPPORTED_COPY_SIZE(roi)){
