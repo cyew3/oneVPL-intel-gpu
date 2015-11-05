49bcca3b98773e2de4630c00dd35c2089317daf3
Zhelybalov, Igor <igor.zhelybalov@intel.com>
2015-11-05 11:53:32 +0300

    Added beh_vpp_10bit suite
    Change-Id: Ie35a71f9f94509ea471a6337d3df4d6d466df690

---
+++ _testsuite/msdk_ts/src/gmock/test_suites/vpp10_fourcc.cpp
@@ -0,0 +1,80 @@
+#include "ts_vpp.h"
+#include "ts_struct.h"
+#include "mfxstructures.h"
+#include "mfx_serializer.h"
+
+namespace vpp_10bit
+{
+
+class TestSuite : tsVideoVPP
+{
+public:
+    TestSuite()
+        : tsVideoVPP(false)
+        , m_sts(MFX_ERR_UNKNOWN)
+    {
+    }
+    ~TestSuite() 
+    {
+        CleanExtParam();
+    }
+    int RunTest(unsigned int id);
+    static const unsigned int n_cases;
+
+private:
+    mfxStatus   m_sts;
+
+    void SetParam(std::string function)
+    {
+        CleanExtParam();
+        DeSerializeFromYaml(m_sts, "mfxStatus." + function, g_tsConfig.cfg_filename);
+        DeSerializeFromYaml(*m_pPar, "mfxVideoParam." + function, g_tsConfig.cfg_filename);
+    }
+
+    void CleanExtParam()
+    {
+        for (int i = 0; i < m_par.NumExtParam; i++)
+        {
+            if (m_par.ExtParam[i])
+            {
+                delete[] m_par.ExtParam[i];
+                m_par.ExtParam[i] = 0;
+            }
+        }
+
+        if (m_par.ExtParam)
+        {
+            delete[] m_par.ExtParam;
+            m_par.ExtParam = 0;
+        }
+
+        m_par.NumExtParam = 0;
+    }
+};
+
+const unsigned int TestSuite::n_cases = 1;
+
+int TestSuite::RunTest(unsigned int id)
+{
+    TS_START;
+
+    mfxStatus sts;
+
+    MFXInit();
+
+    SetParam("Query"); g_tsStatus.expect(m_sts);
+    sts = Query();
+
+    SetParam("Init"); g_tsStatus.expect(m_sts);
+    sts = Init();
+
+    SetParam("QueryIOSurf"); g_tsStatus.expect(m_sts);
+    sts = QueryIOSurf();
+
+    TS_END;
+    return 0;
+}
+
+TS_REG_TEST_SUITE_CLASS(vpp_10bit);
+
+}
