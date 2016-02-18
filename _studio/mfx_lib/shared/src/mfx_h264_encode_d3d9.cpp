c73799fbace23a7d37f3e43c6787fcfa0d9d885c
Shaporenko Artem <artem.shaporenko@intel.com>
2016-02-18 19:15:07 +0300

    HW AVC Encode: fix for Win7 WiDi do not check for WiDi function when retriving HW counter to keep backward compatibility with old app

--- _studio/mfx_lib/shared/src/mfx_h264_encode_d3d9.cpp
+++ _studio/mfx_lib/shared/src/mfx_h264_encode_d3d9.cpp
@@ -1732,7 +1732,7 @@ mfxStatus D3D9Encoder::QueryStatus(
             task.m_mad[fieldId] = feedback->MAD;
         task.m_resetBRC = !!feedback->reserved0; //WiDi w/a
         //for KBL we need retrive counter from HW instead of incrementing ourselfs.
-        if(m_caps.HWCounterAutoIncrement && (m_forcedCodingFunction & ENCODE_WIDI))
+        if(m_caps.HWCounterAutoIncrement)
         {
             task.m_aesCounter[0].Count = feedback->aes_counter.Counter;
             task.m_aesCounter[0].IV = feedback->aes_counter.IV;
