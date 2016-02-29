353ad888f089116c7dfae3993c516e4562b6edb3
Kokoshnikov, Alexey S <alexey.s.kokoshnikov@intel.com>
2016-02-29 14:15:26 +0300

    Add beh_hevcd_query_io_surf
    Affect tests: behavior_hevcd_query_io_surf*
    
    Move behavior_hevcd_query_io_surf to gmock

--- _testsuite/msdk_ts/src/gmock/test_suites/hevcd_query_io_surf.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/hevcd_query_io_surf.cpp
@@ -3,141 +3,183 @@
 namespace hevcd_query_io_surf
 {
 
-typedef struct 
+enum
 {
-    mfxSession            session;
-    mfxVideoParam*        pPar;
-    mfxFrameAllocRequest* pRequest;
-}QIOSpar;
+    ZERO_SESSION = 1,
+    ZERO_PARAM = 2,
+    ZERO_REQUEST = 3
+};
 
-typedef void (*callback)(QIOSpar&);
-
-void zero_session   (QIOSpar& p) { p.session = 0; }
-void zero_param     (QIOSpar& p) { p.pPar = 0; }
-void zero_request   (QIOSpar& p) { p.pRequest = 0; }
-
-typedef struct 
+typedef struct
 {
-    callback    set_par;
+    mfxU32      set_par;
     mfxU32      IOPattern;
     mfxU32      AsyncDepth;
     mfxStatus   sts;
 } tc_struct;
 
-tc_struct test_case[] = 
+class TestSuite : tsVideoDecoder
+{
+public:
+    TestSuite() : tsVideoDecoder(MFX_CODEC_HEVC){}
+    ~TestSuite() { }
+    int RunTest(unsigned int id);
+    static const unsigned int n_cases;
+    static const tc_struct test_case[];
+};
+
+
+const tc_struct TestSuite::test_case[] =
 {
     {// 0
-        zero_session, 
-        MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 
-        0, 
+        ZERO_SESSION,
+        MFX_IOPATTERN_OUT_SYSTEM_MEMORY,
+        1,
         MFX_ERR_INVALID_HANDLE
     },
     {// 1
-        zero_param, 
-        MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 
-        0, 
+        ZERO_PARAM,
+        MFX_IOPATTERN_OUT_SYSTEM_MEMORY,
+        1,
         MFX_ERR_NULL_PTR
     },
     {// 2
-        zero_request, 
-        MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 
-        0, 
+        ZERO_REQUEST,
+        MFX_IOPATTERN_OUT_SYSTEM_MEMORY,
+        1,
         MFX_ERR_NULL_PTR
     },
     {// 3
-        0, 
-        MFX_IOPATTERN_OUT_SYSTEM_MEMORY, 
-        5, 
+        0,
+        MFX_IOPATTERN_OUT_SYSTEM_MEMORY,
+        5,
         MFX_ERR_NONE
     },
     {// 4
-        0, 
-        MFX_IOPATTERN_OUT_VIDEO_MEMORY, 
-        5, 
+        0,
+        MFX_IOPATTERN_OUT_VIDEO_MEMORY,
+        5,
         MFX_ERR_NONE
+    },
+    {// 5
+        0,
+        MFX_IOPATTERN_OUT_OPAQUE_MEMORY,
+        5,
+        MFX_ERR_NONE
+    },
+    //IOPattern
+    {// 6
+        0,
+        MFX_IOPATTERN_OUT_VIDEO_MEMORY + 2,
+        1,
+        MFX_ERR_NONE
+    },
+    {// 7
+        0,
+        MFX_IOPATTERN_OUT_VIDEO_MEMORY + 1,
+        1,
+        MFX_ERR_NONE
+    },
+    {// 8
+        0,
+        MFX_IOPATTERN_OUT_SYSTEM_MEMORY + 2,
+        1,
+        MFX_ERR_NONE
+    },
+    {// 9
+        0,
+        MFX_IOPATTERN_OUT_SYSTEM_MEMORY + 1,
+        1,
+        MFX_ERR_NONE
+    },
+    {// 10
+        0,
+        MFX_IOPATTERN_OUT_OPAQUE_MEMORY - 4,
+        1,
+        MFX_ERR_INVALID_VIDEO_PARAM
+    },
+    {// 11
+        0,
+        6,
+        1,
+        MFX_ERR_INVALID_VIDEO_PARAM
+    },
+    {// 12
+        0,
+        1,
+        1,
+        MFX_ERR_INVALID_VIDEO_PARAM
+    },
+    {// 13
+        0,
+        2,
+        1,
+        MFX_ERR_INVALID_VIDEO_PARAM
+    },
+    {// 14
+        0,
+        0,
+        1,
+        MFX_ERR_INVALID_VIDEO_PARAM
     },
 };
 
-enum
-{
-    IOP_IN  = (MFX_IOPATTERN_IN_VIDEO_MEMORY|MFX_IOPATTERN_IN_SYSTEM_MEMORY|MFX_IOPATTERN_IN_OPAQUE_MEMORY),
-    IOP_OUT = (MFX_IOPATTERN_OUT_VIDEO_MEMORY|MFX_IOPATTERN_OUT_SYSTEM_MEMORY|MFX_IOPATTERN_OUT_OPAQUE_MEMORY),
-    IOP_NUM = (1<<(3+3)),
-};
+const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case)/sizeof(tc_struct);
 
-
-tc_struct GetTestCase(unsigned int id)
-{
-    if(id < sizeof(test_case)/sizeof(tc_struct))
-        return test_case[id];
-
-    //all possible combinations of IOPattern
-    tc_struct tc = {};
-    id -= sizeof(test_case)/sizeof(tc_struct); // exclude pre-defined cases
-
-    // get IOPattern IN from test-case id
-    tc.IOPattern = (id & IOP_IN);
-
-    // get IOPattern OUT from rest of test-case id
-    id &= ~IOP_IN;
-    while(id && (id & (~IOP_OUT)))
-        id <<= 1;
-    tc.IOPattern |= id;
-
-    // only one IOPattern OUT is valid
-    tc.sts = (!id || (id & (id - 1))) ? MFX_ERR_INVALID_VIDEO_PARAM : MFX_ERR_NONE; 
-
-
-    return tc;
-}
-
-
-int test(unsigned int id)
+int TestSuite::RunTest(unsigned int id)
 {
     TS_START;
-    tsVideoDecoder dec(MFX_CODEC_HEVC);
-    tc_struct tc = GetTestCase(id);
+    auto& tc = test_case[id];
 
-    dec.MFXInit();
+    MFXInit();
+    *(m_uid) = MFX_PLUGINID_HEVCD_SW;
+    if(m_uid)
+        Load();
 
-    if(dec.m_uid)
-        dec.Load();
-
-    QIOSpar par = {dec.m_session, &dec.m_par, &dec.m_request};
-    dec.m_par.IOPattern  = tc.IOPattern;
-    dec.m_par.AsyncDepth = 1;
-
-    if(tc.set_par)
-    {
-        (*tc.set_par)(par);
-    }
+    m_par.IOPattern  = tc.IOPattern;
+    m_par.AsyncDepth = 1;
 
     g_tsStatus.expect(tc.sts);
-    dec.QueryIOSurf(par.session, par.pPar, par.pRequest);
+
+    if (tc.set_par == ZERO_SESSION)
+        QueryIOSurf(NULL, m_pPar, m_pRequest);
+    else if (tc.set_par == ZERO_PARAM)
+        QueryIOSurf(m_session, NULL, m_pRequest);
+    else if (tc.set_par == ZERO_REQUEST)
+        QueryIOSurf(m_session, m_pPar, NULL);
+    else
+        QueryIOSurf(m_session, m_pPar, m_pRequest);
 
     if(g_tsStatus.get() >= MFX_ERR_NONE)
     {
         mfxU32 expectedType = MFX_MEMTYPE_FROM_DECODE|MFX_MEMTYPE_EXTERNAL_FRAME;
 
-        if(dec.m_par.IOPattern & MFX_IOPATTERN_OUT_SYSTEM_MEMORY)
+        if(m_par.IOPattern & MFX_IOPATTERN_OUT_SYSTEM_MEMORY)
             expectedType |= MFX_MEMTYPE_SYSTEM_MEMORY;
-        else 
+        else if (m_par.IOPattern & MFX_IOPATTERN_OUT_VIDEO_MEMORY)
             expectedType |= MFX_MEMTYPE_VIDEO_MEMORY_DECODER_TARGET;
+        else
+        {
+            if (0 == memcmp(m_uid->Data, MFX_PLUGINID_HEVCD_HW.Data, sizeof(MFX_PLUGINID_HEVCD_HW.Data)))
+                expectedType = MFX_MEMTYPE_FROM_DECODE|MFX_MEMTYPE_DXVA2_DECODER_TARGET|MFX_MEMTYPE_OPAQUE_FRAME;
+            else
+                expectedType = MFX_MEMTYPE_FROM_DECODE|MFX_MEMTYPE_SYSTEM_MEMORY|MFX_MEMTYPE_OPAQUE_FRAME;
+        }
 
-        EXPECT_EQ( expectedType, dec.m_request.Type );
-        EXPECT_EQ( dec.m_par.mfx.FrameInfo, dec.m_request.Info );
-        EXPECT_GT( dec.m_request.NumFrameMin, 0 );
-        EXPECT_GE( dec.m_request.NumFrameSuggested, dec.m_request.NumFrameMin );
+        EXPECT_EQ( expectedType, m_request.Type );
+        EXPECT_EQ( m_par.mfx.FrameInfo, m_request.Info );
+        EXPECT_GT( m_request.NumFrameMin, 0 );
+        EXPECT_GE( m_request.NumFrameSuggested, m_request.NumFrameMin );
 
         if(tc.AsyncDepth)
         {
-            mfxFrameAllocRequest& request1 = dec.m_request;
+            mfxFrameAllocRequest& request1 = m_request;
             mfxFrameAllocRequest  request2 = {};
-            dec.m_par.AsyncDepth = tc.AsyncDepth;
+            m_par.AsyncDepth = tc.AsyncDepth;
 
-            dec.QueryIOSurf(dec.m_session, &dec.m_par, &request2);
+            QueryIOSurf(m_session, &m_par, &request2);
 
-            EXPECT_EQ( request1.NumFrameMin + dec.m_par.AsyncDepth - 1, request2.NumFrameMin );
+            EXPECT_EQ( request1.NumFrameMin + m_par.AsyncDepth - 1, request2.NumFrameMin );
         }
     }
 
@@ -145,5 +187,5 @@ int test(unsigned int id)
     return 0;
 }
 
-TS_REG_TEST_SUITE(hevcd_query_io_surf, test, sizeof(test_case)/sizeof(tc_struct) + IOP_NUM);
+TS_REG_TEST_SUITE_CLASS(hevcd_query_io_surf);
 }
