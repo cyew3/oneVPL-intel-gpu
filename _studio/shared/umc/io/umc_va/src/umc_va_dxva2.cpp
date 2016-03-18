80d869f4831eb75709ca451a4b04046a6d8950f2
Shellunts, Artur <artur.shellunts@intel.com>
2016-03-18 15:59:17 +0300

    Disabled MSFT guid for vp9 decode 8 bit due to issues

--- _studio/shared/umc/io/umc_va/src/umc_va_dxva2.cpp
+++ _studio/shared/umc/io/umc_va/src/umc_va_dxva2.cpp
@@ -390,7 +390,7 @@ static const GuidProfile guidProfiles[] =
     { VP8_VLD,   sDXVA_Intel_ModeVP8_VLD},
 
 #if defined(NTDDI_WIN10_TH2)
-    { VP9_VLD,                                    DXVA_ModeVP9_VLD_Profile0},
+//  { VP9_VLD,                                    DXVA_ModeVP9_VLD_Profile0},                   // Disable until problems fixed
     { VP9_VLD | VA_PROFILE_10,                    DXVA_ModeVP9_VLD_10bit_Profile2_private_copy},
 #endif
     { VP9_VLD,                                    DXVA_Intel_ModeVP9_Profile0_VLD },
