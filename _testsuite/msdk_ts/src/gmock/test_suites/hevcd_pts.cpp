de80148a12ee85a201fa28163130d8a19e6e798f
Kokoshnikov, Alexey S <alexey.s.kokoshnikov@intel.com>
2016-03-18 14:17:17 +0300

    Fix hevcd_pts
    Affect tests: beh_hevcd_pts-plugin_hevcdhw
    
    Change algorithm of select expected time stamp
    
    JIRA: MDP-559

--- _testsuite/msdk_ts/src/gmock/test_suites/hevcd_pts.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/hevcd_pts.cpp
@@ -5,6 +5,11 @@ namespace hevcd_pts
 {
 
 const mfxU32 n_frames = 30;
+enum
+{
+    SMALL = 20,
+    NONE = 1000
+};
 
 struct tc_struct
 {
@@ -22,11 +27,13 @@ public:
     std::map<mfxU64, mfxU64>::iterator m_it;
     mfxU64 m_pos;
     mfxU64 m_pts;
+    mfxU64 m_buff_size;
 
     Reader(const tc_struct& tc)
         : tsBitstreamReader(g_tsStreamPool.Get(tc.stream), tc.bufSz * 1000)
         , m_pos(0)
         , m_pts(tc.pts[0])
+        , m_buff_size(tc.bufSz)
     {
         tsParserHEVC p;
         std::map<mfxU64, mfxU64> offset_by_poc;
@@ -55,11 +62,11 @@ public:
 
     mfxStatus ProcessBitstream(mfxBitstream& bs, mfxU32 nFrames)
     {
-        if(m_it == pts_by_offset.end())
+        if(pts_by_offset.empty())
             return MFX_ERR_MORE_DATA;
 
         mfxStatus sts = MFX_ERR_NONE;
-        mfxU64 curr = m_it->first;
+        std::map<mfxU64, mfxU64>::iterator curr = pts_by_offset.begin();
         m_pos -= bs.DataLength;
 
         if(!m_eos)
@@ -67,18 +74,44 @@ public:
             sts = tsBitstreamReader::ProcessBitstream(bs, nFrames);
         }
 
-        if(std::distance(m_it, pts_by_offset.end()) > 1)
+        if (m_pos)
         {
-            m_it++;
-            mfxU64 next = m_it->first;
+            if (m_buff_size == NONE)
+            {
+                std::map<mfxU64, mfxU64>::iterator it = pts_by_offset.begin();
+                while (it->first <= m_pos)
+                {
+                    it++;
+                    if (it == pts_by_offset.end()) break;
 
-            if(!(curr < m_pos && next <= (m_pos + bs.DataLength)))
-                m_it--;
+                    if (curr->second > it->second)
+                    {
+                        curr = it;
+                    }
+                }
+                bs.TimeStamp = curr->second;
+                pts_by_offset.erase(curr);
+            }
+            else
+            {
+                mfxU64 last = curr->first;
+                curr++;
+                mfxU64 next = curr->first;
 
-            bs.TimeStamp = m_it->second;
-        } else if(curr < m_pos)
-            m_it ++;
+                if (!(last < m_pos && next <= (m_pos + bs.DataLength)))
+                {
+                    curr--;
+                    bs.TimeStamp = curr->second;
+                }
+                else
+                {
+                    bs.TimeStamp = curr->second;
+                    curr--;
+                    pts_by_offset.erase(curr);
+                }
+            }
 
+        }
         m_pos += bs.DataLength;
 
         return sts;
@@ -137,7 +170,7 @@ const tc_struct TestSuite::test_case[] =
     {/* 0*/
         "/conformance/hevc/itu/DELTAQP_A_BRCM_4.bit",
         30000, 1000,
-        1000,
+        NONE,
         {     0,  3600,  7200, 10800, 14400, 18000, 21600, 25200,  28800,  32400,
           36000, 39600, 43200, 46800, 50400, 54000, 57600, 61200,  64800,  68400,
           72000, 75600, 79200, 82800, 86400, 90000, 93600, 97200, 100800, 104400 }
@@ -145,7 +178,7 @@ const tc_struct TestSuite::test_case[] =
     {/* 1*/
         "/conformance/hevc/itu/DELTAQP_A_BRCM_4.bit",
         30000, 1001,
-        1000,
+        NONE,
         {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
          -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
          -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 }
@@ -153,7 +186,7 @@ const tc_struct TestSuite::test_case[] =
     {/* 2*/
         "/conformance/hevc/itu/DELTAQP_A_BRCM_4.bit",
         60000, 1001,
-        1000,
+        NONE,
         {100000, -1, -1, -1, -1, -1, -1, -1, -1, -1,
          -1, -1, -1, -1, 0, -1, -1, -1, -1, -1,
          -1, -1, -1, -1, -1, -1, 1000, -1, -1, -1, }
@@ -161,7 +194,7 @@ const tc_struct TestSuite::test_case[] =
     {/* 3*/
         "/conformance/hevc/itu/DELTAQP_A_BRCM_4.bit",
         30000, 1000,
-        20,
+        SMALL,
         {     0,  3600,  7200, 10800, 14400, 18000, 21600, 25200,  28800,  32400,
           36000, 39600, 43200, 46800, 50400, 54000, 57600, 61200,  64800,  68400,
           72000, 75600, 79200, 82800, 86400, 90000, 93600, 97200, 100800, 104400 }
@@ -169,7 +202,7 @@ const tc_struct TestSuite::test_case[] =
     {/* 4*/
         "/conformance/hevc/itu/DELTAQP_A_BRCM_4.bit",
         30000, 1001,
-        20,
+        SMALL,
         {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
          -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
          -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, }
@@ -177,7 +210,7 @@ const tc_struct TestSuite::test_case[] =
     {/* 5*/
         "/conformance/hevc/itu/DELTAQP_A_BRCM_4.bit",
         60000, 1001,
-        20,
+        SMALL,
         {100000, -1, -1, -1, -1, -1, -1, -1, -1, -1,
          -1, -1, -1, -1, 0, -1, -1, -1, -1, -1,
          -1, -1, -1, -1, -1, -1, 1000, -1, -1, -1,}
@@ -201,7 +234,6 @@ int TestSuite::RunTest(unsigned int id)
     DecodeHeader();
     m_par.mfx.FrameInfo.FrameRateExtN = tc.frN;
     m_par.mfx.FrameInfo.FrameRateExtD = tc.frD;
-
     DecodeFrames(n_frames);
 
     TS_END;
