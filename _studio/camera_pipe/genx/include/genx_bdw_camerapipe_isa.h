28344d7d6460fc4e2acbaff22e1ba35438e7c9e0
Dmitry Sharipo <dmitry.sharipo@intel.com>
2016-04-01 13:57:08 +0300

    Eliminated personal data from raw accelerator kernels
    Change-Id: Id46bcdd084483c83092d906c243563a1b629664f

--- _studio/camera_pipe/genx/include/genx_bdw_camerapipe_isa.h
+++ _studio/camera_pipe/genx/include/genx_bdw_camerapipe_isa.h
@@ -3,11 +3,11 @@
 //     This software is supplied under the terms of a license agreement or
 //     nondisclosure agreement with Intel Corporation and may not be copied
 //     or disclosed except in accordance with the terms of that agreement.
-//          Copyright(c) 2012-2014 Intel Corporation. All Rights Reserved.
+//          Copyright(c) 2012-2016 Intel Corporation. All Rights Reserved.
 //
 */
 
 #ifndef __genx_bdw_camerapipe__
 #define __genx_bdw_camerapipe__
-extern const unsigned char genx_bdw_camerapipe[1100211];
+extern const unsigned char genx_bdw_camerapipe[990805];
 #endif
