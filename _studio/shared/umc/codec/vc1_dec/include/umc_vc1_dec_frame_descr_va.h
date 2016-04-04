e110b27ed3c051d0826fc49fdb437076d2ec72e7
U. Artie Eoff <ullysses.a.eoff@intel.com>
2016-04-04 13:07:22 -0700

    explicit MemID cast for comparison
    MemID is unsigned, yet it appears most of the implementation here
    assigns -1 to MemID variables to indicate an invalid id.  So in
    order to do a comparison of -1 and MemID, we need to explicitly
    cast -1 to a MemID.  Otherwise the compiler will complain about
    comparisons between signed and unsigned integers.
    
    This fixes compilation error on GCC 5.x:
      error: comparison between signed and unsigned integer expressions
      [-Werror=sign-compare]
    
    Change-Id: I7d1fd651086942a2ac2d1bed9e8156f43062b72c
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- _studio/shared/umc/codec/vc1_dec/include/umc_vc1_dec_frame_descr_va.h
+++ _studio/shared/umc/codec/vc1_dec/include/umc_vc1_dec_frame_descr_va.h
@@ -4,7 +4,7 @@
 //     This software is supplied under the terms of a license agreement or
 //     nondisclosure agreement with Intel Corporation and may not be copied
 //     or disclosed except in accordance with the terms of that agreement.
-//          Copyright(c) 2004-2013 Intel Corporation. All Rights Reserved.
+//          Copyright(c) 2004-2016 Intel Corporation. All Rights Reserved.
 //
 //
 //          VC-1 (VC1) decoder, Template class Frame description VA support
@@ -785,7 +785,7 @@ namespace UMC
             {
                 if(m_pMemoryAllocator)
                 {
-                    if (m_iPicHeaderID != -1)
+                    if (m_iPicHeaderID != (MemID)-1)
                     {
                         m_pMemoryAllocator->Unlock(m_iPicHeaderID);
                         m_pMemoryAllocator->Free(m_iPicHeaderID);
@@ -793,7 +793,7 @@ namespace UMC
                     }
                     m_pContext->m_InitPicLayer = NULL;
 
-                    if (m_iBitplaneID != -1)
+                    if (m_iBitplaneID != (MemID)-1)
                     {
                         m_pMemoryAllocator->Unlock(m_iBitplaneID);
                         m_pMemoryAllocator->Free(m_iBitplaneID);
@@ -801,7 +801,7 @@ namespace UMC
                     }
                     m_pContext->m_pBitplane.m_databits = NULL;
 
-                    if (m_ipBufferStartID != -1)
+                    if (m_ipBufferStartID != (MemID)-1)
                     {
                         m_pMemoryAllocator->Unlock(m_ipBufferStartID);
                         m_pMemoryAllocator->Free(m_ipBufferStartID);
@@ -809,7 +809,7 @@ namespace UMC
                     }
                     m_pContext->m_pBufferStart = NULL;
 
-                    if (m_ipContextID != -1)
+                    if (m_ipContextID != (MemID)-1)
                     {
                         m_pMemoryAllocator->Unlock(m_ipContextID);
                         m_pMemoryAllocator->Free(m_ipContextID);
