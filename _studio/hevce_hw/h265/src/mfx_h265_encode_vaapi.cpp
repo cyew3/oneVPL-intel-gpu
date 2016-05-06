0c3a2d60f93dd02348e4feb64bf9b88b3f8225ba
Olga Golikova <olga.golikova@intel.com>
2016-05-06 15:34:21 +0300

    HW HEVC E: Parallel BRC in VA

--- _studio/hevce_hw/h265/src/mfx_h265_encode_vaapi.cpp
+++ _studio/hevce_hw/h265/src/mfx_h265_encode_vaapi.cpp
@@ -18,6 +18,7 @@
 #include "mfx_h265_encode_hw_utils.h"
 
 #define MFX_CHECK_WITH_ASSERT(EXPR, ERR) { assert(EXPR); MFX_CHECK(EXPR, ERR); }
+#define PARALLEL_BRC_support
 
 namespace MfxHwH265Encode
 {
