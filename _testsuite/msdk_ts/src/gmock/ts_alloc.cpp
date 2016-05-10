cyew3/oneVPL-intel-gpu

commit d27896dc45f1cae9f100d313b89dce6f93e2e81b

[MQ829] set oversized surface for avc legacy/fei encoder
Change-Id: I4f7bfd2624a0c891db18a50052dc3a9b4587e566

_testsuite/msdk_ts/src/gmock/ts_alloc.cpp
@@ -106,7 +106,6 @@ void tsSurfacePool::AllocOpaque(mfxFrameAllocRequest request, mfxExtOpaqueSurfac
 mfxStatus tsSurfacePool::AllocSurfaces(mfxFrameAllocRequest request, bool direct_pointers)
 {
     bool isSW = !(request.Type & (MFX_MEMTYPE_VIDEO_MEMORY_DECODER_TARGET|MFX_MEMTYPE_VIDEO_MEMORY_PROCESSOR_TARGET));
-    mfxFrameAllocResponse response = {};
     mfxFrameSurface1 s = {};
     mfxFrameAllocRequest* pRequest = &request;
 
@@ -123,16 +122,16 @@ mfxStatus tsSurfacePool::AllocSurfaces(mfxFrameAllocRequest request, bool direct
         UseDefaultAllocator(isSW);
     }
 
-    TRACE_FUNC3(m_allocator->AllocFrame, m_allocator, pRequest, &response);
-    g_tsStatus.check( m_allocator->AllocFrame(m_allocator, pRequest, &response) );
+    TRACE_FUNC3(m_allocator->AllocFrame, m_allocator, pRequest, &m_response);
+    g_tsStatus.check( m_allocator->AllocFrame(m_allocator, pRequest, &m_response) );
     TS_CHECK_MFX;
 
-    for(mfxU16 i = 0; i < response.NumFrameActual; i++)
+    for(mfxU16 i = 0; i < m_response.NumFrameActual; i++)
     {
-        if(response.mids)
+        if(m_response.mids)
         {
             s.Data.Y = 0;
-            s.Data.MemId = response.mids[i];
+            s.Data.MemId = m_response.mids[i];
             if(direct_pointers && isSW)
             {
                 LockSurface(s);
@@ -141,6 +140,21 @@ mfxStatus tsSurfacePool::AllocSurfaces(mfxFrameAllocRequest request, bool direct
         }
         m_pool.push_back(s);
     }
+
+    return g_tsStatus.get();
+}
+
+mfxStatus tsSurfacePool::FreeSurfaces()
+{
+    if (m_pool.size() == 0)
+        return MFX_ERR_NONE;
+
+    TRACE_FUNC2(m_allocator->Free, m_allocator, &m_response);
+    g_tsStatus.check( m_allocator->Free(m_allocator, &m_response) );
+    TS_CHECK_MFX;
+
+    m_pool.clear();
+
     return g_tsStatus.get();
 }
 

_testsuite/msdk_ts/include/gmock/ts_alloc.h
@@ -1,3 +1,13 @@
+/* /////////////////////////////////////////////////////////////////////////////
+//
+//                  INTEL CORPORATION PROPRIETARY INFORMATION
+//     This software is supplied under the terms of a license agreement or
+//     nondisclosure agreement with Intel Corporation and may not be copied
+//     or disclosed except in accordance with the terms of that agreement.
+//          Copyright(c) 2014-2016 Intel Corporation. All Rights Reserved.
+//
+*/
+
 #pragma once
 
 #include "ts_common.h"
@@ -17,6 +27,7 @@ private:
     bool             m_isd3d11;
     std::vector<mfxFrameSurface1> m_pool;
     std::vector<mfxFrameSurface1*> m_opaque_pool;
+    mfxFrameAllocResponse m_response;
 
     void Close();
 
@@ -32,6 +43,7 @@ public:
     void AllocOpaque        (mfxFrameAllocRequest request, mfxExtOpaqueSurfaceAlloc& osa);
 
     mfxStatus AllocSurfaces (mfxFrameAllocRequest request, bool direct_pointers = true);
+    mfxStatus FreeSurfaces  ();
     mfxStatus LockSurface   (mfxFrameSurface1& s);
     mfxStatus UnlockSurface (mfxFrameSurface1& s);
 

_testsuite/msdk_ts/msdk_gmock.vcxproj
@@ -347,6 +347,8 @@
     <ClCompile Include="src\gmock\test_suites\vpp_frame_async_ex.cpp" />
     <ClCompile Include="src\gmock\test_suites\vpp_frc_interp.cpp" />
     <ClCompile Include="src\gmock\test_suites\vpp_uyvy.cpp" />
+    <ClCompile Include="src\gmock\test_suites\avce_oversized_surfaces.cpp" />
+    <ClCompile Include="src\gmock\test_suites\fei_encode_oversized_surfaces.cpp" />
     <ClCompile Include="src\gmock\ts_alloc.cpp" />
     <ClCompile Include="src\gmock\ts_bitstream.cpp" />
     <ClCompile Include="src\gmock\ts_common.cpp" />

_testsuite/msdk_ts/msdk_gmock.vcxproj.filters
@@ -595,5 +595,11 @@
     <ClCompile Include="src\gmock\test_suites\avcd_reset.cpp">
       <Filter>Source Files\test_suites\avcd</Filter>
     </ClCompile>
+    <ClCompile Include="src\gmock\test_suites\avce_oversized_surfaces.cpp">
+      <Filter>Source Files\test_suites\avce</Filter>
+    </ClCompile>
+    <ClCompile Include="src\gmock\test_suites\fei_encode_oversized_surfaces.cpp">
+      <Filter>Source Files\test_suites\FEI</Filter>
+    </ClCompile>
   </ItemGroup>
 </Project>

_testsuite/msdk_ts/src/gmock/test_suites/avce_oversized_surfaces.cpp
@@ -1,0 +1,277 @@
+/* ****************************************************************************** *\
+INTEL CORPORATION PROPRIETARY INFORMATION
+This software is supplied under the terms of a license agreement or nondisclosure
+agreement with Intel Corporation and may not be copied or disclosed except in
+accordance with the terms of that agreement
+Copyright(c) 2015-2016 Intel Corporation. All Rights Reserved.
+
+\* ****************************************************************************** */
+
+
+#include "ts_encoder.h"
+#include "ts_struct.h"
+
+/*
+ * This test is for VCSD100025510 - create tests on oversized surfaces
+ * Issue reproduced in calling: AllocFrames(oversized res)->Init(oversized res)
+ *                              ->Reset(correct res)->EncodeFrames(with oversized surfaces)
+ *
+ * Tests configuration:
+ *  1. Oversized Surface Resolution:
+ *     -1920x1080
+ *  2. Source Stream Resolution:
+ *     -1280x720 (16 aligned only)
+ *     -720x480 (16&32 aligned)
+ *     -720x240 (16 aligned only)
+ *     -868x588 (neither 16 nor 32 aligned, but 4 aligned for interlaced test)
+ */
+
+namespace avce_oversized_surfaces
+{
+#if !defined(MSDK_ALIGN16)
+#define MSDK_ALIGN16(value)                      (((value + 15) >> 4) << 4) // round up to a multiple of 16
+#endif
+#if !defined(MSDK_ALIGN32)
+#define MSDK_ALIGN32(value)                      (((value + 31) >> 5) << 5) // round up to a multiple of 16
+#endif
+
+class TestSuite : public tsVideoEncoder, public tsSurfaceProcessor
+{
+public:
+    TestSuite()
+        : tsVideoEncoder(MFX_CODEC_AVC, true)
+        , m_reader()
+    {
+        srand(0);
+        m_filler = this;
+    }
+    ~TestSuite()
+    {
+        delete m_reader;
+    }
+    int RunTest(unsigned int id);
+    static const unsigned int n_cases;
+
+    mfxStatus ProcessSurface(mfxFrameSurface1& s)
+    {
+        //tsRawReader.ProcessSurface() doesn't rely on input s.Info to copy the raw data.
+        mfxStatus sts = m_reader->ProcessSurface(s);
+
+        if (m_reader->m_eos)  // re-read stream from the beginning to reach target NumFrames
+        {
+            m_reader->ResetFile();
+            sts = m_reader->ProcessSurface(s);
+        }
+
+        return sts;
+    }
+
+private:
+    enum
+    {
+        MFX_PAR = 1,
+    };
+
+    struct tc_struct
+    {
+        mfxStatus sts;
+        mfxU32 s_width; //oversized surface's width to create
+        mfxU32 s_height;
+        const char* stream; //raw stream name
+        struct f_pair
+        {
+            mfxU32 ext_type;
+            const  tsStruct::Field* f;
+            mfxU32 v;
+        } set_par[MAX_NPARS];
+    };
+
+    static const tc_struct test_case[];
+
+    tsRawReader *m_reader;
+};
+
+
+#define mfx_PicStruct     tsStruct::mfxVideoParam.mfx.FrameInfo.PicStruct
+#define mfx_CropW         tsStruct::mfxVideoParam.mfx.FrameInfo.CropW
+#define mfx_CropH         tsStruct::mfxVideoParam.mfx.FrameInfo.CropH
+#define mfx_GopPicSize    tsStruct::mfxVideoParam.mfx.GopPicSize
+#define mfx_GopRefDist    tsStruct::mfxVideoParam.mfx.GopRefDist
+#define mfx_ChromaFormat  tsStruct::mfxVideoParam.mfx.FrameInfo.ChromaFormat
+#define mfx_FourCC        tsStruct::mfxVideoParam.mfx.FrameInfo.FourCC
+#define mfx_IOPattern     tsStruct::mfxVideoParam.IOPattern
+const TestSuite::tc_struct TestSuite::test_case[] =
+{
+    {/*00*/ MFX_ERR_NONE, 1920, 1080, "YUV/videoconf1_720p_60.yuv",
+            {{MFX_PAR, &mfx_CropW, 1280},
+             {MFX_PAR, &mfx_CropH, 720},
+             {MFX_PAR, &mfx_GopPicSize, 32},
+             {MFX_PAR, &mfx_GopRefDist, 2},
+             {MFX_PAR, &mfx_ChromaFormat, MFX_CHROMAFORMAT_YUV420},
+             {MFX_PAR, &mfx_FourCC, MFX_FOURCC_NV12},
+             {MFX_PAR, &mfx_IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY}}},
+    {/*01*/ MFX_ERR_NONE, 1920, 1080, "YUV/videoconf1_720p_60.yuv",
+            {{MFX_PAR, &mfx_CropW, 1280},
+             {MFX_PAR, &mfx_CropH, 720},
+             {MFX_PAR, &mfx_GopPicSize, 32},
+             {MFX_PAR, &mfx_GopRefDist, 2},
+             {MFX_PAR, &mfx_ChromaFormat, MFX_CHROMAFORMAT_YUV420},
+             {MFX_PAR, &mfx_FourCC, MFX_FOURCC_NV12},
+             {MFX_PAR, &mfx_PicStruct, MFX_PICSTRUCT_FIELD_TFF},
+             {MFX_PAR, &mfx_IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY}}},
+    {/*02*/ MFX_ERR_NONE, 1920, 1080, "YUV/videoconf1_720p_60.yuv",
+            {{MFX_PAR, &mfx_CropW, 1280},
+             {MFX_PAR, &mfx_CropH, 720},
+             {MFX_PAR, &mfx_GopPicSize, 32},
+             {MFX_PAR, &mfx_GopRefDist, 2},
+             {MFX_PAR, &mfx_ChromaFormat, MFX_CHROMAFORMAT_YUV420},
+             {MFX_PAR, &mfx_FourCC, MFX_FOURCC_NV12},
+             {MFX_PAR, &mfx_PicStruct, MFX_PICSTRUCT_FIELD_BFF},
+             {MFX_PAR, &mfx_IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY}}},
+    {/*03*/ MFX_ERR_NONE, 1920, 1080, "YUV/iceage_720x480_491.yuv",
+            {{MFX_PAR, &mfx_CropW, 720},
+             {MFX_PAR, &mfx_CropH, 480},
+             {MFX_PAR, &mfx_ChromaFormat, MFX_CHROMAFORMAT_YUV420},
+             {MFX_PAR, &mfx_FourCC, MFX_FOURCC_NV12},
+             {MFX_PAR, &mfx_IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY}}},
+    {/*04*/ MFX_ERR_NONE, 1920, 1080, "YUV/iceage_720x480_491.yuv",
+            {{MFX_PAR, &mfx_CropW, 720},
+             {MFX_PAR, &mfx_CropH, 480},
+             {MFX_PAR, &mfx_ChromaFormat, MFX_CHROMAFORMAT_YUV420},
+             {MFX_PAR, &mfx_FourCC, MFX_FOURCC_NV12},
+             {MFX_PAR, &mfx_PicStruct, MFX_PICSTRUCT_FIELD_TFF},
+             {MFX_PAR, &mfx_IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY}}},
+    {/*05*/ MFX_ERR_NONE, 1920, 1080, "YUV/iceage_720x480_491.yuv",
+            {{MFX_PAR, &mfx_CropW, 720},
+             {MFX_PAR, &mfx_CropH, 480},
+             {MFX_PAR, &mfx_ChromaFormat, MFX_CHROMAFORMAT_YUV420},
+             {MFX_PAR, &mfx_FourCC, MFX_FOURCC_NV12},
+             {MFX_PAR, &mfx_PicStruct, MFX_PICSTRUCT_FIELD_BFF},
+             {MFX_PAR, &mfx_IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY}}},
+    {/*06*/ MFX_ERR_NONE, 1920, 1080, "YUV/iceage_720x240_491.yuv",
+            {{MFX_PAR, &mfx_CropW, 720},
+             {MFX_PAR, &mfx_CropH, 240},
+             {MFX_PAR, &mfx_ChromaFormat, MFX_CHROMAFORMAT_YUV420},
+             {MFX_PAR, &mfx_FourCC, MFX_FOURCC_NV12},
+             {MFX_PAR, &mfx_IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY}}},
+    {/*07*/ MFX_ERR_NONE, 1920, 1080, "YUV/iceage_720x240_491.yuv",
+            {{MFX_PAR, &mfx_CropW, 720},
+             {MFX_PAR, &mfx_CropH, 240},
+             {MFX_PAR, &mfx_ChromaFormat, MFX_CHROMAFORMAT_YUV420},
+             {MFX_PAR, &mfx_FourCC, MFX_FOURCC_NV12},
+             {MFX_PAR, &mfx_PicStruct, MFX_PICSTRUCT_FIELD_TFF},
+             {MFX_PAR, &mfx_IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY}}},
+    {/*08*/ MFX_ERR_NONE, 1920, 1080, "YUV/iceage_720x240_491.yuv",
+            {{MFX_PAR, &mfx_CropW, 720},
+             {MFX_PAR, &mfx_CropH, 240},
+             {MFX_PAR, &mfx_ChromaFormat, MFX_CHROMAFORMAT_YUV420},
+             {MFX_PAR, &mfx_FourCC, MFX_FOURCC_NV12},
+             {MFX_PAR, &mfx_PicStruct, MFX_PICSTRUCT_FIELD_BFF},
+             {MFX_PAR, &mfx_IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY}}},
+    {/*09*/ MFX_ERR_NONE, 1920, 1080, "YUV/barcelona2_868x588_1.yuv",
+            {{MFX_PAR, &mfx_CropW, 868},
+             {MFX_PAR, &mfx_CropH, 588},
+             {MFX_PAR, &mfx_ChromaFormat, MFX_CHROMAFORMAT_YUV420},
+             {MFX_PAR, &mfx_FourCC, MFX_FOURCC_NV12},
+             {MFX_PAR, &mfx_IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY}}},
+    {/*10*/ MFX_ERR_NONE, 1920, 1080, "YUV/barcelona2_868x588_1.yuv",
+            {{MFX_PAR, &mfx_CropW, 868},
+             {MFX_PAR, &mfx_CropH, 588},
+             {MFX_PAR, &mfx_ChromaFormat, MFX_CHROMAFORMAT_YUV420},
+             {MFX_PAR, &mfx_FourCC, MFX_FOURCC_NV12},
+             {MFX_PAR, &mfx_PicStruct, MFX_PICSTRUCT_FIELD_TFF},
+             {MFX_PAR, &mfx_IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY}}},
+    {/*11*/ MFX_ERR_NONE, 1920, 1080, "YUV/barcelona2_868x588_1.yuv",
+            {{MFX_PAR, &mfx_CropW, 868},
+             {MFX_PAR, &mfx_CropH, 588},
+             {MFX_PAR, &mfx_ChromaFormat, MFX_CHROMAFORMAT_YUV420},
+             {MFX_PAR, &mfx_FourCC, MFX_FOURCC_NV12},
+             {MFX_PAR, &mfx_PicStruct, MFX_PICSTRUCT_FIELD_BFF},
+             {MFX_PAR, &mfx_IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY}}},
+};
+
+const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case)/sizeof(TestSuite::tc_struct);
+
+const int frameNumber = 30;
+
+int TestSuite::RunTest(unsigned int id)
+{
+    TS_START;
+    const tc_struct& tc = test_case[id];
+
+    //set input parameters for mfxVideoParam
+    SETPARS(m_pPar, MFX_PAR);
+    //workaround for VCSD100026230, explicitly set level in the tests. So this test only verify the stream generated by driver.
+    m_pPar->mfx.CodecLevel = MFX_LEVEL_AVC_4;
+    if (m_pPar->mfx.FrameInfo.PicStruct == MFX_PICSTRUCT_UNKNOWN) {
+        //in case PicStruct is not set explicitly.
+        m_pPar->mfx.FrameInfo.PicStruct = MFX_PICSTRUCT_PROGRESSIVE;
+    }
+    m_pPar->mfx.FrameInfo.Width = MSDK_ALIGN16(m_pPar->mfx.FrameInfo.CropW);
+    m_pPar->mfx.FrameInfo.Height = (m_par.mfx.FrameInfo.PicStruct == MFX_PICSTRUCT_PROGRESSIVE)?
+                                   MSDK_ALIGN16(m_pPar->mfx.FrameInfo.CropH) : MSDK_ALIGN32(m_pPar->mfx.FrameInfo.CropH);
+
+    m_reader = new tsRawReader(g_tsStreamPool.Get(tc.stream), m_pPar->mfx.FrameInfo);
+    g_tsStreamPool.Reg();
+
+    //save the correct resolution frame information
+    mfxFrameInfo SavedInfo;
+    SavedInfo = m_pPar->mfx.FrameInfo;
+    m_pPar->mfx.FrameInfo.CropW = tc.s_width;
+    m_pPar->mfx.FrameInfo.CropH = tc.s_height;
+    m_pPar->mfx.FrameInfo.Width = MSDK_ALIGN16(m_pPar->mfx.FrameInfo.CropW);
+    m_pPar->mfx.FrameInfo.Height = (m_pPar->mfx.FrameInfo.PicStruct == MFX_PICSTRUCT_PROGRESSIVE)?
+                                   MSDK_ALIGN16(m_pPar->mfx.FrameInfo.CropH) : MSDK_ALIGN32(m_pPar->mfx.FrameInfo.CropH);
+
+    mfxU32 nf = frameNumber;
+    ///////////////////////////////////////////////////////////////////////////
+    g_tsStatus.expect(tc.sts);
+
+// 1. encode with oversized surfaces
+    tsBitstreamCRC32 bs_crc;
+    m_bs_processor = &bs_crc;
+
+    MFXInit();
+    if (m_pPar->IOPattern & MFX_IOPATTERN_IN_VIDEO_MEMORY) {
+        SetFrameAllocator(m_session, m_pVAHandle);
+        m_pFrameAllocator = m_pVAHandle;
+    }
+    if(m_pFrameAllocator && !GetAllocator()) {
+        SetAllocator(m_pFrameAllocator, true);
+    }
+    AllocSurfaces();
+    Init(m_session, m_pPar);
+    //re-load the correct surface resolution and reset encoder
+    m_pPar->mfx.FrameInfo = SavedInfo;
+    Reset();
+    AllocBitstream((m_par.mfx.FrameInfo.Width * m_par.mfx.FrameInfo.Height) * 1024 * 1024 * nf);
+    EncodeFrames(nf);
+    Ipp32u crc = bs_crc.GetCRC();
+
+// reset
+    Close();
+    m_reader->ResetFile();
+    memset(&m_bitstream, 0, sizeof(mfxBitstream));
+
+// 2. encode with "correct"-size surfaces
+    tsBitstreamCRC32 bs_cmp_crc;
+    m_bs_processor = &bs_cmp_crc;
+
+    AllocSurfaces();
+    AllocBitstream((m_par.mfx.FrameInfo.Width * m_par.mfx.FrameInfo.Height) * 1024 * 1024 * nf);
+    EncodeFrames(nf);
+    Ipp32u cmp_crc = bs_cmp_crc.GetCRC();
+    g_tsLog << "crc = " << crc << "\n";
+    g_tsLog << "cmp_crc = " << cmp_crc << "\n";
+    if (crc != cmp_crc)
+    {
+        g_tsLog << "ERROR: the 2 crc values should be the same\n";
+        g_tsStatus.check(MFX_ERR_ABORTED);
+    }
+
+    TS_END;
+    return 0;
+}
+
+TS_REG_TEST_SUITE_CLASS(avce_oversized_surfaces);
+};

commit 39c615fcf516598cf8d8aa46248e44d37294fd17

[mdp7814] fix the regression caused by commit d27896dc
Change-Id: I4e1e7078c1e696b351211756c6e4cc3d34c6be93

_testsuite/msdk_ts/src/gmock/ts_alloc.cpp
@@ -144,16 +144,21 @@ mfxStatus tsSurfacePool::AllocSurfaces(mfxFrameAllocRequest request, bool direct
     return g_tsStatus.get();
 }
 
+//surface pool created in AllocOpaque() or/and AllocSurfaces() is freed in this function
 mfxStatus tsSurfacePool::FreeSurfaces()
 {
     if (m_pool.size() == 0)
         return MFX_ERR_NONE;
 
-    TRACE_FUNC2(m_allocator->Free, m_allocator, &m_response);
-    g_tsStatus.check( m_allocator->Free(m_allocator, &m_response) );
-    TS_CHECK_MFX;
+    //If opaque memory is not set, (m_pool.size() > 0) && (m_opaque_pool.size() == 0)
+    if (m_opaque_pool.size() == 0 && m_allocator) {
+        TRACE_FUNC2(m_allocator->Free, m_allocator, &m_response);
+        g_tsStatus.check( m_allocator->Free(m_allocator, &m_response) );
+        TS_CHECK_MFX;
+    }
 
     m_pool.clear();
+    m_opaque_pool.clear();
 
     return g_tsStatus.get();
 }