cyew3/oneVPL-intel-gpu

commit a72b9240f12fd4b59c7641295a9df22049c1d241

ts/msdk_ts: do not cast integral to address type
Make sure we cast the address of c.par, not the integral
value. Fixes -Wint-to-pointer cast warnings.

Change-Id: Id27d74653d0cabdf2f8f018abf5a05388405a5f4
Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

_testsuite/msdk_ts/src/gmock/test_suites/hevcd_decode_frame_async.cpp
@@ -1,3 +1,13 @@
+/* ****************************************************************************** *\
+
+INTEL CORPORATION PROPRIETARY INFORMATION
+This software is supplied under the terms of a license agreement or nondisclosure
+agreement with Intel Corporation and may not be copied or disclosed except in
+accordance with the terms of that agreement
+Copyright(c) 2014-2016 Intel Corporation. All Rights Reserved.
+
+\* ****************************************************************************** */
+
 #include "ts_decoder.h"
 #include "ts_struct.h"
 
@@ -84,7 +94,7 @@ private:
                 if(c.field)
                     tsStruct::set(*base, *c.field, c.par[0]);
                 else
-                    *base = (void*)c.par[0];
+                    *base = (void*)c.par;
             }
         }
     }

_testsuite/msdk_ts/src/gmock/test_suites/hevcd_query.cpp
@@ -74,7 +74,7 @@ private:
                 if(c.field)
                     tsStruct::set(*base, *c.field, c.par[0]);
                 else
-                    *base = (void*)c.par[0];
+                    *base = (void*)c.par;
             }
         }
     }

_testsuite/msdk_ts/src/gmock/test_suites/hevcd_reset.cpp
@@ -95,7 +95,7 @@ private:
                 if(c.field)
                     tsStruct::set(*base, *c.field, c.par[0]);
                 else
-                    *base = (void*)c.par[0];
+                    *base = (void*)c.par;
             }
         }
     }

_testsuite/msdk_ts/src/gmock/test_suites/mjpegd_init.cpp
@@ -85,7 +85,7 @@ private:
                 if(c.field)
                     tsStruct::set(*base, *c.field, c.par);
                 else
-                    *base = (void*)c.par;
+                    *base = (void*)&c.par;
             }
         }
     }

_testsuite/msdk_ts/src/gmock/test_suites/mjpegd_payload.cpp
@@ -115,7 +115,7 @@ namespace mjpegd_payload
                     if(c.field)
                         tsStruct::set(*base, *c.field, c.par);
                     else
-                        *base = (void*)c.par;
+                        *base = (void*)&c.par;
                 }
             }
 

_testsuite/msdk_ts/src/gmock/test_suites/mjpegd_tables.cpp
@@ -324,7 +324,7 @@ exit:
                     if(c.field)
                         tsStruct::set(*base, *c.field, c.par);
                     else
-                        *base = (void*)c.par;
+                        *base = (void*)&c.par;
                 }
             }
 

_testsuite/msdk_ts/src/gmock/test_suites/ptir_get_video_param.cpp
@@ -1,3 +1,13 @@
+/* ****************************************************************************** *\
+
+INTEL CORPORATION PROPRIETARY INFORMATION
+This software is supplied under the terms of a license agreement or nondisclosure
+agreement with Intel Corporation and may not be copied or disclosed except in
+accordance with the terms of that agreement
+Copyright(c) 2014-2016 Intel Corporation. All Rights Reserved.
+
+\* ****************************************************************************** */
+
 #include "ts_vpp.h"
 #include "ts_struct.h"
 
@@ -86,7 +96,7 @@ private:
                 if(c.field)
                     tsStruct::set(*base, *c.field, c.par[0]);
                 else
-                    *base = (void*)c.par[0];
+                    *base = (void*)c.par;
             }
         }
     }

_testsuite/msdk_ts/src/gmock/test_suites/ptir_reset.cpp
@@ -1,3 +1,13 @@
+/* ****************************************************************************** *\
+
+INTEL CORPORATION PROPRIETARY INFORMATION
+This software is supplied under the terms of a license agreement or nondisclosure
+agreement with Intel Corporation and may not be copied or disclosed except in
+accordance with the terms of that agreement
+Copyright(c) 2014-2016 Intel Corporation. All Rights Reserved.
+
+\* ****************************************************************************** */
+
 #include "ts_vpp.h"
 #include "ts_struct.h"
 #include "ts_alloc.h"
@@ -82,7 +92,7 @@ private:
                 if(c.field)
                     tsStruct::set(*base, *c.field, c.par[0]);
                 else
-                    *base = (void*)c.par[0];
+                    *base = (void*)c.par;
             }
         }
 

commit 2e43910ae9c082a00c204cae21b75ada6bf6514c

ts/msdk_ts: fix regression introduced by a72b9240f1
The following commit...

commit a72b9240f12fd4b59c7641295a9df22049c1d241
Author: U. Artie Eoff <ullysses.a.eoff@intel.com>
Date:   Tue May 10 11:52:28 2016 -0700
    ts/msdk_ts: do not cast integral to address type

...was not correct and resulted in the wrong value to
be stored at the base pointer.  This fixes it so that
no warning is generated and the correct value is stored
at the base pointer.

Thanks to Zhelybalov, Igor for catching this.

Change-Id: I2246e4bc74d2c1baf58b5072c4655786659b53ab
Depends-On: Ie81768d028b6dea59cfbc4311853ef035f386b35
Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

_testsuite/msdk_ts/src/gmock/test_suites/hevcd_decode_frame_async.cpp
@@ -94,7 +94,7 @@ private:
                 if(c.field)
                     tsStruct::set(*base, *c.field, c.par[0]);
                 else
-                    *base = (void*)c.par;
+                    *((mfxU32*)base) = c.par[0];
             }
         }
     }

_testsuite/msdk_ts/src/gmock/test_suites/hevcd_query.cpp
@@ -74,7 +74,7 @@ private:
                 if(c.field)
                     tsStruct::set(*base, *c.field, c.par[0]);
                 else
-                    *base = (void*)c.par;
+                    *((mfxU32*)base) = c.par[0];
             }
         }
     }

_testsuite/msdk_ts/src/gmock/test_suites/hevcd_reset.cpp
@@ -95,7 +95,7 @@ private:
                 if(c.field)
                     tsStruct::set(*base, *c.field, c.par[0]);
                 else
-                    *base = (void*)c.par;
+                    *((mfxU32*)base) = c.par[0];
             }
         }
     }

_testsuite/msdk_ts/src/gmock/test_suites/mjpegd_init.cpp
@@ -85,7 +85,7 @@ private:
                 if(c.field)
                     tsStruct::set(*base, *c.field, c.par);
                 else
-                    *base = (void*)&c.par;
+                    *((mfxU32*)base) = c.par;
             }
         }
     }

_testsuite/msdk_ts/src/gmock/test_suites/mjpegd_payload.cpp
@@ -115,7 +115,7 @@ namespace mjpegd_payload
                     if(c.field)
                         tsStruct::set(*base, *c.field, c.par);
                     else
-                        *base = (void*)&c.par;
+                        *((mfxU32*)base) = c.par;
                 }
             }
 

_testsuite/msdk_ts/src/gmock/test_suites/mjpegd_tables.cpp
@@ -324,7 +324,7 @@ exit:
                     if(c.field)
                         tsStruct::set(*base, *c.field, c.par);
                     else
-                        *base = (void*)&c.par;
+                        *((mfxU32*)base) = c.par;
                 }
             }
 

_testsuite/msdk_ts/src/gmock/test_suites/ptir_get_video_param.cpp
@@ -96,7 +96,7 @@ private:
                 if(c.field)
                     tsStruct::set(*base, *c.field, c.par[0]);
                 else
-                    *base = (void*)c.par;
+                    *((mfxU32*)base) = c.par[0];
             }
         }
     }

_testsuite/msdk_ts/src/gmock/test_suites/ptir_reset.cpp
@@ -92,7 +92,7 @@ private:
                 if(c.field)
                     tsStruct::set(*base, *c.field, c.par[0]);
                 else
-                    *base = (void*)c.par;
+                    *((mfxU32*)base) = c.par[0];
             }
         }
 