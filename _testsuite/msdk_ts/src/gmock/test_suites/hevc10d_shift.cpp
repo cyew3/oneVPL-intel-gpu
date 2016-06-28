0dde4d404a321c167324418f791c9700bf673694
Dmitry Gurulev <dmitry.gurulev@intel.com>
2016-06-28 15:28:09 +0300

    [tests/h265d]: fix and add cases to validate [Shift = 0/1] for 10b input
    Tracked-On: https://jira01.devtools.intel.com/browse/MDP-17396
    Change-Id: Ie59636625614264d856e3018fc579c351aca1b62

--- _testsuite/msdk_ts/src/gmock/test_suites/hevc10d_shift.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/hevc10d_shift.cpp
@@ -1,3 +1,13 @@
+/* /////////////////////////////////////////////////////////////////////////////
+//
+//                  INTEL CORPORATION PROPRIETARY INFORMATION
+//     This software is supplied under the terms of a license agreement or
+//     nondisclosure agreement with Intel Corporation and may not be copied
+//     or disclosed except in accordance with the terms of that agreement.
+//          Copyright(c) 2014-2016 Intel Corporation. All Rights Reserved.
+//
+*/
+
 #include "ts_decoder.h"
 #include "ts_struct.h"
 
@@ -8,23 +18,35 @@ namespace hevc10d_shift
 
 class TestSuite : tsVideoDecoder
 {
+
 public:
+
     TestSuite() : tsVideoDecoder(MFX_CODEC_HEVC){}
     ~TestSuite() { }
+
     int RunTest(unsigned int id);
     static const unsigned int n_cases;
 
 private:
 
+    mfxStatus DecodeFrameAsync();
+    mfxStatus DecodeFrameAsync(mfxSession session, mfxBitstream *bs, mfxFrameSurface1 *surface_work, mfxFrameSurface1 **surface_out, mfxSyncPoint *syncp);
+
+private:
+
     enum
     {
-        CHANGE_SHIFT = 1
+        SHIFT_INIT,
+        SHIFT_DECODE,
     };
 
     struct tc_struct
     {
         mfxStatus sts;
-        mfxU32 mode;
+
+        mfxI32    mode;
+        mfxU32    shift;
+
         struct f_pair
         {
             mfxU32 ext_type;
@@ -39,80 +61,141 @@ private:
     };
 
     static const tc_struct test_case[];
+
+    tc_struct tc;
 };
 
 const TestSuite::tc_struct TestSuite::test_case[] =
 {
-    {/* 0*/ MFX_ERR_NONE, 0, {
-        {MFXPAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_SYSTEM_MEMORY}}},
-    {/* 1*/ MFX_ERR_NONE, 0, {
-        {MFXPAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_VIDEO_MEMORY}}},
+    { /* 0 */ MFX_ERR_NONE, SHIFT_INIT, 0,
+        { { MFXPAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_SYSTEM_MEMORY } }
+    },
+    { /* 1 */ MFX_ERR_NONE, SHIFT_INIT, 1,
+        { { MFXPAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_SYSTEM_MEMORY } }
+    },
 
-    // Change shift flag
-    {/* 2*/ MFX_ERR_INVALID_VIDEO_PARAM, CHANGE_SHIFT, {
-        {MFXPAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_SYSTEM_MEMORY}}},
-    {/* 3*/ MFX_ERR_INVALID_VIDEO_PARAM, CHANGE_SHIFT, {
-        {MFXPAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_VIDEO_MEMORY}}},
+    { /* 2 */ MFX_ERR_NONE, SHIFT_DECODE, 0,
+        { { MFXPAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_SYSTEM_MEMORY } }
+    },
+    { /* 3 */ MFX_ERR_NONE, SHIFT_DECODE, 1,
+        { { MFXPAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_SYSTEM_MEMORY } }
+    },
+
+    { /* 4 */ MFX_ERR_INVALID_VIDEO_PARAM, SHIFT_INIT, 0,
+        { { MFXPAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_VIDEO_MEMORY } }
+    },
+    { /* 5 */ MFX_ERR_INVALID_VIDEO_PARAM, SHIFT_DECODE, 0,
+        { { MFXPAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_VIDEO_MEMORY } }
+    },
+
+    { /* 6 */ MFX_ERR_NONE, SHIFT_INIT, 1,
+        { { MFXPAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_VIDEO_MEMORY } }
+    },
+    { /* 7 */ MFX_ERR_NONE, SHIFT_DECODE, 1,
+        { { MFXPAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_VIDEO_MEMORY } }
+    },
 };
 
 const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case)/sizeof(TestSuite::tc_struct);
 
 class CRC32 : public tsSurfaceProcessor
 {
+
 private:
+
     mfxU16 shift;
     mfxU32 crc32[2];
+
+    static int const ZERO_SHIFT_CRC = 0x87053A0C;
+    static int const ONE_SHIFT_CRC  = 0x0DEF5FD5;
+
 public:
-    CRC32(mfxFrameInfo& finfo)
-        : shift(finfo.Shift)
+
+    CRC32(mfxU16 shift)
+        : shift(shift)
     {
-        crc32[0] = 0x87053A0C;
-        crc32[1] = 0x0DEF5FD5;
+        crc32[0] = ZERO_SHIFT_CRC;
+        crc32[1] = ONE_SHIFT_CRC;
     };
     ~CRC32() {};
+
     mfxU32 calc_crc32(mfxFrameSurface1& s)
     {
-        Ipp32u crc = 0;
         mfxU32 w = s.Info.CropW;
         mfxU32 h = s.Info.CropH;
 
-        Ipp32u nbytes = w*h*2 + w*h/2 + w*h/2;
+        Ipp32u const nbytes = w*h*2 + w*h/2 + w*h/2;
+        if (!nbytes)
+        {
+            g_tsLog << "ERROR: cannot calculate CRC32, input frame has zero dimensions\n";
+            return 0;
+        }
 
-        Ipp8u* frame = new Ipp8u[nbytes];
-        IppiSize sz;
-        sz.height = h;
-        sz.width = w * 2;
-        ippiCopy_8u_C1R(s.Data.Y, s.Data.Pitch, frame, w*2, sz);
+        std::vector<Ipp8u> frame(nbytes);
+        IppiSize sz = { mfxI32(w * 2), mfxI32(h) };
+        ippiCopy_8u_C1R(s.Data.Y, s.Data.Pitch, &frame[0], w*2, sz);
+
         sz.height /= 2;
-        ippiCopy_8u_C1R(s.Data.UV, s.Data.Pitch, frame + w*h*2, w*2, sz);
+        ippiCopy_8u_C1R(s.Data.UV, s.Data.Pitch, &frame[0] + w*h*2, w*2, sz);
 
-        /*FILE* o = fopen("C:\\0_work\\msdk\\build\\win_Win32\\bin\\test.p010.yuv", "wb");
-        fwrite(frame, 1, nbytes, o);
-        fclose(o);*/
-
-        IppStatus sts = ippsCRC32_8u(frame, nbytes, &crc);
+        Ipp32u crc = 0;
+        IppStatus sts = ippsCRC32_8u(&frame[0], nbytes, &crc);
         if (sts != ippStsNoErr)
         {
             g_tsLog << "ERROR: cannot calculate CRC32 IppStatus=" << sts << "\n";
             return 0;
         }
 
-        delete [] frame;
-
         return crc;
     }
+
     mfxStatus ProcessSurface(mfxFrameSurface1& s)
     {
         g_tsLog << "Checking CRC...\n";
-        mfxU32 crc = calc_crc32(s);
-        if (crc != crc32[shift]) {
+
+        mfxU32 const crc = calc_crc32(s);
+        if (crc != crc32[shift])
+        {
             g_tsLog << "ERROR: reference CRC (" << crc32[shift]
                     << ") != test CRC (" << crc << ")\n";
             return MFX_ERR_NOT_FOUND;
         }
+
         return MFX_ERR_NONE;
     }
 };
+
+mfxStatus TestSuite::DecodeFrameAsync()
+{
+    if (m_default)
+    {
+        SetPar4_DecodeFrameAsync();
+    }
+
+    mfxStatus sts = DecodeFrameAsync(m_session, m_pBitstream, m_pSurf, &m_pSurfOut, m_pSyncPoint);
+    if (sts == MFX_ERR_NONE || (sts == MFX_WRN_VIDEO_PARAM_CHANGED && *m_pSyncPoint != NULL))
+    {
+        m_surf_out.insert(std::make_pair(*m_pSyncPoint, m_pSurfOut));
+        if (m_pSurfOut)
+        {
+            m_pSurfOut->Data.Locked ++;
+        }
+    }
+
+    return sts;
+}
+
+mfxStatus TestSuite::DecodeFrameAsync(mfxSession session, mfxBitstream *bs, mfxFrameSurface1 *surface_work, mfxFrameSurface1 **surface_out, mfxSyncPoint *syncp)
+{
+    if (tc.mode == SHIFT_DECODE)
+    {
+        surface_work->Info.Shift = tc.shift;
+        g_tsStatus.expect(tc.sts);
+    }
+
+    return
+        tsVideoDecoder::DecodeFrameAsync(session, bs, surface_work, surface_out, syncp);
+}
 
 int TestSuite::RunTest(unsigned int id)
 {
@@ -121,7 +204,7 @@ int TestSuite::RunTest(unsigned int id)
     g_tsStreamPool.Reg();
     tsBitstreamReader reader(sname, 1000000);
     m_bs_processor = &reader;
-    auto tc = test_case[id];
+    tc = test_case[id];
 
     Load();
     SETPARS(m_pPar, MFXPAR);
@@ -147,33 +230,22 @@ int TestSuite::RunTest(unsigned int id)
     DecodeHeader();
     QueryIOSurf();
 
-    if (tc.mode == CHANGE_SHIFT)
+    if (tc.mode == SHIFT_INIT)
     {
-        if (m_par.mfx.FrameInfo.Shift == 1)
-        {
-            m_par.mfx.FrameInfo.Shift = 0;
-        }
-        else if (m_par.mfx.FrameInfo.Shift == 0)
-        {
-            m_par.mfx.FrameInfo.Shift = 1;
-        }
-        else
-        {
-            g_tsLog << "ERROR: m_par.mfx.FrameInfo.Shift == " << m_par.mfx.FrameInfo.Shift
-                    << ". Should be 0 or 1.\n";
-            g_tsStatus.check(MFX_ERR_UNSUPPORTED);  // exit here
-        }
+        m_par.mfx.FrameInfo.Shift = tc.shift;
+        g_tsStatus.expect(tc.sts);
     }
 
-    g_tsStatus.expect(tc.sts);
     Init();
-    if (tc.sts >= 0)
+
+    if (m_initialized)
     {
-        CRC32 check_crc(m_par.mfx.FrameInfo);
+        CRC32 check_crc(tc.shift);
         m_surf_processor = &check_crc;
 
         DecodeFrames(1);
     }
+
     TS_END;
     return 0;
 }
