3f18e4da6d6fa764ddfa1f785c6df0ddc7efe461
Kokoshnikov, Alexey S <alexey.s.kokoshnikov@intel.com>
2016-07-13 14:13:31 +0300

    Fix load dll
    Load driver's dlls (igfxcmrt) from DriverStore, path must be defined in
    registry
    
    WARNING: this is temporary method,
    driver's installer must write to
    HKML\SOFTWARE\Intel\MDF\DrivrStorePath
    (HKML\SOFTWARE\WOW6432Note\Intel\MDF\DriverStorePath for x32
    app),
    current user must have rights for read from this keys.
    
    Change-Id: I3d949ccde14fcf5b81d8d4267c03a853c72391c0

--- _studio/shared/umc/core/vm/src/vm_shared_object_win32.c
+++ _studio/shared/umc/core/vm/src/vm_shared_object_win32.c
@@ -4,7 +4,7 @@
 //     This software is supplied under the terms of a license agreement or
 //     nondisclosure agreement with Intel Corporation and may not be copied
 //     or disclosed except in accordance with the terms of that agreement.
-//       Copyright(c) 2003-2013 Intel Corporation. All Rights Reserved.
+//       Copyright(c) 2003-2016 Intel Corporation. All Rights Reserved.
 //
 */
 
@@ -16,11 +16,39 @@
 
 vm_so_handle vm_so_load(const vm_char *so_file_name)
 {
-      /* check error(s) */
+    //mfx_trace_get_reg_string
+    HKEY hkey;
+    vm_so_handle handle = NULL;
+    TCHAR path[MAX_PATH] = _T("");
+    DWORD size = MAX_PATH;
+
+    /* check error(s) */
     if (NULL == so_file_name)
         return NULL;
 
-    return ((vm_so_handle) LoadLibraryExW(so_file_name, NULL, 0));
+    handle = ((vm_so_handle)LoadLibraryExW(so_file_name, NULL, 0));
+
+    /* try load from DriverStore */
+    /* WARNING, this is temporary solution */
+    if (handle == NULL)
+    {
+        if( RegOpenKeyEx( HKEY_LOCAL_MACHINE,
+        _T("SOFTWARE\\Intel\\MDF"),
+        0,
+        KEY_READ,
+        &hkey) == ERROR_SUCCESS)
+        {
+            if (ERROR_SUCCESS == RegQueryValueEx(hkey, _T("DriverStorePath"), 0, NULL, (LPBYTE)path, &size ))
+                {
+                    wcscat(path, _T("\\"));
+                    wcscat(path, so_file_name);
+                    handle = ((vm_so_handle)LoadLibraryExW(path, NULL, 0));
+                }
+        }
+        RegCloseKey(hkey);
+    }
+
+    return handle;
 
 } /* vm_so_handle vm_so_load(vm_char *so_file_name) */
 
