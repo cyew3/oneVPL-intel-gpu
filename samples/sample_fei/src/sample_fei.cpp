d0ad7617cf1a0c8449daf91bc9f2d9bff22e34fd
Goncharenko, Miroslav <miroslav.goncharenko@intel.com>
2016-08-03 01:32:37 +0800

    Revert "new default for input params in sample_fei"
    This reverts commit 37bb0fb6176c9fbc0de94692b1e790ae1acbd17d.
    
    Change-Id: Ibb178620ac0efa07d9073cbc2d255ebaf8f10370

--- samples/sample_fei/src/sample_fei.cpp
+++ samples/sample_fei/src/sample_fei.cpp
@@ -1151,7 +1151,7 @@ int main(int argc, char *argv[])
     Params.FTEnable        = false;
     Params.AdaptiveSearch  = false;
     Params.DistortionType  = false;
-    Params.bPassHeaders    = true;
+    Params.bPassHeaders    = false;
     Params.RepartitionCheckEnable = false;
     Params.MultiPredL0 = false;
     Params.MultiPredL1 = false;
