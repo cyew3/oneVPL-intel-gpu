425ae4f07e27d52139426774c1e90b9da89fc7c4
Nabiullin, Oleg <oleg.nabiullin@intel.com>
2016-10-07 20:02:29 +0300

    [KW] Fix uninitialized variables issues in mfx_frames.h
    Issues:
    MDP-25128, MDP-25127, MDP-25126, MDP-23432, MDP-23431, MDP-23430, MDP-23243, MDP-23242, MDP-23241
    
    Change-Id: I03c25b2cecab7dca81b219a671b06e5f1de427aa

--- _studio/mfx_lib/shared/include/mfx_frames.h
+++ _studio/mfx_lib/shared/include/mfx_frames.h
@@ -4,7 +4,7 @@
 //     This software is supplied under the terms of a license agreement or
 //     nondisclosure agreement with Intel Corporation and may not be copied
 //     or disclosed except in accordance with the terms of that agreement.
-//          Copyright(c) 2010-2015 Intel Corporation. All Rights Reserved.
+//          Copyright(c) 2010-2016 Intel Corporation. All Rights Reserved.
 //
 //
 //     GOP reorder buffer and waiting list implementation
@@ -32,6 +32,7 @@
       vm_event                  m_new_frame_event;
 
       sExtTask1()
+          : m_inputInternalParams()
       {
           m_pInput_surface = 0;
           m_pBs = 0;
