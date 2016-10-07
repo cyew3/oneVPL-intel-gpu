38fd8f46a352022c90f7cf51dc49e4093ac767bd
Alexei Leonenko <alexei.leonenko@intel.com>
2016-10-07 12:30:46 +0300

    [KW] Initialize CVAAPIDeviceWayland members in constructor
    Added default initialization to the CVAAPIDeviceWayland class memebers to prevent KW issues.
    
    Change-Id: Ia7c5de4f283cd6392d67fb9cc43a15b246ebd338
    Signed-off-by: Alexei Leonenko <alexei.leonenko@intel.com>

--- samples/sample_common/include/vaapi_device.h
+++ samples/sample_common/include/vaapi_device.h
@@ -131,7 +131,14 @@ class Wayland;
 class CVAAPIDeviceWayland : public CHWDevice
 {
 public:
-    CVAAPIDeviceWayland(){}
+    CVAAPIDeviceWayland(){
+        m_nRenderWinX = 0;
+        m_nRenderWinY = 0;
+        m_nRenderWinW = 0;
+        m_nRenderWinH = 0;
+        m_isMondelloInputEnabled = false;
+        m_Wayland = NULL;
+    }
     virtual ~CVAAPIDeviceWayland(void);
 
     virtual mfxStatus Init(mfxHDL hWindow, mfxU16 nViews, mfxU32 nAdapterNum);
