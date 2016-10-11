a252dd0daf744e5ff1133dbe194488e5b8aa5cfb
Bykadorov, Roman <roman.bykadorov@intel.com>
2016-10-10 20:38:09 -0700

    [KW]: Initialization in constructor for umc_h264_heap.cpp
    Change-Id: Ie751a089416ac0e2bd6e581293f6304a8783160c

--- _studio/shared/umc/codec/h264_dec/src/umc_h264_heap.cpp
+++ _studio/shared/umc/codec/h264_dec/src/umc_h264_heap.cpp
@@ -4,7 +4,7 @@
 //  This software is supplied under the terms of a license  agreement or
 //  nondisclosure agreement with Intel Corporation and may not be copied
 //  or disclosed except in  accordance  with the terms of that agreement.
-//        Copyright (c) 2003-2013 Intel Corporation. All Rights Reserved.
+//        Copyright (c) 2003-2016 Intel Corporation. All Rights Reserved.
 //
 //
 */
@@ -37,7 +37,7 @@ H264CoeffsBuffer::H264CoeffsBuffer(void)
     m_pbFree = 0;
     m_lFreeSize = 0;
     m_pBuffers = 0;
-
+    m_lItemSize = 0;
 } // H264CoeffsBuffer::H264CoeffsBuffer(void)
 
 H264CoeffsBuffer::~H264CoeffsBuffer(void)
