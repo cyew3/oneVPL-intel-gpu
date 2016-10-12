24140002f4c1f24747fe4383db43f47d76947b20
Zhelybalov, Igor <igor.zhelybalov@intel.com>
2016-10-12 13:02:18 +0300

    KW fixes for mfx_video_analysis_vpp
    Change-Id: I6b579366fd08ec10e21f92d4a5381dd3c9082d32

--- _studio/mfx_lib/vpp/src/mfx_video_analysis_vpp.cpp
+++ _studio/mfx_lib/vpp/src/mfx_video_analysis_vpp.cpp
@@ -4,7 +4,7 @@
 //     This software is supplied under the terms of a license agreement or
 //     nondisclosure agreement with Intel Corporation and may not be copied
 //     or disclosed except in accordance with the terms of that agreement.
-//          Copyright(c) 2008 - 2015 Intel Corporation. All Rights Reserved.
+//          Copyright(c) 2008 - 2016 Intel Corporation. All Rights Reserved.
 //
 //
 //          Analysis of Video Content
@@ -45,6 +45,9 @@ VPPSceneAnalyzerParams::~VPPSceneAnalyzerParams(void)
 
 VPPSceneAnalyzer::VPPSceneAnalyzer( void )
 {
+  m_spatialComplexity  = 0;
+  m_temporalComplexity = 0;
+  memset(m_pSliceMV, 0, sizeof(m_pSliceMV));
   return;
 }
 
