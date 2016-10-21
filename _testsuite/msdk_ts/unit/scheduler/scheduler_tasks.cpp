b102375fc89ac0080606cf5a66c88e6246fc11e4
Roman Arzumanyan <roman.arzumanyan@intel.com>
2016-10-21 16:49:53 +0300

    tests/scheduler: task tests were parameterized
    Change-Id: I39f8d6aa81d091c483f6cf513f31252a18313a2c

--- _testsuite/msdk_ts/unit/scheduler/scheduler_tasks.cpp
+++ _testsuite/msdk_ts/unit/scheduler/scheduler_tasks.cpp
@@ -9,23 +9,11 @@ Copyright(c) 2016 Intel Corporation. All Rights Reserved.
 **********************************************************************************/
 
 #include <thread>
+#include <vector>
 
 #include "gtest/gtest.h"
 
 #include "scheduler_tasks_provider.h"
-
-class TasksTest : public ::testing::Test {
-protected:
-    TasksTest() {
-        core_ = new mfxSchedulerCore;
-    }
-
-    ~TasksTest() {
-        core_->Release();
-    }
-
-    mfxSchedulerCore* core_;
-};
 
 struct TaskConfig
 {
@@ -39,105 +27,104 @@ struct TaskConfig
     std::chrono::duration<int, std::milli> tolerance_;
 };
 
-static TaskConfig g_TaskConfigs[] =
-{
-    // TODO is 10ms tolerance ok for Windows?
-    { "Succeeded 100ms Task", MFX_ERR_NONE, std::chrono::milliseconds(100) },
-    { "Succeeded 250ms Task", MFX_ERR_NONE, std::chrono::milliseconds(250) },
-    { "Succeeded 1024ms Task", MFX_ERR_NONE, std::chrono::milliseconds(1024) },
-    { "Failed MFX_ERR_UNKNOWN 100ms Task", MFX_ERR_UNKNOWN, std::chrono::milliseconds(100) },
-    { "Failed MFX_ERR_UNKNOWN 250ms Task", MFX_ERR_UNKNOWN, std::chrono::milliseconds(250) },
-    { "Failed MFX_ERR_UNKNOWN 1024ms Task", MFX_ERR_UNKNOWN, std::chrono::milliseconds(1024) },
-    //{ "Failed (mfxStatus)666 100ms Task", (mfxStatus)666, std::chrono::milliseconds(100) },
+class TasksTest : public ::testing::TestWithParam<::TaskConfig> {
+protected:
+    TasksTest() {
+        core_ = new mfxSchedulerCore;
+    }
+
+    ~TasksTest() {
+        core_->Release();
+    }
+
+    mfxSchedulerCore* core_;
 };
 
-TEST_F(TasksTest, SingleTask) {
-    for (size_t i=0; i < sizeof(g_TaskConfigs)/sizeof(g_TaskConfigs[0]); ++i) {
-        SCOPED_TRACE(g_TaskConfigs[i].scope_);
+TEST_P(TasksTest, SingleTask) {
+    ::TaskConfig task_cfg = GetParam();
+    SCOPED_TRACE(task_cfg.scope_);
 
-        mfxStatus sts;
-        mfxSyncPoint syncp;
-        utils::Task task(new utils::TaskParam(g_TaskConfigs[i].sts_));
+    mfxStatus sts;
+    mfxSyncPoint syncp;
+    utils::Task task(new utils::TaskParam(task_cfg.sts_));
 
-        task.param_->sync_obj_ = std::make_shared<utils::SleepSyncObject>(g_TaskConfigs[i].dur_);
+    task.param_->sync_obj_ = std::make_shared<utils::SleepSyncObject>(task_cfg.dur_);
 
-        sts = core_->Initialize(NULL);
-        EXPECT_EQ(MFX_ERR_NONE, sts);
+    sts = core_->Initialize(NULL);
+    EXPECT_EQ(MFX_ERR_NONE, sts);
 
-        sts = core_->AddTask(task, &syncp);
-        EXPECT_EQ(MFX_ERR_NONE, sts);
+    sts = core_->AddTask(task, &syncp);
+    EXPECT_EQ(MFX_ERR_NONE, sts);
 
-        auto start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
 
-        sts = core_->Synchronize(syncp, 0);
+    sts = core_->Synchronize(syncp, 0);
+    EXPECT_EQ(MFX_WRN_IN_EXECUTION, sts);
+
+    mfxU32 msec_task_dur;
+    ASSERT_TRUE(task.get_expected_exectime(msec_task_dur));
+
+    sts = core_->Synchronize(syncp, msec_task_dur/2);
+    EXPECT_EQ(MFX_WRN_IN_EXECUTION, sts);
+
+    sts = core_->Synchronize(syncp, msec_task_dur*2);
+    EXPECT_EQ(task.get_expected_status(), sts);
+
+    utils::compare(
+        std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start),
+        task_cfg.dur_ + std::chrono::milliseconds(task.get_tolerance()));
+
+    sts = core_->Reset();
+    EXPECT_EQ(MFX_ERR_NONE, sts);
+}
+
+TEST_P(TasksTest, MultipleWaiters) {
+    ::TaskConfig task_cfg = GetParam();
+    SCOPED_TRACE(task_cfg.scope_);
+
+    mfxStatus sts;
+    mfxSyncPoint syncp;
+    utils::Task task(new utils::TaskParam(task_cfg.sts_));
+
+    task.param_->sync_obj_ = std::make_shared<utils::SleepSyncObject>(task_cfg.dur_);
+
+    sts = core_->Initialize(NULL);
+    EXPECT_EQ(MFX_ERR_NONE, sts);
+
+    sts = core_->AddTask(task, &syncp);
+    EXPECT_EQ(MFX_ERR_NONE, sts);
+
+    mfxU32 msec_task_dur;
+    ASSERT_TRUE(task.get_expected_exectime(msec_task_dur));
+
+    auto start = std::chrono::high_resolution_clock::now();
+    auto syncf = [&] {
+        mfxStatus sts = core_->Synchronize(syncp, 0);
         EXPECT_EQ(MFX_WRN_IN_EXECUTION, sts);
-
-        mfxU32 msec_task_dur;
-        ASSERT_TRUE(task.get_expected_exectime(msec_task_dur));
 
         sts = core_->Synchronize(syncp, msec_task_dur/2);
         EXPECT_EQ(MFX_WRN_IN_EXECUTION, sts);
 
         sts = core_->Synchronize(syncp, msec_task_dur*2);
         EXPECT_EQ(task.get_expected_status(), sts);
+    };
 
-        utils::compare(
-            std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start),
-            g_TaskConfigs[i].dur_ + std::chrono::milliseconds(task.get_tolerance()));
+    std::thread th1(syncf);
+    std::thread th2(syncf);
+    std::thread th3(syncf);
+    std::thread th4(syncf);
 
-        sts = core_->Reset();
-        EXPECT_EQ(MFX_ERR_NONE, sts);
-    }
-}
+    th1.join();
+    th2.join();
+    th3.join();
+    th4.join();
 
-TEST_F(TasksTest, MultipleWaiters) {
-    for (size_t i=0; i < sizeof(g_TaskConfigs)/sizeof(g_TaskConfigs[0]); ++i) {
-        SCOPED_TRACE(g_TaskConfigs[i].scope_);
+    utils::compare(
+        std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start),
+        task_cfg.dur_ + std::chrono::milliseconds(task.get_tolerance()));
 
-        mfxStatus sts;
-        mfxSyncPoint syncp;
-        utils::Task task(new utils::TaskParam(g_TaskConfigs[i].sts_));
-
-        task.param_->sync_obj_ = std::make_shared<utils::SleepSyncObject>(g_TaskConfigs[i].dur_);
-
-        sts = core_->Initialize(NULL);
-        EXPECT_EQ(MFX_ERR_NONE, sts);
-
-        sts = core_->AddTask(task, &syncp);
-        EXPECT_EQ(MFX_ERR_NONE, sts);
-
-        mfxU32 msec_task_dur;
-        ASSERT_TRUE(task.get_expected_exectime(msec_task_dur));
-
-        auto start = std::chrono::high_resolution_clock::now();
-        auto syncf = [&] {
-            mfxStatus sts = core_->Synchronize(syncp, 0);
-            EXPECT_EQ(MFX_WRN_IN_EXECUTION, sts);
-
-            sts = core_->Synchronize(syncp, msec_task_dur/2);
-            EXPECT_EQ(MFX_WRN_IN_EXECUTION, sts);
-
-            sts = core_->Synchronize(syncp, msec_task_dur*2);
-            EXPECT_EQ(task.get_expected_status(), sts);
-        };
-
-        std::thread th1(syncf);
-        std::thread th2(syncf);
-        std::thread th3(syncf);
-        std::thread th4(syncf);
-
-        th1.join();
-        th2.join();
-        th3.join();
-        th4.join();
-
-        utils::compare(
-            std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start),
-            g_TaskConfigs[i].dur_ + std::chrono::milliseconds(task.get_tolerance()));
-
-        sts = core_->Reset();
-        EXPECT_EQ(MFX_ERR_NONE, sts);
-    }
+    sts = core_->Reset();
+    EXPECT_EQ(MFX_ERR_NONE, sts);
 }
 
 TEST_F(TasksTest, MultiframeHerd) {
@@ -203,3 +190,15 @@ TEST_F(TasksTest, TasksLatency) {
     sts = core_->Reset();
     EXPECT_EQ(MFX_ERR_NONE, sts);
 }
+
+static std::vector<::TaskConfig> configs({
+    { "Succeeded 100ms Task", MFX_ERR_NONE, std::chrono::milliseconds(100) },
+    { "Succeeded 250ms Task", MFX_ERR_NONE, std::chrono::milliseconds(250) },
+    { "Succeeded 1024ms Task", MFX_ERR_NONE, std::chrono::milliseconds(1024) },
+    { "Failed MFX_ERR_UNKNOWN 100ms Task", MFX_ERR_UNKNOWN, std::chrono::milliseconds(100) },
+    { "Failed MFX_ERR_UNKNOWN 250ms Task", MFX_ERR_UNKNOWN, std::chrono::milliseconds(250) },
+    { "Failed MFX_ERR_UNKNOWN 1024ms Task", MFX_ERR_UNKNOWN, std::chrono::milliseconds(1024) },
+});
+
+INSTANTIATE_TEST_CASE_P(SingleTask, TasksTest, ::testing::ValuesIn(configs));
+INSTANTIATE_TEST_CASE_P(MultipleWaiters, TasksTest, ::testing::ValuesIn(configs));
