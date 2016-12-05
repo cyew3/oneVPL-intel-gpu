d2871d9352f4105d26195969115dd2c732aad33a
Dmitry Gurulev <dmitry.gurulev@intel.com>
2016-12-05 11:58:35 +0300

    h265d/tests]: add add 8b/10b-420/422/444 for [frametype]
    Change-Id: I88c95f888cddb6888d5150ad3aba86e158f0efb7

--- _testsuite/msdk_ts/src/gmock/test_suites/hevcd_frametype.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/hevcd_frametype.cpp
@@ -22,6 +22,20 @@ class TestSuite : tsVideoDecoder
 
 public:
 
+    static const mfxU32 max_frames = 20;
+
+    struct tc_struct
+    {
+        mfxStatus sts;
+        std::string stream;
+
+        mfxU16 n_frames;
+        mfxU16 frame_type[max_frames];
+
+    };
+
+public:
+
     TestSuite()
         : tsVideoDecoder(MFX_CODEC_HEVC)
         , frame(0)
@@ -29,7 +43,7 @@ public:
         frame_type.AddExtBuffer(MFX_EXTBUFF_DECODED_FRAME_INFO, sizeof(mfxExtDecodedFrameInfo));
     }
 
-    int RunTest(unsigned int id);
+    int RunTest(tc_struct const&);
     static const unsigned int n_cases;
 
     mfxStatus DecodeFrameAsync(mfxSession session, mfxBitstream *bs, mfxFrameSurface1 *surface_work, mfxFrameSurface1 **surface_out, mfxSyncPoint *syncp)
@@ -90,53 +104,12 @@ private:
 
         return MFX_ERR_NONE;
     }
-
-private:
-
-    static const mfxU32 max_frames = 20;
-
-    struct tc_struct
-    {
-        mfxStatus sts;
-        std::string stream;
-
-        mfxU16 n_frames;
-        mfxU16 frame_type[max_frames];
-
-    };
-
-    static const tc_struct test_case[];
 };
 
-const TestSuite::tc_struct TestSuite::test_case[] =
-{
-    {
-        /*01*/ MFX_ERR_NONE, "conformance/hevc/self_coded/stockholm_1280x720_604.x265",
-        11,
-        {
-            /* 0*/ MFX_FRAMETYPE_IDR | MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
-            /* 1*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF,
-            /* 2*/ MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
-            /* 3*/ MFX_FRAMETYPE_B,
-            /* 4*/ MFX_FRAMETYPE_B,
-            /* 5*/ MFX_FRAMETYPE_B,
-            /* 6*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
-            /* 7*/ MFX_FRAMETYPE_B,
-            /* 8*/ MFX_FRAMETYPE_B,
-            /* 9*/ MFX_FRAMETYPE_B,
-            /* 10*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF
-       },
-    }
-};
-
-const unsigned int TestSuite::n_cases =
-    sizeof(TestSuite::test_case) / sizeof(TestSuite::tc_struct);
-
-int TestSuite::RunTest(unsigned int id)
+int TestSuite::RunTest(tc_struct const& tc)
 {
     TS_START;
 
-    const tc_struct& tc = test_case[id];
     expected.assign(tc.frame_type, tc.frame_type + tc.n_frames);
 
     MFXInit();
@@ -157,5 +130,198 @@ int TestSuite::RunTest(unsigned int id)
     return 0;
 }
 
-TS_REG_TEST_SUITE_CLASS(hevcd_frametype);
+template <unsigned fourcc>
+TestSuite::tc_struct const& query_case(unsigned int, std::integral_constant<unsigned, fourcc>);
+
+/* 8 bit */
+TestSuite::tc_struct const& query_case(unsigned int id, std::integral_constant<unsigned, MFX_FOURCC_NV12>)
+{
+    static TestSuite::tc_struct const cases[] =
+    {
+        /*01*/ MFX_ERR_NONE, "conformance/hevc/self_coded/stockholm_1280x720_604.x265",
+        11,
+        {
+            /*  0*/ MFX_FRAMETYPE_IDR | MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
+            /*  1*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF,
+            /*  2*/ MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
+            /*  3*/ MFX_FRAMETYPE_B,
+            /*  4*/ MFX_FRAMETYPE_B,
+            /*  5*/ MFX_FRAMETYPE_B,
+            /*  6*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  7*/ MFX_FRAMETYPE_B,
+            /*  8*/ MFX_FRAMETYPE_B,
+            /*  9*/ MFX_FRAMETYPE_B,
+            /* 10*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF
+       },
+    };
+
+    assert(!(id > 1));
+    return cases[id];
+}
+
+TestSuite::tc_struct const& query_case(unsigned int id, std::integral_constant<unsigned, MFX_FOURCC_YUY2>)
+{
+    static TestSuite::tc_struct const cases[] =
+    {
+        MFX_ERR_NONE, "forBehaviorTest/picturetype/inter422_8b_1920x1080.h265",
+        20,
+        {
+            /*  0*/ MFX_FRAMETYPE_IDR | MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
+            /*  1*/ MFX_FRAMETYPE_B,
+            /*  2*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  3*/ MFX_FRAMETYPE_B,
+            /*  4*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  5*/ MFX_FRAMETYPE_B,
+            /*  6*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  7*/ MFX_FRAMETYPE_B,
+            /*  8*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  9*/ MFX_FRAMETYPE_I,
+            /* 10*/ MFX_FRAMETYPE_IDR | MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
+            /* 11*/ MFX_FRAMETYPE_B,
+            /* 12*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /* 13*/ MFX_FRAMETYPE_B,
+            /* 14*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /* 15*/ MFX_FRAMETYPE_B,
+            /* 16*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /* 17*/ MFX_FRAMETYPE_B,
+            /* 18*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /* 19*/ MFX_FRAMETYPE_I,
+        }
+    };
+
+    assert(!(id > 1));
+    return cases[id];
+}
+
+TestSuite::tc_struct const& query_case(unsigned int id, std::integral_constant<unsigned, MFX_FOURCC_AYUV>)
+{
+    static TestSuite::tc_struct const cases[] =
+    {
+        MFX_ERR_NONE, "forBehaviorTest/picturetype/CCP_8bit_RExt_QCOM_2g.h265",
+        18,
+        {
+            /*  0*/ MFX_FRAMETYPE_IDR | MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
+            /*  1*/ MFX_FRAMETYPE_B,
+            /*  2*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  3*/ MFX_FRAMETYPE_B,
+            /*  4*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  5*/ MFX_FRAMETYPE_B,
+            /*  6*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  7*/ MFX_FRAMETYPE_B,
+            /*  8*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  9*/ MFX_FRAMETYPE_IDR | MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
+            /* 10*/ MFX_FRAMETYPE_B,
+            /* 11*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /* 12*/ MFX_FRAMETYPE_B,
+            /* 13*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /* 14*/ MFX_FRAMETYPE_B,
+            /* 15*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /* 16*/ MFX_FRAMETYPE_B,
+            /* 17*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+        }
+    };
+
+    assert(!(id > 1));
+    return cases[id];
+}
+
+/* 10 bit */
+TestSuite::tc_struct const& query_case(unsigned int id, std::integral_constant<unsigned, MFX_FOURCC_P010>)
+{
+    static TestSuite::tc_struct const cases[] =
+    {
+        MFX_ERR_NONE, "forBehaviorTest/picturetype/sbe_main10_448x256.h265 ",
+        14,
+        {
+            /*  0*/ MFX_FRAMETYPE_IDR | MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
+            /*  1*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF,
+            /*  2*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF,
+            /*  3*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  4*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  5*/ MFX_FRAMETYPE_B,
+            /*  6*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF,
+            /*  7*/ MFX_FRAMETYPE_IDR | MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
+            /*  8*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF,
+            /*  9*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF,
+            /* 10*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /* 11*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /* 12*/ MFX_FRAMETYPE_B,
+            /* 13*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF
+        }
+    };
+
+    assert(!(id > 1));
+    return cases[id];
+}
+
+TestSuite::tc_struct const& query_case(unsigned int id, std::integral_constant<unsigned, MFX_FOURCC_Y216>)
+{
+    static TestSuite::tc_struct const cases[] =
+    {
+        MFX_ERR_NONE, "forBehaviorTest/picturetype/sbe_rext_422_10b_448x256.h265",
+        10,
+        {
+            /*  0*/ MFX_FRAMETYPE_IDR | MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
+            /*  1*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  2*/ MFX_FRAMETYPE_B,
+            /*  3*/ MFX_FRAMETYPE_B,
+            /*  4*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF,
+            /*  5*/ MFX_FRAMETYPE_IDR | MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
+            /*  6*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  7*/ MFX_FRAMETYPE_B,
+            /*  8*/ MFX_FRAMETYPE_B,
+            /*  9*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF,
+        }
+    };
+
+    assert(!(id > 1));
+    return cases[id];
+}
+
+TestSuite::tc_struct const& query_case(unsigned int id, std::integral_constant<unsigned, MFX_FOURCC_Y410>)
+{
+    static TestSuite::tc_struct const cases[] =
+    {
+        MFX_ERR_NONE, "forBehaviorTest/picturetype/sbe_rext_444_10b_448x256.h265",
+        10,
+        {
+            /*  0*/ MFX_FRAMETYPE_IDR | MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
+            /*  1*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  2*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  3*/ MFX_FRAMETYPE_B,
+            /*  4*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF,
+            /*  5*/ MFX_FRAMETYPE_IDR | MFX_FRAMETYPE_I | MFX_FRAMETYPE_REF,
+            /*  6*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  7*/ MFX_FRAMETYPE_B | MFX_FRAMETYPE_REF,
+            /*  8*/ MFX_FRAMETYPE_B,
+            /*  9*/ MFX_FRAMETYPE_P | MFX_FRAMETYPE_REF,
+        }
+    };
+
+    assert(!(id > 1));
+    return cases[id];
+}
+
+template <unsigned fourcc>
+struct TestSuiteEx
+    : public TestSuite
+{
+    static
+    int RunTest(unsigned int id)
+    {
+        auto tc = query_case(id, std::integral_constant<unsigned, fourcc>());
+
+        TestSuite suite;
+        return suite.RunTest(tc);
+    }
+};
+
+TS_REG_TEST_SUITE(hevcd_frametype,     TestSuiteEx<MFX_FOURCC_NV12>::RunTest, 1);
+TS_REG_TEST_SUITE(hevcd_422_frametype, TestSuiteEx<MFX_FOURCC_YUY2>::RunTest, 1);
+TS_REG_TEST_SUITE(hevcd_444_frametype, TestSuiteEx<MFX_FOURCC_AYUV>::RunTest, 1);
+
+TS_REG_TEST_SUITE(hevc10d_frametype,     TestSuiteEx<MFX_FOURCC_P010>::RunTest, 1);
+TS_REG_TEST_SUITE(hevc10d_422_frametype, TestSuiteEx<MFX_FOURCC_Y216>::RunTest, 1);
+TS_REG_TEST_SUITE(hevc10d_444_frametype, TestSuiteEx<MFX_FOURCC_Y410>::RunTest, 1);
+
 }
