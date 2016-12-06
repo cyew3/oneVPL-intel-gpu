cyew3/oneVPL-intel-gpu

commit e77f746263dd7d2b61fd83bfecda3c6370a63010

[MDP-31695] mfx_transcoder field weaving/splitting support
Cherry-picked from: I1930c5a4b7528a8ef827a7aae0d3c94faf494521

Change-Id: I3337427ebdcf6d561e2f2972b373075c2dbb56bf

_testsuite/mfx_player/src/mfx_pipeline_dec.cpp
@@ -1021,6 +1021,8 @@ mfxStatus MFXDecPipeline::CreateVPP()
     ENABLE_VPP(m_components[eDEC].m_bufType != m_components[eREN].m_bufType)
     ENABLE_VPP(m_components[eDEC].m_params.mfx.FrameInfo.Shift != m_components[eREN].m_params.mfx.FrameInfo.Shift);
     ENABLE_VPP(m_inParams.nDenoiseFactorPlus1);
+    ENABLE_VPP(m_inParams.bFieldWeaving);
+    ENABLE_VPP(m_inParams.bFieldSplitting);
     ENABLE_VPP(m_inParams.bFieldProcessing);
     ENABLE_VPP(m_inParams.nDetailFactorPlus1);
     ENABLE_VPP(m_inParams.bUseProcAmp);
@@ -1218,6 +1220,24 @@ if (m_inParams.bFieldProcessing)
     {
         m_inParams.FrameInfo.Height = m_components[eVPP].m_params.vpp.Out.CropH + m_inParams.FrameInfo.CropY;
         m_inParams.FrameInfo.CropH = m_components[eVPP].m_params.vpp.Out.CropH;
+    }
+
+    //turn on field weaving
+    if (m_inParams.bFieldWeaving)
+    {
+        m_components[eVPP].m_params.vpp.Out.PicStruct = MFX_PICSTRUCT_UNKNOWN;
+        m_inParams.FrameInfo.PicStruct = MFX_PICSTRUCT_UNKNOWN;
+        m_inParams.FrameInfo.Height = m_inParams.FrameInfo.Height << 1;
+        m_inParams.FrameInfo.CropH = m_inParams.FrameInfo.CropH << 1;
+    }
+
+    //turn on field splitting
+    if (m_inParams.bFieldSplitting)
+    {
+        m_components[eVPP].m_params.vpp.Out.PicStruct = MFX_PICSTRUCT_FIELD_SINGLE;
+        m_inParams.FrameInfo.PicStruct = MFX_PICSTRUCT_FIELD_SINGLE;
+        m_inParams.FrameInfo.Height = m_inParams.FrameInfo.Height >> 1;
+        m_inParams.FrameInfo.CropH = m_inParams.FrameInfo.CropH >> 1;
     }
 
     if (m_components[eVPP].m_zoomx != 0)
@@ -3410,7 +3430,36 @@ mfxStatus  MFXDecPipeline::RunVPP(mfxFrameSurface1 *pSurface)
         }
         else
         {
-            // Usual VPP processing
+            if (m_inParams.bFieldWeaving)
+            {
+                if (!(m_nDecFrames % 2))
+                {
+                    if (pSurface)
+                    {
+                        if (pSurface->Info.PicStruct & MFX_PICSTRUCT_FIELD_TFF)
+                        {
+                            vppOut.pSurface->Info.PicStruct = MFX_PICSTRUCT_FIELD_TFF;
+                        }
+                        if (pSurface->Info.PicStruct & MFX_PICSTRUCT_FIELD_BFF)
+                        {
+                            vppOut.pSurface->Info.PicStruct = MFX_PICSTRUCT_FIELD_BFF;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (m_inParams.bFieldSplitting)
+                {
+                    if (pSurface)
+                    {
+                        if (pSurface->Info.PicStruct & MFX_PICSTRUCT_FIELD_TFF || pSurface->Info.PicStruct & MFX_PICSTRUCT_FIELD_BFF)
+                        {
+                            vppOut.pSurface->Info.PicStruct = MFX_PICSTRUCT_FIELD_SINGLE;
+                        }
+                    }
+                }
+            }
             sts = m_pVPP->RunFrameVPPAsync(pSurface, vppOut.pSurface, (mfxExtVppAuxData*)vppOut.pCtrl->ExtParam[0], &syncp);
         }
 
@@ -5012,7 +5061,14 @@ mfxStatus MFXDecPipeline::ProcessCommandInternal(vm_char ** &argv, mfxI32 argc, 
               argv++;
               MFX_PARSE_INT(m_inParams.nFramesAfterRecovery,argv[0]);
           }
-          else HANDLE_INT_OPTION(m_inParams.nDecoderSurfs, VM_STRING("-dec:surfs"), VM_STRING("specifies number of surfaces in decoder's pool"))
+          else if(m_OptProc.Check(argv[0], VM_STRING("-field_weaving"), VM_STRING("Turn on field weaving. Need to use specific streams"), OPT_UINT_32))
+          {
+              m_inParams.bFieldWeaving = true;
+          }
+          else if(m_OptProc.Check(argv[0], VM_STRING("-field_splitting"), VM_STRING("Turn on field splitting. Need to use specific streams"), OPT_UINT_32))
+          {
+              m_inParams.bFieldSplitting = true;
+          }
           else
           {
                MFX_TRACE_AT_EXIT_IF( MFX_ERR_UNSUPPORTED

_testsuite/mfx_player/include/mfx_pipeline_dec.h
@@ -183,6 +183,8 @@ struct sCommandlineParams
   mfxU16         nFieldProcessing;
   bool           bFieldProcessing;
   bool           bSwapFieldProcessing;
+  bool           bFieldWeaving;
+  bool           bFieldSplitting;
 
   bool bUseProcAmp;
   mfxExtVPPProcAmp m_ProcAmp;
@@ -288,6 +290,8 @@ struct sCommandlineParams
       m_FieldProcessing.Header.BufferSz = sizeof(mfxExtVPPFieldProcessing);
       bSwapFieldProcessing = false;
       bFieldProcessing = false;
+      bFieldWeaving = false;
+      bFieldSplitting = false;
 
       // ProcAmp default values
       m_ProcAmp.Brightness = 0.0f;

commit e24ae986294862292829a0429873eb39b785c33a

[TS] Fix regression in conf.bigsurfpool tests
Fixed regression by e77f746263dd7d2b61fd83bfecda3c6370a63010 commit re-enabling -dec:surfs cmd option

Change-Id: I6b23fde29d1c7e74fc8d7d3b4a8783ca75b9e489
Signed-off-by: Alexei Leonenko <alexei.leonenko@intel.com>

_testsuite/mfx_player/src/mfx_pipeline_dec.cpp
@@ -5087,7 +5087,8 @@ mfxStatus MFXDecPipeline::ProcessCommandInternal(vm_char ** &argv, mfxI32 argc, 
           {
               m_inParams.bFieldSplitting = true;
           }
-          else
+          else HANDLE_INT_OPTION(m_inParams.nDecoderSurfs, VM_STRING("-dec:surfs"), VM_STRING("specifies number of surfaces in decoder's pool"))
+   		  else
           {
                MFX_TRACE_AT_EXIT_IF( MFX_ERR_UNSUPPORTED
                     , !bReportError