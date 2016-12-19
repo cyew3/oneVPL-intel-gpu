3096a25ac0e530e2a4469da602b872367875c9e5
Baygulov, Aleksei <aleksei.baygulov@intel.com>
2016-12-19 14:05:57 +0300

    [core] implement intrinsic analog of ippiCopyManaged_8u_C1R for fast copy from video to sys.

--- _studio/shared/include/fast_copy.h
+++ _studio/shared/include/fast_copy.h
@@ -31,17 +31,80 @@ enum
     COPY_VIDEO_TO_SYS = 2,
 };
 
+#include <immintrin.h>
+inline void copyVideoToSys(const mfxU8* src, mfxU8* dst, int width)
+{
+    static const int item_size = 4*sizeof(__m128i);
+
+    int align16 = (0x10 - (reinterpret_cast<size_t>(src) & 0xf)) & 0xf;
+    for (int i = 0; i < align16; i++)
+        *dst++ = *src++;
+
+    int w = width - align16;
+    if (w < 0)
+        return;
+
+    int width4 = w & (-item_size);
+
+    __m128i * src_reg = (__m128i *)src;
+    __m128i * dst_reg = (__m128i *)dst;
+
+    int i = 0;
+    for (; i < width4; i += item_size)
+    {
+        __m128i xmm0 = _mm_stream_load_si128(src_reg);
+        __m128i xmm1 = _mm_stream_load_si128(src_reg+1);
+        __m128i xmm2 = _mm_stream_load_si128(src_reg+2);
+        __m128i xmm3 = _mm_stream_load_si128(src_reg+3);
+        _mm_storeu_si128(dst_reg, xmm0);
+        _mm_storeu_si128(dst_reg+1, xmm1);
+        _mm_storeu_si128(dst_reg+2, xmm2);
+        _mm_storeu_si128(dst_reg+3, xmm3);
+
+        src_reg += 4;
+        dst_reg += 4;
+    }
+
+    size_t tail_data_sz = w & (item_size - 1);
+    if (tail_data_sz)
+    {
+        for (; tail_data_sz >= sizeof(__m128i); tail_data_sz -= sizeof(__m128i))
+        {
+            __m128i xmm0 = _mm_stream_load_si128(src_reg);
+            _mm_storeu_si128(dst_reg, xmm0);
+            src_reg += 1;
+            dst_reg += 1;
+        }
+
+        src = (const mfxU8 *)src_reg;
+        dst = (mfxU8 *)dst_reg;
+
+        for (; tail_data_sz > 0; tail_data_sz--)
+            *dst++ = *src++;
+    }
+}
+
 template<typename T>
-inline int mfxCopyRect(const T* pSrc, int srcStep, T* pDst, int dstStep, IppiSize roiSize, int )
+inline int mfxCopyRect(const T* pSrc, int srcStep, T* pDst, int dstStep, IppiSize roiSize, int flag)
 {
     if (!pDst || !pSrc || roiSize.width < 0 || roiSize.height < 0 || srcStep < 0 || dstStep < 0)
         return -1;
 
-    for(int h = 0; h < roiSize.height; h++ ) {
-        std::copy(pSrc, pSrc + roiSize.width, pDst);
-        //memcpy_s(pDst, roiSize.width, pSrc,roiSize.width);
-        pSrc = (T *)((unsigned char*)pSrc + srcStep);
-        pDst = (T *)((unsigned char*)pDst + dstStep);
+    if (flag & COPY_VIDEO_TO_SYS)
+    {
+        for(int h = 0; h < roiSize.height; h++ )
+        {
+            copyVideoToSys((const mfxU8*)pSrc, (mfxU8*)pDst, roiSize.width*sizeof(T));
+            pSrc = (T *)((mfxU8*)pSrc + srcStep);
+            pDst = (T *)((mfxU8*)pDst + dstStep);
+        }
+    } else {
+        for(int h = 0; h < roiSize.height; h++ )
+        {
+            std::copy(pSrc, pSrc + roiSize.width, pDst);
+            pSrc = (T *)((mfxU8*)pSrc + srcStep);
+            pDst = (T *)((mfxU8*)pDst + dstStep);
+        }
     }
 
     return 0;
