cyew3/oneVPL-intel-gpu

commit 91e3855dc38080dd47eb85eeaa1f7a6f747651ac

AVC FEI: improved checks on Init stage
Change-Id: I9bcc3e16ac41edc04b5e00c2f333b555f8a94813

_studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -5,7 +5,7 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2009-2016 Intel Corporation. All Rights Reserved.
+// Copyright(C) 2009-2017 Intel Corporation. All Rights Reserved.
 //
 
 #include "mfx_common.h"
@@ -1820,9 +1820,46 @@ mfxStatus MfxHwH264Encode::CheckVideoParam(
         MFX_CHECK(extOpaq->In.NumSurface >= numFrameMin, MFX_ERR_INVALID_VIDEO_PARAM);
     }
 
+    checkSts = CheckVideoParamFEI(par);
+
     return checkSts;
 }
 
+mfxStatus MfxHwH264Encode::CheckVideoParamFEI(
+    MfxVideoParam &     par)
+{
+    mfxStatus checkSts = MFX_ERR_NONE;
+
+    mfxExtFeiParam* feiParam = (mfxExtFeiParam*)MfxHwH264Encode::GetExtBuffer(par.ExtParam, par.NumExtParam, MFX_EXTBUFF_FEI_PARAM);
+    if (!feiParam) return checkSts;
+
+    switch (feiParam->Func)
+    {
+    case MFX_FEI_FUNCTION_PREENC:
+    case MFX_FEI_FUNCTION_ENCODE:
+    case MFX_FEI_FUNCTION_ENC:
+    case MFX_FEI_FUNCTION_PAK:
+    case MFX_FEI_FUNCTION_DEC:
+        break;
+    default:
+        return MFX_ERR_INCOMPATIBLE_VIDEO_PARAM;
+    }
+
+    mfxExtCodingOption3 * extCodingOpt3 = GetExtBuffer(par);
+
+    if (IsOn(extCodingOpt3->EnableMBQP))
+    {
+        return MFX_ERR_INCOMPATIBLE_VIDEO_PARAM;
+    }
+
+    /* FEI works with CQP only */
+    if (MFX_RATECONTROL_CQP != par.mfx.RateControlMethod)
+    {
+        return MFX_ERR_INCOMPATIBLE_VIDEO_PARAM;
+    }
+
+    return checkSts;
+}
 /*
  * utils for debug purposes
  */

_studio/mfx_lib/encode_hw/h264/src/mfx_h264_encode_hw.cpp
@@ -5,7 +5,7 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2009-2016 Intel Corporation. All Rights Reserved.
+// Copyright(C) 2009-2017 Intel Corporation. All Rights Reserved.
 //
 
 #include "mfx_common.h"
@@ -754,33 +754,9 @@ mfxStatus ImplementationAvc::Init(mfxVideoParam * par)
     if ((NULL != feiParam) && (feiParam->Func != MFX_FEI_FUNCTION_ENCODE))
             return MFX_ERR_INVALID_VIDEO_PARAM;
 
-    m_video = *par;
-
-    mfxExtCodingOption3 * extCodingOpt3 = GetExtBuffer(m_video);
-
-    if (m_isENCPAK && IsOn(extCodingOpt3->EnableMBQP))
-    {
-        extCodingOpt3->EnableMBQP = MFX_CODINGOPTION_OFF;
-        return MFX_ERR_INCOMPATIBLE_VIDEO_PARAM;
-    }
-
-    /* FEI works with CQP only */
-    if ((m_isENCPAK) && (MFX_RATECONTROL_CQP != par->mfx.RateControlMethod)){
-        m_video.mfx.RateControlMethod =  MFX_RATECONTROL_CQP;
-    }
-    /* One more checking for FEI */
-    if ((m_isENCPAK) &&
-        ((MFX_PROFILE_AVC_BASELINE == m_video.mfx.CodecProfile) ||(MFX_PROFILE_AVC_MAIN == m_video.mfx.CodecProfile)) )
-    {
-        mfxExtCodingOption *       extOpt  = GetExtBuffer(m_video);
-        extOpt->InterPredBlockSize = MFX_BLOCKSIZE_MIN_16X16;
-    }
-
-    /*!!! This is WA for number of references frames for FEI case*/
-    if ((m_isENCPAK) && (par->mfx.NumRefFrame >4))
-        m_video.mfx.NumRefFrame = 4;
 
     m_video = *par;
+
     sts = ReadSpsPpsHeaders(m_video);
     MFX_CHECK_STS(sts);
 
@@ -1354,13 +1330,7 @@ mfxStatus ImplementationAvc::ProcessAndCheckNewParameters(
     mfxExtSpsHeader const * extSpsOld = GetExtBuffer(m_video);
     mfxExtCodingOption2 const * extOpt2New = GetExtBuffer(newPar);
     mfxExtCodingOption2 const * extOpt2Old = GetExtBuffer(m_video);
-    mfxExtCodingOption3 * extOpt3New = GetExtBuffer(newPar);
-
-    if (m_isENCPAK && IsOn(extOpt3New->EnableMBQP))
-    {
-        extOpt3New->EnableMBQP = MFX_CODINGOPTION_OFF;
-        return MFX_ERR_INCOMPATIBLE_VIDEO_PARAM;
-    }
+    mfxExtCodingOption3 const * extOpt3New = GetExtBuffer(newPar);
 
     if(!IsOn(m_video.mfx.LowPower))
     {
@@ -2820,7 +2790,7 @@ mfxStatus ImplementationAvc::AsyncRoutine(mfxBitstream * bs)
                 task->m_bsDataLength[0] = task->m_bsDataLength[1] = 0;
 
                 if ((sts = QueryStatus(*task, task->m_fid[f])) != MFX_ERR_NONE)
-                    return sts;
+                    return Error(sts);
 
                 if ((NULL == task->m_bs) && (bs != NULL))
                     task->m_bs = bs;

_studio/mfx_lib/shared/include/mfx_h264_enc_common_hw.h
@@ -5,7 +5,7 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2008-2016 Intel Corporation. All Rights Reserved.
+// Copyright(C) 2008-2017 Intel Corporation. All Rights Reserved.
 //
 
 #ifndef __MFX_H264_ENC_COMMON_HW_H__
@@ -793,6 +793,9 @@ namespace MfxHwH264Encode
         bool                setExtAlloc,
         eMFXHWType          platform = MFX_HW_UNKNOWN,
         eMFXVAType          vaType = MFX_HW_NO);
+
+    mfxStatus CheckVideoParamFEI(
+        MfxVideoParam &     par);
 
     mfxStatus CheckVideoParamQueryLike(
         MfxVideoParam &     par,

commit 05ec8d1c0ffa0a1c4d14df8d778039062cccaf43

[h264e hw] fix regresion caused by 91e3855
checkSts = CheckVideoParamFEI(par) should not override MFX_WRN_INCOMPATIBLE_VIDEO_PARAM returned from CheckVideoParamQueryLike

Failed test - MDP-32452 behavior_avce_. msdk_ts::ts::b_MFXVideoENCODE_Init: Returned status MFX_ERR_NONE(0) is wrong! Expected MFX_WRN_INCOMPATIBLE_VIDEO_PARAM(5).25-100% failed. *Regression!

Change-Id: I6e11722a85f8160dfa6b2d0256b1341fb00edd99
Signed-off-by: Sergey Solodkov <Sergey.Solodkov@intel.com>

_studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -1827,7 +1827,8 @@ mfxStatus MfxHwH264Encode::CheckVideoParam(
         MFX_CHECK(extOpaq->In.NumSurface >= numFrameMin, MFX_ERR_INVALID_VIDEO_PARAM);
     }
 
-    checkSts = CheckVideoParamFEI(par);
+    sts = CheckVideoParamFEI(par);
+    MFX_CHECK(sts >= MFX_ERR_NONE, sts);
 
     return checkSts;
 }