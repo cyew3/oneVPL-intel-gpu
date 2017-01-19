a9527ef7976d752c6fcf802a44b9a4bf9f5cb2c6
Ryabinin, Sergey <sergey.ryabinin@intel.com>
2017-01-19 08:02:03 -0800

    fix VP9 decode 10b 420 behavior tests: shift should be set for P010 format
    Change-Id: Ib2e2ca21bd548924b4e2f2af956bb718846b6aae

--- _testsuite/msdk_ts/src/gmock/test_suites/vp9d_get_video_param.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/vp9d_get_video_param.cpp
@@ -4,7 +4,7 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2016 Intel Corporation. All Rights Reserved.
+Copyright(c) 2016-2017 Intel Corporation. All Rights Reserved.
 
 \* ****************************************************************************** */
 
@@ -315,6 +315,8 @@ int TestSuite::RunTest_fourcc(const unsigned int id)
 
     if(MFX_FOURCC_P010 == fourcc || MFX_FOURCC_Y410 == fourcc)
         m_par.mfx.FrameInfo.BitDepthChroma = m_par.mfx.FrameInfo.BitDepthLuma = 10;
+    if (MFX_FOURCC_P010 == fourcc)
+        m_par.mfx.FrameInfo.Shift = 1;
     m_par_set = true; //we are not testing DecodeHeader here
 
     const tc_struct& tc = test_case[id];
