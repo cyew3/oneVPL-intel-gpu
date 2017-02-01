72c91cc6b7b87b084c5e6f550d4e419e596442b7
Alexandr Smirnov <alexander.smirnov@intel.com>
2017-02-01 06:53:50 -0800

    TYPEDEF_MEMBER statements from ts_trace.h are isolated to ts_typedef.h
    Issue: MDP-32285
    Test: N/A
    
    Change-Id: I5065db7fd393f62badd932a5e66d1e1ebd54b8bf

--- _testsuite/msdk_ts/include/gmock/ts_trace.h
+++ _testsuite/msdk_ts/include/gmock/ts_trace.h
@@ -4,7 +4,7 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2014-2016 Intel Corporation. All Rights Reserved.
+Copyright(c) 2014-2017 Intel Corporation. All Rights Reserved.
 
 \* ****************************************************************************** */
 
@@ -25,20 +25,7 @@ Copyright(c) 2014-2016 Intel Corporation. All Rights Reserved.
 
 #include "bs_parser.h"
 #include <iostream>
-
-#define TYPEDEF_MEMBER(base, member, name) typedef std::remove_reference<decltype(((base*)0)->member)>::type name;
-TYPEDEF_MEMBER(mfxExtOpaqueSurfaceAlloc,  In,                  mfxExtOpaqueSurfaceAlloc_InOut)
-TYPEDEF_MEMBER(mfxExtAVCRefListCtrl,      PreferredRefList[0], mfxExtAVCRefListCtrl_Entry)
-TYPEDEF_MEMBER(mfxExtPictureTimingSEI,    TimeStamp[0],        mfxExtPictureTimingSEI_TimeStamp)
-TYPEDEF_MEMBER(mfxExtAvcTemporalLayers,   Layer[0],            mfxExtAvcTemporalLayers_Layer)
-TYPEDEF_MEMBER(mfxExtAVCEncodedFrameInfo, UsedRefListL0[0],    mfxExtAVCEncodedFrameInfo_RefList)
-TYPEDEF_MEMBER(mfxExtVPPVideoSignalInfo,  In,                  mfxExtVPPVideoSignalInfo_InOut)
-TYPEDEF_MEMBER(mfxExtEncoderROI,          ROI[0],              mfxExtEncoderROI_Entry)
-TYPEDEF_MEMBER(mfxExtDirtyRect,           Rect[0],             mfxExtDirtyRect_Entry)
-TYPEDEF_MEMBER(mfxExtMoveRect,            Rect[0],             mfxExtMoveRect_Entry)
-typedef union { mfxU32 n; char c[4]; } mfx4CC;
-typedef mfxExtAVCRefLists::mfxRefPic mfxExtAVCRefLists_mfxRefPic;
-typedef mfxExtFeiEncMV::mfxExtFeiEncMVMB mfxExtFeiEncMV_MB;
+#include "ts_typedef.h"
 
 class tsAutoTrace : public std::ostream
 {
