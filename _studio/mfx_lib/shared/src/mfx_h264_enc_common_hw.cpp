cyew3/oneVPL-intel-gpu

commit ed89953a3c5c147bccc7d61196b4b5f26e3d819a

FEI ENCODE merge from 16.4.4 (v5) FEI ENCODE merge from 16.4.4: (Common portion) FEI merge from 16.4.4: PRE-ENC, ENC, PAK (Separate portion)
Change-Id: Ica6b2af7b90fd163b32654043330f5600581b339

_studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -1145,10 +1145,9 @@ mfxU8 MfxHwH264Encode::GetCabacInitIdc(mfxU32 targetUsage)
 
 bool MfxHwH264Encode::IsLookAheadSupported(
     MfxVideoParam const & /*video*/,
-    eMFXHWType            /*platform*/)
+    eMFXHWType            platform)
 {
-    //probably need to ad specific for some IOT target linux platforms where it is not required
-    return true;
+    return ((platform >= MFX_HW_HSW) && (platform != MFX_HW_VLV));
 }
 
 // determine and return mode of Query operation (valid modes are 1, 2, 3, 4 - see MSDK spec for details)
@@ -1409,6 +1408,12 @@ bool MfxHwH264Encode::IsRunTimeExtBufferIdSupported(mfxU32 id)
         || id == MFX_EXTBUFF_FEI_ENC_CTRL
         || id == MFX_EXTBUFF_FEI_ENC_MB
         || id == MFX_EXTBUFF_FEI_ENC_MV_PRED
+        || id == MFX_EXTBUFF_FEI_ENC_MB
+        || id == MFX_EXTBUFF_FEI_ENC_MV_PRED
+        || id == MFX_EXTBUFF_FEI_ENC_CTRL
+        || id == MFX_EXTBUFF_FEI_PREENC_QP
+        || id == MFX_EXTBUFF_FEI_SLICE
+
 #endif
         );
 }
@@ -1441,6 +1446,7 @@ bool MfxHwH264Encode::IsVideoParamExtBufferIdSupported(mfxU32 id)
         || id == MFX_EXTBUFF_FEI_CODING_OPTION
 #if defined (MFX_ENABLE_H264_VIDEO_FEI_ENCPAK)
         || id == MFX_EXTBUFF_FEI_PARAM
+        || id == MFX_EXTBUFF_FEI_SLICE
 #endif
         );
 }
@@ -1456,12 +1462,17 @@ mfxStatus MfxHwH264Encode::CheckExtBufferId(mfxVideoParam const & par)
             return MFX_ERR_INVALID_VIDEO_PARAM;
 
         // check if buffer presents twice in video param
-        if (MfxHwH264Encode::GetExtBuffer(
-            par.ExtParam + i + 1,
-            par.NumExtParam - i - 1,
-            par.ExtParam[i]->BufferId) != 0)
+#if defined (MFX_ENABLE_H264_VIDEO_FEI_ENCPAK)
+        if (par.ExtParam[i]->BufferId != MFX_EXTBUFF_FEI_SLICE)
+#endif
         {
-            return MFX_ERR_INVALID_VIDEO_PARAM;
+            if (MfxHwH264Encode::GetExtBuffer(
+                par.ExtParam + i + 1,
+                par.NumExtParam - i - 1,
+                par.ExtParam[i]->BufferId) != 0)
+            {
+                return MFX_ERR_INVALID_VIDEO_PARAM;
+            }
         }
     }
 
@@ -2002,6 +2013,16 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
     {
         changed = true;
         par.mfx.RateControlMethod = MFX_RATECONTROL_CBR;
+    }
+    if(MFX_HW_VAAPI == vaType &&
+       par.mfx.RateControlMethod != 0 &&
+       par.mfx.RateControlMethod != MFX_RATECONTROL_CBR &&
+       par.mfx.RateControlMethod != MFX_RATECONTROL_VBR &&
+       par.mfx.RateControlMethod != MFX_RATECONTROL_CQP &&
+       !bRateControlLA(par.mfx.RateControlMethod))
+    {
+        unsupported = true;
+        par.mfx.RateControlMethod = 0;
     }
     if (bRateControlLA(par.mfx.RateControlMethod) && IsOn(extOpt->CAVLC))
     {
@@ -4363,6 +4384,8 @@ void MfxHwH264Encode::SetDefaults(
     mfxExtSVCSeqDesc *         extSvc  = GetExtBuffer(par);
     mfxExtSVCRateControl *     extRc   = GetExtBuffer(par);
     mfxExtChromaLocInfo*       extCli  = GetExtBuffer(par);
+    mfxExtFeiParam* feiParam = (mfxExtFeiParam*)GetExtBuffer(par);
+    bool isENCPAK = feiParam && (feiParam->Func == MFX_FEI_FUNCTION_ENCPAK);
 
     if (extOpt2->UseRawRef)
         extDdi->RefRaw = extOpt2->UseRawRef;
@@ -4524,14 +4547,36 @@ void MfxHwH264Encode::SetDefaults(
         extDdi->NumActiveRefP = extDdi->NumActiveRefBL0 = extDdi->NumActiveRefBL1 = 1;
     }
     if (extDdi->NumActiveRefP == 0)
+    {
         extDdi->NumActiveRefP = GetDefaultMaxNumRefActivePL0(par.mfx.TargetUsage, platform,IsOn(par.mfx.LowPower));
+        /* WA: it will be fixed for legacy encoder too
+         * Additional for FEI ENCODE: we can use 4 ref in L0 references for TU4
+         * */
+        if (isENCPAK)
+            extDdi->NumActiveRefP = 4;
+    }
 
     if (par.mfx.GopRefDist > 1)
     {
         if (extDdi->NumActiveRefBL0 == 0)
+        {
             extDdi->NumActiveRefBL0 = GetDefaultMaxNumRefActiveBL0(par.mfx.TargetUsage, platform);
+            /* WA: it will be fixed for legacy encoder too
+             * Additional for FEI ENCODE: we can use 4 ref in L0 references for TU4
+             * */
+            if (isENCPAK)
+                extDdi->NumActiveRefBL0 = 4;
+        } /* if (extDdi->NumActiveRefBL0 == 0) */
+
         if (extDdi->NumActiveRefBL1 == 0)
+        {
             extDdi->NumActiveRefBL1 = GetDefaultMaxNumRefActiveBL1(par.mfx.TargetUsage, platform);
+            /* Additional for FEI ENCODE: we can use 2 L1 references */
+            if ((isENCPAK) &&
+                    ( (MFX_PICSTRUCT_FIELD_TFF == par.mfx.FrameInfo.PicStruct) ||
+                      (MFX_PICSTRUCT_FIELD_BFF == par.mfx.FrameInfo.PicStruct)) )
+                extDdi->NumActiveRefBL1 = 2;
+        } /* if (extDdi->NumActiveRefBL1 == 0) */
     }
 
     if (par.mfx.GopPicSize == 0)
@@ -5314,6 +5359,8 @@ mfxStatus MfxHwH264Encode::CheckRunTimeExtBuffers(
     mfxEncodeCtrl *       ctrl)
 {
     mfxStatus checkSts = MFX_ERR_NONE;
+    mfxExtFeiParam const * feiParam = (mfxExtFeiParam*)GetExtBuffer(video);
+    bool isFeiENCPAK = feiParam && (feiParam->Func == MFX_FEI_FUNCTION_ENCPAK);
 
     for (mfxU32 i = 0; i < ctrl->NumExtParam; i++)
         MFX_CHECK_NULL_PTR1(ctrl->ExtParam[i]);
@@ -5331,7 +5378,8 @@ mfxStatus MfxHwH264Encode::CheckRunTimeExtBuffers(
                 ctrl->NumExtParam - i - 1,
                 ctrl->ExtParam[i]->BufferId) != 0)
             {
-                if (!(ctrl->ExtParam[i]->BufferId == MFX_EXTBUFF_AVC_REFLISTS && video.mfx.FrameInfo.PicStruct != MFX_PICSTRUCT_PROGRESSIVE))
+                if ((!(ctrl->ExtParam[i]->BufferId == MFX_EXTBUFF_AVC_REFLISTS && video.mfx.FrameInfo.PicStruct != MFX_PICSTRUCT_PROGRESSIVE)) &&
+                     !isFeiENCPAK)
                 {
                     // if buffer is attached twice, ignore second one and return warning
                     // the only exception is MFX_EXTBUFF_AVC_REFLISTS (it can be attached twice for interlace case)
@@ -6294,6 +6342,11 @@ void MfxVideoParam::Construct(mfxVideoParam const & par)
     if (type * opts = GetExtBuffer(par))        \
         name = *opts;                           \
     m_extParam[NumExtParam++] = &name.Header;
+#define CONSTRUCT_EXT_BUFFER_EX(type, name, field)        \
+    InitExtBufHeader(name[field]);                     \
+    if (type * opts = GetExtBuffer(par, field))        \
+    name[field] = *opts;                           \
+    m_extParam[NumExtParam++] = &name[field].Header;
 
     CONSTRUCT_EXT_BUFFER(mfxExtCodingOption,         m_extOpt);
     CONSTRUCT_EXT_BUFFER(mfxExtCodingOptionSPSPPS,   m_extOptSpsPps);
@@ -6320,7 +6373,10 @@ void MfxVideoParam::Construct(mfxVideoParam const & par)
     CONSTRUCT_EXT_BUFFER(mfxExtDirtyRect,            m_extDirtyRect);
     CONSTRUCT_EXT_BUFFER(mfxExtMoveRect,             m_extMoveRect);
     CONSTRUCT_EXT_BUFFER(mfxExtFeiCodingOption,      m_extFeiOpt);
+    CONSTRUCT_EXT_BUFFER_EX(mfxExtFeiSliceHeader,   m_extFeiSlice, 0);
+    CONSTRUCT_EXT_BUFFER_EX(mfxExtFeiSliceHeader,   m_extFeiSlice, 1);
 #undef CONSTRUCT_EXT_BUFFER
+#undef CONSTRUCT_EXT_BUFFER_EX
 
     ExtParam = m_extParam;
     assert(NumExtParam == mfxU16(sizeof m_extParam / sizeof m_extParam[0]));
@@ -7942,7 +7998,45 @@ mfxU32 HeaderPacker::WriteSlice(
             pps.weightedBipredIdc == 1 && sliceType == SLICE_TYPE_B)
         {
             mfxU32 chromaArrayType = sps.separateColourPlaneFlag ? 0 : sps.chromaFormatIdc;
-            WritePredWeightTable(obs, m_hwCaps, task, fieldId, chromaArrayType);
+            mfxI32 numRefIdxL0ActiveMinus1 = IPP_MAX(1, task.m_list0[fieldId].Size()) - 1;
+            //mfxI32 numRefIdxL1ActiveMinus1 = IPP_MAX(1, task.m_list1[fieldId].Size()) - 1;
+
+            if(sliceType == SLICE_TYPE_P)
+            {
+                obs.PutUe(task.m_lumaLog2WeightDenom[fieldId]);
+                if( chromaArrayType != 0 )
+                {
+                    obs.PutUe(task.m_chromaLog2WeightDenom[fieldId]);
+                }
+
+                for(int i=0; i<=numRefIdxL0ActiveMinus1; i++)
+                {
+                    const mfxU8 lumaFlag = task.m_weightTab[fieldId][i].m_lumaWeightL0Flag;
+                    obs.PutBit(lumaFlag);
+                    if( lumaFlag )
+                    {
+                        obs.PutSe(task.m_weightTab[fieldId][i].m_lumaWeightL0);
+                        obs.PutSe(task.m_weightTab[fieldId][i].m_lumaOffsetL0);
+                    }
+                    if( chromaArrayType != 0 )
+                    {
+                        const mfxU8 chromaFlag = task.m_weightTab[fieldId][i].m_chromaWeightL0Flag;
+                        obs.PutBit(chromaFlag);
+                        if( chromaFlag )
+                        {
+                            for(int j=0; j<2; j++)
+                            {
+                                obs.PutSe(task.m_weightTab[fieldId][i].m_chromaWeightL0[j]);
+                                obs.PutSe(task.m_weightTab[fieldId][i].m_chromaOffsetL0[j]);
+                            }
+                        }
+                    }
+                }
+            }
+            else if( sliceType == SLICE_TYPE_B)
+            {
+                assert(!"explicit weighted prediction for B slices is unsupported");
+            }
         }
         if (refPicFlag || task.m_nalRefIdc[fieldId])
         {
@@ -7957,9 +8051,9 @@ mfxU32 HeaderPacker::WriteSlice(
     obs.PutSe(task.m_cqpValue[fieldId] - (pps.picInitQpMinus26 + 26));
     if (pps.deblockingFilterControlPresentFlag)
     {
-        mfxU32 disableDeblockingFilterIdc = task.m_disableDeblockingIdc;
-        mfxI32 sliceAlphaC0OffsetDiv2     = 0;
-        mfxI32 sliceBetaOffsetDiv2        = 0;
+        mfxU32 disableDeblockingFilterIdc = task.m_disableDeblockingIdc[fieldId][sliceId];
+        mfxI32 sliceAlphaC0OffsetDiv2     = task.m_sliceAlphaC0OffsetDiv2[fieldId][sliceId];
+        mfxI32 sliceBetaOffsetDiv2        = task.m_sliceBetaOffsetDiv2[fieldId][sliceId];
 
         obs.PutUe(disableDeblockingFilterIdc);
         if (disableDeblockingFilterIdc != 1)
@@ -8153,7 +8247,45 @@ mfxU32 HeaderPacker::WriteSlice(
             pps.weightedBipredIdc == 1 && sliceType == SLICE_TYPE_B)
         {
             mfxU32 chromaArrayType = sps.separateColourPlaneFlag ? 0 : sps.chromaFormatIdc;
-            WritePredWeightTable(obs, m_hwCaps, task, fieldId, chromaArrayType);
+            mfxI32 numRefIdxL0ActiveMinus1 = IPP_MAX(1, task.m_list0[fieldId].Size()) - 1;
+            //mfxI32 numRefIdxL1ActiveMinus1 = IPP_MAX(1, task.m_list1[fieldId].Size()) - 1;
+
+            if(sliceType == SLICE_TYPE_P)
+            {
+                obs.PutUe(task.m_lumaLog2WeightDenom[fieldId]);
+                if( chromaArrayType != 0 )
+                {
+                    obs.PutUe(task.m_chromaLog2WeightDenom[fieldId]);
+                }
+
+                for(int i=0; i<=numRefIdxL0ActiveMinus1; i++)
+                {
+                    const mfxU8 lumaFlag = task.m_weightTab[fieldId][i].m_lumaWeightL0Flag;
+                    obs.PutBit(lumaFlag);
+                    if( lumaFlag )
+                    {
+                        obs.PutSe(task.m_weightTab[fieldId][i].m_lumaWeightL0);
+                        obs.PutSe(task.m_weightTab[fieldId][i].m_lumaOffsetL0);
+                    }
+                    if( chromaArrayType != 0 )
+                    {
+                        const mfxU8 chromaFlag = task.m_weightTab[fieldId][i].m_chromaWeightL0Flag;
+                        obs.PutBit(chromaFlag);
+                        if( chromaFlag )
+                        {
+                            for(int j=0; j<2; j++)
+                            {
+                                obs.PutSe(task.m_weightTab[fieldId][i].m_chromaWeightL0[j]);
+                                obs.PutSe(task.m_weightTab[fieldId][i].m_chromaOffsetL0[j]);
+                            }
+                        }
+                    }
+                }
+            }
+            else if( sliceType == SLICE_TYPE_B)
+            {
+                assert(!"explicit weighted prediction for B slices is unsupported");
+            }
         }
         if (refPicFlag)
         {
@@ -8168,9 +8300,13 @@ mfxU32 HeaderPacker::WriteSlice(
     obs.PutSe(task.m_cqpValue[fieldId] - (pps.picInitQpMinus26 + 26));
     if (pps.deblockingFilterControlPresentFlag)
     {
-        mfxU32 disableDeblockingFilterIdc = task.m_disableDeblockingIdc;
-        mfxI32 sliceAlphaC0OffsetDiv2     = 0;
-        mfxI32 sliceBetaOffsetDiv2        = 0;
+        // task.m_disableDeblockingIdc[fieldId] is initialized for fixed number of slices
+        // it can't be used to feed slice header generation for adaptive slice mode
+        // always use slice id 0
+        // the same for task.m_sliceAlphaC0OffsetDiv2 and task.m_sliceBetaOffsetDiv2
+        mfxU32 disableDeblockingFilterIdc = task.m_disableDeblockingIdc[fieldId][0];
+        mfxI32 sliceAlphaC0OffsetDiv2     = task.m_sliceAlphaC0OffsetDiv2[fieldId][0];
+        mfxI32 sliceBetaOffsetDiv2        = task.m_sliceBetaOffsetDiv2[fieldId][0];
 
         obs.PutUe(disableDeblockingFilterIdc);
         if (disableDeblockingFilterIdc != 1)

_studio/mfx_lib/encode_hw/h264/include/mfx_h264_encode_hw_utils.h
@@ -905,7 +905,6 @@ namespace MfxHwH264Encode
             , m_timeStamp(0)
             , m_minQP(0)
             , m_maxQP(0)
-            , m_disableDeblockingIdc(0)
             , m_resetBRC(false)
 
             , m_idxMBQP(NO_INDEX)
@@ -958,6 +957,11 @@ namespace MfxHwH264Encode
         Pair<ArrayRefListMod> m_refPicList1Mod;
         Pair<mfxU32>          m_initSizeList0;
         Pair<mfxU32>          m_initSizeList1;
+
+        //weight table support
+        Pair<mfxU8> m_lumaLog2WeightDenom;
+        Pair<mfxU8> m_chromaLog2WeightDenom;
+        Pair<ArrayWeightTab>  m_weightTab;
 
         // currently used for dpb control when svc temporal layers enabled
         mfxExtAVCRefListCtrl  m_internalListCtrl;
@@ -1059,7 +1063,9 @@ namespace MfxHwH264Encode
 
         mfxU8   m_minQP;
         mfxU8   m_maxQP;
-        mfxU8   m_disableDeblockingIdc;
+        std::vector<mfxU8>   m_disableDeblockingIdc[2];
+        std::vector<mfxI8>   m_sliceAlphaC0OffsetDiv2[2];
+        std::vector<mfxI8>   m_sliceBetaOffsetDiv2[2];
 
         bool m_resetBRC;
 
@@ -2010,6 +2016,7 @@ namespace MfxHwH264Encode
         eMFXVAType  m_currentVaType;
         bool        m_useWAForHighBitrates;  // FIXME: w/a for SNB issue with HRD at high bitrates
         bool        m_isENCPAK;
+        DdiTask     m_lastFeiTask;
         bool        m_isWiDi;
         bool        m_resetBRC;
 

_studio/mfx_lib/encode_hw/h264/src/mfx_h264_encode_hw.cpp
@@ -739,11 +739,24 @@ mfxStatus ImplementationAvc::Init(mfxVideoParam * par)
 
     mfxExtFeiParam* feiParam = (mfxExtFeiParam*)GetExtBuffer(par->ExtParam, par->NumExtParam, MFX_EXTBUFF_FEI_PARAM);
     m_isENCPAK = feiParam && (feiParam->Func == MFX_FEI_FUNCTION_ENCPAK);
-    if(m_isENCPAK){
-        //force rc to cqp only
-        par->mfx.RateControlMethod = MFX_RATECONTROL_CQP;
+
+    m_video = *par;
+    /* FEI works with CQP only */
+    if ((m_isENCPAK) && (MFX_RATECONTROL_CQP != par->mfx.RateControlMethod)){
+        m_video.mfx.RateControlMethod =  MFX_RATECONTROL_CQP;
+    }
+    /* One more checking for FEI */
+    if ((m_isENCPAK) &&
+        ((MFX_PROFILE_AVC_BASELINE == m_video.mfx.CodecProfile) ||(MFX_PROFILE_AVC_MAIN == m_video.mfx.CodecProfile)) )
+    {
+        mfxExtCodingOption *       extOpt  = GetExtBuffer(m_video);
+        extOpt->InterPredBlockSize = MFX_BLOCKSIZE_MIN_16X16;
     }
 
+    /*!!! This is WA for number of references frames for FEI case*/
+    if ((m_isENCPAK) && (par->mfx.NumRefFrame >4))
+        m_video.mfx.NumRefFrame = 4;
+    
     m_video = *par;
     sts = ReadSpsPpsHeaders(m_video);
     MFX_CHECK_STS(sts);
@@ -1155,6 +1168,24 @@ mfxStatus ImplementationAvc::Init(mfxVideoParam * par)
         m_bestGOPCost[i] = MAX_SEQUENCE_COST;
     }
 #endif
+
+    if (m_isENCPAK)
+    {
+        // In case of FEI ENCPAK only stream level control is supported 
+        // for slice header parameters. The application must provide exactly the same
+        // number for slices to the number of slices specified during initialization
+        mfxU32 fieldCount = m_video.mfx.FrameInfo.PicStruct == MFX_PICSTRUCT_PROGRESSIVE ? 1 : 2;
+        for (mfxU32 i = 0; i < fieldCount; i++)
+        {
+            mfxExtFeiSliceHeader *pDataSliceHeader = GetExtBuffer(m_video, i);
+            if (NULL != pDataSliceHeader->Slice)
+            {
+                if ((pDataSliceHeader->NumSlice != pDataSliceHeader->NumSliceAlloc)
+                    || (GetMaxNumSlices(m_video) != pDataSliceHeader->NumSlice))
+                    return MFX_ERR_INVALID_VIDEO_PARAM;
+            }
+        }
+    }
 
     m_videoInit = m_video;
 
@@ -2094,6 +2125,20 @@ mfxStatus ImplementationAvc::AsyncRoutine(mfxBitstream * bs)
         m_stagesToGo = m_emulatorForAsyncPart.Go(!m_incoming.empty());
     }
 
+
+    mfxExtFeiParam const * extFeiParams = GetExtBuffer(m_video, 0);
+    mfxU32 stagesToGo = 0;
+    if ((MFX_CODINGOPTION_ON == extFeiParams->SingleFieldProcessing ) && (1 == m_fieldCounter))
+    {
+        stagesToGo = m_stagesToGo;
+        /* Coding last field in FEI Field processing mode
+        * task is ready, just need to call m_ddi->Execute() and
+        * m_ddi->QueryStatus()... And free encoding task
+        */
+        m_stagesToGo = AsyncRoutineEmulator::STG_BIT_RESTART*2;
+    }
+
+
 #if USE_AGOP
 #if 0
     fprintf(stderr,"num_calls: %d ", numCall);
@@ -2630,6 +2675,26 @@ mfxStatus ImplementationAvc::AsyncRoutine(mfxBitstream * bs)
             mfxStatus sts = m_ddi->Execute(task->m_handleRaw.first, *task, task->m_fid[f], m_sei);
             if (sts != MFX_ERR_NONE)
                 return Error(sts);
+            /* FEI Field processing mode: store first field */
+            if ((MFX_CODINGOPTION_ON == extFeiParams->SingleFieldProcessing)&& (0 == m_fieldCounter))
+            {
+                m_fieldCounter = 1;
+                m_lastFeiTask = *task;
+
+                task->m_bsDataLength[0] = task->m_bsDataLength[1] = 0;
+
+                if ((sts = QueryStatus(*task, task->m_fid[f])) != MFX_ERR_NONE)
+                    return sts;
+
+                if ((NULL == task->m_bs) && (bs != NULL))
+                    task->m_bs = bs;
+
+                if ((sts = UpdateBitstream(*task, task->m_fid[f])) != MFX_ERR_NONE)
+                    return Error(sts);
+                /*DO NOT submit second filed for execution in this case
+                 * (FEI Field processing mode)*/
+                break;
+            }
         }
 
         //printf("\rENC_SUBMITTED do=%4d eo=%4d type=%d\n", task->m_frameOrder, task->m_encOrder, task->m_type[0]); fflush(stdout);
@@ -2827,19 +2892,29 @@ mfxStatus ImplementationAvc::AsyncRoutine(mfxBitstream * bs)
         }
         else if (IsOff(extOpt->FieldOutput))
         {
-            for (mfxU32 f = 0; f <= task->m_fieldPicFlag; f++)
-            {
-                if ((sts = QueryStatus(*task, task->m_fid[f])) != MFX_ERR_NONE)
-                    return sts;
-            }
-            task->m_bs = bs;
-            for (mfxU32 f = 0; f <= task->m_fieldPicFlag; f++)
-            {
+            mfxU32 f = 0;
+            mfxU32 f_start = 0;
+            mfxU32 f_end = task->m_fieldPicFlag;
 
-                if ((sts = UpdateBitstream(*task, task->m_fid[f])) != MFX_ERR_NONE)
-                    return Error(sts);
-            }
-            OnEncodingQueried(task);
+            /* Query results if NO FEI Field processing mode (this is legacy encoding) */
+            if ((MFX_CODINGOPTION_OFF == extFeiParams->SingleFieldProcessing) ||
+                (MFX_CODINGOPTION_UNKNOWN == extFeiParams->SingleFieldProcessing))
+            {
+                for (f = f_start; f <= f_end; f++)
+                {
+                    if ((sts = QueryStatus(*task, task->m_fid[f])) != MFX_ERR_NONE)
+                        return sts;
+                }
+                task->m_bs = bs;
+                for (f = f_start; f <= f_end; f++)
+                {
+
+                    if ((sts = UpdateBitstream(*task, task->m_fid[f])) != MFX_ERR_NONE)
+                        return Error(sts);
+                }
+
+                OnEncodingQueried(task);
+            } // if (MFX_CODINGOPTION_OFF == extFeiParams->SingleFieldProcessing)
         }
         else
         {
@@ -2860,9 +2935,40 @@ mfxStatus ImplementationAvc::AsyncRoutine(mfxBitstream * bs)
                 m_listOfPairsForStupidFieldOutputMode.pop_front();
                 m_listOfPairsForStupidFieldOutputMode.pop_front();
             }       
-        
         }
     }
+
+    /* FEI Field processing mode: second (last) field processing */
+    if ( ((AsyncRoutineEmulator::STG_BIT_RESTART*2) == m_stagesToGo ) &&
+         (MFX_CODINGOPTION_ON== extFeiParams->SingleFieldProcessing) &&
+         (1 == m_fieldCounter) )
+    {
+        std::list<DdiTask>  tempTaskList;
+        tempTaskList.push_back(m_lastFeiTask);
+        DdiTaskIter task = FindFrameToWaitEncode(tempTaskList.begin(), tempTaskList.end());
+        mfxU32 f = 1; // coding second field
+        PrepareSeiMessageBuffer(m_video, *task, task->m_fid[f], m_sei);
+
+        mfxStatus sts = m_ddi->Execute(task->m_handleRaw.first, *task, task->m_fid[f], m_sei);
+        if (sts != MFX_ERR_NONE)
+            return Error(sts);
+
+        task->m_bsDataLength[0] = task->m_bsDataLength[1] = 0;
+
+        if ((sts = QueryStatus(*task, task->m_fid[f])) != MFX_ERR_NONE)
+            return sts;
+
+        if ((NULL == task->m_bs) && (bs != NULL))
+            task->m_bs = bs;
+
+        if ((sts = UpdateBitstream(*task, task->m_fid[f])) != MFX_ERR_NONE)
+            return Error(sts);
+
+        m_fieldCounter = 0;
+        m_stagesToGo = stagesToGo;
+        OnEncodingQueried(task);
+    } // if (( (AsyncRoutineEmulator::STG_BIT_RESTART*2) == m_stagesToGo ) &&
+
 
     if (m_stagesToGo & AsyncRoutineEmulator::STG_BIT_RESTART)
     {
@@ -2989,6 +3095,26 @@ mfxStatus ImplementationAvc::EncodeFrameCheckNormalWay(
         return checkSts;
 
     mfxStatus status = checkSts;
+
+    mfxExtFeiParam const * extFeiParams = GetExtBuffer(m_video, 0);
+    /* If (1): encoder in state "FEI field processing mode"
+     * and (2): first field is coded already
+     * scheduler can start execution immediately as task stored and ready
+     * */
+    if ((MFX_CODINGOPTION_ON == extFeiParams->SingleFieldProcessing ) && (1 == m_fieldCounter))
+    {
+        entryPoints[0].pState               = this;
+        entryPoints[0].pParam               = bs;
+        entryPoints[0].pCompleteProc        = 0;
+        entryPoints[0].pGetSubTaskProc      = 0;
+        entryPoints[0].pCompleteSubTaskProc = 0;
+        entryPoints[0].requiredNumThreads   = 1;
+        entryPoints[0].pRoutineName         = "AsyncRoutine";
+        entryPoints[0].pRoutine             = AsyncRoutineHelper;
+        numEntryPoints = 1;
+
+        return status;
+    }
 
     {
         UMC::AutomaticUMCMutex guard(m_listMutex);

commit 058e5c7fc629796fff0702289f044f897d395da6

[AVCe] Fixed pred. weights packing broken by ed89953a3c5c147bccc7d61196b4b5f26e3d819a.

_studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -8616,45 +8616,7 @@ mfxU32 HeaderPacker::WriteSlice(
             (pps.weightedBipredIdc == 1 && sliceType == SLICE_TYPE_B))
         {
             mfxU32 chromaArrayType = sps.separateColourPlaneFlag ? 0 : sps.chromaFormatIdc;
-            mfxI32 numRefIdxL0ActiveMinus1 = IPP_MAX(1, task.m_list0[fieldId].Size()) - 1;
-            //mfxI32 numRefIdxL1ActiveMinus1 = IPP_MAX(1, task.m_list1[fieldId].Size()) - 1;
-
-            if(sliceType == SLICE_TYPE_P)
-            {
-                obs.PutUe(task.m_lumaLog2WeightDenom[fieldId]);
-                if( chromaArrayType != 0 )
-                {
-                    obs.PutUe(task.m_chromaLog2WeightDenom[fieldId]);
-                }
-
-                for(int i=0; i<=numRefIdxL0ActiveMinus1; i++)
-                {
-                    const mfxU8 lumaFlag = task.m_weightTab[fieldId][i].m_lumaWeightL0Flag;
-                    obs.PutBit(lumaFlag);
-                    if( lumaFlag )
-                    {
-                        obs.PutSe(task.m_weightTab[fieldId][i].m_lumaWeightL0);
-                        obs.PutSe(task.m_weightTab[fieldId][i].m_lumaOffsetL0);
-                    }
-                    if( chromaArrayType != 0 )
-                    {
-                        const mfxU8 chromaFlag = task.m_weightTab[fieldId][i].m_chromaWeightL0Flag;
-                        obs.PutBit(chromaFlag);
-                        if( chromaFlag )
-                        {
-                            for(int j=0; j<2; j++)
-                            {
-                                obs.PutSe(task.m_weightTab[fieldId][i].m_chromaWeightL0[j]);
-                                obs.PutSe(task.m_weightTab[fieldId][i].m_chromaOffsetL0[j]);
-                            }
-                        }
-                    }
-                }
-            }
-            else if( sliceType == SLICE_TYPE_B)
-            {
-                assert(!"explicit weighted prediction for B slices is unsupported");
-            }
+            WritePredWeightTable(obs, m_hwCaps, task, fieldId, chromaArrayType);
         }
         if (refPicFlag || task.m_nalRefIdc[fieldId])
         {
@@ -8872,45 +8834,7 @@ mfxU32 HeaderPacker::WriteSlice(
             (pps.weightedBipredIdc == 1 && sliceType == SLICE_TYPE_B))
         {
             mfxU32 chromaArrayType = sps.separateColourPlaneFlag ? 0 : sps.chromaFormatIdc;
-            mfxI32 numRefIdxL0ActiveMinus1 = IPP_MAX(1, task.m_list0[fieldId].Size()) - 1;
-            //mfxI32 numRefIdxL1ActiveMinus1 = IPP_MAX(1, task.m_list1[fieldId].Size()) - 1;
-
-            if(sliceType == SLICE_TYPE_P)
-            {
-                obs.PutUe(task.m_lumaLog2WeightDenom[fieldId]);
-                if( chromaArrayType != 0 )
-                {
-                    obs.PutUe(task.m_chromaLog2WeightDenom[fieldId]);
-                }
-
-                for(int i=0; i<=numRefIdxL0ActiveMinus1; i++)
-                {
-                    const mfxU8 lumaFlag = task.m_weightTab[fieldId][i].m_lumaWeightL0Flag;
-                    obs.PutBit(lumaFlag);
-                    if( lumaFlag )
-                    {
-                        obs.PutSe(task.m_weightTab[fieldId][i].m_lumaWeightL0);
-                        obs.PutSe(task.m_weightTab[fieldId][i].m_lumaOffsetL0);
-                    }
-                    if( chromaArrayType != 0 )
-                    {
-                        const mfxU8 chromaFlag = task.m_weightTab[fieldId][i].m_chromaWeightL0Flag;
-                        obs.PutBit(chromaFlag);
-                        if( chromaFlag )
-                        {
-                            for(int j=0; j<2; j++)
-                            {
-                                obs.PutSe(task.m_weightTab[fieldId][i].m_chromaWeightL0[j]);
-                                obs.PutSe(task.m_weightTab[fieldId][i].m_chromaOffsetL0[j]);
-                            }
-                        }
-                    }
-                }
-            }
-            else if( sliceType == SLICE_TYPE_B)
-            {
-                assert(!"explicit weighted prediction for B slices is unsupported");
-            }
+            WritePredWeightTable(obs, m_hwCaps, task, fieldId, chromaArrayType);
         }
         if (refPicFlag)
         {