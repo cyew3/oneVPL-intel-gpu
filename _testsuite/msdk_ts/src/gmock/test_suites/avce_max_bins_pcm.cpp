0fce80714a2a08298faf2442750405710be43e25
Ryabinin, Sergey <sergey.ryabinin@intel.com>
2017-02-16 04:06:45 -0800

    Correct error message (for better error message parsing) for AVC encode max bins test
    Change-Id: I82e300318edff2e4a7ea21bcd694b24cdb6336eb

--- _testsuite/msdk_ts/src/gmock/test_suites/avce_max_bins_pcm.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/avce_max_bins_pcm.cpp
@@ -4,7 +4,7 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2014-2016 Intel Corporation. All Rights Reserved.
+Copyright(c) 2014-2017 Intel Corporation. All Rights Reserved.
 
 \* ****************************************************************************** */
 
@@ -153,8 +153,8 @@ mfxStatus BitstreamChecker::ProcessBitstream(mfxBitstream& bs, mfxU32 nFrames)
                 {
                     if (mb.NumBits > 128 + RawMbBits / max_bits_per_mb_denom) {
                         if (check_max_bins) {
-                            g_tsLog << "ERROR: ";
                             EXPECT_LE( mb.NumBits, 128 + RawMbBits / max_bits_per_mb_denom );
+                            g_tsLog << "ERROR: ";
                         } else {
                             g_tsLog << "ERROR_SKIPPED: ";
                         }
