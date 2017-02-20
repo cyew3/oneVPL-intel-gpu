8d1d578f9e1c84b447bdf8bd7130f20cb9fbae8c
Dmitry Ermilov <dmitry.ermilov@intel.com>
2017-02-20 15:29:18 +0300

    [beh] Fixed avce_co2_use_rawref
    Assignment operator was used instead of the equality operator.
    
    MDP-32674
    
    Change-Id: I6a06e6fa02810d3371ddc9e7fc1333175c7d0bef
    Signed-off-by: Dmitry Ermilov <dmitry.ermilov@intel.com>

--- _testsuite/msdk_ts/src/gmock/test_suites/avce_co2_use_rawref.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/avce_co2_use_rawref.cpp
@@ -4,7 +4,7 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2014-2016 Intel Corporation. All Rights Reserved.
+Copyright(c) 2014-2017 Intel Corporation. All Rights Reserved.
 
 \* ****************************************************************************** */
 
@@ -185,7 +185,7 @@ int TestSuite::RunTest(unsigned int id)
     mfxVideoParam pars = {0};
     memcpy(&pars, m_pPar, sizeof(mfxVideoParam));
 
-    if ((m_par.mfx.LowPower = MFX_CODINGOPTION_ON) && (m_par.mfx.GopRefDist > 1))
+    if ((MFX_CODINGOPTION_ON == m_par.mfx.LowPower) && (m_par.mfx.GopRefDist > 1))
     {
         g_tsLog << "WARNING: CASE WAS SKIPPED\n";
     }
