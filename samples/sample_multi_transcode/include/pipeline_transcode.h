cyew3/oneVPL-intel-gpu

commit 0e1f8c7d283f0d3c7d763b1668e5a91898ae7ccf

samples KW fixes

samples/sample_multi_transcode/include/pipeline_transcode.h
@@ -283,7 +283,7 @@ namespace TranscodingSample
               : CTimeStatistics()
               , ofile(stdout)
             {
-                msdk_strcopy(bufDir, dir);
+                msdk_strncopy_s(bufDir, dir, MAX_PREF_LEN-1);
             }
 
             ~CIOStat()
@@ -298,7 +298,9 @@ namespace TranscodingSample
             inline void SetDirection(const msdk_char *dir)
             {
                 if (dir)
-                    msdk_strcopy(bufDir, dir);
+                {
+                    msdk_strncopy_s(bufDir, dir,MAX_PREF_LEN-1);
+                }
             }
 
             inline void PrintStatistics(mfxU32 numPipelineid, mfxF64 target_framerate = -1 /*default stands for infinite*/)

samples/sample_vpp/src/sample_vpp_utils.cpp
@@ -1822,7 +1822,7 @@ mfxStatus GeneralWriter::Init(
             }
 #else
             {
-                msdk_strcopy(fname,strFileName);
+                msdk_strncopy_s(fname,strFileName, MSDK_MAX_FILENAME_LEN-1);
                 char* pFound = strrchr(fname,'.');
                 if(pFound)
                 {

samples/sample_camera/include/pipeline_camera.h
@@ -1,5 +1,5 @@
 /******************************************************************************\
-Copyright (c) 2005-2015, Intel Corporation
+Copyright (c) 2005-2017, Intel Corporation
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:

samples/sample_common/include/abstract_splitter.h
@@ -1,5 +1,5 @@
 /******************************************************************************\
-Copyright (c) 2005-2015, Intel Corporation
+Copyright (c) 2005-2017, Intel Corporation
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:

samples/sample_common/include/avc_spl.h
@@ -1,5 +1,5 @@
 /******************************************************************************\
-Copyright (c) 2005-2015, Intel Corporation
+Copyright (c) 2005-2017, Intel Corporation
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:

samples/sample_common/include/current_date.h
@@ -1,5 +1,5 @@
 /******************************************************************************\
-Copyright (c) 2005-2015, Intel Corporation
+Copyright (c) 2005-2017, Intel Corporation
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:

samples/sample_common/include/intrusive_ptr.h
@@ -1,5 +1,5 @@
 /******************************************************************************\
-Copyright (c) 2005-2015, Intel Corporation
+Copyright (c) 2005-2017, Intel Corporation
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:

samples/sample_common/include/mfx_plugin_base.h
@@ -1,5 +1,5 @@
 /******************************************************************************\
-Copyright (c) 2005-2015, Intel Corporation
+Copyright (c) 2005-2017, Intel Corporation
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:

commit 58f615918c2ac80135074f782ba2ad6b3d5f77fe

Linux: fix compilation errors introduced by commit 0e1f8c7d283f0d3c7d763b1668e5a91898ae7ccf.
Change-Id: Ia41a2060a034bd6ce624ce7516ee76e527722743

samples/sample_multi_transcode/include/pipeline_transcode.h
@@ -283,7 +283,7 @@ namespace TranscodingSample
               : CTimeStatistics()
               , ofile(stdout)
             {
-                msdk_strncopy_s(bufDir, dir, MAX_PREF_LEN-1);
+                msdk_strncopy_s(bufDir, MAX_PREF_LEN, dir, MAX_PREF_LEN - 1);
             }
 
             ~CIOStat()
@@ -299,7 +299,7 @@ namespace TranscodingSample
             {
                 if (dir)
                 {
-                    msdk_strncopy_s(bufDir, dir,MAX_PREF_LEN-1);
+                    msdk_strncopy_s(bufDir, MAX_PREF_LEN, dir, MAX_PREF_LEN - 1);
                 }
             }
 

samples/sample_vpp/src/sample_vpp_utils.cpp
@@ -1822,7 +1822,7 @@ mfxStatus GeneralWriter::Init(
             }
 #else
             {
-                msdk_strncopy_s(fname,strFileName, MSDK_MAX_FILENAME_LEN-1);
+                msdk_strncopy_s(fname, MSDK_MAX_FILENAME_LEN, strFileName, MSDK_MAX_FILENAME_LEN - 1);
                 char* pFound = strrchr(fname,'.');
                 if(pFound)
                 {