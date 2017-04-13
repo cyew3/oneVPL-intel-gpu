a286bc0137c2c892d20ec12d1d91be4664330b1d
Baygulov, Aleksei <aleksei.baygulov@intel.com>
2017-04-13 12:16:44 +0300

    [tools] ignore seek result
    UMC File reader always try to get file size.
    But it impossible for pipes.
    
    Issue: N/A
    Test: N/A
    
    Change-Id: Ib1449ba3069a0cc612bb3551116f42522c980244

--- _studio/shared/umc/io/umc_io/src/umc_fio_reader.cpp
+++ _studio/shared/umc/io/umc_io/src/umc_fio_reader.cpp
@@ -5,7 +5,7 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2003-2016 Intel Corporation. All Rights Reserved.
+// Copyright(C) 2003-2017 Intel Corporation. All Rights Reserved.
 //
 
 #include "umc_defs.h"
@@ -33,139 +33,114 @@ UMC::Status UMC::FIOReader::Init(DataReaderParams *pInit)
     FileReaderParams* pParams = DynamicCast<FileReaderParams, DataReaderParams>(pInit);
 
     if (NULL == pParams)
-    {   umcRes = UMC_ERR_NULL_PTR;  }
+        return UMC_ERR_NULL_PTR;
 
-    if (UMC_OK == umcRes)
-    {
-        m_pFile = vm_file_open(pParams->m_file_name, VM_STRING("rb"));
-        if (NULL == m_pFile)
-        {   umcRes = UMC_ERR_FAILED;  }
-    }
+    m_pFile = vm_file_open(pParams->m_file_name, VM_STRING("rb"));
+    if (NULL == m_pFile)
+        return UMC_ERR_FAILED;
 
-    if (UMC_OK == umcRes)
-    {
-        if (0 != vm_file_fseek(m_pFile, 0, VM_FILE_SEEK_END))
-        {   umcRes = UMC_ERR_FAILED;  }
-    }
-
-    if (UMC_OK == umcRes)
+    if (0 == vm_file_fseek(m_pFile, 0, VM_FILE_SEEK_END))
     {
         Ipp64u lRes = vm_file_ftell(m_pFile);
-        if (-1 == static_cast<int>(lRes))
-        {   umcRes = UMC_ERR_FAILED;  }
-        else
-        {   m_stFileSize = (Ipp64u)lRes;    }
+        if (-1 != static_cast<int>(lRes))
+        {
+            m_stFileSize = (Ipp64u)lRes;
+        }
+
+        if (0 != vm_file_fseek(m_pFile, 0, VM_FILE_SEEK_SET))
+        {
+            return UMC_ERR_FAILED;
+        }
     }
 
-    if (UMC_OK == umcRes)
-    {
-        if (0 != vm_file_fseek(m_pFile, 0, VM_FILE_SEEK_SET))
-        {   umcRes = UMC_ERR_FAILED;  }
-    }
     return umcRes;
 }
 
 UMC::Status UMC::FIOReader::Close()
 {
     if (NULL != m_pFile)
-    {   vm_file_fclose(m_pFile);  m_pFile = NULL; }
+    {
+        vm_file_fclose(m_pFile);
+        m_pFile = NULL;
+    }
+
     m_stFileSize = 0;
     m_stPos = 0;
-
     return UMC_OK;
 }
 
 UMC::Status UMC::FIOReader::Reset()
 {
-    UMC::Status umcRes = UMC_OK;
+    if (!m_pFile)
+        return UMC_ERR_NOT_INITIALIZED;
 
-    if (UMC_OK == umcRes && NULL == m_pFile)
-    {   umcRes = UMC_ERR_NOT_INITIALIZED;   }
+    if (0 != vm_file_fseek(m_pFile, 0, VM_FILE_SEEK_SET))
+        return UMC_ERR_FAILED;
 
-    if (UMC_OK == umcRes)
-    {
-        if (0 != vm_file_fseek(m_pFile, 0, VM_FILE_SEEK_SET))
-        {   umcRes = UMC_ERR_FAILED;  }
-        else
-        {   m_stPos = 0;    }
-    }
-    return umcRes;
+    m_stPos = 0;
+    return UMC_OK;
 }
 
 UMC::Status UMC::FIOReader::ReadData(void *data, Ipp32u *nsize)
 {
-    Status  umcRes = UMC_OK;
+    if (!m_pFile)
+        return UMC_ERR_NOT_INITIALIZED;
 
-    if (UMC_OK == umcRes && NULL == m_pFile)
-    {   umcRes = UMC_ERR_NOT_INITIALIZED;   }
-
-    if (UMC_OK == umcRes)
+    size_t stRes = vm_file_fread(data, 1, *nsize, m_pFile);
+    if (stRes < *nsize)
     {
-        size_t stRes = vm_file_fread(data, 1, *nsize, m_pFile);
-        if (stRes < *nsize)
+        if (vm_file_feof(m_pFile))
         {
-            if (vm_file_feof(m_pFile))
-            {
-               *nsize = (Ipp32u) stRes;
-               umcRes = UMC_ERR_END_OF_STREAM;
-            }
-            else
-            {
-                *nsize = 0;
-                umcRes = UMC_ERR_FAILED;
-            }
+            *nsize = (Ipp32u) stRes;
+            return UMC_ERR_END_OF_STREAM;
+        }
+        else
+        {
+            *nsize = 0;
+            return UMC_ERR_FAILED;
         }
     }
 
-    if (UMC_OK == umcRes)
-    {   m_stPos = vm_file_ftell(m_pFile);   }
+    m_stPos = vm_file_ftell(m_pFile);
 
-    return umcRes;
+    return UMC_OK;
 }
 
 UMC::Status UMC::FIOReader::CacheData(void *data, Ipp32u *nsize, Ipp32s how_far)
 {
-    Status umcRes = UMC_OK;
     Ipp64u stInitialPos = m_stPos;
 
-    if (UMC_OK == umcRes && NULL == m_pFile)
-    {   return UMC_ERR_NOT_INITIALIZED;   }
+    if (!m_pFile)
+        return UMC_ERR_NOT_INITIALIZED;
 
-    if (UMC_OK == umcRes)
-    {
-        if (0 != vm_file_fseek(m_pFile, how_far, VM_FILE_SEEK_CUR))
-        {   umcRes = UMC_ERR_FAILED;  }
-    }
+    if (0 != vm_file_fseek(m_pFile, how_far, VM_FILE_SEEK_CUR))
+        return UMC_ERR_FAILED;
 
-    if (UMC_OK == umcRes)
-    {   umcRes = GetData(data, nsize);  }
+    Status umcRes = GetData(data, nsize);
 
     if (UMC_OK == umcRes || UMC_ERR_END_OF_STREAM == umcRes)
     {
         if (0 != vm_file_fseek(m_pFile, static_cast<long>(stInitialPos), VM_FILE_SEEK_SET))
-        {   umcRes = UMC_ERR_FAILED; }
+            return UMC_ERR_FAILED;
         else
-        {   m_stPos = stInitialPos; }
+            m_stPos = stInitialPos;
     }
     return umcRes;
 }
 
 UMC::Status UMC::FIOReader::MovePosition(Ipp64u npos)
 {
-    UMC::Status  umcRes = UMC_OK;
+    if (!m_pFile)
+        return UMC_ERR_NOT_INITIALIZED;
 
-    if (UMC_OK == umcRes && NULL == m_pFile)
-    {   umcRes = UMC_ERR_NOT_INITIALIZED;   }
-
-    if (UMC_OK == umcRes && 0 != npos)
+    if (0 != npos)
     {
         if (0 != vm_file_fseek(m_pFile, (long)npos, VM_FILE_SEEK_CUR))
-        {   umcRes = UMC_ERR_FAILED;  }
+            return UMC_ERR_FAILED;
 
-        if (UMC_OK == umcRes)
-        {   m_stPos = vm_file_ftell(m_pFile);   }
+        m_stPos = vm_file_ftell(m_pFile);
     }
-    return umcRes;
+    return UMC_OK;
 }
 
 Ipp64u UMC::FIOReader::GetPosition()
@@ -175,22 +150,16 @@ Ipp64u UMC::FIOReader::GetPosition()
 
 UMC::Status UMC::FIOReader::SetPosition(Ipp64f position)
 {
-    UMC::Status umcRes = UMC_OK;
+    if (!m_pFile)
+        return UMC_ERR_NOT_INITIALIZED;
 
-    if (UMC_OK == umcRes && NULL == m_pFile)
-    {   return UMC_ERR_NOT_INITIALIZED;   }
+    long lOffset = (long) (((Ipp64s) m_stFileSize) * position);
+    if (0 != vm_file_fseek(m_pFile, lOffset, VM_FILE_SEEK_SET))
+        return UMC_ERR_FAILED;
 
-    if (UMC_OK == umcRes)
-    {
-        long lOffset = (long) (((Ipp64s) m_stFileSize) * position);
-        if (0 != vm_file_fseek(m_pFile, lOffset, VM_FILE_SEEK_SET))
-        {   umcRes = UMC_ERR_FAILED;  }
-    }
+    m_stPos = vm_file_ftell(m_pFile);
 
-    if (UMC_OK == umcRes)
-    {   m_stPos = vm_file_ftell(m_pFile);   }
-
-    return umcRes;
+    return UMC_OK;
 }
 
 Ipp64u UMC::FIOReader::GetSize()
