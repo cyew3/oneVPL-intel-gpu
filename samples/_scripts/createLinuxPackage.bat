7a8566b5d1b115d25db5c74f2f7544dd249a0705
fzharino <Fedor.Zharinov@intel.com>
2017-05-24 10:31:34 +0300

    [samples]Linux packaging scripts
    Issue: N/A
    Test: packaged
    
    Change-Id: I36605cbe2a2037928909dbce28d16a541029905b

--- samples/_scripts/createLinuxPackage.bat
+++ samples/_scripts/createLinuxPackage.bat
@@ -1,23 +1,34 @@
 @echo off
 IF "%~1%"=="" (
 	echo Target Dir is not set
-	echo usage: createLinuxPackage.bat ^<TargetDir^> ^<BinarySourceDir^> ^<DocSourceDir^>
+	echo usage: createLinuxPackage.bat ^<TargetDir^> ^<BinarySourceDir^> ^<DocSourceDir^> ^<OpenCLSamplesSourceDir^>
 	exit /b
 )
 
 IF "%~2%"=="" (
 	echo Binary Source Dir is not set
-	echo usage: createLinuxPackage.bat ^<TargetDir^> ^<BinarySourceDir^> ^<DocSourceDir^>
+	echo usage: createLinuxPackage.bat ^<TargetDir^> ^<BinarySourceDir^> ^<DocSourceDir^> ^<OpenCLSamplesSourceDir^>
 	exit /b
 )
 
 IF "%~3%"=="" (
 	echo Docs Source Dir is not set
-	echo usage: createLinuxPackage.bat ^<TargetDir^> ^<BinarySourceDir^> ^<DocSourceDir^>
+	echo usage: createLinuxPackage.bat ^<TargetDir^> ^<BinarySourceDir^> ^<DocSourceDir^> ^<OpenCLSamplesSourceDir^>
 	exit /b
 )
 
-xcopy /S builder\*.* %1\builder\
+IF "%~4%"=="" (
+	echo OpenCL Samples Source Dir is not set
+	echo usage: createLinuxPackage.bat ^<TargetDir^> ^<BinarySourceDir^> ^<DocSourceDir^> ^<OpenCLSamplesSourceDir^>
+	exit /b
+)
+
+xcopy /S ..\..\builder\*.* %1\builder\
+erase %1\builder\open_source_symbols_undefine.list
+erase %1\builder\open_source_symbols_define.list
+erase %1\builder\open_source_symbols.list
+erase %1\builder\open_source_files.txt
+erase %1\builder\FindFunctions.cmake.ext 
 
 xcopy /S ..\*.c %1\samples\ /EXCLUDE:exclusions.txt
 xcopy /S ..\*.cl %1\samples\ /EXCLUDE:exclusions.txt
@@ -43,6 +54,7 @@ xcopy %2\sample_encode %1\samples\_bin\x64\*
 xcopy %2\sample_h265_gaa %1\samples\_bin\x64\*
 xcopy %2\sample_multi_transcode %1\samples\_bin\x64\*
 xcopy %2\sample_vpp %1\samples\_bin\x64\*
+xcopy %2\sample_fei %1\samples\_bin\x64\*
 xcopy %2\ocl_rotate.cl %1\samples\_bin\x64\*
 xcopy %2\libsample_plugin_opencl.so %1\samples\_bin\x64\*
 xcopy %2\libsample_rotate_plugin.so %1\samples\_bin\x64\*
@@ -55,8 +67,19 @@ move "%1\samples\Media_Samples_Guide.pdf" "%1\Media_Samples_Guide.pdf"
 #xcopy %3\..\..\site_license_materials.txt %1\
 xcopy %3\..\..\README %1\Samples\*
 
+xcopy /S %4\ocl_motion_estimation\* %1\samples\ocl_motion_estimation\*
+xcopy /S %4\ocl_motion_estimation_advanced\* %1\samples\ocl_motion_estimation_advanced\*
 
-xcopy /S content\* %1\samples\_bin\content\
+xcopy /S ..\content\* %1\samples\_bin\content\
+rem !!! Possibly need to remove this later
+erase %1\samples\_bin\content\test_stream_vp9.ivf
 
 
-echo !!!!!!!! DO NOT FORGET TO COPY OPENCL SAMPLES !!!!!!!!
+Set filename=%1
+For %%A in ("%filename%") do (
+    Set Folder=%%~dpA
+    Set Name=%%~nxA
+)
+"c:\Program Files\7-Zip\7z.exe" a -ttar -so %Name%.tar %1 | "c:\Program Files\7-Zip\7z.exe" a -tgzip -si %1.tar.gz
+
+rem echo !!!!!!!! DO NOT FORGET TO COPY OPENCL SAMPLES !!!!!!!!
