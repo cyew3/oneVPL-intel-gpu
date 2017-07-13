bf6385adb6a626864832c543621b21eaff8a77d8
Krivoruchko, GeorgyX <georgyx.krivoruchko@intel.com>
2017-07-13 21:40:00 +0300

    [CNL][HEVC] Enable SAO for non-CQP, but ExtBRC and LA
    Disabling SAO for non-CQP BRC until it not supports in drivers
    
    Issue: HSD 1406314088
    Change-Id: I0e8019c60d881d1d821e8df5dd91441c5b955f3f
    Test: hevce_tu_cmp-plugin_hevcehw

--- _studio/hevce_hw/h265/src/mfx_h265_encode_hw_par.cpp
+++ _studio/hevce_hw/h265/src/mfx_h265_encode_hw_par.cpp
@@ -1149,7 +1149,7 @@ inline bool isSAOSupported(MfxVideoParam const & par)
                     || par.mfx.FrameInfo.FourCC == MFX_FOURCC_P010)
 #endif //defined(PRE_SI_TARGET_PLATFORM_GEN11)
                 || (IsOn(par.mfx.LowPower) && (par.m_ext.CO2.MaxSliceSize != 0))
-                || par.mfx.RateControlMethod != MFX_RATECONTROL_CQP //@TODO Remove this line when support will ready
+                || (par.mfx.RateControlMethod != MFX_RATECONTROL_CQP && !par.isSWBRC()) //@TODO Remove this line when support will ready
                )
            )
        )
