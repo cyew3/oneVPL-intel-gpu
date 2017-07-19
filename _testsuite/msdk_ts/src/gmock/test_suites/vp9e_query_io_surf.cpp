c6ee80e592cc5557afc2e22cd420fc58a48e4197
Dmitry Malishev <dmitryx.malishev@intel.com>
2017-07-19 14:31:29 +0300

    [VP9e-tests] Added beh. test query_io_surf for P010, AYUV, Y410
    Issue: VSMGWL-3331
    
    Test: manual testing
    
    Change-Id: I34b8774be6b14616b767a7e998cebb3173e589da

--- _testsuite/msdk_ts/src/gmock/test_suites/vp9e_query_io_surf.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/vp9e_query_io_surf.cpp
@@ -4,7 +4,7 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2016 Intel Corporation. All Rights Reserved.
+Copyright(c) 2016-2017 Intel Corporation. All Rights Reserved.
 
 \* ****************************************************************************** */
 
@@ -14,35 +14,14 @@ Copyright(c) 2016 Intel Corporation. All Rights Reserved.
 
 namespace vp9e_query_io_surf
 {
-
     class TestSuite : tsVideoEncoder
     {
     public:
-        static const unsigned int n_cases;
-
         TestSuite() : tsVideoEncoder(MFX_CODEC_VP9)
         {
 
         }
         ~TestSuite() {}
-        int RunTest(unsigned int id);
-
-    private:
-        enum
-        {
-            MFX_PAR,
-            NULL_SESSION,
-            NULL_PAR,
-            NULL_REQUEST,
-            NONE
-        };
-
-        enum
-        {
-            MFX_MEMTYPE_D3D_EXT = MFX_MEMTYPE_FROM_ENCODE | MFX_MEMTYPE_DXVA2_DECODER_TARGET | MFX_MEMTYPE_EXTERNAL_FRAME,
-            MFX_MEMTYPE_SYS_EXT = MFX_MEMTYPE_FROM_ENCODE | MFX_MEMTYPE_SYSTEM_MEMORY | MFX_MEMTYPE_EXTERNAL_FRAME,
-            MFX_MEMTYPE_SYS = MFX_MEMTYPE_FROM_ENCODE | MFX_MEMTYPE_SYSTEM_MEMORY,
-        };
 
         struct tc_struct
         {
@@ -56,84 +35,180 @@ namespace vp9e_query_io_surf
             } set_par[MAX_NPARS];
         };
 
+        template<mfxU32 fourcc>
+        int RunTest_Subtype(const unsigned int id);
+
+        int RunTest(const tc_struct& tc, unsigned int fourcc_id);
+        static const unsigned int n_cases;
+
+        static const unsigned int n_cases_nv12;
+        static const unsigned int n_cases_p010;
+        static const unsigned int n_cases_ayuv;
+        static const unsigned int n_cases_y410;
+
+    private:
+        enum
+        {
+            MFX_PAR,
+            NULL_SESSION,
+            NULL_PAR,
+            NULL_REQUEST,
+            ASYNC5,
+            ASYNC10,
+            NONE
+        };
+
+        enum
+        {
+            MFX_MEMTYPE_D3D_EXT = MFX_MEMTYPE_FROM_ENCODE | MFX_MEMTYPE_DXVA2_DECODER_TARGET | MFX_MEMTYPE_EXTERNAL_FRAME,
+            MFX_MEMTYPE_SYS_EXT = MFX_MEMTYPE_FROM_ENCODE | MFX_MEMTYPE_SYSTEM_MEMORY | MFX_MEMTYPE_EXTERNAL_FRAME,
+            MFX_MEMTYPE_SYS = MFX_MEMTYPE_FROM_ENCODE | MFX_MEMTYPE_SYSTEM_MEMORY,
+        };
+
         static const tc_struct test_case[];
     };
 
     const TestSuite::tc_struct TestSuite::test_case[] =
     {
-        {/* 5*/ MFX_ERR_NONE, NONE,
+        {/*00*/ MFX_ERR_NONE, NONE,
             {
                 { MFX_PAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY },
             }
         },
-        {/* 1*/ MFX_ERR_NONE, NONE,
-            {
-                { MFX_PAR, &tsStruct::mfxVideoParam.AsyncDepth, 5 },
-            }
-        },
-        {/* 2*/ MFX_ERR_INVALID_HANDLE, NULL_SESSION },
-        {/* 3*/ MFX_ERR_NULL_PTR, NULL_PAR },
-        {/* 4*/ MFX_ERR_NULL_PTR, NULL_REQUEST },
-        {/* 5*/ MFX_ERR_NONE, NONE,
+        {/*01*/ MFX_ERR_NONE, NONE,
             {
                 { MFX_PAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_IN_VIDEO_MEMORY },
             }
         },
-        {/* 6*/ MFX_ERR_NONE, NONE,
+        {/*02*/ MFX_ERR_NONE, NONE,
             {
                 { MFX_PAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_IN_OPAQUE_MEMORY },
             }
         },
-        {/* 7*/ MFX_ERR_INVALID_VIDEO_PARAM, NONE,
+        {/*03*/ MFX_ERR_INVALID_VIDEO_PARAM, NONE,
             {
                 { MFX_PAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_VIDEO_MEMORY },
             }
         },
-        {/* 8*/ MFX_ERR_INVALID_VIDEO_PARAM, NONE,
+        {/*04*/ MFX_ERR_INVALID_VIDEO_PARAM, NONE,
             {
                 { MFX_PAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_SYSTEM_MEMORY },
             }
         },
-        {/* 9*/ MFX_ERR_INVALID_VIDEO_PARAM, NONE,
+        {/*05*/ MFX_ERR_INVALID_VIDEO_PARAM, NONE,
             {
                 { MFX_PAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_OUT_OPAQUE_MEMORY },
             }
         },
-        {/* 10*/ MFX_ERR_INVALID_VIDEO_PARAM, NONE,
+        {/*06*/ MFX_ERR_INVALID_VIDEO_PARAM, NONE,
             {
                 { MFX_PAR, &tsStruct::mfxVideoParam.IOPattern, 0 },
             }
         },
+        {/*07*/ MFX_ERR_NONE, ASYNC5,
+            {
+                { MFX_PAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY },
+            }
+        },
+        {/*08*/ MFX_ERR_NONE, ASYNC10,
+            {
+                { MFX_PAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_IN_SYSTEM_MEMORY },
+            }
+        },
+        {/*09*/ MFX_ERR_NONE, ASYNC5,
+            {
+                { MFX_PAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_IN_VIDEO_MEMORY },
+            }
+        },
+        {/*10*/ MFX_ERR_NONE, ASYNC10,
+            {
+                { MFX_PAR, &tsStruct::mfxVideoParam.IOPattern, MFX_IOPATTERN_IN_VIDEO_MEMORY },
+            }
+        },
+        {/*11*/ MFX_ERR_INVALID_HANDLE, NULL_SESSION },
+        {/*12*/ MFX_ERR_NULL_PTR, NULL_PAR },
+        {/*13*/ MFX_ERR_NULL_PTR, NULL_REQUEST },
     };
 
     const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case) / sizeof(TestSuite::tc_struct);
 
-    int TestSuite::RunTest(unsigned int id)
+    const unsigned int TestSuite::n_cases_nv12 = n_cases;
+    const unsigned int TestSuite::n_cases_p010 = n_cases;
+    const unsigned int TestSuite::n_cases_ayuv = n_cases;
+    const unsigned int TestSuite::n_cases_y410 = n_cases;
+
+    template<mfxU32 fourcc>
+    int TestSuite::RunTest_Subtype(const unsigned int id)
+    {
+        const tc_struct& tc = test_case[id];
+        return RunTest(tc, fourcc);
+    }
+
+    int TestSuite::RunTest(const tc_struct& tc, unsigned int fourcc_id)
     {
         TS_START;
 
-        const tc_struct& tc = test_case[id];
         mfxU32 type = 0;
 
         MFXInit();
 
         Load();
 
+        if (fourcc_id == MFX_FOURCC_NV12)
+        {
+            m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_NV12;
+            m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV420;
+            m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 8;
+        }
+        else if (fourcc_id == MFX_FOURCC_P010)
+        {
+            m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_P010;
+            m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV420;
+            m_par.mfx.FrameInfo.Shift = 1;
+            m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 10;
+        }
+        else if (fourcc_id == MFX_FOURCC_AYUV)
+        {
+            m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_AYUV;
+            m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV444;
+            m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 8;
+        }
+        else if (fourcc_id == MFX_FOURCC_Y410)
+        {
+            m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_Y410;
+            m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV444;
+            m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 10;
+        }
+        else
+        {
+            g_tsLog << "ERROR: invalid fourcc_id parameter: " << fourcc_id << "\n";
+            return 0;
+        }
+
         SETPARS(m_pPar, MFX_PAR);
         g_tsStatus.expect(tc.sts);
 
         if (0 == memcmp(m_uid->Data, MFX_PLUGINID_VP9E_HW.Data, sizeof(MFX_PLUGINID_VP9E_HW.Data)))
         {
-            if (g_tsHWtype < MFX_HW_CNL) // unsupported on platform less SKL
+            // MFX_PLUGIN_VP9E_HW unsupported on platform less CNL(NV12) and ICL(P010, AYUV, Y410)
+            if ((fourcc_id == MFX_FOURCC_NV12 && g_tsHWtype < MFX_HW_CNL)
+                || ((fourcc_id == MFX_FOURCC_P010 || fourcc_id == MFX_FOURCC_AYUV
+                    || fourcc_id == MFX_FOURCC_Y410) && g_tsHWtype < MFX_HW_ICL))
             {
                 g_tsStatus.expect(MFX_ERR_UNSUPPORTED);
                 g_tsLog << "WARNING: Unsupported HW Platform!\n";
                 Query();
                 return 0;
             }
-        } else {
+        }
+        else {
             g_tsLog << "WARNING: loading encoder from plugin failed!\n";
             return 0;
+        }
+
+        if (tc.type == ASYNC5 || tc.type == ASYNC10)
+        {
+            m_pPar->AsyncDepth = 1;
         }
 
         mfxVideoParam tmp_par = m_par;
@@ -156,7 +231,7 @@ namespace vp9e_query_io_surf
             QueryIOSurf();
         }
 
-        //check
+        //check call result
         if (tc.sts == MFX_ERR_NONE)
         {
             mfxU16 nFrameMin;
@@ -166,8 +241,11 @@ namespace vp9e_query_io_surf
             EXPECT_EQ(0, memcmp(&(m_request.Info), &(m_pPar->mfx.FrameInfo), sizeof(mfxFrameInfo)))
                 << "ERROR: QueryIOSurf didn't fill up frame info in returned mfxFrameAllocRequest structure";
 
-            EXPECT_EQ(0, memcmp(&tmp_par, m_pPar, sizeof(mfxVideoParam)))
-                << "ERROR: Input parameters must not be changed in QueryIOSurf()";
+            if (memcmp(&tmp_par, m_pPar, sizeof(mfxVideoParam)) != 0)
+            {
+                TS_TRACE(m_pPar);
+                ADD_FAILURE() << "ERROR: Input parameters were changed by QueryIOSurf(), expected not to be"; throw tsFAIL;
+            }
 
             switch (m_par.IOPattern)
             {
@@ -184,17 +262,31 @@ namespace vp9e_query_io_surf
             }
 
             EXPECT_EQ(type, m_request.Type)
-                << "ERROR: Wrong Type";
+                << "ERROR: Wrong Type in returned structure (" << m_request.Type << ")";
 
             if (nFrameMin)
             {
                 EXPECT_EQ(nFrameMin, m_request.NumFrameMin)
-                    << "ERROR: Wrong NumFrameMin";
+                    << "ERROR: NumFrameMin expected to be " << nFrameMin << ", but it is " << m_request.NumFrameMin;
             }
             if (nFrameSuggested)
             {
                 EXPECT_EQ(nFrameSuggested, m_request.NumFrameSuggested)
-                    << "ERROR: Wrong NumFrameSuggested";
+                    << "ERROR: NumFrameSuggested expected to be " << nFrameSuggested
+                    << ", but it is " << m_request.NumFrameSuggested;
+            }
+
+            if (tc.type == ASYNC5 || tc.type == ASYNC10)
+            {
+                mfxFrameAllocRequest  request2 = {};
+                m_pPar->AsyncDepth = (tc.type == ASYNC5 ? 5 : 10);
+
+                QueryIOSurf(m_session, m_pPar, &request2);
+
+                mfxU16 expected_numframemin = m_pRequest->NumFrameMin + m_pPar->AsyncDepth - 1;
+                EXPECT_EQ(expected_numframemin, request2.NumFrameMin)
+                    << "ERROR: For AsyncDepth=" << m_pPar->AsyncDepth << " NumFrameMin expected to be "
+                    << expected_numframemin << ", but it is " << request2.NumFrameMin;
             }
         }
 
@@ -202,5 +294,8 @@ namespace vp9e_query_io_surf
         return 0;
     }
 
-    TS_REG_TEST_SUITE_CLASS(vp9e_query_io_surf);
+    TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9e_query_io_surf,              RunTest_Subtype<MFX_FOURCC_NV12>, n_cases_nv12);
+    TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9e_10b_420_p010_query_io_surf, RunTest_Subtype<MFX_FOURCC_P010>, n_cases_p010);
+    TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9e_8b_444_ayuv_query_io_surf,  RunTest_Subtype<MFX_FOURCC_AYUV>, n_cases_ayuv);
+    TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9e_10b_444_y410_query_io_surf, RunTest_Subtype<MFX_FOURCC_Y410>, n_cases_y410);
 }
