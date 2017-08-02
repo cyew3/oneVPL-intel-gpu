035b50e7f6d13892c97f3becb272c85967fd9614
Sergey Solodkov <Sergey.Solodkov@intel.com>
2017-08-02 15:22:14 +0300

    [gmock] mfxExtThreadsParam unsupported for Windows
    regardless of test mode ext buffer mfxExtThreadsParam
    is attached to mfxInitParam. Windows doesn't support
    external treading so doesn't allow any ext buffers
    to be attached.
    MFX_ERR_UNSUPPORTED is expected return value for Windows
    
    Test: beh_threading_mfxinitex
    Issue: MDP-34080
    
    Change-Id: Ia7381d926bcc67ef6d8a5b24ce1629de4ea6
    Signed-off-by: Sergey Solodkov <Sergey.Solodkov@intel.com>

--- _testsuite/msdk_ts/src/gmock/test_suites/threading_mfxinitex.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/threading_mfxinitex.cpp
@@ -1,3 +1,13 @@
+/* ****************************************************************************** *\
+
+INTEL CORPORATION PROPRIETARY INFORMATION
+This software is supplied under the terms of a license agreement or nondisclosure
+agreement with Intel Corporation and may not be copied or disclosed except in
+accordance with the terms of that agreement
+Copyright(c) 2014-2017 Intel Corporation. All Rights Reserved.
+
+\* ****************************************************************************** */
+
 #if (defined(LINUX32) || defined(LINUX64))
 #include <stdlib.h>
 #include <unistd.h>
@@ -192,6 +202,11 @@ int TestSuite::RunTest(unsigned int id)
 
     SETPARS(&m_init_par, INIT_PAR);
 
+    // regardless of test mode ext buffer mfxExtThreadsParam is attached to mfxInitParam
+    // Windows doesn't support external treading so doesn't allow any ext buffers to be attached
+    if (g_tsOSFamily == MFX_OS_FAMILY_WINDOWS)
+        expect = MFX_ERR_UNSUPPORTED;
+
     if (tc.mode == EXT_BUF)
     {
         tp.NumThread = 1;
@@ -203,8 +218,6 @@ int TestSuite::RunTest(unsigned int id)
             tp.NumThread = std::thread::hardware_concurrency();
         }
 
-        if (g_tsOSFamily == MFX_OS_FAMILY_WINDOWS)
-            expect = MFX_ERR_UNSUPPORTED;
 #if (defined(LINUX32) || defined(LINUX64))
         if (((tp.SchedulingType == TS_SCHED_FIFO) || (tp.SchedulingType == TS_SCHED_RR)) &&
             geteuid()) { // this last condition checks whether test was started under root
