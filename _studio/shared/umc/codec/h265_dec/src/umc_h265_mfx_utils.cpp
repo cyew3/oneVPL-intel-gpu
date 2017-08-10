cyew3/oneVPL-intel-gpu

commit f3c37f51875f03855696d5ab0ec25fa6bb31315e

[HEVCd HW] Apply bigsurfpool workaround for BXT
Added BXT platform to the Ic64c5a2629e1a2c2538077b863bb163efed7b003
fix.

Issue: MDP-33543

Test: hevcd_conf-plugin_hevcdhw.bigsurfpool,
hevcd_conf_ext-plugin_hevcdhw.bigsurfpool,
hevcd_conf_allegro-plugin_hevcdhw.bigsurfpool,
hevcd_sbe-plugin_hevcdhw.bigsurfpool

Change-Id: I50a629d0ec37bd8329ae86248c27ee5e223f81ea

_studio/shared/umc/codec/h265_dec/src/umc_h265_mfx_utils.cpp
@@ -189,8 +189,8 @@ bool IsBugSurfacePoolApplicable(eMFXHWType hwtype, mfxVideoParam * par)
     if (hwtype >= MFX_HW_KBL)
         return true;
 
-    //On SKL 10 bit is hybrid (not work) and 8 bit supported by FF decoder that tested to works well
-    if (hwtype == MFX_HW_SCL && par->mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN)
+    //On SKL and BXT 10 bit decoder is hybrid (doesn't work) and 8 bit is supported by FF decoder that tested to works well
+    if ((hwtype == MFX_HW_SCL) || (hwtype == MFX_HW_APL) && par->mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN)
         return true;
 #endif
 

commit 6be30e792a799782578d46cce25dee6946b5f303

[HEVC10d HW] Fix for Nightly Regression
In the f3c37f5 commit was an incorrect check
which made BigSurface mode applicable on the SKL
platform during 10-bit HEVCd.

Issue: MDP-36274

Test: hevc10d_conf-plugin_hevcdhw,
hevc10d_conf_ext-plugin_hevcdhw

Change-Id: I55fa23277939c5ebab85ca411afa7b2afc82aaf9

_studio/shared/umc/codec/h265_dec/src/umc_h265_mfx_utils.cpp
@@ -190,7 +190,7 @@ bool IsBugSurfacePoolApplicable(eMFXHWType hwtype, mfxVideoParam * par)
         return true;
 
     //On SKL and BXT 10 bit decoder is hybrid (doesn't work) and 8 bit is supported by FF decoder that tested to works well
-    if ((hwtype == MFX_HW_SCL) || (hwtype == MFX_HW_APL) && par->mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN)
+    if (hwtype >= MFX_HW_SCL && par->mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN)
         return true;
 #endif
 