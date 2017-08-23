cedb917c27ec946807a8dfe1dc3c8e35f76b32e4
Kulakov, Leonid A <leonid.a.kulakov@intel.com>
2017-08-23 19:47:51 +0300

    [HEVCe FEI][samples] Add Quality-mode of the MV predictors re-packer
    Preliminary version averages collocated MV predictors.
    It is 16 for no DS, 4 for DS x2.
    Predictros are sorted in ascending distortion order.
    
    Issue: MDP-36255
    Test: not yet
    
    Change-Id: I96a51b81b4592b2d9ac2f090dfdabcb97b9e165e

--- samples/sample_hevc_fei/src/fei_predictors_repaking.cpp
+++ samples/sample_hevc_fei/src/fei_predictors_repaking.cpp
@@ -32,7 +32,7 @@ PredictorsRepaking::PredictorsRepaking() :
     m_heightCU_ds(0),
     m_widthCU_enc(0),
     m_heightCU_enc(0)
-{};
+{}
 
 mfxStatus PredictorsRepaking::Init(const mfxVideoParam& videoParams, mfxU8 downsample_ratio)
 {
@@ -49,7 +49,7 @@ mfxStatus PredictorsRepaking::Init(const mfxVideoParam& videoParams, mfxU8 downs
     m_heightCU_enc = (MSDK_ALIGN32(m_height)) >> 4;
 
     return MFX_ERR_NONE;
-};
+}
 
 mfxU8 PredictorsRepaking::ConvertDSratioPower2(mfxU8 downsample_ratio)
 {
@@ -85,7 +85,7 @@ mfxStatus PredictorsRepaking::RepackPredictors(const HevcTask& eTask, mfxExtFeiH
     }
 
     return sts;
-};
+}
 
 mfxStatus PredictorsRepaking::RepackPredictorsPerformance(const HevcTask& eTask, mfxExtFeiHevcEncMVPredictors& mvp)
 {
@@ -126,7 +126,7 @@ mfxStatus PredictorsRepaking::RepackPredictorsPerformance(const HevcTask& eTask,
                 + ((rowIdx & 1) << 1);          // zero or double offset depending on the number of row index,
                                                 // zero shift for top 16x16 blocks into 32x32 layout and double for bottom blocks;
 
-            for (mfxU8 j = 0; j < numPredPairs; ++j)
+            for (mfxU32 j = 0; j < numPredPairs; ++j)
             {
                 mvp.Data[permutEncIdx].RefIdx[j].RefL0 = refIdx_vec[j]->RefL0;
                 mvp.Data[permutEncIdx].RefIdx[j].RefL1 = refIdx_vec[j]->RefL1;
@@ -189,9 +189,187 @@ mfxStatus PredictorsRepaking::RepackPredictorsPerformance(const HevcTask& eTask,
     }
 
     return MFX_ERR_NONE;
-};
+}
+
+void SelectFromMV(const mfxI16Pair(*mv)[2], mfxI32 count, mfxI16Pair(&res)[2]);
 
 mfxStatus PredictorsRepaking::RepackPredictorsQuality(const HevcTask& eTask, mfxExtFeiHevcEncMVPredictors& mvp)
 {
-    return MFX_ERR_UNSUPPORTED;
-};
+    std::vector<mfxExtFeiPreEncMVExtended*>     mvs_vec;
+    std::vector<mfxExtFeiPreEncMBStatExtended*> mbs_vec;
+    std::vector<const RefIdxPair*>              refIdx_vec;
+
+    mfxU8 numPredPairs = (std::min)(m_max_fei_enc_mvp_num, (std::max)(eTask.m_numRefActive[0], eTask.m_numRefActive[1]));
+    if (numPredPairs != eTask.m_preEncOutput.size())
+        return MFX_ERR_UNDEFINED_BEHAVIOR;
+
+    if (numPredPairs == 0 || (eTask.m_frameType & MFX_FRAMETYPE_I))
+        return MFX_ERR_NONE; // I-frame
+
+    mvs_vec.reserve(m_max_fei_enc_mvp_num);
+    mbs_vec.reserve(m_max_fei_enc_mvp_num);
+    refIdx_vec.reserve(m_max_fei_enc_mvp_num);
+
+    // PreENC parameters reading
+    for (std::list<PreENCOutput>::const_iterator it = eTask.m_preEncOutput.begin(); it != eTask.m_preEncOutput.end(); ++it)
+    {
+        mvs_vec.push_back((*it).m_mv);
+        mbs_vec.push_back((*it).m_mb);
+        refIdx_vec.push_back(&(*it).m_refIdxPair);
+    }
+
+    const mfxI16Pair zeroPair = { 0, 0 };
+
+    mfxU32 i = 0;
+    // get nPred_actual L0/L1 predictors for each CU
+    for (mfxU32 rowIdx = 0; rowIdx < m_heightCU_enc; ++rowIdx) // row index for full surface (raster-scan order)
+    {
+        for (mfxU32 colIdx = 0; colIdx < m_widthCU_enc; ++colIdx, ++i) // column index for full surface (raster-scan order)
+        {
+            // intermediate arrays to be sorted by distortion
+            mfxU8 ref[4][2];
+            mfxI16Pair mv[4][2];
+            mfxU16 distortion[4][2];
+            // calculation of the input index for encoder after permutation from raster scan order index into 32x32 layout
+            // HEVC encoder works with 32x32 layout
+            mfxU32 permutEncIdx =
+                ((colIdx >> 1) << 2)            // column offset;
+                + (rowIdx & ~1) * m_widthCU_enc // offset for new line of 32x32 blocks layout;
+                + (colIdx & 1)                  // zero or single offset depending on the number of comumn index;
+                + ((rowIdx & 1) << 1);          // zero or double offset depending on the number of row index,
+                                                // zero shift for top 16x16 blocks into 32x32 layout and double for bottom blocks;
+
+            for (mfxU32 j = 0; j < numPredPairs; ++j)
+            {
+                ref[j][0] = refIdx_vec[j]->RefL0;
+                ref[j][1] = refIdx_vec[j]->RefL1;
+
+                if (m_downsample_power2 == 0)// w/o VPP
+                {
+                    if (colIdx >= m_widthCU_ds || rowIdx >= m_heightCU_ds) // TODO move check to the beginning of the loop
+                    {
+                        mv[j][0] = zeroPair;
+                        mv[j][1] = zeroPair;
+                    }
+                    else
+                    {
+                        SelectFromMV(&mvs_vec[j]->MB[i].MV[0], 16, mv[j]);
+                        distortion[j][0] = mbs_vec[j]->MB[i].Inter[0].BestDistortion;
+                        distortion[j][1] = mbs_vec[j]->MB[i].Inter[1].BestDistortion;
+                    }
+                }
+                else
+                {
+                    mfxU32 preencCUIdx; // index CU from PreENC output
+                    mfxU32 rowMVIdx;    // row index for motion vector
+                    mfxU32 colMVIdx;    // column index for motion vector
+                    mfxU32 preencMVIdx; // linear index for motion vector
+
+                    switch (m_downsample_power2)
+                    {
+                    case 1:
+                        preencCUIdx = (rowIdx >> 1) * m_widthCU_ds + (colIdx >> 1);
+                        rowMVIdx = rowIdx & 1;
+                        colMVIdx = colIdx & 1;
+                        preencMVIdx = rowMVIdx * 8 + colMVIdx * 4;
+                        SelectFromMV(&mvs_vec[j]->MB[preencCUIdx].MV[preencMVIdx], 4, mv[j]);
+                        break;
+                    case 2:
+                        preencCUIdx = (rowIdx >> 2) * m_widthCU_ds + (colIdx >> 2);
+                        rowMVIdx = rowIdx & 3;
+                        colMVIdx = colIdx & 3;
+                        preencMVIdx = rowMVIdx * 4 + colMVIdx;
+                        mv[j][0] = mvs_vec[j]->MB[preencCUIdx].MV[ZigzagOrder[preencMVIdx]][0];
+                        mv[j][1] = mvs_vec[j]->MB[preencCUIdx].MV[ZigzagOrder[preencMVIdx]][1];
+                        break;
+                    case 3:
+                        preencCUIdx = (rowIdx >> 3) * m_widthCU_ds + (colIdx >> 3);
+                        rowMVIdx = rowIdx & 7;
+                        colMVIdx = colIdx & 7;
+                        preencMVIdx = rowMVIdx / 2 * 4 + colMVIdx / 2;
+                        mv[j][0] = mvs_vec[j]->MB[preencCUIdx].MV[ZigzagOrder[preencMVIdx]][0];
+                        mv[j][1] = mvs_vec[j]->MB[preencCUIdx].MV[ZigzagOrder[preencMVIdx]][1];
+                        break;
+                    default:
+                        break;
+                    }
+
+                    mv[j][0].x <<= m_downsample_power2;
+                    mv[j][0].y <<= m_downsample_power2;
+                    mv[j][1].x <<= m_downsample_power2;
+                    mv[j][1].y <<= m_downsample_power2;
+
+                    distortion[j][0] = (j < eTask.m_numRefActive[0]) ? mbs_vec[j]->MB[preencCUIdx].Inter[0].BestDistortion : 0xffff;
+                    distortion[j][1] = (j < eTask.m_numRefActive[1]) ? mbs_vec[j]->MB[preencCUIdx].Inter[1].BestDistortion : 0xffff;
+                }
+            }
+
+            // sort predcitors by ascending distortion
+            if (numPredPairs < 2) // nothing to sort
+            {
+                mvp.Data[permutEncIdx].MV[0][0] = mv[0][0];
+                mvp.Data[permutEncIdx].MV[0][1] = mv[0][1];
+                mvp.Data[permutEncIdx].RefIdx[0].RefL0 = ref[0][0];
+                mvp.Data[permutEncIdx].RefIdx[0].RefL1 = ref[0][1];
+                continue;
+            }
+
+            // smaller idx to be first argument to be preffered if equal
+            #define CMP_DIST(k,l) {                               \
+                mfxU8 res0 = distortion[k][0] > distortion[l][0]; \
+                mfxU8 res1 = distortion[k][1] > distortion[l][1]; \
+                worse[k][0] += res0; worse[k][0] += res0 ^ 1;     \
+                worse[k][1] += res1; worse[k][1] += res1 ^ 1;     \
+            }
+
+            // fill unused
+            for (mfxU32 j = numPredPairs; j < 4; ++j)
+            {
+                distortion[j][1] = distortion[j][0] = 0xffff;
+                ref[j][1] = ref[j][0] = 0xff;
+                mv[j][0].y = mv[j][0].x = 0x8000;
+                mv[j][1].y = mv[j][1].x = 0x8000;
+            }
+
+            mfxU8 worse[4][2] = { {0,} };
+            CMP_DIST(0, 1); CMP_DIST(2, 3);
+            CMP_DIST(0, 2); CMP_DIST(1, 3);
+            CMP_DIST(0, 3); CMP_DIST(1, 2);
+            // here 'worse' tells how many cases are better, so it is position in sorted array
+            for (mfxU32 j = 0; j < 4; j++)
+            {
+                mvp.Data[permutEncIdx].MV[worse[j][0]][0] = mv[j][0];
+                mvp.Data[permutEncIdx].MV[worse[j][1]][1] = mv[j][1];
+                mvp.Data[permutEncIdx].RefIdx[worse[j][0]].RefL0 = ref[j][0];
+                mvp.Data[permutEncIdx].RefIdx[worse[j][1]].RefL1 = ref[j][1];
+            }
+        }
+    }
+
+    return MFX_ERR_NONE;
+}
+
+// Selects best MV pair from set of consequent MV pairs
+// Count is expected to be 4 or 16
+// May be improved later
+void SelectFromMV(const mfxI16Pair(* mv)[2], mfxI32 count, mfxI16Pair (&res)[2])
+{
+    for (int ref = 0; ref < 2; ref++)
+    {
+        mfxI32 found = 0, xsum = 0, ysum = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (mv[i][ref].x == 0x8000) // ignore intra
+                continue;
+            found++;
+            xsum += mv[i][ref].x;
+            ysum += mv[i][ref].y;
+        }
+        if (!found)
+            res[ref] = mv[0][ref]; // all MV are fill with 0x8000
+        else {
+            res[ref].x = xsum / found;
+            res[ref].y = ysum / found;
+        }
+    }
+}
