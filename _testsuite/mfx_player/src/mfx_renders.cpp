cyew3/oneVPL-intel-gpu

commit b21367d711fbdad32f6fe3887f0b36d1cbad0632

[player]: add support for 12b formats
 - add support for writing y416 as-is
 - add conversion from p016/y416 into
   MFX_FOURCC_YUV420_16/MFX_FOURCC_YUV444_16
 - if p016/y416 is not explicitly specified as
   output format (-o:p016/y416) it is implied
   that output to be converted to planar
   yuv420_16/yuv444_16
 - fix typo for P016/Y216 fourcc

Issue: VSMG-502, VSMG-514, VSMG-520
Test: 12b conformance, manual w/ explicit '-o:p016/y416' switch

Change-Id: Ic43707a0def01a95343d1734ecefd61f50411f28

_testsuite/mfx_player/src/mfx_renders.cpp
@@ -985,6 +985,20 @@ mfxStatus MFXFileWriteRender::WriteSurface(mfxFrameSurface1 * pConvertedSurface)
             }
             break;
         }
+        case MFX_FOURCC_Y416:
+        {
+            m_Current.m_comp = VM_STRING('U');
+            m_Current.m_pixX = 0;
+            mfxU8* plane = pData->U + pInfo->CropX * 8;
+
+            for (i = 0; i <pInfo->CropH; i++)
+            {
+                m_Current.m_pixY = i;
+                MFX_CHECK_STS(PutData(plane, pInfo->CropW * 8));
+                plane += pitch;
+            }
+            break;
+        }
         default:
         {
             MFX_TRACE_AND_EXIT(MFX_ERR_UNSUPPORTED, (VM_STRING("[MFXFileWriteRender] file writing in %s colorformat not supported\n"), 
@@ -1808,9 +1822,11 @@ mfxFrameSurface1* ConvertSurface(mfxFrameSurface1* pSurfaceIn, mfxFrameSurface1*
         pSurfaceIn->Info.FourCC != MFX_FOURCC_P010 &&
         pSurfaceIn->Info.FourCC != MFX_FOURCC_NV16 &&
         pSurfaceIn->Info.FourCC != MFX_FOURCC_P210 &&
+        pSurfaceIn->Info.FourCC != MFX_FOURCC_P016 &&
         pSurfaceIn->Info.FourCC != MFX_FOURCC_Y210 &&
         pSurfaceIn->Info.FourCC != MFX_FOURCC_Y216 &&
         pSurfaceIn->Info.FourCC != MFX_FOURCC_Y410 &&
+        pSurfaceIn->Info.FourCC != MFX_FOURCC_Y416 &&
         pSurfaceIn->Info.FourCC != MFX_FOURCC_AYUV &&
         pSurfaceIn->Info.FourCC != MFX_FOURCC_YUY2)
     {
@@ -1854,49 +1870,43 @@ mfxFrameSurface1* ConvertSurface(mfxFrameSurface1* pSurfaceIn, mfxFrameSurface1*
         pSurfaceOut->Data.V
     };
 
-    Ipp32s pDstStep[3] = 
+    Ipp32s pDstStep[3] =
     {
         static_cast<Ipp32s>(pitchOut),
         static_cast<Ipp32s>(pitchOut >> 1),
         static_cast<Ipp32s>(pitchOut >> 1)
     };
 
+    mfxU32 const originalBDLuma = pSurfaceIn->Info.BitDepthLuma ? pSurfaceIn->Info.BitDepthLuma : 8;
+    mfxU32 const originalBDChroma = pSurfaceIn->Info.BitDepthChroma ? pSurfaceIn->Info.BitDepthChroma : 8;
+    mfxU32 const finalBitDepth = params->use10bitOutput ? 10 : IPP_MAX(originalBDLuma, originalBDChroma);
+
+    mfxU32 const finalBitDepthLuma =
+        params->useSameBitDepthForComponents ? finalBitDepth : originalBDLuma;
+    mfxU32 const finalBitDepthChroma =
+        params->useSameBitDepthForComponents ? finalBitDepth : originalBDChroma;
+
+    mfxI32 const l_shift = params->VpxDec16bFormat ? 0 : (pSurfaceIn->Info.Shift ? (16 - finalBitDepth) : originalBDLuma   - finalBitDepthLuma);
+    mfxI32 const c_shift = params->VpxDec16bFormat ? 0 : (pSurfaceIn->Info.Shift ? (16 - finalBitDepth) : originalBDChroma - finalBitDepthChroma);
+
     if (pSurfaceOut->Info.FourCC == MFX_FOURCC_YUV420_16)
     {
-        mfxU32 originalBDLuma = pSurfaceIn->Info.BitDepthLuma ? pSurfaceIn->Info.BitDepthLuma : 8;
-        mfxU32 originalBDChroma = pSurfaceIn->Info.BitDepthChroma ? pSurfaceIn->Info.BitDepthChroma : 8;
-
-        mfxU32 finalBitDepth = params->use10bitOutput ? 10 : IPP_MAX(originalBDLuma, originalBDChroma);
-        if (!params->useSameBitDepthForComponents)
-            finalBitDepth = originalBDLuma;
-
-        mfxI32 shift = pSurfaceIn->Info.Shift ? (16 - finalBitDepth) : originalBDLuma - finalBitDepth;
-        if (params->VpxDec16bFormat) //No need to remove shift if vpxdec format
-            shift = 0;
         if (pSurfaceIn->Info.FourCC == MFX_FOURCC_NV12)
-            copyPlane<mfxU8, mfxU16>(pSurfaceIn->Data.Y, pSurfaceIn->Data.Pitch, pSurfaceOut->Data.Y, pSurfaceOut->Data.Pitch, pSurfaceIn->Info.Height, pSurfaceIn->Info.Width, shift);
+            copyPlane<mfxU8, mfxU16>(pSurfaceIn->Data.Y, pSurfaceIn->Data.Pitch, pSurfaceOut->Data.Y, pSurfaceOut->Data.Pitch, pSurfaceIn->Info.Height, pSurfaceIn->Info.Width, l_shift);
         else
-            copyPlane<mfxU16, mfxU16>(pSurfaceIn->Data.Y, pSurfaceIn->Data.Pitch, pSurfaceOut->Data.Y, pSurfaceOut->Data.Pitch, pSurfaceIn->Info.Height, pSurfaceIn->Info.Width, shift);
+            copyPlane<mfxU16, mfxU16>(pSurfaceIn->Data.Y, pSurfaceIn->Data.Pitch, pSurfaceOut->Data.Y, pSurfaceOut->Data.Pitch, pSurfaceIn->Info.Height, pSurfaceIn->Info.Width, l_shift);
 
-        if (!params->useSameBitDepthForComponents)
-            finalBitDepth = originalBDChroma;
-
-        shift = pSurfaceIn->Info.Shift ? (16 - finalBitDepth) : originalBDChroma - finalBitDepth;
-        if (params->VpxDec16bFormat) //No need to remove shift if vpxdec format
-            shift = 0;
         if (pSurfaceIn->Info.FourCC == MFX_FOURCC_NV12)
-            copyChromaPlane<mfxU8, mfxU16>(pSurfaceIn->Data.UV, pSurfaceIn->Data.Pitch, pSurfaceOut->Data.U, pSurfaceOut->Data.V, pSurfaceOut->Data.Pitch >> 1, pSurfaceIn->Info.Height / 2, pSurfaceIn->Info.Width / 2, shift);
+            copyChromaPlane<mfxU8, mfxU16>(pSurfaceIn->Data.UV, pSurfaceIn->Data.Pitch, pSurfaceOut->Data.U, pSurfaceOut->Data.V, pSurfaceOut->Data.Pitch >> 1, pSurfaceIn->Info.Height / 2, pSurfaceIn->Info.Width / 2, c_shift);
         else
-            copyChromaPlane<mfxU16, mfxU16>(pSurfaceIn->Data.UV, pSurfaceIn->Data.Pitch, pSurfaceOut->Data.U, pSurfaceOut->Data.V, pSurfaceOut->Data.Pitch >> 1, pSurfaceIn->Info.Height / 2, pSurfaceIn->Info.Width / 2, shift);
+            copyChromaPlane<mfxU16, mfxU16>(pSurfaceIn->Data.UV, pSurfaceIn->Data.Pitch, pSurfaceOut->Data.U, pSurfaceOut->Data.V, pSurfaceOut->Data.Pitch >> 1, pSurfaceIn->Info.Height / 2, pSurfaceIn->Info.Width / 2, c_shift);
 
         return pSurfaceOut;
     }
     else if (pSurfaceOut->Info.FourCC == MFX_FOURCC_YUV422_16)
     {
-        if (pSurfaceIn->Info.FourCC == MFX_FOURCC_NV12)
-            VM_ASSERT(false);
-        else if (pSurfaceIn->Info.FourCC == MFX_FOURCC_Y210 ||
-                 pSurfaceIn->Info.FourCC == MFX_FOURCC_Y216)
+        if (pSurfaceIn->Info.FourCC == MFX_FOURCC_Y210 ||
+            pSurfaceIn->Info.FourCC == MFX_FOURCC_Y216)
         {
 #pragma pack(push, 1)
             struct Y216Pixel
@@ -1912,9 +1922,6 @@ mfxFrameSurface1* ConvertSurface(mfxFrameSurface1* pSurfaceIn, mfxFrameSurface1*
             pitchOut /= 2;
             pitchIn /= 2;
 
-            //NOTE: currect [Fulsim] implementation supports only Y216 fourcc (at least for h.265)
-            //but actually writes Y210 w/ shift
-            //TODO: change this when correct Y216 will be supported
             for (size_t i = 0; i < pSurfaceIn->Info.Height; i++)
             {
                 Y216Pixel const* pSrc
@@ -1922,11 +1929,11 @@ mfxFrameSurface1* ConvertSurface(mfxFrameSurface1* pSurfaceIn, mfxFrameSurface1*
 
                 for (size_t j = 0; j < pSurfaceIn->Info.Width; j += 2)
                 {
-                    pDstY[i * pitchOut + j + 0] = pSrc->y0 >> 6;
-                    pDstY[i * pitchOut + j + 1] = pSrc->y1 >> 6;
+                    pDstY[i * pitchOut + j + 0] = pSrc->y0 >> l_shift;
+                    pDstY[i * pitchOut + j + 1] = pSrc->y1 >> l_shift;
 
-                    pDstU[i * pitchOut / 2 + j / 2] = pSrc->u >> 6;
-                    pDstV[i * pitchOut / 2 + j / 2] = pSrc->v >> 6;
+                    pDstU[i * pitchOut / 2 + j / 2] = pSrc->u >> c_shift;
+                    pDstV[i * pitchOut / 2 + j / 2] = pSrc->v >> c_shift;
 
                     ++pSrc;
                 }
@@ -1934,60 +1941,67 @@ mfxFrameSurface1* ConvertSurface(mfxFrameSurface1* pSurfaceIn, mfxFrameSurface1*
         }
         else
         {
-            mfxU32 originalBDLuma = pSurfaceIn->Info.BitDepthLuma ? pSurfaceIn->Info.BitDepthLuma : 8;
-            mfxU32 originalBDChroma = pSurfaceIn->Info.BitDepthChroma ? pSurfaceIn->Info.BitDepthChroma : 8;
-
-            mfxU32 finalBitDepth = params->use10bitOutput ? 10 : IPP_MAX(originalBDLuma, originalBDChroma);
-            if (!params->useSameBitDepthForComponents)
-                finalBitDepth = originalBDLuma;
-
-            mfxI32 shift = pSurfaceIn->Info.Shift ? (16 - finalBitDepth) : originalBDLuma - finalBitDepth;
-            copyPlane<mfxU16, mfxU16>(pSurfaceIn->Data.Y, pSurfaceIn->Data.Pitch, pSurfaceOut->Data.Y, pSurfaceOut->Data.Pitch, pSurfaceIn->Info.Height, pSurfaceIn->Info.Width, shift);
-
-            if (!params->useSameBitDepthForComponents)
-                finalBitDepth = originalBDChroma;
-
-            shift = pSurfaceIn->Info.Shift ? (16 - finalBitDepth) : originalBDChroma - finalBitDepth;
-            copyChromaPlane<mfxU16, mfxU16>(pSurfaceIn->Data.UV, pSurfaceIn->Data.Pitch, pSurfaceOut->Data.U, pSurfaceOut->Data.V, pSurfaceOut->Data.Pitch >> 1, pSurfaceIn->Info.Height, pSurfaceIn->Info.Width / 2, shift);
+            copyPlane<mfxU16, mfxU16>(pSurfaceIn->Data.Y, pSurfaceIn->Data.Pitch, pSurfaceOut->Data.Y, pSurfaceOut->Data.Pitch, pSurfaceIn->Info.Height, pSurfaceIn->Info.Width, l_shift);
+            copyChromaPlane<mfxU16, mfxU16>(pSurfaceIn->Data.UV, pSurfaceIn->Data.Pitch, pSurfaceOut->Data.U, pSurfaceOut->Data.V, pSurfaceOut->Data.Pitch >> 1, pSurfaceIn->Info.Height, pSurfaceIn->Info.Width / 2, c_shift);
         }
 
         return pSurfaceOut;
     }
     else if (pSurfaceOut->Info.FourCC == MFX_FOURCC_YUV444_16)
     {
-        VM_ASSERT(pSurfaceIn->Info.FourCC == MFX_FOURCC_Y410);
-        //full unpack Y410 to YUV444_10b not using alpha
-#pragma pack(push, 1)
-        union Y410Pixel
+        if (pSurfaceIn->Info.FourCC == MFX_FOURCC_Y410)
         {
-            mfxU32 force4bytes;
-            struct
+            //full unpack Y410 to YUV444_10b not using alpha
+#pragma pack(push, 1)
+            union Y410Pixel
             {
-                mfxU32 u : 10;
-                mfxU32 y : 10;
-                mfxU32 v : 10;
-                mfxU32 a : 2;
-            } y410;
-        };
+                mfxU32 force4bytes;
+                struct
+                {
+                    mfxU32 u : 10;
+                    mfxU32 y : 10;
+                    mfxU32 v : 10;
+                    mfxU32 a : 2;
+                } y410;
+            };
 #pragma pack(pop)
 
-        mfxU16* pDstY = pSurfaceOut->Data.Y16;
-        mfxU16* pDstU = pSurfaceOut->Data.U16;
-        mfxU16* pDstV = pSurfaceOut->Data.V16;
+            mfxU16* pDstY = pSurfaceOut->Data.Y16;
+            mfxU16* pDstU = pSurfaceOut->Data.U16;
+            mfxU16* pDstV = pSurfaceOut->Data.V16;
 
-        Y410Pixel* pSrc = (Y410Pixel*) pSurfaceIn->Data.U;
+            Y410Pixel const* pSrc = (Y410Pixel*)pSurfaceIn->Data.U;
 
-        pitchOut = pitchOut / 2;
-        pitchIn = pitchIn / 4;
+            pitchOut /= sizeof(mfxU16);
+            pitchIn  /= pitchIn / sizeof(Y410Pixel);
 
-        //Y
-        for (size_t i = 0; i < pSurfaceIn->Info.Height; i++)
-        {
-            for (size_t j = 0; j < pSurfaceIn->Info.Width; j++)
+            for (size_t i = 0; i < pSurfaceIn->Info.Height; i++)
             {
-                pDstY[i*pitchOut + j] = pSrc[i*pitchIn + j].y410.y;
-                pDstU[i*pitchOut + j] = pSrc[i*pitchIn + j].y410.u;
-                pDstV[i*pitchOut + j] = pSrc[i*pitchIn + j].y410.v;
+                for (size_t j = 0; j < pSurfaceIn->Info.Width; j++)
+                {
+                    pDstY[i*pitchOut + j] = pSrc[i*pitchIn + j].y410.y;
+                    pDstU[i*pitchOut + j] = pSrc[i*pitchIn + j].y410.u;
+                    pDstV[i*pitchOut + j] = pSrc[i*pitchIn + j].y410.v;
+                }
+            }
+        }
+        else if (pSurfaceIn->Info.FourCC == MFX_FOURCC_Y416)
+        {
+            mfxU16* pDstY = pSurfaceOut->Data.Y16;
+            mfxU16* pDstU = pSurfaceOut->Data.U16;
+            mfxU16* pDstV = pSurfaceOut->Data.V16;
+
+            pitchOut /= sizeof(mfxU16);
+            pitchIn  /= sizeof(mfxU16);
+
+            for (size_t i = 0; i < pSurfaceIn->Info.Height; i++)
+            {
+                for (size_t j = 0; j < pSurfaceIn->Info.Width; j++)
+                {
+                    pDstU[i * pitchOut + j] = pSurfaceIn->Data.U16[i * pitchIn + 4 * j] >> l_shift;
+                    pDstY[i * pitchOut + j] = pSurfaceIn->Data.Y16[i * pitchIn + 4 * j] >> c_shift;
+                    pDstV[i * pitchOut + j] = pSurfaceIn->Data.V16[i * pitchIn + 4 * j] >> c_shift;
+                }
             }
         }
     }

_testsuite/mfx_player/include/mfx_pipeline_utils.h
@@ -86,7 +86,7 @@ mfxF64           ConvertMFXTime2mfxF64(mfxU64 nTime);
 mfxU32           GetMinPlaneSize(mfxFrameInfo & info);
 
 //possible extension to the end of this enum
-#define MFX_FOURCC_PATTERN() "nv12( |:mono)|yv12( |:mono)|rgb24|rgb32|yuy2(:h|:v|:mono)|ayuv|p010|a2rgb10|r16|argb16|nv16|p210|y410|y210|y216|i444|yv16"
+#define MFX_FOURCC_PATTERN() "nv12( |:mono)|yv12( |:mono)|rgb24|rgb32|yuy2(:h|:v|:mono)|ayuv|p010|a2rgb10|r16|argb16|nv16|p210|y410|y210|y216|i444|yv16|p016|y416"
 //gets and mediasdk fourcc & chroma format fields set from given fourcc index
 mfxStatus        GetMFXFrameInfoFromFOURCCPatternIdx(int idx_in_pattern, mfxFrameInfo &info);
 

_testsuite/mfx_player/src/mfx_d3d11_allocator.cpp
@@ -272,10 +272,10 @@ mfxStatus D3D11FrameAllocator::LockFrame(mfxMemId mid, mfxFrameData *ptr)
             break;
 
         case DXGI_FORMAT_Y416:
-            ptr->A = (mfxU8 *)lockedRect.pData;
-            ptr->V16 = ((mfxU16*)ptr->A) + 1;
-            ptr->Y16 = ptr->V16 + 1;
-            ptr->U16 = ptr->Y16 + 1;
+            ptr->U16 = (mfxU16*)lockedRect.pData;
+            ptr->Y16 = ptr->U16 + 1;
+            ptr->V16 = ptr->Y16 + 1;
+            ptr->A   = (mfxU8 *)ptr->V16 + 1;
             break;
 
         default:

_testsuite/mfx_player/src/mfx_d3d_allocator.cpp
@@ -24,8 +24,8 @@ Copyright(c) 2008-2017 Intel Corporation. All Rights Reserved.
 #define D3DFMT_P210 (D3DFORMAT)MFX_FOURCC_P210
 #define D3DFMT_Y410 (D3DFORMAT)MFX_FOURCC_Y410
 #define D3DFMT_AYUV (D3DFORMAT)MFX_FOURCC_AYUV
-#define D3DFMT_Y216 (D3DFORMAT)MFX_FOURCC_P016
-#define D3DFMT_P016 (D3DFORMAT)MFX_FOURCC_Y216
+#define D3DFMT_P016 (D3DFORMAT)MFX_FOURCC_P016
+#define D3DFMT_Y216 (D3DFORMAT)MFX_FOURCC_Y216
 #define D3DFMT_Y416 (D3DFORMAT)MFX_FOURCC_Y416
 
 D3DFORMAT ConvertMfxFourccToD3dFormat(mfxU32 fourcc)
@@ -293,10 +293,10 @@ mfxStatus D3DFrameAllocator::LockFrame(mfxMemId mid, mfxFrameData *ptr)
         break;
 
     case D3DFMT_Y416:
-        ptr->A = (mfxU8 *)locked.pBits;
-        ptr->V16 = ((mfxU16*)ptr->A) + 1;
-        ptr->Y16 = ptr->V16 + 1;
-        ptr->U16 = ptr->Y16 + 1;
+        ptr->U16 = (mfxU16*)locked.pBits;
+        ptr->Y16 = ptr->U16 + 1;
+        ptr->V16 = ptr->Y16 + 1;
+        ptr->A   = (mfxU8 *)ptr->V16 + 1;
         break;
     }
 

_testsuite/mfx_player/src/mfx_pipeline_dec.cpp
@@ -506,10 +506,13 @@ mfxStatus MFXDecPipeline::BuildPipeline()
 
     m_components[eDEC].m_params.mfx.CodecId = m_inParams.InputCodecType;
     if ( MFX_FOURCC_P010 == m_inParams.FrameInfo.FourCC ||
+         MFX_FOURCC_P016 == m_inParams.FrameInfo.FourCC ||
          MFX_FOURCC_P210 == m_inParams.FrameInfo.FourCC ||
          MFX_FOURCC_Y210 == m_inParams.FrameInfo.FourCC ||
+         MFX_FOURCC_Y216 == m_inParams.FrameInfo.FourCC ||
          MFX_FOURCC_YUY2 == m_inParams.FrameInfo.FourCC ||
          MFX_FOURCC_Y410 == m_inParams.FrameInfo.FourCC ||
+         MFX_FOURCC_Y416 == m_inParams.FrameInfo.FourCC ||
          MFX_FOURCC_A2RGB10 == m_inParams.FrameInfo.FourCC ||
          MFX_FOURCC_AYUV == m_inParams.FrameInfo.FourCC ||
          MFX_FOURCC_NV16 == m_inParams.FrameInfo.FourCC ||
@@ -519,6 +522,7 @@ mfxStatus MFXDecPipeline::BuildPipeline()
 
     if ( MFX_FOURCC_P210 == m_inParams.FrameInfo.FourCC ||
          MFX_FOURCC_Y210 == m_inParams.FrameInfo.FourCC ||
+         MFX_FOURCC_Y216 == m_inParams.FrameInfo.FourCC ||
          MFX_FOURCC_NV16 == m_inParams.FrameInfo.FourCC) {
          m_components[eDEC].m_params.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV422;
     }
@@ -529,6 +533,7 @@ mfxStatus MFXDecPipeline::BuildPipeline()
     }
 
     if ( MFX_FOURCC_Y410 == m_inParams.FrameInfo.FourCC ||
+         MFX_FOURCC_Y416 == m_inParams.FrameInfo.FourCC ||
          MFX_FOURCC_A2RGB10 == m_inParams.FrameInfo.FourCC ||
          MFX_FOURCC_AYUV == m_inParams.FrameInfo.FourCC) {
          m_components[eDEC].m_params.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV444;
@@ -1862,9 +1867,13 @@ mfxStatus MFXDecPipeline::CreateRender()
         if (m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_AYUV)
             m_inParams.outFrameInfo.FourCC = MFX_FOURCC_AYUV;
 
-        if (m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_P010 || m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_P210 ||
-            m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_Y210 || m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_Y216 ||
-            m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_Y410)
+        if (m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_P010 ||
+            m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_P210 ||
+            m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_P016 ||
+            m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_Y210 ||
+            m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_Y216 ||
+            m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_Y410 ||
+            m_components[eDEC].m_params.mfx.FrameInfo.FourCC == MFX_FOURCC_Y416)
         {
             switch (m_components[eDEC].m_params.mfx.FrameInfo.ChromaFormat)
             {

_testsuite/mfx_player/src/mfx_pipeline_utils.cpp
@@ -745,8 +745,8 @@ mfxF64 ConvertMFXTime2mfxF64(mfxU64 nTime)
 
 mfxStatus GetMFXFrameInfoFromFOURCCPatternIdx(int idx_in_pattern, mfxFrameInfo &info)
 {
-    //                                       1       2     3       4      5     6       7  8     9    10   11      12  13     14   15   16   17   18   19   20   21
-    static const char valid_pattern [] = "nv12( |:mono)|yv12( |:mono)|rgb24|rgb32|yuy2(:h|:v|:mono)|ayuv|p010|a2rgb10|r16|argb16|nv16|p210|y410|y210|y216|i444|yv16";
+    //                                       1       2     3       4      5     6       7  8     9    10   11      12  13     14   15   16   17   18   19   20   21   22   23
+    static const char valid_pattern [] = "nv12( |:mono)|yv12( |:mono)|rgb24|rgb32|yuy2(:h|:v|:mono)|ayuv|p010|a2rgb10|r16|argb16|nv16|p210|y410|y210|y216|i444|yv16|p016|y416";
 
     //if external pattern changed parsing need to be updated
     int const check = std::string(MFX_FOURCC_PATTERN()).compare(valid_pattern);
@@ -887,6 +887,18 @@ mfxStatus GetMFXFrameInfoFromFOURCCPatternIdx(int idx_in_pattern, mfxFrameInfo &
         {
             info.FourCC = MFX_MAKEFOURCC('Y','V','1','6');
             info.ChromaFormat = MFX_CHROMAFORMAT_YUV422;
+            break;
+        }
+        case 22:
+        {
+            info.FourCC = MFX_FOURCC_P016;
+            info.ChromaFormat = MFX_CHROMAFORMAT_YUV420;
+            break;
+        }
+        case 23:
+        {
+            info.FourCC = MFX_FOURCC_Y416;
+            info.ChromaFormat = MFX_CHROMAFORMAT_YUV444;
             break;
         }
         default:

_testsuite/mfx_player/src/mfx_sysmem_allocator.cpp
@@ -198,10 +198,10 @@ mfxStatus SysMemFrameAllocator::LockFrame(mfxMemId mid, mfxFrameData *ptr)
         break;
 
     case MFX_FOURCC_Y416:
-        ptr->A = (mfxU8 *)ptr->B;
-        ptr->V16 = ((mfxU16*)ptr->A) + 1;
-        ptr->Y16 = ptr->V16 + 1;
-        ptr->U16 = ptr->Y16 + 1;
+        ptr->U16 = (mfxU16*)ptr->B;
+        ptr->Y16 = ptr->U16 + 1;
+        ptr->V16 = ptr->Y16 + 1;
+        ptr->A   = (mfxU8 *)ptr->V16 + 1;
         ptr->PitchHigh = (mfxU16)((8 * (mfxU32)Width2) / (1 << 16));
         ptr->PitchLow  = (mfxU16)((8 * (mfxU32)Width2) % (1 << 16));
         break;

commit 0b5e7e522b6c41f672e4a65ce77bb3130094d37f

[player]: fix conversion y410->i410
 - conversion from packed y410 to planar 10b 444
   was broken by b21367d7.

Issue: VSMGWL-3374
Test: hevc10d_444_sbe

Change-Id: I798e2b1e92ade016e28c525ea4a609664f46e186

_testsuite/mfx_player/src/mfx_renders.cpp
@@ -1979,7 +1979,7 @@ mfxFrameSurface1* ConvertSurface(mfxFrameSurface1* pSurfaceIn, mfxFrameSurface1*
             Y410Pixel const* pSrc = (Y410Pixel*)pSurfaceIn->Data.U;
 
             pitchOut /= sizeof(mfxU16);
-            pitchIn  /= pitchIn / sizeof(Y410Pixel);
+            pitchIn  /= sizeof(Y410Pixel);
 
             for (size_t i = 0; i < pSurfaceIn->Info.Height; i++)
             {