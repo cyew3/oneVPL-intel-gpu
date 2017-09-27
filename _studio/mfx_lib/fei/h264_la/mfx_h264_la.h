d1b5f826549d3fdeffd323abb5b117527b632553
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2017-09-27 08:08:01 -0700

    [h264/fei_la] switch from DEDICATED to INTRA policy
    Issue: MDP-25971
    Test: manual, automated
    
    Change-Id: Ie4d62da7553383bb627acf2829f3c8b5a231ec3b
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- _studio/mfx_lib/fei/h264_la/mfx_h264_la.h
+++ _studio/mfx_lib/fei/h264_la/mfx_h264_la.h
@@ -216,7 +216,7 @@ public:
     virtual
     mfxStatus Close(void);
     virtual
-    mfxTaskThreadingPolicy GetThreadingPolicy(void) {return MFX_TASK_THREADING_DEDICATED;}
+    mfxTaskThreadingPolicy GetThreadingPolicy(void) {return MFX_TASK_THREADING_INTRA;}
 
     static 
     mfxStatus Query(VideoCORE*, mfxVideoParam *in, mfxVideoParam *out);
