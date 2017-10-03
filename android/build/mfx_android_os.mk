a23d5d9d81c4eaa964e395bbc796dce57b3f9774
Dmitry Brazhkin <dmitry.brazhkin@intel.com>
2017-10-03 20:55:39 +0300

    Removed Android version from OTC
    Issue: no
    Test: Manual
    
    Change-Id: I5af8b23cafb4b89ec85f5b6db2758a0f52dd38d5
    Signed-off-by: Dmitry Brazhkin <dmitry.brazhkin@intel.com>

--- android/build/mfx_android_os.mk
+++ android/build/mfx_android_os.mk
@@ -16,13 +16,11 @@ UFO_ENABLE_GEN ?= gen7
 
 # libpavp.h
 ifeq ($(MFX_OMX_PAVP),true)
-  ifneq ($(filter $(MFX_ANDROID_VERSION), MFX_MCG_JB MFX_MCG_KK MFX_MCG_LD MFX_MM MFX_N MFX_O),)
-    MFX_C_INCLUDES_OMX += \
-      $(TOP)/vendor/intel/hardware/PRIVATE/ufo/inc/libpavp \
-      $(TOP)/vendor/intel/ufo/$(UFO_ENABLE_GEN)/x86 \
-      $(TOP)/vendor/intel/ufo/$(UFO_ENABLE_GEN)/include/libpavp \
-      $(TOP)/vendor/intel/ufo/$(UFO_ENABLE_GEN)_dev/include/libpavp
-  endif
+  MFX_C_INCLUDES_OMX += \
+    $(TOP)/vendor/intel/hardware/PRIVATE/ufo/inc/libpavp \
+    $(TOP)/vendor/intel/ufo/$(UFO_ENABLE_GEN)/x86 \
+    $(TOP)/vendor/intel/ufo/$(UFO_ENABLE_GEN)/include/libpavp \
+    $(TOP)/vendor/intel/ufo/$(UFO_ENABLE_GEN)_dev/include/libpavp
 endif
 
 ifeq ($(BOARD_USES_GRALLOC1),true)
--- android/include/mfx_config.h
+++ android/include/mfx_config.h
@@ -11,26 +11,12 @@ Copyright(c) 2011-2017 Intel Corporation. All Rights Reserved.
 #ifndef __MFX_CONFIG_H__
 #define __MFX_CONFIG_H__
 
-/* Versions of Android inside Intel */
-#define MFX_OTC     0x100
-#define MFX_MCG     0x200
-
 /* Google versions of Android */
-#define MFX_ANDROID_NAME 0xff
-#define MFX_JB           0x01
-#define MFX_KK           0x02
-#define MFX_LD           0x04
-#define MFX_MM           0x08
-#define MFX_N            0x10
-#define MFX_O            0x20
-
-#define MFX_OTC_JB  MFX_OTC + MFX_JB
-#define MFX_MCG_JB  MFX_MCG + MFX_JB
-
-#define MFX_OTC_KK  MFX_OTC + MFX_KK
-#define MFX_MCG_KK  MFX_MCG + MFX_KK
-
-#define MFX_OTC_LD  MFX_OTC + MFX_LD
-#define MFX_MCG_LD  MFX_MCG + MFX_LD
+#define MFX_JB   0x01
+#define MFX_KK   0x02
+#define MFX_LD   0x04
+#define MFX_MM   0x08
+#define MFX_N    0x10
+#define MFX_O    0x20
 
 #endif // #ifndef __MFX_CONFIG_H__
