4314aecb661dff04cf7d3ca6d5c6cda278144d74
Miroslav Goncharenko <miroslav.goncharenko@intel.com>
2017-10-18 15:03:01 +0300

    [sample_hevc_fei] Initialize internal predictors independently with MVP
    Issue: N/A
    Test: manual
    
    Change-Id: I2556459df7c3d36e53e2074f4174bd019dbf1f37

--- samples/sample_hevc_fei/src/hevc_fei_encode.cpp
+++ samples/sample_hevc_fei/src/hevc_fei_encode.cpp
@@ -296,20 +296,22 @@ mfxStatus FEI_Encode::SetCtrlParams(const HevcTask& task)
 
     FillHEVCRefLists(task, *pRefLists);
 
+    // Get Frame Control buffer
+    mfxExtFeiHevcEncFrameCtrl* ctrl = m_encodeCtrl.GetExtBuffer<mfxExtFeiHevcEncFrameCtrl>();
+    MSDK_CHECK_POINTER(ctrl, MFX_ERR_NOT_INITIALIZED);
+
+    // Switch on internal L0 predictors; 1 means spatial predictors
+    ctrl->MultiPred[0] = !!(m_encodeCtrl.FrameType & (MFX_FRAMETYPE_P | MFX_FRAMETYPE_B));
+
+    // Switch on internal L1 predictors; 1 means spatial predictors
+    ctrl->MultiPred[1] = (m_encodeCtrl.FrameType & MFX_FRAMETYPE_B) ||
+        ((m_encodeCtrl.FrameType & MFX_FRAMETYPE_P) && (m_videoParams.GetExtBuffer<mfxExtCodingOption3>()->GPB != MFX_CODINGOPTION_OFF));
+
     if (m_repacker.get() || m_pFile_MVP_in.get())
     {
-        mfxExtFeiHevcEncFrameCtrl* ctrl = m_encodeCtrl.GetExtBuffer<mfxExtFeiHevcEncFrameCtrl>();
-        MSDK_CHECK_POINTER(ctrl, MFX_ERR_NOT_INITIALIZED);
-
         // Switch predictors off for I-frames.
         // 0 - no predictors, 7 - inherit size of block from buffers CTU setting
         ctrl->MVPredictor = (m_encodeCtrl.FrameType & MFX_FRAMETYPE_I) ? 0 : 7;
-
-        // Switch on internal L0 predictors; 1 means spatial predictors
-        ctrl->MultiPred[0] = !!(m_encodeCtrl.FrameType & (MFX_FRAMETYPE_P | MFX_FRAMETYPE_B));
-
-        // Switch on internal L1 predictors; 1 means spatial predictors
-        ctrl->MultiPred[1] = !!(m_encodeCtrl.FrameType & MFX_FRAMETYPE_B);
 
         ctrl->NumMvPredictors[0] = ctrl->NumMvPredictors[1] = 0;
 
