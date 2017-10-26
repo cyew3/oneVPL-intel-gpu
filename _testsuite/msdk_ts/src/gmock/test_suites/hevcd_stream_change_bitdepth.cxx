9005f7e318eb064aee7899171db959fe29d2c77a
Dmitry Gurulev <dmitry.gurulev@intel.com>
2017-10-26 12:51:02 +0300

    [test/h265d]: add 12b 420/422/444 bit depth change beh. test
    Issue: VSMG-505, VSMG-522, VSMG-523
    Test: hevcd_12b_420_p016_bitdepth_change,
          hevcd_12b_422_y216_bitdepth_change
          hevcd_12b_444_y416_bitdepth_change
    
    Change-Id: I76ae1cf3a34f275fe7b91bf3b38dd1902dc2a0e5

--- _testsuite/msdk_ts/src/gmock/test_suites/hevcd_stream_change_bitdepth.cxx
+++ _testsuite/msdk_ts/src/gmock/test_suites/hevcd_stream_change_bitdepth.cxx
@@ -65,11 +65,46 @@ TestSuite::tc_struct const TestSuiteExt<MFX_FOURCC_Y410, bitdepth_change_tag>::t
 template <>
 unsigned int const TestSuiteExt<MFX_FOURCC_Y410, bitdepth_change_tag>::n_cases = sizeof(TestSuiteExt<MFX_FOURCC_Y410, bitdepth_change_tag>::test_cases) / sizeof(TestSuite::tc_struct[max_num_ctrl]);
 
+/* 12b 420 */
+template <>
+TestSuite::tc_struct const TestSuiteExt<MFX_FOURCC_P016, bitdepth_change_tag>::test_cases[][TestSuite::max_num_ctrl] =
+{
+    {/* 0*/ {"hevc/420format_12bit/GENERAL_12b_420_RExt_Sony_1.bit"},    {"hevc/420format/Kimono1_704x576_24_420_8.265", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+    {/* 1*/ {"hevc/420format_12bit/GENERAL_12b_420_RExt_Sony_1.bit", 5}, {"hevc/420format/Kimono1_704x576_24_420_8.265", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+};
+template <>
+unsigned int const TestSuiteExt<MFX_FOURCC_P016, bitdepth_change_tag>::n_cases = sizeof(TestSuiteExt<MFX_FOURCC_P016, bitdepth_change_tag>::test_cases) / sizeof(TestSuite::tc_struct[max_num_ctrl]);
+
+/* 12b 422 */
+template <>
+TestSuite::tc_struct const TestSuiteExt<MFX_FOURCC_Y216, bitdepth_change_tag>::test_cases[][TestSuite::max_num_ctrl] =
+{
+    {/* 0*/ {"hevc/422format_12bit/GENERAL_12b_422_RExt_Sony_1.bit"},    {"hevc/422format/Kimono1_704x576_24_422_8.265", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+    {/* 1*/ {"hevc/422format_12bit/GENERAL_12b_422_RExt_Sony_1.bit", 5}, {"hevc/422format/Kimono1_704x576_24_422_8.265", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+};
+template <>
+unsigned int const TestSuiteExt<MFX_FOURCC_Y216, bitdepth_change_tag>::n_cases = sizeof(TestSuiteExt<MFX_FOURCC_Y216, bitdepth_change_tag>::test_cases) / sizeof(TestSuite::tc_struct[max_num_ctrl]);
+
+/* 12b 444 */
+template <>
+TestSuite::tc_struct const TestSuiteExt<MFX_FOURCC_Y416, bitdepth_change_tag>::test_cases[][TestSuite::max_num_ctrl] =
+{
+    {/* 0*/ {"hevc/444format_12bit/GENERAL_12b_444_RExt_Sony_1.bit"},    {"hevc/444format/Kimono1_704x576_24_444_8.265", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+    {/* 1*/ {"hevc/444format_12bit/GENERAL_12b_444_RExt_Sony_1.bit", 5}, {"hevc/444format/Kimono1_704x576_24_444_8.265", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+};
+template <>
+unsigned int const TestSuiteExt<MFX_FOURCC_Y416, bitdepth_change_tag>::n_cases = sizeof(TestSuiteExt<MFX_FOURCC_Y416, bitdepth_change_tag>::test_cases) / sizeof(TestSuite::tc_struct[max_num_ctrl]);
+
 TS_REG_TEST_SUITE(hevcd_bitdepth_change,       (TestSuiteExt<MFX_FOURCC_NV12, bitdepth_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_NV12, bitdepth_change_tag>::n_cases));
 TS_REG_TEST_SUITE(hevcd_422_bitdepth_change,   (TestSuiteExt<MFX_FOURCC_YUY2, bitdepth_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_YUY2, bitdepth_change_tag>::n_cases));
 TS_REG_TEST_SUITE(hevcd_444_bitdepth_change,   (TestSuiteExt<MFX_FOURCC_AYUV, bitdepth_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_AYUV, bitdepth_change_tag>::n_cases));
+
 TS_REG_TEST_SUITE(hevc10d_bitdepth_change,     (TestSuiteExt<MFX_FOURCC_P010, bitdepth_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_P010, bitdepth_change_tag>::n_cases));
 TS_REG_TEST_SUITE(hevc10d_422_bitdepth_change, (TestSuiteExt<MFX_FOURCC_Y210, bitdepth_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_Y210, bitdepth_change_tag>::n_cases));
 TS_REG_TEST_SUITE(hevc10d_444_bitdepth_change, (TestSuiteExt<MFX_FOURCC_Y410, bitdepth_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_Y410, bitdepth_change_tag>::n_cases));
+
+TS_REG_TEST_SUITE(hevcd_12b_420_p016_bitdepth_change, (TestSuiteExt<MFX_FOURCC_P016, bitdepth_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_P016, bitdepth_change_tag>::n_cases));
+TS_REG_TEST_SUITE(hevcd_12b_422_y216_bitdepth_change, (TestSuiteExt<MFX_FOURCC_Y216, bitdepth_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_Y216, bitdepth_change_tag>::n_cases));
+TS_REG_TEST_SUITE(hevcd_12b_444_y416_bitdepth_change, (TestSuiteExt<MFX_FOURCC_Y416, bitdepth_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_Y416, bitdepth_change_tag>::n_cases));
 
 #endif //TS_REG_INCLUDE_HEVCD_STREAM_CHANGE
