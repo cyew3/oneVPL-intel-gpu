882302fd8e32cc8931f5acc43f3af35e7c7c3e99
Dmitry Gurulev <dmitry.gurulev@intel.com>
2017-10-26 13:02:35 +0300

    [test/h265d]: add 12b 420/422/444 chroma change beh. test
    Issue: VSMG-505, VSMG-522, VSMG-523
    Test: hevcd_12b_420_p016_chroma_change,
          hevcd_12b_422_y216_chroma_change
          hevcd_12b_444_y416_chroma_change
    
    Change-Id: I9aa437aa92301ceac9d228cecc62132f853b373b

--- _testsuite/msdk_ts/src/gmock/test_suites/hevcd_stream_change_chroma.cxx
+++ _testsuite/msdk_ts/src/gmock/test_suites/hevcd_stream_change_chroma.cxx
@@ -77,11 +77,52 @@ TestSuite::tc_struct const TestSuiteExt<MFX_FOURCC_Y410, chroma_change_tag>::tes
 template <>
 unsigned int const TestSuiteExt<MFX_FOURCC_Y410, chroma_change_tag>::n_cases = sizeof(TestSuiteExt<MFX_FOURCC_Y410, chroma_change_tag>::test_cases) / sizeof(TestSuite::tc_struct[max_num_ctrl]);
 
+/* 12b 420 */
+template <>
+TestSuite::tc_struct const TestSuiteExt<MFX_FOURCC_P016, chroma_change_tag>::test_cases[][TestSuite::max_num_ctrl] =
+{
+    {/* 0*/ {"hevc/420format_12bit/GENERAL_12b_420_RExt_Sony_1.bit"},    {"hevc/422format_12bit/GENERAL_12b_422_RExt_Sony_1.bit", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+    {/* 1*/ {"hevc/420format_12bit/GENERAL_12b_420_RExt_Sony_1.bit", 5}, {"hevc/422format_12bit/GENERAL_12b_422_RExt_Sony_1.bit", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+    {/* 2*/ {"hevc/420format_12bit/GENERAL_12b_420_RExt_Sony_1.bit"},    {"hevc/444format_12bit/GENERAL_12b_444_RExt_Sony_1.bit", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+    {/* 3*/ {"hevc/420format_12bit/GENERAL_12b_420_RExt_Sony_1.bit", 5}, {"hevc/444format_12bit/GENERAL_12b_444_RExt_Sony_1.bit", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+};
+template <>
+unsigned int const TestSuiteExt<MFX_FOURCC_P016, chroma_change_tag>::n_cases = sizeof(TestSuiteExt<MFX_FOURCC_P016, chroma_change_tag>::test_cases) / sizeof(TestSuite::tc_struct[max_num_ctrl]);
+
+/* 12b 422 */
+template <>
+TestSuite::tc_struct const TestSuiteExt<MFX_FOURCC_Y216, chroma_change_tag>::test_cases[][TestSuite::max_num_ctrl] =
+{
+    {/* 0*/ {"hevc/422format_12bit/GENERAL_12b_422_RExt_Sony_1.bit"},    {"hevc/420format_12bit/GENERAL_12b_420_RExt_Sony_1.bit", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+    {/* 1*/ {"hevc/422format_12bit/GENERAL_12b_422_RExt_Sony_1.bit", 5}, {"hevc/420format_12bit/GENERAL_12b_420_RExt_Sony_1.bit", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+    {/* 2*/ {"hevc/422format_12bit/GENERAL_12b_422_RExt_Sony_1.bit"},    {"hevc/444format_12bit/GENERAL_12b_444_RExt_Sony_1.bit", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+    {/* 3*/ {"hevc/422format_12bit/GENERAL_12b_422_RExt_Sony_1.bit", 5}, {"hevc/444format_12bit/GENERAL_12b_444_RExt_Sony_1.bit", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+};
+template <>
+unsigned int const TestSuiteExt<MFX_FOURCC_Y216, chroma_change_tag>::n_cases = sizeof(TestSuiteExt<MFX_FOURCC_Y216, chroma_change_tag>::test_cases) / sizeof(TestSuite::tc_struct[max_num_ctrl]);
+
+/* 12b 444 */
+template <>
+TestSuite::tc_struct const TestSuiteExt<MFX_FOURCC_Y416, chroma_change_tag>::test_cases[][TestSuite::max_num_ctrl] =
+{
+    {/* 0*/ {"hevc/444format_12bit/GENERAL_12b_444_RExt_Sony_1.bit"},    {"hevc/420format_12bit/GENERAL_12b_420_RExt_Sony_1.bit", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+    {/* 1*/ {"hevc/444format_12bit/GENERAL_12b_444_RExt_Sony_1.bit", 5}, {"hevc/420format_12bit/GENERAL_12b_420_RExt_Sony_1.bit", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+    {/* 2*/ {"hevc/444format_12bit/GENERAL_12b_444_RExt_Sony_1.bit"},    {"hevc/422format_12bit/GENERAL_12b_422_RExt_Sony_1.bit", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+    {/* 3*/ {"hevc/444format_12bit/GENERAL_12b_444_RExt_Sony_1.bit", 5}, {"hevc/422format_12bit/GENERAL_12b_422_RExt_Sony_1.bit", 0, MFX_ERR_INCOMPATIBLE_VIDEO_PARAM}},
+};
+template <>
+unsigned int const TestSuiteExt<MFX_FOURCC_Y416, chroma_change_tag>::n_cases = sizeof(TestSuiteExt<MFX_FOURCC_Y416, chroma_change_tag>::test_cases) / sizeof(TestSuite::tc_struct[max_num_ctrl]);
+
 TS_REG_TEST_SUITE(hevcd_chroma_change,       (TestSuiteExt<MFX_FOURCC_NV12, chroma_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_NV12, chroma_change_tag>::n_cases));
 TS_REG_TEST_SUITE(hevcd_422_chroma_change,   (TestSuiteExt<MFX_FOURCC_YUY2, chroma_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_YUY2, chroma_change_tag>::n_cases));
 TS_REG_TEST_SUITE(hevcd_444_chroma_change,   (TestSuiteExt<MFX_FOURCC_AYUV, chroma_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_AYUV, chroma_change_tag>::n_cases));
+
 TS_REG_TEST_SUITE(hevc10d_chroma_change,     (TestSuiteExt<MFX_FOURCC_P010, chroma_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_P010, chroma_change_tag>::n_cases));
 TS_REG_TEST_SUITE(hevc10d_422_chroma_change, (TestSuiteExt<MFX_FOURCC_Y210, chroma_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_Y210, chroma_change_tag>::n_cases));
 TS_REG_TEST_SUITE(hevc10d_444_chroma_change, (TestSuiteExt<MFX_FOURCC_Y410, chroma_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_Y410, chroma_change_tag>::n_cases));
+
+TS_REG_TEST_SUITE(hevcd_12b_420_p016_chroma_change, (TestSuiteExt<MFX_FOURCC_P016, chroma_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_P016, chroma_change_tag>::n_cases));
+TS_REG_TEST_SUITE(hevcd_12b_422_y216_chroma_change, (TestSuiteExt<MFX_FOURCC_Y216, chroma_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_Y216, chroma_change_tag>::n_cases));
+TS_REG_TEST_SUITE(hevcd_12b_444_y416_chroma_change, (TestSuiteExt<MFX_FOURCC_Y416, chroma_change_tag>::RunTest), (TestSuiteExt<MFX_FOURCC_Y416, chroma_change_tag>::n_cases));
 
 #endif //TS_REG_INCLUDE_HEVCD_STREAM_CHANGE
