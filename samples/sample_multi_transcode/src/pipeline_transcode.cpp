cyew3/oneVPL-intel-gpu

commit d3980ae5049b98bc033ec1b9329a45f866586967

[samples][smt] do not use weaving along with di
Issue: MDP-38113
Test: h264d_hevcd_to_hevce_multiprocess_multisession-plugin_hevcdhw_hevcehw

Change-Id: If3555fc3d95a987234b759dc0afb987d1556e0df

samples/sample_multi_transcode/src/pipeline_transcode.cpp
@@ -367,12 +367,14 @@ mfxStatus CTranscodingPipeline::VPPPreInit(sInputParams *pParams)
 
     if (m_bEncodeEnable || m_bDecodeEnable)
     {
-        if (m_mfxDecParams.mfx.FrameInfo.PicStruct == MFX_PICSTRUCT_FIELD_SINGLE && pParams->EncodeId != MFX_CODEC_HEVC)
+        if (m_mfxDecParams.mfx.FrameInfo.PicStruct == MFX_PICSTRUCT_FIELD_SINGLE &&
+            pParams->EncodeId != MFX_CODEC_HEVC && !pParams->bEnableDeinterlacing)
         {
             m_bIsFieldWeaving = true;
             m_bIsVpp = true;
         }
-        if (m_mfxDecParams.mfx.FrameInfo.PicStruct != MFX_PICSTRUCT_PROGRESSIVE && pParams->EncodeId == MFX_CODEC_HEVC && pParams->DecodeId != MFX_CODEC_HEVC)
+        if ((m_mfxDecParams.mfx.FrameInfo.PicStruct & MFX_PICSTRUCT_FIELD_TFF || m_mfxDecParams.mfx.FrameInfo.PicStruct & MFX_PICSTRUCT_FIELD_BFF)
+            && pParams->EncodeId == MFX_CODEC_HEVC && pParams->DecodeId != MFX_CODEC_HEVC )
         {
             m_bIsFieldSplitting = true;
             m_bIsVpp = true;

commit 3aadcbf329164acad838df559aab939d87a550aa

[samples][smt] regression on Linux after d3980ae5 commit
Issue: MDP-38165
Test: h264d_to_hevce_field_splitting-plugin_hevcehw

In d3980ae5 commit was fix in Windows.
But it caused regression on Linux

Change-Id: Ibd9052652a5b18c3500e6c3aac5a002ede508103
Signed-off-by: Andrey Larionov <andrey.larionov@intel.com>

samples/sample_multi_transcode/src/pipeline_transcode.cpp
@@ -373,8 +373,9 @@ mfxStatus CTranscodingPipeline::VPPPreInit(sInputParams *pParams)
             m_bIsFieldWeaving = true;
             m_bIsVpp = true;
         }
-        if ((m_mfxDecParams.mfx.FrameInfo.PicStruct & MFX_PICSTRUCT_FIELD_TFF || m_mfxDecParams.mfx.FrameInfo.PicStruct & MFX_PICSTRUCT_FIELD_BFF)
-            && pParams->EncodeId == MFX_CODEC_HEVC && pParams->DecodeId != MFX_CODEC_HEVC )
+
+        if ((m_mfxDecParams.mfx.FrameInfo.PicStruct != MFX_PICSTRUCT_PROGRESSIVE) &&
+            (pParams->EncodeId == MFX_CODEC_HEVC) && (pParams->DecodeId != MFX_CODEC_HEVC) )
         {
             m_bIsFieldSplitting = true;
             m_bIsVpp = true;