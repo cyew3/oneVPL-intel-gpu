955bfef0f62721822bbe85720588af93c4413de1
Starov, Evgeniy <evgeniy.starov@intel.com>
2017-11-30 19:00:51 +0300

    [VPP][Test] Query test for REXT
    Issue: N/A
    Test: beh_vpp_8b_420_yv12_query
    beh_vpp_8b_422_uyvy_query
    beh_vpp_8b_422_yuy2_query
    beh_vpp_8b_444_ayuv_query
    beh_vpp_8b_444_rgb4_query
    beh_vpp_10b_420_p010_query
    beh_vpp_10b_422_y210_query
    beh_vpp_10b_444_y410_query
    
    Change-Id: I340a9f6055f963ebb1b04e5fffe1b59608994c90

--- _testsuite/msdk_ts/src/gmock/test_suites/vpp_query.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/vpp_query.cpp
@@ -64,7 +64,7 @@ namespace vpp_query
     };
 
     //!\brief Main test class
-    class TestSuite:tsVideoVPP
+    class TestSuite : protected tsVideoVPP
     {
     public:
         //! \brief A constructor (AVC VPP)
@@ -79,7 +79,8 @@ namespace vpp_query
         ~TestSuite() {}
         //! \brief Main method. Runs test case
         //! \param id - test case number
-        int RunTest(unsigned int id);
+        int RunTest(unsigned int id) { return RunTest(test_case[id]); }
+        int RunTest(const tc_struct& tc);
         //! The number of test cases
         static const unsigned int n_cases;
         //! \brief Set of test cases
@@ -293,11 +294,10 @@ namespace vpp_query
 
     const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case) / sizeof(TestSuite::test_case[0]);
 
-    int TestSuite::RunTest(unsigned int id)
+    int TestSuite::RunTest(const tc_struct& tc)
     {
         TS_START;
         MFXInit();
-        auto& tc = test_case[id];
         mfxSession work_ses = m_session;
 
         SETPARS(&m_par, MFX_PAR);
@@ -321,6 +321,112 @@ namespace vpp_query
 
     //! \brief Regs test suite into test system
     TS_REG_TEST_SUITE_CLASS(vpp_query);
+}
+
+namespace vpp_rext_query
+{
+    const struct
+    {
+        mfxU32 FourCC;
+        mfxU16 ChromaFormat;
+        mfxU16 BdY;
+        mfxU16 BdC;
+        mfxU16 Shift;
+    } Formats[] =
+    {
+        /*00*/{ MFX_FOURCC_NV12, MFX_CHROMAFORMAT_YUV420, 8, 8, 0 },
+        /*01*/{ MFX_FOURCC_YV12, MFX_CHROMAFORMAT_YUV420, 8, 8, 0 },
+        /*02*/{ MFX_FOURCC_UYVY, MFX_CHROMAFORMAT_YUV422, 8, 8, 0 },
+        /*03*/{ MFX_FOURCC_YUY2, MFX_CHROMAFORMAT_YUV422, 8, 8, 0 },
+        /*04*/{ MFX_FOURCC_AYUV, MFX_CHROMAFORMAT_YUV444, 8, 8, 0 },
+        /*05*/{ MFX_FOURCC_RGB4, MFX_CHROMAFORMAT_YUV444, 8, 8, 0 },
+        /*06*/{ MFX_FOURCC_P010, MFX_CHROMAFORMAT_YUV420, 10, 10, 1 },
+        /*07*/{ MFX_FOURCC_Y210, MFX_CHROMAFORMAT_YUV422, 10, 10, 1 },
+        /*08*/{ MFX_FOURCC_Y410, MFX_CHROMAFORMAT_YUV444, 10, 10, 0 },
+        /*09*/{ MFX_FOURCC_A2RGB10, MFX_CHROMAFORMAT_YUV444, 10, 10, 0 },
+    };
+    const unsigned int nFormats = sizeof(vpp_rext_query::Formats) / sizeof(vpp_rext_query::Formats[0]);
+    const mfxStatus HW = MFX_ERR_NONE;
+    const mfxStatus SW = MFX_WRN_PARTIAL_ACCELERATION;
+    const mfxStatus NO = MFX_ERR_UNSUPPORTED;
+
+    const mfxStatus CCSupport[][nFormats][nFormats] =
+    {
+        {//gen9
+        //   in\out NV12  YV12  UYVY  YUY2  AYUV  RGB4  P010  Y210  Y410  A2RGB10
+        /*   NV12*/{  HW,   SW,   NO,   HW,   NO,   HW,   SW,   NO,   NO,      NO },
+        /*   YV12*/{  HW,   SW,   NO,   HW,   NO,   HW,   SW,   NO,   NO,      NO },
+        /*   UYVY*/{  SW,   SW,   NO,   SW,   NO,   SW,   SW,   NO,   NO,      NO },
+        /*   YUY2*/{  HW,   SW,   NO,   HW,   NO,   HW,   SW,   NO,   NO,      NO },
+        /*   AYUV*/{  NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,      NO },
+        /*   RGB4*/{  HW,   SW,   NO,   HW,   NO,   HW,   SW,   NO,   NO,      NO },
+        /*   P010*/{  SW,   NO,   NO,   NO,   NO,   HW,   SW,   NO,   NO,      NO },
+        /*   Y210*/{  NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,      NO },
+        /*   Y410*/{  NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,      NO },
+        /*A2RGB10*/{  NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,      NO },
+        },
+        {//gen11
+        //   in\out NV12  YV12  UYVY  YUY2  AYUV  RGB4  P010  Y210  Y410  A2RGB10
+        /*   NV12*/{  HW,   SW,   NO,   HW,   HW,   HW,   HW,   HW,   HW,      HW },
+        /*   YV12*/{  HW,   SW,   NO,   HW,   HW,   HW,   HW,   HW,   HW,      HW },
+        /*   UYVY*/{  HW,   SW,   NO,   HW,   HW,   HW,   HW,   HW,   HW,      HW },
+        /*   YUY2*/{  HW,   SW,   NO,   HW,   HW,   HW,   HW,   HW,   HW,      HW },
+        /*   AYUV*/{  HW,   NO,   NO,   HW,   HW,   HW,   HW,   HW,   HW,      HW },
+        /*   RGB4*/{  HW,   SW,   NO,   HW,   HW,   HW,   HW,   HW,   HW,      HW },
+        /*   P010*/{  HW,   NO,   NO,   HW,   HW,   HW,   HW,   HW,   HW,      HW },
+        /*   Y210*/{  HW,   NO,   NO,   HW,   HW,   HW,   HW,   HW,   HW,      HW },
+        /*   Y410*/{  HW,   NO,   NO,   HW,   HW,   HW,   HW,   HW,   HW,      HW },
+        /*A2RGB10*/{  HW,   NO,   NO,   HW,   HW,   HW,   HW,   HW,   HW,      HW },
+        },
+    };
+
+    template<mfxU16 FmtID>
+    class TestSuite : public vpp_query::TestSuite
+    {
+    public:
+        TestSuite()
+            : vpp_query::TestSuite()
+        {}
+
+        static const unsigned int n_cases = nFormats;
+
+        int RunTest(unsigned int id)
+        {
+            m_par.vpp.Out.FourCC            = vpp_rext_query::Formats[FmtID].FourCC;
+            m_par.vpp.Out.ChromaFormat      = vpp_rext_query::Formats[FmtID].BdY;
+            m_par.vpp.Out.BitDepthLuma      = vpp_rext_query::Formats[FmtID].BdC;
+            m_par.vpp.Out.BitDepthChroma    = vpp_rext_query::Formats[FmtID].ChromaFormat;
+            m_par.vpp.Out.Shift             = vpp_rext_query::Formats[FmtID].Shift;
+
+            m_par.vpp.In.FourCC             = vpp_rext_query::Formats[id].FourCC;
+            m_par.vpp.In.ChromaFormat       = vpp_rext_query::Formats[id].BdY;
+            m_par.vpp.In.BitDepthLuma       = vpp_rext_query::Formats[id].BdC;
+            m_par.vpp.In.BitDepthChroma     = vpp_rext_query::Formats[id].ChromaFormat;
+            m_par.vpp.In.Shift              = vpp_rext_query::Formats[id].Shift;
+
+            vpp_query::tc_struct tc = { vpp_query::STANDART };
+            tc.expected_sts = vpp_rext_query::CCSupport[g_tsHWtype >= MFX_HW_ICL][id][FmtID];
+
+            return  vpp_query::TestSuite::RunTest(tc);
+        }
+    };
+
+#define REG_VPP_REXT_QUERY_TEST(NAME, FMT_ID)                \
+    namespace NAME                                           \
+    {                                                        \
+        typedef vpp_rext_query::TestSuite<FMT_ID> TestSuite; \
+        TS_REG_TEST_SUITE_CLASS(NAME);                       \
+    }
+
+    REG_VPP_REXT_QUERY_TEST(vpp_8b_420_yv12_query, 1);
+    REG_VPP_REXT_QUERY_TEST(vpp_8b_422_uyvy_query, 2);
+    REG_VPP_REXT_QUERY_TEST(vpp_8b_422_yuy2_query, 3);
+    REG_VPP_REXT_QUERY_TEST(vpp_8b_444_ayuv_query, 4);
+    REG_VPP_REXT_QUERY_TEST(vpp_8b_444_rgb4_query, 5);
+    REG_VPP_REXT_QUERY_TEST(vpp_10b_420_p010_query, 6);
+    REG_VPP_REXT_QUERY_TEST(vpp_10b_422_y210_query, 7);
+    REG_VPP_REXT_QUERY_TEST(vpp_10b_444_y410_query, 8);
+#undef REG_VPP_REXT_QUERY_TEST
 }
 
 
