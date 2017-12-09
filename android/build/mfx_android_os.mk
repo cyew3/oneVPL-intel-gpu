c0f71c49c6107059808c53397c01d78730322cb9
Dmitry Brazhkin <dmitry.brazhkin@intel.com>
2017-12-09 09:53:30 +0300

    Remove libpavp dependency for Android N and later
    Issue: no
    Test: manual
    
    Change-Id: Id5a7a7ac4eb3c594435dd44ae43f7ef67ac084d8
    Signed-off-by: Dmitry Brazhkin <dmitry.brazhkin@intel.com>

--- android/build/mfx_android_os.mk
+++ android/build/mfx_android_os.mk
@@ -20,15 +20,18 @@ MFX_C_INCLUDES_OMX := \
   frameworks/native/include/media/openmax \
   frameworks/native/include/media/hardware
 
-UFO_ENABLE_GEN ?= gen7
+# libpavp.h, no need for N+
+ifneq ($(filter MFX_JB MFX_KK MFX_LD MFX_MM ,$(MFX_ANDROID_VERSION)),)
 
-# libpavp.h
-ifeq ($(MFX_OMX_PAVP),true)
-  MFX_C_INCLUDES_OMX += \
-    $(TOP)/vendor/intel/hardware/PRIVATE/ufo/inc/libpavp \
-    $(TOP)/vendor/intel/ufo/$(UFO_ENABLE_GEN)/x86 \
-    $(TOP)/vendor/intel/ufo/$(UFO_ENABLE_GEN)/include/libpavp \
-    $(TOP)/vendor/intel/ufo/$(UFO_ENABLE_GEN)_dev/include/libpavp
+  UFO_ENABLE_GEN ?= gen7
+
+  ifeq ($(MFX_OMX_PAVP),true)
+    MFX_C_INCLUDES_OMX += \
+      $(TOP)/vendor/intel/hardware/PRIVATE/ufo/inc/libpavp \
+      $(TOP)/vendor/intel/ufo/$(UFO_ENABLE_GEN)/x86 \
+      $(TOP)/vendor/intel/ufo/$(UFO_ENABLE_GEN)/include/libpavp \
+      $(TOP)/vendor/intel/ufo/$(UFO_ENABLE_GEN)_dev/include/libpavp
+  endif
 endif
 
 ifeq ($(BOARD_USES_GRALLOC1),true)
