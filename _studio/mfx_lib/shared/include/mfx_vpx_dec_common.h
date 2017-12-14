8ccf1c4740f64979f129df1aca6ba572a9d0f5e8
Efremov, Stanislav <stanislav.efremov@intel.com>
2017-12-14 09:16:34 +0300

    [vp9d][vp8d] Move common VPX dec code to separate file
    Issue: no
    Test: manual
    
    Change-Id: I43fab84dcdaf1f8c1a88ce9268f7028d0c09e5c4
    Signed-off-by: Efremov, Stanislav <stanislav.efremov@intel.com>

---
+++ _studio/mfx_lib/shared/include/mfx_vpx_dec_common.h
@@ -0,0 +1,27 @@
+//
+// INTEL CORPORATION PROPRIETARY INFORMATION
+//
+// This software is supplied under the terms of a license agreement or
+// nondisclosure agreement with Intel Corporation and may not be copied
+// or disclosed except in accordance with the terms of that agreement.
+//
+// Copyright(C) 2008-2017 Intel Corporation. All Rights Reserved.
+//
+
+#ifndef __MFX_VPX_DEC_COMMON__H__
+#define __MFX_VPX_DEC_COMMON__H__
+
+#include "mfx_config.h"
+#include "mfx_common_int.h"
+
+namespace MFX_VPX_Utility
+{
+    mfxStatus Query(VideoCORE*, mfxVideoParam const* in, mfxVideoParam* out, mfxU32 codecId, eMFXHWType type);
+
+    bool CheckVideoParam(mfxVideoParam const*, mfxU32 codecId, eMFXPlatform platform = MFX_PLATFORM_HARDWARE);
+    mfxStatus QueryIOSurfInternal(mfxVideoParam const*, mfxFrameAllocRequest*);
+
+    mfxStatus Convert_YV12_to_NV12(mfxFrameData const* inData,  mfxFrameInfo const* inInfo, mfxFrameData* outData, mfxFrameInfo const* outInfo);
+}
+
+#endif //__MFX_VPX_DEC_COMMON__H__
