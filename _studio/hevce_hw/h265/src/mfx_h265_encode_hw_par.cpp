226e6d356be177a50d78895f26edafc1d591cf77
Bykadorov, Roman <roman.bykadorov@intel.com>
2018-01-10 03:21:48 -0800

    [HEVCe][VDEnc][CNL] Updated SAO flags
    Disabled SAO chroma/luma only in case of CQP
    
    Issue: MDP-38745
    Tests: hevce_8b_420_nv12_sao-plugin_hevcehw-lowpower
    
    Change-Id: I051e418ff789abb693f927b1c3889b5e3b8acfb6

--- _studio/hevce_hw/h265/src/mfx_h265_encode_hw_par.cpp
+++ _studio/hevce_hw/h265/src/mfx_h265_encode_hw_par.cpp
@@ -2461,6 +2461,17 @@ mfxStatus CheckVideoParam(MfxVideoParam& par, ENCODE_CAPS_HEVC const & caps, boo
             , (mfxU16)MFX_SAO_ENABLE_CHROMA
             , (mfxU16)(MFX_SAO_ENABLE_LUMA | MFX_SAO_ENABLE_CHROMA)
         );
+        //On CNL VDEnc SAO for only Luma/Chroma in CQP mode isn't supported by driver until real customer usage
+        //For TU 1 and TU 4 SAO isn't supported due to HuC restrictions, for TU 7 SAO isn't supported at all
+        //It's only for CNL because for now we don't know will be ACQP used as default for CQP VDEnc on ICL or not
+        if (par.m_platform.CodeName == MFX_PLATFORM_CANNONLAKE &&
+            par.mfx.RateControlMethod == MFX_RATECONTROL_CQP &&
+            par.mfx.LowPower == MFX_CODINGOPTION_ON &&
+            (par.m_ext.HEVCParam.SampleAdaptiveOffset == (mfxU16)MFX_SAO_ENABLE_LUMA || par.m_ext.HEVCParam.SampleAdaptiveOffset == (mfxU16)MFX_SAO_ENABLE_CHROMA))
+        {
+            par.m_ext.HEVCParam.SampleAdaptiveOffset = (mfxU16)MFX_SAO_DISABLE;
+            changed++;
+        }
     }
     else
     {
