743b84058236833b3b8a67bc2e0b46f325a21d5b
Tyugina, Alexandra <alexandra.tyugina@intel.com>
2018-01-29 14:17:52 +0300

    [HEVC_FEI][beh] fix review remarks in level5 test
    1) add lookup table instead of computing the following expression:
       sqrt((mfxF32)LevelParam.MaxLumaPs * 8)
    2) add 13 more test cases to check maxHeight as well as maxWidth
    3) replace TS_MIN with std::min
    4) add a possibility to print out all set VideoParam values to files:
       - first 13 test cases - to file hevce_fei_level_profile1.log
       - next 13 test cases - to file hevce_fei_level_profile2.log
    5) remove obsolete constraints for NumSlice regarding the LCUsize
    6) update copyright year
    
    For more information, see the reviewers' comments at
    https://git-ccr-1.devtools.intel.com/gerrit/#/c/70516/.
    
    Issue: MDP-37046
    Test: manual run on Cent OS 7.3, driver version 16.8.68326
    
    Change-Id: Ifc39ddfc38291da8a1d3d1c22e181ee8e2e3b092

--- _testsuite/msdk_ts/src/gmock/test_suites/hevce_fei_level_profile.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/hevce_fei_level_profile.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************* *\
 
-Copyright (C) 2017 Intel Corporation.  All rights reserved.
+Copyright (C) 2017-2018 Intel Corporation.  All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -31,9 +31,11 @@ Description:
 This test suite checks what Levels are supported by HEVC FEI Encoder.
 
 In HEVC Standard there are 13 Levels:  1, 2, 2.1, 3, 3.1, 4, 4.1, 5, 5.1, 5.2, 6, 6.1, 6.2
-First 13 positive test cases are intended to test those 13 Levels (Level ID = test_case ID).
-For each test case, input values are: Level itself and resolution, frame rate and NumSlice
-specific for the given Level.
+First 26 positive test cases are intended to test those 13 Levels.
+For test cases #0-12, Level ID = test_case ID and Width is computed as
+maximum available value for the given Level.
+For test cases #13-25, Level ID = (test_case ID - NumberOfLevels) and
+Height is computed as maximum available value for the given Level.
 
 Algorithm:
 - MFXInit
@@ -41,7 +43,6 @@ Algorithm:
 - load Hevc FEI plugin
 - Init
 - EncodeFrames
-
 */
 
 #include "ts_encoder.h"
@@ -49,50 +50,58 @@ Algorithm:
 #include "ts_fei_warning.h"
 #include "ts_decoder.h"
 
+#include <iostream>
+#include <fstream>
+
 namespace hevce_fei_level_profile {
 
-// In case you need to verify the output manually, define the MANUAL_DEBUG_MODE macro.
-// In this case the encoded bitstream will be written to the test_out.h265 file.
+// In case you need to verify the output manually, define the MANUAL_DEBUG_MODE macro. The output files are:
+// hevce_fei_level_profile.h265 stores encoded bitstream,
+// hevce_fei_level_profile1.log stores video parameters which are set in TestSuite::SetParsPositiveTC() for the first 13 test cases,
+// hevce_fei_level_profile2.log stores video parameters which are set in TestSuite::SetParsPositiveTC() for the next 13 test cases.
 //#define MANUAL_DEBUG_MODE
 
-// struct to store constraints values for one Level
+// A structure to store constraints values for one Level
 struct LevelConstraints
 {
-    mfxU16 Level;     // to ease math operations, let's work with integer Level values:
-                      // 10, 20, 21, 30, 31, 40, 41, 50, 51, 52, 60, 61, 62
-    mfxU32 MaxLumaPs; // max luma picture size (samples)
-    mfxU32 MaxSSPP;   // max number of slice segments per picture which means:
-                      // max number of slices alowed per picture at both the max resolution and max Frame Rate
-    mfxU32 MaxLumaSr; // max luma sample rate (samples per second)
+    mfxU16 Level;         // numerical representation of Level values: 10, 20, 21, 30, 31, 40, 41, 50, 51, 52, 60, 61, 62
+    mfxU32 MaxLumaPs;     // max luma picture size (samples)
+    mfxU16 MaxResolution; // max Height or Width for the given Level which is computed as sqrt(MaxLumaPS*8) according to the HEVC Standard
+    mfxU32 MaxSSPP;       // max number of slice segments per picture which means:
+                          // max number of slices allowed per picture at both the max resolution and max Frame Rate
+    mfxU32 MaxLumaSr;     // max luma sample rate (samples per second)
 };
 
 // The following values are given according to the Table A.6 from the HEVC International Standard
-const LevelConstraints TableA6[13] =
+constexpr LevelConstraints TableA6[13] =
 {
-    // LevelID |  Level   | MaxLumaPs | Max Slice  |  MaxLumaSr
-    //                                |  Segments  |
-    //                                | PerPicture |
-    /*  0  */     { 10,      36864,         16,         552960      }, // only MAIN TIER is available
-    /*  1  */     { 20,      122880,        16,         3686400     }, // for all Levels < 4
-    /*  2  */     { 21,      245760,        20,         7372800     },
-    /*  3  */     { 30,      552960,        30,         16588800    },
-    /*  4  */     { 31,      983040,        40,         33177600    },
-    /*  5  */     { 40,      2228224,       75,         66846720    }, // MAIN and HIGH TIERs are available
-    /*  6  */     { 41,      2228224,       75,         133693440   }, // for all Levels >= 4
-    /*  7  */     { 50,      8912896,       200,        267386880   },
-    /*  8  */     { 51,      8912896,       200,        534773760   },
-    /*  9  */     { 52,      8912896,       200,        1069547520  },
-    /*  10 */     { 60,      35651584,      600,        1069547520  },
-    /*  11 */     { 61,      35651584,      600,        2139095040  },
-    /*  12 */     { 62,      35651584,      600,        4278190080  },
+    // LevelID |  Level   | MaxLumaPs | MaxWidth/ | Max Slice  |  MaxLumaSr
+    //                                | MaxHeight |  Segments  |
+    //                                |           | PerPicture |
+    /*  0  */     { 10,      36864,       543,         16,         552960      }, // only MAIN TIER is available
+    /*  1  */     { 20,      122880,      991,         16,         3686400     }, // for all Levels < 4
+    /*  2  */     { 21,      245760,      1402,        20,         7372800     },
+    /*  3  */     { 30,      552960,      2103,        30,         16588800    },
+    /*  4  */     { 31,      983040,      2804,        40,         33177600    },
+    /*  5  */     { 40,      2228224,     4222,        75,         66846720    }, // MAIN and HIGH TIERs are available
+    /*  6  */     { 41,      2228224,     4222,        75,         133693440   }, // for all Levels >= 4
+    /*  7  */     { 50,      8912896,     8444,        200,        267386880   },
+    /*  8  */     { 51,      8912896,     8444,        200,        534773760   },
+    /*  9  */     { 52,      8912896,     8444,        200,        1069547520  },
+    /*  10 */     { 60,      35651584,    16888,       600,        1069547520  },
+    /*  11 */     { 61,      35651584,    16888,       600,        2139095040  },
+    /*  12 */     { 62,      35651584,    16888,       600,        4278190080  },
 };
 
-inline mfxU16 CeilDiv(mfxU16 x, mfxU16 y) { return (x + y - 1) / y; }
+constexpr mfxU16 NumberOfLevels = sizeof(TableA6)/sizeof(LevelConstraints);
 
 class TestSuite : tsVideoEncoder, tsParserHEVC, tsBitstreamProcessor
 {
 public:
-    static const mfxU32 n_cases;
+
+    // There're 26 test cases (see more details in the test description in the beginning)
+    static constexpr mfxU16 n_cases = NumberOfLevels*2;
+
 #ifdef MANUAL_DEBUG_MODE
     tsBitstreamWriter m_tsBsWriter;
 #endif
@@ -102,7 +111,7 @@ public:
     , tsParserHEVC()
     , tsBitstreamProcessor()
 #ifdef MANUAL_DEBUG_MODE
-    , m_tsBsWriter("test_out.h265")
+    , m_tsBsWriter("hevce_fei_level_profile.h265")
 #endif
     {
         m_bs_processor = this;
@@ -111,21 +120,22 @@ public:
 
         // Set all parameters necessary for working with HEVC FEI encoder:
         mfxExtFeiHevcEncFrameCtrl& feiCtrl =  m_ctrl;
-        feiCtrl.SubPelMode         = 3;
-        feiCtrl.SearchWindow       = 5;
-        feiCtrl.NumFramePartitions = 4;
+        feiCtrl.SubPelMode         = 3; // quarter-pixel motion estimation
+        feiCtrl.SearchWindow       = 5; // 48 SUs 48x40 window full search
+        feiCtrl.NumFramePartitions = 4; // number of partitions in frame that encoder processes concurrently
     }
-
-    ~TestSuite() { delete m_filler; }
 
     mfxI32 RunTest(mfxU16 id);
 
 private:
 
-    void SetParsPositiveTC(const LevelConstraints& LevelParam) // LevelId = test_case ID
+    void SetParsPositiveTC(const LevelConstraints& LevelParam, bool maxWidthFlag)
     {
-        mfxU16 alignment = 16; // 32
-        mfxU16 maxFrameRateHEVC = 300; // The maximum frame rate supported by HEVC is 300 frames per second
+        constexpr mfxU16 alignment    = 16;
+        constexpr mfxU16 MaxFrameRate = 300;  // the maximum frame rate supported by HEVC is 300 frames per second
+        constexpr mfxU16 MaxWidth     = 4096; // width limitation in HEVC hardware encoder
+        constexpr mfxU16 MaxHeight    = 2176; // height limitation in HEVC hardware encoder
+        constexpr mfxU16 MaxNumSlice  = 200;  // due to driver issue, NumSlice is limited to 200
 
         // On the basis of formulas given in the HEVC Standard, Frame Width is defined as
         // the aligned value of maximum allowed width for specified Max Luma pic size:
@@ -138,31 +148,59 @@ private:
         // where MaxLumaPs is specified in Table A.6.
         // Due to the restriction of HEVC FEI Encoder implementation, the maximum supported resolution is 4096x2176.
         // If a greater resolution is specified for HEVC FEI Encoder, the Query function terminates with MFX_ERR_UNSUPPORTED status.
-        m_par.mfx.FrameInfo.Width  = TS_MIN(( (mfxU16)(sqrt((mfxF32)LevelParam.MaxLumaPs * 8)) & ~(alignment-1)), 4096);
-        m_par.mfx.FrameInfo.Height = TS_MIN(((LevelParam.MaxLumaPs / m_par.mfx.FrameInfo.Width) & ~(alignment-1)), 2176 );
-
+        if (maxWidthFlag)
+        {
+            m_par.mfx.FrameInfo.Width  = std::min((mfxU16)(LevelParam.MaxResolution & ~(alignment-1)), MaxWidth);
+            m_par.mfx.FrameInfo.Height = std::min((mfxU16)((LevelParam.MaxLumaPs / m_par.mfx.FrameInfo.Width) & ~(alignment-1)), MaxHeight );
+        }
+        else
+        {
+            m_par.mfx.FrameInfo.Height = std::min((mfxU16)(LevelParam.MaxResolution & ~(alignment-1)), MaxHeight);
+            m_par.mfx.FrameInfo.Width  = std::min((mfxU16)((LevelParam.MaxLumaPs / m_par.mfx.FrameInfo.Height) & ~(alignment-1)), MaxWidth );
+        }
         m_par.mfx.FrameInfo.CropW = m_par.mfx.FrameInfo.Width; // as the Frame Width and Height are aligned, we can use them as the Cropped Width and Height
         m_par.mfx.FrameInfo.CropH = m_par.mfx.FrameInfo.Height;
 
-        m_par.mfx.FrameInfo.FrameRateExtN = TS_MIN( (LevelParam.MaxLumaSr / (m_par.mfx.FrameInfo.Width * m_par.mfx.FrameInfo.Height)), maxFrameRateHEVC);
+        m_par.mfx.FrameInfo.FrameRateExtN = std::min((mfxU16)(LevelParam.MaxLumaSr / (m_par.mfx.FrameInfo.Width * m_par.mfx.FrameInfo.Height)), MaxFrameRate);
         m_par.mfx.FrameInfo.FrameRateExtD = 1;
 
-        m_par.mfx.CodecLevel = LevelParam.Level; // for Main tier, CodecLevel values are: 10, 20, 21, 30, 31, 40, 41, 50, 51, 52, 60, 61, 62
-        m_par.mfx.CodecProfile = MFX_PROFILE_HEVC_MAIN;   // Only the case of Main tier is tested in this version of test suite.
+        m_par.mfx.CodecLevel   = LevelParam.Level;      // for Main tier, CodecLevel values are: 10, 20, 21, 30, 31, 40, 41, 50, 51, 52, 60, 61, 62
+        m_par.mfx.CodecProfile = MFX_PROFILE_HEVC_MAIN; // only the case of Main tier is tested in this version of test suite
 
-        m_par.mfx.NumSlice = TS_MIN( TS_MIN( LevelParam.MaxSSPP, 68 ), CeilDiv(m_par.mfx.FrameInfo.Height, 64) );
+        // Due to driver issue, NumSlice is limited to MaxNumSlice (see definition above).
+        // Otherwise MFXVideoENCODE_Init will fail with MFX_WRN_INCOMPATIBLE_VIDEO_PARAM status.
+        m_par.mfx.NumSlice = std::min((mfxU16)LevelParam.MaxSSPP, MaxNumSlice);
 
-        g_tsLog << "======== MaxLumaPs:     " << LevelParam.MaxLumaPs << "\n";
-        g_tsLog << "======== MaxLumaSr:     " << LevelParam.MaxLumaSr << "\n";
-        g_tsLog << "======== Width:         " << m_par.mfx.FrameInfo.Width << "\n";
-        g_tsLog << "======== Height:        " << m_par.mfx.FrameInfo.Height << "\n";
-        g_tsLog << "======== CropW:         " << m_par.mfx.FrameInfo.CropW << "\n";
-        g_tsLog << "======== CropH:         " << m_par.mfx.FrameInfo.CropH << "\n";
-        g_tsLog << "======== FrameRateExtN: " << m_par.mfx.FrameInfo.FrameRateExtN << "\n";
-        g_tsLog << "======== FrameRateExtD: " << m_par.mfx.FrameInfo.FrameRateExtD << "\n";
-        g_tsLog << "======== NumSlice:      " << m_par.mfx.NumSlice << "\n";
-        g_tsLog << "======== CodecLevel:    " << m_par.mfx.CodecLevel << "\n";
-        g_tsLog << "======== CodecProfile:  " << m_par.mfx.CodecProfile << "\n";
+    }
+
+    void PrintDebugInfo(const LevelConstraints& LevelParam, bool maxWidthFlag)
+    {
+        #ifdef MANUAL_DEBUG_MODE
+            std::ofstream stream;
+            if (maxWidthFlag)
+                stream.open("hevce_fei_level_profile1.log", std::ios::app);
+            else
+                stream.open("hevce_fei_level_profile2.log", std::ios::app);
+        #else
+            std::ostream &stream = g_tsLog;
+        #endif
+
+        stream << "================================== " << std::fixed << std::setprecision(1) << (mfxF32)(m_par.mfx.CodecLevel/10.0f) << "\n";
+        stream << "======== MaxLumaPs:     " << LevelParam.MaxLumaPs << "\n";
+        stream << "======== MaxLumaSr:     " << LevelParam.MaxLumaSr << "\n";
+        stream << "======== Width:         " << m_par.mfx.FrameInfo.Width << "\n";
+        stream << "======== Height:        " << m_par.mfx.FrameInfo.Height << "\n";
+        stream << "======== CropW:         " << m_par.mfx.FrameInfo.CropW << "\n";
+        stream << "======== CropH:         " << m_par.mfx.FrameInfo.CropH << "\n";
+        stream << "======== FrameRateExtN: " << m_par.mfx.FrameInfo.FrameRateExtN << "\n";
+        stream << "======== FrameRateExtD: " << m_par.mfx.FrameInfo.FrameRateExtD << "\n";
+        stream << "======== NumSlice:      " << m_par.mfx.NumSlice << "\n";
+        stream << "======== CodecLevel:    " << m_par.mfx.CodecLevel << "\n";
+        stream << "======== CodecProfile:  " << m_par.mfx.CodecProfile << "\n";
+
+        #ifdef MANUAL_DEBUG_MODE
+            stream.close();
+        #endif
     }
 
     mfxStatus ProcessBitstream(mfxBitstream& bs, mfxU32 nFrames)
@@ -182,7 +220,7 @@ private:
                 if (au.nalu[i] && au.nalu[i]->nal_unit_type == SPS_NUT)
                 {
                     g_tsLog << "======== Input Level: " << (m_par.mfx.CodecLevel & 0xFF) << "\n";
-                    // Values of the NAL unit Level stored in header, are defined as ( (m_par.mfx.CodecLevel & 0xFF) * 3 ):
+                    // Values of the NAL unit Level stored in header are defined as ( (m_par.mfx.CodecLevel & 0xFF) * 3 ):
                     // 30, 60, 63, 90, 93, 120, 123, 150, 153, 156, 180, 183, 186
                     g_tsLog << "======== Output Level:  " << (au.nalu[i]->sps->ptl.general.level_idc/3) << "\n";
                     EXPECT_EQ(au.nalu[i]->sps->ptl.general.level_idc/3 , (m_par.mfx.CodecLevel & 0xFF ) ) << "ERROR: Level value is not as expected\n";
@@ -195,16 +233,18 @@ private:
 
 }; // end of class TestSuite
 
-const mfxU32 TestSuite::n_cases = sizeof(TableA6)/sizeof(LevelConstraints);
-
 mfxI32 TestSuite::RunTest(mfxU16 id)
 {
     TS_START;
     CHECK_HEVC_FEI_SUPPORT();
 
     MFXInit();
+
+    mfxU16 table_id = id % NumberOfLevels;
+    bool maxWidthFlag = id < NumberOfLevels;
     // Set FrameInfo values according to specified Level
-    SetParsPositiveTC(TableA6[id]);
+    SetParsPositiveTC(TableA6[table_id], maxWidthFlag);
+    PrintDebugInfo(TableA6[table_id], maxWidthFlag);
     // Load HEVC FEI plugin
     Load();
     Init();
