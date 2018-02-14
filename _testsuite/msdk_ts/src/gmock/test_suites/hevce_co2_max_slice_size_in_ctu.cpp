e0680c6c84959fa385e5e57f67d42d474ce89569
Sergey Osipov <sergey.a.osipov@intel.com>
2018-02-14 18:27:50 +0300

    [gmock][hevce] 12b for max_slice_size_in_ctu
    TsParser changed to HEVC2 to fix fails with REXt
    Added 12b test cases
    
    Issue: VSMGWL-7230
    Test: beh_co2_max_slice_size_in_ctu
    
    Change-Id: I45261cd1b02aa1761e7a9a6221bc4363ff5d32cb

--- _testsuite/msdk_ts/src/gmock/test_suites/hevce_co2_max_slice_size_in_ctu.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/hevce_co2_max_slice_size_in_ctu.cpp
@@ -32,7 +32,6 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     only init/reset.
 */
 
-
 #include "ts_encoder.h"
 #include "ts_struct.h"
 #include "ts_parser.h"
@@ -40,17 +39,17 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 namespace hevce_co2_max_slice_size_in_ctu
 {
 
-class TestSuite : public tsVideoEncoder, public tsSurfaceProcessor, public tsBitstreamProcessor, public tsParserHEVCAU
+class TestSuite : public tsVideoEncoder, public tsSurfaceProcessor, public tsBitstreamProcessor, public tsParserHEVC2
 {
 public:
     TestSuite()
         : tsVideoEncoder(MFX_CODEC_HEVC)
-        , tsParserHEVCAU(BS_HEVC::INIT_MODE_CABAC)
-        , m_mode(0)
+        , tsParserHEVC2(BS_HEVC2::PARSE_SSD)
         , m_expected(0)
         , m_nFrame(0)
+        , framesToEncode(0)
     {
-        srand(time(NULL));
+        srand((mfxU32)time(NULL));
         m_bs_processor = this;
         m_filler = this;
     }
@@ -79,15 +78,16 @@ public:
     static const unsigned int n_cases;
 
 private:
-    mfxU32 m_mode;
     mfxU16 m_expected;
     mfxU32 m_nFrame;
     mfxU32 m_numLCU; //LCU number in frame
+    mfxU32 framesToEncode;
 
     enum
     {
         MFX_PAR = 1,
         EXT_COD2,
+        INIT_ONLY
     };
 
     enum
@@ -105,44 +105,42 @@ private:
 
     mfxStatus ProcessBitstream(mfxBitstream& bs, mfxU32 nFrames)
     {
-        set_buffer(bs.Data + bs.DataOffset, bs.DataLength);
+        if (bs.Data)
+            set_buffer(bs.Data + bs.DataOffset, bs.DataLength);
 
         while (nFrames--)
         {
-            UnitType& au = ParseOrDie();
-            for (Bs32u i = 0; i < au.NumUnits; i++)
+            auto& hdr = ParseOrDie();
+
+            for (auto pNALU = &hdr; pNALU; pNALU = pNALU->next)
             {
-                Bs16u type = au.nalu[i]->nal_unit_type;
-                //check slice segment only
-                if (type > 21 || ((type > 9) && (type < 16)))
-                {
+                if (!IsHEVCSlice(pNALU->nal_unit_type))
                     continue;
-                }
 
                 if (m_expected == 0) {
                     //not set, so expected value is m_numLCU
                     m_expected = m_numLCU;
                 }
 
-                auto& s = *au.nalu[i]->slice;
-                if (s.NumCU != m_expected) {
-                    if (s.NumCU > m_expected)
+                auto& S = *pNALU->slice;
+                if (S.NumCTU != m_expected) {
+                    if (S.NumCTU > m_expected)
                     {
-                        g_tsLog << "ERROR: Slice's LCU num exceeds expected value.\n"
-                            << "frame#" << m_nFrame <<": Slice's LCU num = " << (mfxU32)s.NumCU
+                        g_tsLog << "ERROR: Slice's CTU num exceeds expected value.\n"
+                            << "frame#" << m_nFrame <<": Slice number of CTUs = " << (mfxU32)S.NumCTU
                             << " > " << (mfxU32)m_expected << " (expected value).\n";
                         return MFX_ERR_ABORTED;
                     } else
-                    if (i != (au.NumUnits - 1))
+                    if (pNALU->next)
                     {
-                        g_tsLog << "ERROR: Slice's LCU num is not as expected and it is not the last slice.\n"
-                            << "frame#" << m_nFrame <<": Slice's LCU num = " << (mfxU32)s.NumCU
+                        g_tsLog << "ERROR: Slice's CTU num is not as expected and it is not the last slice.\n"
+                            << "frame#" << m_nFrame <<": Slice number of CTUs = " << (mfxU32)S.NumCTU
                             << " != " << (mfxU32)m_expected << " (expected value).\n";
                         return MFX_ERR_ABORTED;
                     }
                 }
+                m_nFrame++;
             }
-            m_nFrame++;
         }
 
         bs.DataLength = 0;
@@ -154,7 +152,7 @@ private:
 const TestSuite::tc_struct TestSuite::test_case[] =
 {
     //If NumMbPerSlice is set > LTU_size_in_frame return WRN_INCOMPATIBLE_VIDEO_PARAM
-    /*00*/{MFX_WRN_INCOMPATIBLE_VIDEO_PARAM, 0, {EXT_COD2, &tsStruct::mfxExtCodingOption2.NumMbPerSlice, 0xFFFF}},
+    /*00*/{MFX_WRN_INCOMPATIBLE_VIDEO_PARAM, INIT_ONLY, {EXT_COD2, &tsStruct::mfxExtCodingOption2.NumMbPerSlice, 0xFFFF}},
     /*01*/{MFX_ERR_NONE, 0, {}},
     /*02*/{MFX_ERR_NONE, INIT, {}},
     /*03*/{MFX_ERR_NONE, RESET, {}},
@@ -174,8 +172,6 @@ const TestSuite::tc_struct TestSuite::test_case[] =
 
 const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case)/sizeof(TestSuite::tc_struct);
 
-const int frameNumber = 20;
-
 template<mfxU32 fourcc>
 int TestSuite::RunTest_Subtype(const unsigned int id)
 {
@@ -187,7 +183,51 @@ int TestSuite::RunTest(tc_struct tc, unsigned int fourcc_id)
 {
     int err = 0;
     TS_START;
-    m_mode = tc.mode;
+
+    if ((0 == memcmp(m_uid->Data, MFX_PLUGINID_HEVCE_HW.Data, sizeof(MFX_PLUGINID_HEVCE_HW.Data))))
+    {
+        if (g_tsHWtype < MFX_HW_SKL) // MFX_PLUGIN_HEVCE_HW - unsupported on platform less SKL
+        {
+            g_tsStatus.expect(MFX_ERR_UNSUPPORTED);
+            g_tsLog << "WARNING: Unsupported HW Platform!\n";
+            Query();
+            return 0;
+        }
+        if ((fourcc_id == MFX_FOURCC_P010)
+            && (g_tsHWtype < MFX_HW_KBL)) {
+            g_tsLog << "\n\nWARNING: P010 format only supported on KBL+!\n\n\n";
+            throw tsSKIP;
+        }
+        if ((fourcc_id == MFX_FOURCC_Y210 || fourcc_id == MFX_FOURCC_YUY2 ||
+            fourcc_id == MFX_FOURCC_AYUV || fourcc_id == MFX_FOURCC_Y410)
+            && (g_tsHWtype < MFX_HW_ICL))
+        {
+            g_tsLog << "\n\nWARNING: RExt formats are only supported starting from ICL!\n\n\n";
+            throw tsSKIP;
+        }
+        if ((fourcc_id == GMOCK_FOURCC_P012 || fourcc_id == GMOCK_FOURCC_Y212 || fourcc_id == GMOCK_FOURCC_Y412)
+            && (g_tsHWtype < MFX_HW_TGL))
+        {
+            g_tsLog << "\n\nWARNING: 12b formats are only supported starting from TGL!\n\n\n";
+            throw tsSKIP;
+        }
+        if ((fourcc_id == MFX_FOURCC_YUY2 || fourcc_id == MFX_FOURCC_Y210 || fourcc_id == GMOCK_FOURCC_Y212)
+            && (g_tsConfig.lowpower == MFX_CODINGOPTION_ON))
+        {
+            g_tsLog << "\n\nWARNING: 4:2:2 formats are NOT supported on VDENC!\n\n\n";
+            throw tsSKIP;
+        }
+        if ((fourcc_id == MFX_FOURCC_AYUV || fourcc_id == MFX_FOURCC_Y410 || fourcc_id == GMOCK_FOURCC_Y412)
+            && (g_tsConfig.lowpower != MFX_CODINGOPTION_ON))
+        {
+            g_tsLog << "\n\nWARNING: 4:4:4 formats are only supported on VDENC!\n\n\n";
+            throw tsSKIP;
+        }
+    }
+    else {
+        g_tsLog << "WARNING: only HEVCe HW plugin is tested\n";
+        return 0;
+    }
 
     //This calculation is based on SKL limitation that LCU is always 32x32.
     //If LCU can be configured, the calcuation should be updated with the
@@ -209,6 +249,7 @@ int TestSuite::RunTest(tc_struct tc, unsigned int fourcc_id)
     {
         m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_NV12;
         m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV420;
+        m_par.mfx.FrameInfo.Shift = 0;
         m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 8;
     }
     else if (fourcc_id == MFX_FOURCC_P010)
@@ -218,37 +259,61 @@ int TestSuite::RunTest(tc_struct tc, unsigned int fourcc_id)
         m_par.mfx.FrameInfo.Shift = 1;
         m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 10;
     }
-    else if (fourcc_id == MFX_FOURCC_AYUV)
+    else if (fourcc_id == GMOCK_FOURCC_P012)
     {
-        m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_AYUV;
-        m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV444;
-        m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 8;
-    }
-    else if (fourcc_id == MFX_FOURCC_Y410)
-    {
-        m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_Y410;
-        m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV444;
-        m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 10;
+        m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_P016;
+        m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV420;
+        m_par.mfx.FrameInfo.Shift = 1;
+        m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 12;
     }
     else if (fourcc_id == MFX_FOURCC_YUY2)
     {
         m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_YUY2;
         m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV422;
+        m_par.mfx.FrameInfo.Shift = 0;
         m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 8;
     }
     else if (fourcc_id == MFX_FOURCC_Y210)
     {
         m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_Y210;
         m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV422;
+        m_par.mfx.FrameInfo.Shift = 1;
         m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 10;
+    }
+    else if (fourcc_id == GMOCK_FOURCC_Y212)
+    {
+        m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_Y216;
+        m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV422;
+        m_par.mfx.FrameInfo.Shift = 1;
+        m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 12;
+    }
+    else if (fourcc_id == MFX_FOURCC_AYUV)
+    {
+        m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_AYUV;
+        m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV444;
+        m_par.mfx.FrameInfo.Shift = 0;
+        m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 8;
+    }
+    else if (fourcc_id == MFX_FOURCC_Y410)
+    {
+        m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_Y410;
+        m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV444;
+        m_par.mfx.FrameInfo.Shift = 0;
+        m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 10;
+    }
+    else if (fourcc_id == GMOCK_FOURCC_Y412)
+    {
+        m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_Y416;
+        m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV444;
+        m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 12;
     }
     else
     {
-        g_tsLog << "WARNING: invalid fourcc_id parameter: " << fourcc_id << "\n";
+        g_tsLog << "ERROR: invalid fourcc_id parameter: " << fourcc_id << "\n";
         return 0;
     }
 
-    if (tc.sts == MFX_WRN_INCOMPATIBLE_VIDEO_PARAM) {
+    if (tc.mode == INIT_ONLY) {
         mfxExtCodingOption2& cod2 = m_par;
         SETPARS(&cod2, EXT_COD2);
     } else if (tc.mode & INIT) {
@@ -266,41 +331,19 @@ int TestSuite::RunTest(tc_struct tc, unsigned int fourcc_id)
         Load();
     }
 
-    //This test is for hw plugin only
-    if (0 == memcmp(m_uid->Data, MFX_PLUGINID_HEVCE_HW.Data, sizeof(MFX_PLUGINID_HEVCE_HW.Data))) {
-        if (g_tsHWtype < MFX_HW_SKL)
-        {
-            g_tsLog << "WARNING: Unsupported HW Platform!\n";
-            return 0;
-        }
-        else if (m_pPar->mfx.FrameInfo.FourCC == MFX_FOURCC_P010 && g_tsHWtype < MFX_HW_KBL) {
-            g_tsLog << "\n\nWARNING: P010 format only supported on KBL+!\n\n\n";
-            throw tsSKIP;
-        }
-        else if ((m_pPar->mfx.FrameInfo.FourCC == MFX_FOURCC_Y210 || m_pPar->mfx.FrameInfo.FourCC == MFX_FOURCC_YUY2)
-            && (g_tsHWtype < MFX_HW_ICL || g_tsConfig.lowpower == MFX_CODINGOPTION_ON))
-        {
-            g_tsLog << "\n\nWARNING: 422 formats only supported on ICL+ and ENC+PAK!\n\n\n";
-            throw tsSKIP;
-        }
-        else if ((m_pPar->mfx.FrameInfo.FourCC == MFX_FOURCC_AYUV || m_pPar->mfx.FrameInfo.FourCC == MFX_FOURCC_Y410)
-            && (g_tsHWtype < MFX_HW_ICL || g_tsConfig.lowpower != MFX_CODINGOPTION_ON))
-        {
-            g_tsLog << "\n\nWARNING: 444 formats only supported on ICL+ and VDENC!\n\n\n";
-            throw tsSKIP;
-        }
-    } else {
-        g_tsLog << "WARNING: only HEVCe HW plugin is tested\n";
-        return 0;
-    }
-
     g_tsStatus.expect(tc.sts);
     Init();
 
+    if (tc.mode == INIT_ONLY) {
+        throw tsOK;
+    }
+
+    framesToEncode = (g_tsConfig.sim) ? 3 : 20;
+
     if (tc.sts == MFX_ERR_NONE) {
-        m_max = frameNumber;
+        m_max = framesToEncode;
         m_cur = 0;
-        EncodeFrames(frameNumber);
+        EncodeFrames(framesToEncode);
 
         if (tc.mode & RESET) {
             mfxExtCodingOption2& cod2 = m_par;
@@ -314,9 +357,9 @@ int TestSuite::RunTest(tc_struct tc, unsigned int fourcc_id)
                 g_tsStatus.expect(MFX_WRN_INCOMPATIBLE_VIDEO_PARAM);
             m_expected = cod2.NumMbPerSlice;
             Reset();
-            m_max = frameNumber;
+            m_max = framesToEncode;
             m_cur = 0;
-            EncodeFrames(frameNumber);
+            EncodeFrames(framesToEncode);
         }
     }
 
@@ -326,8 +369,12 @@ int TestSuite::RunTest(tc_struct tc, unsigned int fourcc_id)
 
 TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_co2_max_slice_size_in_ctu, RunTest_Subtype<MFX_FOURCC_NV12>, n_cases);
 TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_10b_420_p010_co2_max_slice_size_in_ctu, RunTest_Subtype<MFX_FOURCC_P010>, n_cases);
-TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_8b_444_ayuv_co2_max_slice_size_in_ctu, RunTest_Subtype<MFX_FOURCC_AYUV>, n_cases);
-TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_10b_444_y410_co2_max_slice_size_in_ctu, RunTest_Subtype<MFX_FOURCC_Y410>, n_cases);
+TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_12b_420_p016_co2_max_slice_size_in_ctu, RunTest_Subtype<GMOCK_FOURCC_P012>, n_cases);
 TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_8b_422_yuy2_co2_max_slice_size_in_ctu, RunTest_Subtype<MFX_FOURCC_YUY2>, n_cases);
 TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_10b_422_y210_co2_max_slice_size_in_ctu, RunTest_Subtype<MFX_FOURCC_Y210>, n_cases);
+TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_12b_422_y216_co2_max_slice_size_in_ctu, RunTest_Subtype<GMOCK_FOURCC_Y212>, n_cases);
+TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_8b_444_ayuv_co2_max_slice_size_in_ctu, RunTest_Subtype<MFX_FOURCC_AYUV>, n_cases);
+TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_10b_444_y410_co2_max_slice_size_in_ctu, RunTest_Subtype<MFX_FOURCC_Y410>, n_cases);
+TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_12b_444_y416_co2_max_slice_size_in_ctu, RunTest_Subtype<GMOCK_FOURCC_Y412>, n_cases);
+
 };
