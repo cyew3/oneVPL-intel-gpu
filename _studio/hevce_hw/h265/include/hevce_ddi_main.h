cyew3/oneVPL-intel-gpu

commit f66d1a24d4c93f30f35517bd33b0d9b6cb52ac25

[hevce/gen12] Add SCC support to HEVC encoder
 + initial implementation with palette coding tool
 + IBC is disabled

Issue: VSMG-779
Test: manual / VCA

Change-Id: I3395aa8e042707456a9b8e0338fad2c72460f617
Signed-off-by: Alexei Leonenko <alexei.leonenko@intel.com>

_studio/hevce_hw/h265/include/hevce_ddi_main.h
@@ -5,13 +5,13 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2017 Intel Corporation. All Rights Reserved.
+// Copyright(C) 2017-2018 Intel Corporation. All Rights Reserved.
 //
 #pragma once
 
 #include "encoding_ddi.h"
 
-#define HEVCE_DDI_VERSION 966
+#define HEVCE_DDI_VERSION 991
 
 typedef struct tagENCODE_CAPS_HEVC
 {

_studio/hevce_hw/h265/include/mfx_h265_encode_hw_ddi_trace.h
@@ -5,7 +5,7 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2014-2017 Intel Corporation. All Rights Reserved.
+// Copyright(C) 2014-2018 Intel Corporation. All Rights Reserved.
 #include "mfx_common.h"
 #if defined(MFX_ENABLE_H265_VIDEO_ENCODE)
 
@@ -49,6 +49,12 @@ public:
     void Trace(ENCODE_SET_PICTURE_PARAMETERS_HEVC_REXT const & b, mfxU32 idx);
     void Trace(ENCODE_SET_SLICE_HEADER_HEVC_REXT const & b, mfxU32 idx);
 #endif // (HEVCE_DDI_VERSION >= 960)
+
+#if (HEVCE_DDI_VERSION >= 991)
+    void Trace(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_SCC const & b, mfxU32 idx);
+    void Trace(ENCODE_SET_PICTURE_PARAMETERS_HEVC_SCC const & b, mfxU32 idx);
+#endif // (HEVCE_DDI_VERSION >= 991)
+
     inline void Trace(GUID const & guid, mfxU32) { TraceGUID(guid, m_log); };
     void Trace(const char* name, mfxU32 value);
     template<mfxU32 N> inline void Trace(const char name[N], mfxU32 value) { Trace((const char*) name, value);  }

_studio/hevce_hw/h265/src/mfx_h265_encode_hw_ddi.cpp
@@ -5,7 +5,7 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2014-2017 Intel Corporation. All Rights Reserved.
+// Copyright(C) 2014-2018 Intel Corporation. All Rights Reserved.
 //
 #include "mfx_common.h"
 #if defined(MFX_ENABLE_H265_VIDEO_ENCODE)
@@ -33,10 +33,10 @@ GUID GetGUID(MfxVideoParam const & par)
 #ifndef OPEN_SOURCE
 #if defined(PRE_SI_TARGET_PLATFORM_GEN11)
     if (par.mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN10 || par.m_ext.CO3.TargetBitDepthLuma == 10)
-        bdId = 1;;
+        bdId = 1;
 #if defined(PRE_SI_TARGET_PLATFORM_GEN12)
     if (par.m_ext.CO3.TargetBitDepthLuma == 12)
-        bdId = 2;;
+        bdId = 2;
 #endif
 
     cfId = Clip3<mfxU16>(MFX_CHROMAFORMAT_YUV420, MFX_CHROMAFORMAT_YUV444, par.m_ext.CO3.TargetChromaFormatPlus1 - 1) - MFX_CHROMAFORMAT_YUV420;
@@ -51,8 +51,14 @@ GUID GetGUID(MfxVideoParam const & par)
 #endif
     if (par.m_platform.CodeName && par.m_platform.CodeName < MFX_PLATFORM_KABYLAKE)
         bdId = 0;
+    
+    mfxU16 cFamily = IsOn(par.mfx.LowPower);
 
-    guid = GuidTable[IsOn(par.mfx.LowPower)][bdId] [cfId];
+#if defined(MFX_ENABLE_HEVCE_SCC)
+    cFamily = (par.mfx.CodecProfile == MFX_PROFILE_HEVC_SCC) ? 2 : IsOn(par.mfx.LowPower);
+#endif
+
+    guid = GuidTable[cFamily][bdId] [cfId];
     DDITracer::TraceGUID(guid, stdout);
 #endif // OPEN_SOURCE
     return guid;
@@ -77,11 +83,19 @@ DriverEncoder* CreatePlatformH265Encoder(MFXCoreInterface* core, ENCODER_TYPE ty
             if (type == ENCODER_REXT)
                 return new D3D9EncoderREXT;
 #endif
+#if defined(MFX_ENABLE_HEVCE_SCC)
+            if (type == ENCODER_SCC)
+                return new D3D9EncoderSCC;
+#endif
             return new D3D9EncoderDefault;
         case MFX_IMPL_VIA_D3D11:
 #if defined(PRE_SI_TARGET_PLATFORM_GEN11)
             if (type == ENCODER_REXT)
                 return new D3D11EncoderREXT;
+#endif
+#if defined(MFX_ENABLE_HEVCE_SCC)
+            if (type == ENCODER_SCC)
+                return new D3D11EncoderSCC;
 #endif
             return new D3D11EncoderDefault;
 #elif defined (MFX_VA_LINUX)
@@ -1136,7 +1150,31 @@ void FillPpsBuffer(
 }
 
 #endif //PRE_SI_TARGET_PLATFORM_GEN11
+#if defined(MFX_ENABLE_HEVCE_SCC)
+void FillSpsBuffer(
+    MfxVideoParam const & par,
+    ENCODE_CAPS_HEVC const & caps,
+    ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_SCC & sps)
+{
+    Zero(sps);
+    FillSpsBuffer(par, caps, (ENCODE_SET_SEQUENCE_PARAMETERS_HEVC&)sps);
 
+    sps.palette_mode_enabled_flag = par.m_sps.palette_mode_enabled_flag;
+    sps.palette_max_size = (mfxU8)par.m_sps.palette_max_size;
+    sps.delta_palette_max_predictor_size = (mfxU8)par.m_sps.delta_palette_max_predictor_size;
+}
+
+void FillPpsBuffer(
+    MfxVideoParam const & par,
+    ENCODE_CAPS_HEVC const & caps,
+    ENCODE_SET_PICTURE_PARAMETERS_HEVC_SCC & pps)
+{
+    Zero(pps);
+    FillPpsBuffer(par, caps, (ENCODE_SET_PICTURE_PARAMETERS_HEVC&)pps);
+
+    pps.pps_curr_pic_ref_enabled_flag = par.m_pps.curr_pic_ref_enabled_flag;
+}
+#endif
 #endif //defined(_WIN32) || defined(_WIN64)
 
 }; // namespace MfxHwH265Encode

_studio/hevce_hw/h265/src/mfx_h265_encode_hw_ddi_trace.cpp
@@ -5,7 +5,7 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2014-2017 Intel Corporation. All Rights Reserved.
+// Copyright(C) 2014-2018 Intel Corporation. All Rights Reserved.
 //
 
 #include "mfx_common.h"
@@ -102,6 +102,12 @@ DECL_START(ENCODE_COMPBUFFERDESC)
     case D3DDDIFMT_INTELENCODE_SPSDATA:
     case D3D11_DDI_VIDEO_ENCODER_BUFFER_SPSDATA:
 #if (HEVCE_DDI_VERSION >= 960)
+#if (HEVCE_DDI_VERSION >= 991)
+        if (m_type == ENCODER_SCC)
+            TraceArray((ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_SCC*)pBuf,
+            (b.DataSize / sizeof(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_SCC)));
+        else
+#endif
         if (m_type == ENCODER_REXT)
             TraceArray((ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_REXT*)pBuf,
                 (b.DataSize / sizeof(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_REXT)));
@@ -113,6 +119,12 @@ DECL_START(ENCODE_COMPBUFFERDESC)
     case D3DDDIFMT_INTELENCODE_PPSDATA:
     case D3D11_DDI_VIDEO_ENCODER_BUFFER_PPSDATA:
 #if (HEVCE_DDI_VERSION >= 960)
+#if (HEVCE_DDI_VERSION >= 991)
+        if (m_type == ENCODER_SCC)
+            TraceArray((ENCODE_SET_PICTURE_PARAMETERS_HEVC_SCC*)pBuf,
+            (b.DataSize / sizeof(ENCODE_SET_PICTURE_PARAMETERS_HEVC_SCC)));
+        else
+#endif
         if (m_type == ENCODER_REXT)
             TraceArray((ENCODE_SET_PICTURE_PARAMETERS_HEVC_REXT*)pBuf,
                 (b.DataSize / sizeof(ENCODE_SET_PICTURE_PARAMETERS_HEVC_REXT)));
@@ -672,6 +684,35 @@ DECL_END
 #undef FIELD_FORMAT
 
 #endif //(HEVCE_DDI_VERSION >= 960)
+
+#if (HEVCE_DDI_VERSION >= 991)
+#define FIELD_FORMAT "%-38s"
+DECL_START(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_SCC)
+   Trace((ENCODE_SET_SEQUENCE_PARAMETERS_HEVC const &)b, idx);
+   TRACE("%d", palette_mode_enabled_flag);
+   TRACE("%d", motion_vector_resolution_control_idc);
+   TRACE("%d", intra_boundary_filtering_disabled_flag);
+   TRACE("%d", palette_max_size);
+   TRACE("%d", delta_palette_max_predictor_size);
+DECL_END
+#undef FIELD_FORMAT
+
+#define FIELD_FORMAT "%-38s"
+DECL_START(ENCODE_SET_PICTURE_PARAMETERS_HEVC_SCC)
+   Trace((ENCODE_SET_PICTURE_PARAMETERS_HEVC const &)b, idx);
+   TRACE("%d", pps_curr_pic_ref_enabled_flag);
+   TRACE("%d", residual_adaptive_colour_transform_enabled_flag);
+   TRACE("%d", pps_slice_act_qp_offsets_present_flag);
+   TRACE("%d", PredictorPaletteSize);
+   TRACE("%d", pps_act_y_qp_offset_plus5);
+   TRACE("%d", pps_act_cb_qp_offset_plus5);
+   TRACE("%d", pps_act_cr_qp_offset_plus3);
+
+// pps.PredictorPaletteEntries[3][128];
+// TRACE_ARRAY_ROW("%d", cb_qp_offset_list, 6);
+DECL_END
+#undef FIELD_FORMAT
+#endif //(HEVCE_DDI_VERSION >= 991)
 
 #endif //#ifdef DDI_TRACE
 }

_studio/hevce_hw/h265/src/mfx_h265_encode_hw_par.cpp
@@ -141,6 +141,24 @@ mfxStatus CheckProfile(mfxVideoParam& par, mfxU16 platform)
     case 0:
         break;
 
+#if defined(MFX_ENABLE_HEVCE_SCC)
+    case MFX_PROFILE_HEVC_SCC:
+        if (platform < MFX_PLATFORM_TIGERLAKE)
+            return MFX_ERR_INVALID_VIDEO_PARAM;
+
+        if (pHevcPar)
+        {
+            mfxU64 REXTConstr = pHevcPar->GeneralConstraintFlags;
+
+            if (((REXTConstr & MFX_HEVC_CONSTR_REXT_MAX_8BIT) && BitDepth > 8)
+                || ((REXTConstr & MFX_HEVC_CONSTR_REXT_MAX_10BIT) && BitDepth > 10)
+                || ((REXTConstr & MFX_HEVC_CONSTR_REXT_MAX_12BIT) && BitDepth > 12)
+                || ((REXTConstr & MFX_HEVC_CONSTR_REXT_MAX_420CHROMA) && ChromaFormat > MFX_CHROMAFORMAT_YUV420)
+                || ((REXTConstr & MFX_HEVC_CONSTR_REXT_MAX_422CHROMA) && ChromaFormat > MFX_CHROMAFORMAT_YUV422))
+                return MFX_ERR_INVALID_VIDEO_PARAM;
+        }
+        break;
+#endif
     case MFX_PROFILE_HEVC_REXT:
         if (platform < MFX_PLATFORM_ICELAKE)
             return MFX_ERR_INVALID_VIDEO_PARAM;
@@ -1371,6 +1389,16 @@ mfxU16 GetMaxChroma(MfxVideoParam const & par)
             c = Min<mfxU16>(c, MFX_CHROMAFORMAT_YUV422);
     }
 
+#if defined(MFX_ENABLE_HEVCE_SCC)
+    if (par.mfx.CodecProfile == MFX_PROFILE_HEVC_SCC)
+    {
+        mfxU64 REXTConstr = par.m_ext.HEVCParam.GeneralConstraintFlags;
+
+        if (REXTConstr & MFX_HEVC_CONSTR_REXT_MAX_420CHROMA)
+            c = Min<mfxU16>(c, MFX_CHROMAFORMAT_YUV420);
+    }
+#endif
+
     switch (par.mfx.FrameInfo.FourCC)
     {
     case MFX_FOURCC_NV12:
@@ -1442,10 +1470,14 @@ mfxU16 GetMaxBitDepth(MfxVideoParam const & par, mfxU32 MaxEncodedBitDepth = 3)
     if (par.mfx.FrameInfo.BitDepthLuma)
         d = Min(d, par.mfx.FrameInfo.BitDepthLuma);
 
-    if (par.mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN10)
+    if ( par.mfx.CodecProfile == MFX_PROFILE_HEVC_MAIN10 )
         d = Min<mfxU16>(d, 10);
 
-    if (par.mfx.CodecProfile == MFX_PROFILE_HEVC_REXT)
+    if (par.mfx.CodecProfile == MFX_PROFILE_HEVC_REXT
+#if defined(MFX_ENABLE_HEVCE_SCC)
+        || par.mfx.CodecProfile == MFX_PROFILE_HEVC_SCC
+#endif
+        )
     {
         mfxU64 REXTConstr = par.m_ext.HEVCParam.GeneralConstraintFlags;
 
@@ -1680,6 +1712,9 @@ mfxStatus CheckVideoParam(MfxVideoParam& par, ENCODE_CAPS_HEVC const & caps, boo
             , (mfxU16)MFX_PROFILE_HEVC_MAINSP
             , (mfxU16)MFX_PROFILE_HEVC_MAIN10
             , (mfxU16)MFX_PROFILE_HEVC_REXT
+#if defined(MFX_ENABLE_HEVCE_SCC)
+            , (mfxU16)MFX_PROFILE_HEVC_SCC
+#endif
             , (mfxU16)0
             );
         break;

builder/open_source_symbols_undefine.list
@@ -122,3 +122,4 @@ AS_VPP_SCD_PLUGIN
 MFX_ENABLE_VPP_SCD_PARALLEL_COPY
 AS_ENC_SCD_PLUGIN
 MFX_ENABLE_IMAGE_STABILIZATION_VPP
+MFX_ENABLE_HEVCE_SCC

_studio/hevce_hw/h265/include/mfx_h265_encode_hw_d3d11.h
@@ -5,7 +5,7 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2014-2017 Intel Corporation. All Rights Reserved.
+// Copyright(C) 2014-2018 Intel Corporation. All Rights Reserved.
 //
 
 #pragma once
@@ -115,6 +115,9 @@ private:
     HRESULT DecoderExtension(D3D11_VIDEO_DECODER_EXTENSION const & ext);
 };
 
+#if defined(MFX_ENABLE_HEVCE_SCC)
+typedef D3D11Encoder<ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_SCC, ENCODE_SET_PICTURE_PARAMETERS_HEVC_SCC, ENCODE_SET_SLICE_HEADER_HEVC> D3D11EncoderSCC;
+#endif
 #if defined(PRE_SI_TARGET_PLATFORM_GEN11)
 typedef D3D11Encoder<ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_REXT, ENCODE_SET_PICTURE_PARAMETERS_HEVC_REXT, ENCODE_SET_SLICE_HEADER_HEVC_REXT> D3D11EncoderREXT;
 #endif //defined(PRE_SI_TARGET_PLATFORM_GEN11)

_studio/hevce_hw/h265/include/mfx_h265_encode_hw_d3d9.h
@@ -5,7 +5,7 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2014-2017 Intel Corporation. All Rights Reserved.
+// Copyright(C) 2014-2018 Intel Corporation. All Rights Reserved.
 //
 #include "mfx_common.h"
 #if defined(MFX_ENABLE_H265_VIDEO_ENCODE)
@@ -136,6 +136,9 @@ private:
     }
 };
 
+#if defined(MFX_ENABLE_HEVCE_SCC)
+typedef D3D9Encoder<ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_SCC, ENCODE_SET_PICTURE_PARAMETERS_HEVC_SCC, ENCODE_SET_SLICE_HEADER_HEVC> D3D9EncoderSCC;
+#endif
 #if defined(PRE_SI_TARGET_PLATFORM_GEN11)
 typedef D3D9Encoder<ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_REXT, ENCODE_SET_PICTURE_PARAMETERS_HEVC_REXT, ENCODE_SET_SLICE_HEADER_HEVC_REXT> D3D9EncoderREXT;
 #endif //defined(PRE_SI_TARGET_PLATFORM_GEN11)

commit 5d41383ccc944a88abb7629a7c6aa3bd14c3b47f

[hevce] Remove HEVCE_DDI_VERSION define
Clearing up "future" DDI support, until such support will be provided
in the mainline driver. Fixing CAPs regression introduced by f66d1a24,
matching the mainline driver / 0.966 spec.

Issue: manual
Test: manual

Change-Id: Ia44c77bb4b76b89508d7a7857f44bde8eb302b5a
Signed-off-by: Alexei Leonenko <alexei.leonenko@intel.com>

_studio/hevce_hw/h265/include/hevce_ddi_main.h
@@ -11,8 +11,6 @@
 
 #include "encoding_ddi.h"
 
-#define HEVCE_DDI_VERSION 991
-
 typedef struct tagENCODE_CAPS_HEVC
 {
     union{
@@ -68,22 +66,6 @@ typedef struct tagENCODE_CAPS_HEVC
 
     union {
         struct {
-#if (HEVCE_DDI_VERSION >= 970)
-            UINT    SliceLevelReportSupport         : 1;
-            UINT    CTULevelReportSupport           : 1;
-            UINT    SearchWindow64Support           : 1;
-            UINT    reserved                        : 2;
-            UINT    IntraRefreshBlockUnitSize       : 2;
-            UINT    LCUSizeSupported                : 3;
-            UINT    MaxNumDeltaQP                   : 4;
-            UINT    DirtyRectSupport                : 1;
-            UINT    MoveRectSupport                 : 1;
-            UINT    FrameSizeToleranceSupport       : 1;
-            UINT    HWCounterAutoIncrementSupport   : 2;
-            UINT    ROIDeltaQPSupport               : 1;
-            UINT    MaxNumOfTileColumnsMinus1       : 5;
-            UINT                                    : 7; // For future expansion
-#else
             UINT    SliceLevelReportSupport         : 1;
             UINT    MaxNumOfTileColumnsMinus1       : 4;
             UINT    IntraRefreshBlockUnitSize       : 2;
@@ -95,17 +77,12 @@ typedef struct tagENCODE_CAPS_HEVC
             UINT    HWCounterAutoIncrementSupport   : 2;
             UINT    ROIDeltaQPSupport               : 1;
             UINT                                    : 12; // For future expansion
-#endif  // (HEVCE_DDI_VERSION >= 970)
         };
         UINT    CodingLimits2;
     };
 
     UCHAR    MaxNum_WeightedPredL0;
     UCHAR    MaxNum_WeightedPredL1;
-#if (HEVCE_DDI_VERSION >= 967)
-    USHORT   MaxNumOfDirtyRect;
-    USHORT   MaxNumOfMoveRect;
-#endif
 } ENCODE_CAPS_HEVC;
 
 #if !defined(OPEN_SOURCE)

_studio/hevce_hw/h265/include/mfx_h265_encode_hw_ddi_trace.h
@@ -44,16 +44,12 @@ public:
 
     void Trace(D3D11_VIDEO_DECODER_EXTENSION const & b, mfxU32 idx);
 
-#if (HEVCE_DDI_VERSION >= 960)
     void Trace(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_REXT const & b, mfxU32 idx);
     void Trace(ENCODE_SET_PICTURE_PARAMETERS_HEVC_REXT const & b, mfxU32 idx);
     void Trace(ENCODE_SET_SLICE_HEADER_HEVC_REXT const & b, mfxU32 idx);
-#endif // (HEVCE_DDI_VERSION >= 960)
 
-#if (HEVCE_DDI_VERSION >= 991)
     void Trace(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_SCC const & b, mfxU32 idx);
     void Trace(ENCODE_SET_PICTURE_PARAMETERS_HEVC_SCC const & b, mfxU32 idx);
-#endif // (HEVCE_DDI_VERSION >= 991)
 
     inline void Trace(GUID const & guid, mfxU32) { TraceGUID(guid, m_log); };
     void Trace(const char* name, mfxU32 value);

_studio/hevce_hw/h265/src/mfx_h265_encode_hw_ddi.cpp
@@ -704,12 +704,8 @@ void FillSpsBuffer(
 //        sps.AVBRConvergence = par.mfx.Convergence;
     }
 
-#if (HEVCE_DDI_VERSION >= 966)
     sps.FrameRate.Numerator     = par.mfx.FrameInfo.FrameRateExtN;
     sps.FrameRate.Denominator   = par.mfx.FrameInfo.FrameRateExtD;
-#else
-    sps.FramesPer100Sec = (mfxU16)(mfxU64(100) * par.mfx.FrameInfo.FrameRateExtN / par.mfx.FrameInfo.FrameRateExtD);
-#endif
     sps.InitVBVBufferFullnessInBit = 8000 * par.InitialDelayInKB;
     sps.VBVBufferSizeInBit         = 8000 * par.BufferSizeInKB;
 
@@ -941,12 +937,10 @@ void FillPpsBuffer(
         pps.MinDeltaQp = -51;
     }
 
-#if (HEVCE_DDI_VERSION >= 960)
     // if ENCODE_BLOCKQPDATA surface is provided
     pps.NumDeltaQpForNonRectROI = 0;    // if no non-rect ROI
     // pps.NumDeltaQpForNonRectROI    // total number of different delta QPs for non-rect ROI ( + the same for rect ROI should not exceed MaxNumDeltaQP in caps
     // pps.NonRectROIDeltaQpList[0..pps.NumDeltaQpForNonRectROI-1] // delta QPs for non-rect ROI
-#endif
 #endif // MFX_ENABLE_HEVCE_ROI
 
 #if defined(PRE_SI_TARGET_PLATFORM_GEN11)
@@ -1000,12 +994,10 @@ void FillPpsBuffer(
         pps.MinDeltaQp = -51;
     }
 
-#if (HEVCE_DDI_VERSION >= 960)
     // if ENCODE_BLOCKQPDATA surface is provided
     pps.NumDeltaQpForNonRectROI = 0;    // if no non-rect ROI
     // pps.NumDeltaQpForNonRectROI    // total number of different delta QPs for non-rect ROI ( + the same for rect ROI should not exceed MaxNumDeltaQP in caps
     // pps.NonRectROIDeltaQpList[0..pps.NumDeltaQpForNonRectROI-1] // delta QPs for non-rect ROI
-#endif
 #endif // MFX_ENABLE_HEVCE_ROI
 
 #ifdef MFX_ENABLE_HEVCE_DIRTY_RECT
@@ -1048,15 +1040,12 @@ void FillPpsBuffer(
     }
     pps.StatusReportFeedbackNumber = task.m_statusReportNumber;
 
-#if (HEVCE_DDI_VERSION >= 960)
     mfxExtAVCEncoderWiDiUsage* extWiDi = ExtBuffer::Get(task.m_ctrl);
 
     if (extWiDi)
         pps.InputType = eType_DRM_SECURE;
     else
         pps.InputType = eType_DRM_NONE;
-#endif
-
     pps.nal_unit_type = task.m_shNUT;
 }
 

_studio/hevce_hw/h265/src/mfx_h265_encode_hw_ddi_trace.cpp
@@ -23,7 +23,6 @@ DDITracer::DDITracer(ENCODER_TYPE type)
     , m_type(type)
 {
     m_log = fopen("MSDK_HEVCE_HW_DDI_LOG.txt", "w");
-    Trace("HEVCE_DDI_VERSION", HEVCE_DDI_VERSION);
 }
 
 DDITracer::~DDITracer()
@@ -101,46 +100,36 @@ DECL_START(ENCODE_COMPBUFFERDESC)
     {
     case D3DDDIFMT_INTELENCODE_SPSDATA:
     case D3D11_DDI_VIDEO_ENCODER_BUFFER_SPSDATA:
-#if (HEVCE_DDI_VERSION >= 960)
-#if (HEVCE_DDI_VERSION >= 991)
         if (m_type == ENCODER_SCC)
             TraceArray((ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_SCC*)pBuf,
             (b.DataSize / sizeof(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_SCC)));
         else
-#endif
         if (m_type == ENCODER_REXT)
             TraceArray((ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_REXT*)pBuf,
                 (b.DataSize / sizeof(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_REXT)));
         else
-#endif //(HEVCE_DDI_VERSION >= 960)
             TraceArray((ENCODE_SET_SEQUENCE_PARAMETERS_HEVC*)pBuf,
                 (b.DataSize / sizeof(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC)));
         break;
     case D3DDDIFMT_INTELENCODE_PPSDATA:
     case D3D11_DDI_VIDEO_ENCODER_BUFFER_PPSDATA:
-#if (HEVCE_DDI_VERSION >= 960)
-#if (HEVCE_DDI_VERSION >= 991)
         if (m_type == ENCODER_SCC)
             TraceArray((ENCODE_SET_PICTURE_PARAMETERS_HEVC_SCC*)pBuf,
             (b.DataSize / sizeof(ENCODE_SET_PICTURE_PARAMETERS_HEVC_SCC)));
         else
-#endif
         if (m_type == ENCODER_REXT)
             TraceArray((ENCODE_SET_PICTURE_PARAMETERS_HEVC_REXT*)pBuf,
                 (b.DataSize / sizeof(ENCODE_SET_PICTURE_PARAMETERS_HEVC_REXT)));
         else
-#endif //(HEVCE_DDI_VERSION >= 960)
             TraceArray((ENCODE_SET_PICTURE_PARAMETERS_HEVC*)pBuf,
                 (b.DataSize / sizeof(ENCODE_SET_PICTURE_PARAMETERS_HEVC)));
         break;
     case D3DDDIFMT_INTELENCODE_SLICEDATA:
     case D3D11_DDI_VIDEO_ENCODER_BUFFER_SLICEDATA:
-#if (HEVCE_DDI_VERSION >= 960)
         if (m_type == ENCODER_REXT)
             TraceArray((ENCODE_SET_SLICE_HEADER_HEVC_REXT*)pBuf,
                 (b.DataSize / sizeof(ENCODE_SET_SLICE_HEADER_HEVC_REXT)));
         else
-#endif //(HEVCE_DDI_VERSION >= 960)
             TraceArray((ENCODE_SET_SLICE_HEADER_HEVC*)pBuf,
                 (b.DataSize / sizeof(ENCODE_SET_SLICE_HEADER_HEVC)));
         break;
@@ -203,11 +192,7 @@ DECL_START(ENCODE_CAPS_HEVC)
     TRACE("%d", ROIDeltaQPSupport         );
     TRACE("%d", ROIBRCPriorityLevelSupport);
 
-#if (HEVCE_DDI_VERSION >= 966)
     TRACE("%d", BlockSize);
-#else
-    TRACE("%d", ROIBlockSize);
-#endif
 
     TRACE("%d", SliceLevelReportSupport      );
     TRACE("%d", MaxNumOfTileColumnsMinus1    );
@@ -240,12 +225,9 @@ DECL_START(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC)
     TRACE("%d", TargetBitRate);
     TRACE("%d", MaxBitRate);
     TRACE("%d", MinBitRate);
-#if (HEVCE_DDI_VERSION >= 966)
     TRACE("%d", FrameRate.Numerator);
     TRACE("%d", FrameRate.Denominator);
-#else
-    TRACE("%d", FramesPer100Sec);
-#endif
+
     TRACE("%d", InitVBVBufferFullnessInBit);
     TRACE("%d", VBVBufferSizeInBit);
 
@@ -261,11 +243,7 @@ DECL_START(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC)
     TRACE("%d", QpAdjustment         );
     TRACE("%d", ROIValueInDeltaQP    );
 
-#if (HEVCE_DDI_VERSION >= 966)
     TRACE("%d", BlockQPforNonRectROI);
-#else
-    TRACE("%d", BlockQPInDeltaQPIndex);
-#endif
 
     TRACE("%d", UserMaxFrameSize );
     //TRACE("%d", AVBRAccuracy     );
@@ -378,10 +356,8 @@ DECL_START(ENCODE_SET_PICTURE_PARAMETERS_HEVC)
     TRACE("%d", MaxDeltaQp);
     TRACE("%d", MinDeltaQp);
 
-#if (HEVCE_DDI_VERSION >= 960)
     TRACE("%d", NumDeltaQpForNonRectROI);
     TRACE_ARRAY_ROW("%d", NonRectROIDeltaQpList, 16);
-#endif
     
     TRACE("%d", SkipFrameFlag);
     TRACE("%d", NumSkipFrames);
@@ -437,9 +413,7 @@ DECL_START(ENCODE_SET_PICTURE_PARAMETERS_HEVC)
         }
     }
 
-#if (HEVCE_DDI_VERSION >= 960)
     TRACE("%d", InputType);
-#endif
 DECL_END
 #undef FIELD_FORMAT
 
@@ -469,11 +443,7 @@ DECL_START(ENCODE_SET_SLICE_HEADER_HEVC)
     TRACE("%d", beta_offset_div2);
     TRACE("%d", tc_offset_div2);
     TRACE("%d", luma_log2_weight_denom);
-#if (HEVCE_DDI_VERSION >= 966)
     TRACE("%d", delta_chroma_log2_weight_denom);
-#else
-    TRACE("%d", chroma_log2_weight_denom);
-#endif
 
     TRACE_ARRAY_ROW("%d", luma_offset[0], 15);
     TRACE_ARRAY_ROW("%d", luma_offset[1], 15);
@@ -545,13 +515,11 @@ DECL_START(ENCODE_SET_SLICE_HEADER_HEVC)
     //TRACE("%d", MaxSlizeSizeInBytes);
     TRACE("%d", SliceQpDeltaBitOffset);
 
-#if (HEVCE_DDI_VERSION >= 966)
     TRACE("%d", SliceSAOFlagBitOffset);
     TRACE("%d", BitLengthSliceHeaderStartingPortion);
     TRACE("%d", SliceHeaderByteOffset);
     TRACE("%d", PredWeightTableBitOffset);
     TRACE("%d", PredWeightTableBitLength);
-#endif
 DECL_END
 #undef FIELD_FORMAT
 
@@ -611,9 +579,6 @@ DECL_START(ENCODE_QUERY_STATUS_SLICE_PARAMS)
     }
 DECL_END
 #undef FIELD_FORMAT
-
-
-#if (HEVCE_DDI_VERSION >= 960)
 
 #define FIELD_FORMAT "%-38s"
 DECL_START(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_REXT)
@@ -683,9 +648,6 @@ DECL_START(ENCODE_SET_SLICE_HEADER_HEVC_REXT)
 DECL_END
 #undef FIELD_FORMAT
 
-#endif //(HEVCE_DDI_VERSION >= 960)
-
-#if (HEVCE_DDI_VERSION >= 991)
 #define FIELD_FORMAT "%-38s"
 DECL_START(ENCODE_SET_SEQUENCE_PARAMETERS_HEVC_SCC)
    Trace((ENCODE_SET_SEQUENCE_PARAMETERS_HEVC const &)b, idx);
@@ -707,12 +669,8 @@ DECL_START(ENCODE_SET_PICTURE_PARAMETERS_HEVC_SCC)
    TRACE("%d", pps_act_y_qp_offset_plus5);
    TRACE("%d", pps_act_cb_qp_offset_plus5);
    TRACE("%d", pps_act_cr_qp_offset_plus3);
-
-// pps.PredictorPaletteEntries[3][128];
-// TRACE_ARRAY_ROW("%d", cb_qp_offset_list, 6);
 DECL_END
 #undef FIELD_FORMAT
-#endif //(HEVCE_DDI_VERSION >= 991)
 
 #endif //#ifdef DDI_TRACE
 }

_studio/hevce_hw/h265/src/mfx_h265_encode_hw_par.cpp
@@ -1068,11 +1068,7 @@ mfxStatus CheckAndFixDirtyRect(ENCODE_CAPS_HEVC const & caps, MfxVideoParam cons
         DirtyRect->NumRect = 0;
     }
 
-#if (HEVCE_DDI_VERSION >= 967)
-    changed += CheckMax(DirtyRect->NumRect, caps.MaxNumOfDirtyRect);
-#else
     changed += CheckMax(DirtyRect->NumRect, MAX_NUM_DIRTY_RECT);
-#endif  // (HEVCE_DDI_VERSION >= 967)
 
     for (mfxU16 i = 0; i < DirtyRect->NumRect; i++)
     {

builder/open_source_symbols.list
@@ -498,7 +498,6 @@ WL_DRM_FORMAT_ENUM
 X11_DRI3_SUPPORT
 __MFXBRC_H__
 MFX_ENABLE_VIDEO_BRC_COMMON_1
-HEVCE_DDI_VERSION
 __MFX_VPX_DEC_COMMON__H__
 LINUX_TARGET_PLATFORM_BXTMIN
 LINUX_TARGET_PLATFORM_CFL

builder/open_source_symbols_undefine.list
@@ -28,7 +28,6 @@ ENABLE_PRE_SI_FEATURES
 ENABLE_TASK_DEBUG
 FEI_DEBUG
 HEADER_PACKING_TEST
-HEVCE_DDI_VERSION
 IPP_NONTEMPORAL_STORE
 LINUX_TARGET_PLATFORM_BSW
 LINUX_TARGET_PLATFORM_TBD