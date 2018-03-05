93297da489df95f870b2ad084d5c7eed7c1a165f
Efremov, Stanislav <stanislav.efremov@intel.com>
2018-03-05 13:58:36 +0300

    [tests][av1d] Add check of params in av1d_decode_header
    Issue: VSMG-1010
    Test: av1d_decode_header
    
    In addition streams changed to Rev 0.25.1. Test name corrected.
    
    Change-Id: I22cf0c10a2d09e8f572b958aaf55f054eb766768
    Signed-off-by: Efremov, Stanislav <stanislav.efremov@intel.com>

--- _testsuite/msdk_ts/src/gmock/test_suites/av1d_decode_header.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/av1d_decode_header.cpp
@@ -63,8 +63,8 @@ const char TestSuite::path[] = "conformance/av1/";
 
 const TestSuite::tc_struct TestSuite::test_case[] =
 {
-    {/* 0*/ MFX_ERR_NONE,  "self_coded/foreman_352x288_420.av1",
-      { { &tsStruct::mfxVideoParam.mfx.CodecProfile,             0 },
+    {/* 0*/ MFX_ERR_NONE,  "from_fulsim/akiyo0_176x144_8b_420_LowLatency_qp12.ivf",
+      { { &tsStruct::mfxVideoParam.mfx.CodecProfile,             MFX_PROFILE_AV1_0 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.BitDepthLuma,   8 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.BitDepthChroma, 8 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.FourCC,         MFX_FOURCC_NV12 },
@@ -72,12 +72,12 @@ const TestSuite::tc_struct TestSuite::test_case[] =
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.ChromaFormat,   MFX_CHROMAFORMAT_YUV420 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.AspectRatioW,   1 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.AspectRatioH,   1 },
-        { &tsStruct::mfxVideoParam.mfx.FrameInfo.Width,          352 },
-        { &tsStruct::mfxVideoParam.mfx.FrameInfo.Height,         288 } },
+        { &tsStruct::mfxVideoParam.mfx.FrameInfo.Width,          176 },
+        { &tsStruct::mfxVideoParam.mfx.FrameInfo.Height,         144 } },
     },
 
     {/* 1*/ MFX_ERR_NONE,  "self_coded/Kimono1_352x288_422.av1",
-      { { &tsStruct::mfxVideoParam.mfx.CodecProfile,             0 },
+      { { &tsStruct::mfxVideoParam.mfx.CodecProfile,             MFX_PROFILE_AV1_1 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.BitDepthLuma,   8 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.BitDepthChroma, 8 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.FourCC,         MFX_FOURCC_YUY2 },
@@ -89,8 +89,8 @@ const TestSuite::tc_struct TestSuite::test_case[] =
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.Height,         288 } },
     },
 
-    {/* 2*/ MFX_ERR_NONE,  "self_coded/Kimono1_352x288_444.av1",
-      { { &tsStruct::mfxVideoParam.mfx.CodecProfile,             0 },
+    {/* 2*/ MFX_ERR_NONE,  "from_fulsim/carphone_176x144_8b_444_LowLatency_qp12.ivf",
+      { { &tsStruct::mfxVideoParam.mfx.CodecProfile,             MFX_PROFILE_AV1_1 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.BitDepthLuma,   8 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.BitDepthChroma, 8 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.FourCC,         MFX_FOURCC_AYUV },
@@ -98,12 +98,12 @@ const TestSuite::tc_struct TestSuite::test_case[] =
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.ChromaFormat,   MFX_CHROMAFORMAT_YUV444 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.AspectRatioW,   1 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.AspectRatioH,   1 },
-        { &tsStruct::mfxVideoParam.mfx.FrameInfo.Width,          352 },
-        { &tsStruct::mfxVideoParam.mfx.FrameInfo.Height,         288 } },
+        { &tsStruct::mfxVideoParam.mfx.FrameInfo.Width,          176 },
+        { &tsStruct::mfxVideoParam.mfx.FrameInfo.Height,         144 } },
     },
 
     {/* 3*/ MFX_ERR_NONE,  "from_fulsim/rain2_640x360_10b_420_LowLatency_qp12.ivf",
-      { { &tsStruct::mfxVideoParam.mfx.CodecProfile,             0 },
+      { { &tsStruct::mfxVideoParam.mfx.CodecProfile,             MFX_PROFILE_AV1_2 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.BitDepthLuma,   10 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.BitDepthChroma, 10 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.FourCC,         MFX_FOURCC_P010 },
@@ -112,10 +112,35 @@ const TestSuite::tc_struct TestSuite::test_case[] =
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.AspectRatioW,   1 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.AspectRatioH,   1 },
         { &tsStruct::mfxVideoParam.mfx.FrameInfo.Width,          640 },
-        { &tsStruct::mfxVideoParam.mfx.FrameInfo.Height,         360 } },
+        { &tsStruct::mfxVideoParam.mfx.FrameInfo.Height,         368 } },
     },
 };
 const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case)/sizeof(TestSuite::tc_struct);
+
+void CheckParams(mfxVideoParam const & expected, mfxVideoParam const & actual)
+{
+    EXPECT_EQ(expected.mfx.CodecProfile, actual.mfx.CodecProfile);
+    EXPECT_EQ(expected.mfx.FrameInfo.BitDepthLuma, actual.mfx.FrameInfo.BitDepthLuma);
+    EXPECT_EQ(expected.mfx.FrameInfo.BitDepthChroma, actual.mfx.FrameInfo.BitDepthChroma);
+    EXPECT_EQ(expected.mfx.FrameInfo.FourCC, actual.mfx.FrameInfo.FourCC);
+    EXPECT_EQ(expected.mfx.FrameInfo.PicStruct, actual.mfx.FrameInfo.PicStruct);
+    EXPECT_EQ(expected.mfx.FrameInfo.ChromaFormat, actual.mfx.FrameInfo.ChromaFormat);
+    EXPECT_EQ(expected.mfx.FrameInfo.Width, actual.mfx.FrameInfo.Width);
+    EXPECT_EQ(expected.mfx.FrameInfo.Height, actual.mfx.FrameInfo.Height);
+
+    mfxVideoParam tmp = expected;
+
+    tmp.mfx.FrameInfo = actual.mfx.FrameInfo;
+    tmp.mfx.CodecProfile = actual.mfx.CodecProfile;
+    tmp.mfx.CodecLevel = actual.mfx.CodecLevel;
+    tmp.mfx.DecodedOrder = actual.mfx.DecodedOrder;
+    tmp.mfx.MaxDecFrameBuffering = actual.mfx.MaxDecFrameBuffering;
+
+    if (memcmp(&actual, &tmp, sizeof(mfxVideoParam)))
+    {
+        ADD_FAILURE() << "FAILED: DecodeHeader should not change fields other than FrameInfo, CodecProfile, CodecLevel, DecodedOrder";
+    }
+}
 
 int TestSuite::RunTest(unsigned int id)
 {
@@ -147,22 +172,11 @@ int TestSuite::RunTest(unsigned int id)
     DecodeHeader(m_session, m_pBitstream, m_pPar);
 
     if(g_tsStatus.get() >= 0)
-    {
-        expectedPar.mfx.FrameInfo       = m_par.mfx.FrameInfo;
-        expectedPar.mfx.CodecProfile    = m_par.mfx.CodecProfile;
-        expectedPar.mfx.CodecLevel      = m_par.mfx.CodecLevel;
-        expectedPar.mfx.DecodedOrder    = m_par.mfx.DecodedOrder;
-        expectedPar.mfx.MaxDecFrameBuffering = m_par.mfx.MaxDecFrameBuffering;
-
-        if (memcmp(m_pPar, &expectedPar, sizeof(mfxVideoParam)))
-        {
-            ADD_FAILURE() << "FAILED: DecodeHeader should not change fields other than FrameInfo, CodecProfile, CodecLevel, DecodedOrder";
-        }
-    }
+        CheckParams(expectedPar, m_par);
 
     TS_END;
     return 0;
 }
 
-TS_REG_TEST_SUITE_CLASS(av1_decode_header);
+TS_REG_TEST_SUITE_CLASS(av1d_decode_header);
 }
