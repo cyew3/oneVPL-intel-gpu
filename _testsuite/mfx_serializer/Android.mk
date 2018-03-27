c2439e1690939d9f119877d11dc3fdbed2e4ba26
Dmitry Brazhkin <dmitry.brazhkin@intel.com>
2018-03-27 18:00:22 +0300

    [Android] Add make file for mfx_serializer
    Need it to detect any build issues on Android platform
    
    Issue: N/A
    Test: manual
    
    Change-Id: I06b5262a50248410b7807a836a836a34e169429d
    Signed-off-by: Dmitry Brazhkin <dmitry.brazhkin@intel.com>

---
+++ _testsuite/mfx_serializer/Android.mk
@@ -0,0 +1,54 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(MFX_HOME)/mdp_msdk-lib/android/mfx_env.mk
+
+include $(CLEAR_VARS)
+include $(MFX_HOME)/mdp_msdk-lib/android/mfx_defs.mk
+
+YAML_CPP_ROOT := ../../../mdp_msdk-contrib/yaml-cpp
+
+LOCAL_SRC_FILES := \
+    $(addprefix src/, $(notdir $(wildcard $(LOCAL_PATH)/src/*.cpp)))
+
+LOCAL_SRC_FILES += \
+    $(YAML_CPP_ROOT)/src/aliasmanager.cpp \
+    $(YAML_CPP_ROOT)/src/binary.cpp \
+    $(YAML_CPP_ROOT)/src/contrib/graphbuilder.cpp \
+    $(YAML_CPP_ROOT)/src/contrib/graphbuilderadapter.cpp \
+    $(YAML_CPP_ROOT)/src/conversion.cpp \
+    $(YAML_CPP_ROOT)/src/directives.cpp \
+    $(YAML_CPP_ROOT)/src/emitfromevents.cpp \
+    $(YAML_CPP_ROOT)/src/emitter.cpp \
+    $(YAML_CPP_ROOT)/src/emitterstate.cpp \
+    $(YAML_CPP_ROOT)/src/emitterutils.cpp \
+    $(YAML_CPP_ROOT)/src/exp.cpp \
+    $(YAML_CPP_ROOT)/src/iterator.cpp \
+    $(YAML_CPP_ROOT)/src/node.cpp \
+    $(YAML_CPP_ROOT)/src/nodebuilder.cpp \
+    $(YAML_CPP_ROOT)/src/nodeownership.cpp \
+    $(YAML_CPP_ROOT)/src/null.cpp \
+    $(YAML_CPP_ROOT)/src/ostream.cpp \
+    $(YAML_CPP_ROOT)/src/parser.cpp \
+    $(YAML_CPP_ROOT)/src/regex.cpp \
+    $(YAML_CPP_ROOT)/src/scanner.cpp \
+    $(YAML_CPP_ROOT)/src/scanscalar.cpp \
+    $(YAML_CPP_ROOT)/src/scantag.cpp \
+    $(YAML_CPP_ROOT)/src/scantoken.cpp \
+    $(YAML_CPP_ROOT)/src/simplekey.cpp \
+    $(YAML_CPP_ROOT)/src/singledocparser.cpp \
+    $(YAML_CPP_ROOT)/src/stream.cpp \
+    $(YAML_CPP_ROOT)/src/tag.cpp
+
+LOCAL_C_INCLUDES := \
+    $(MFX_C_INCLUDES) \
+    $(MFX_C_INCLUDES_STL) \
+    $(MFX_HOME)/mdp_msdk-contrib/yaml-cpp/include
+
+LOCAL_CFLAGS := \
+    $(MFX_CFLAGS) \
+    $(MFX_CFLAGS_STL)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libmfx_serializer
+
+include $(BUILD_STATIC_LIBRARY)
