f3c0cef5c217fb7fb5d48b85e10306e7d6f184f4
Mikhail Aksenov <mikhailx.aksenov@intel.com>
2018-04-27 16:28:02 +0300

    [vp9d,test] Add resolution change tests
    vp9d_10b_444_y410_res_change
    vp9d_12b_420_p016_res_change
    vp9d_12b_444_y416_res_change
    
    Issue: vsmg-784,vsmg-785
    Test: manual
    
    Change-Id: I1bd2bd8fdeac992ee90e28f0305231a71913d1f2

--- _testsuite/msdk_ts/src/gmock/test_suites/vp9d_res_change.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/vp9d_res_change.cpp
@@ -25,6 +25,8 @@ public:
     static const size_t n_cases_p010;
     static const size_t n_cases_ayuv;
     static const size_t n_cases_y410;
+    static const size_t n_cases_p016;
+    static const size_t n_cases_y416;
 
     template<decltype(MFX_FOURCC_YV12) fourcc>
     int RunTest_fourcc(const size_t id);
@@ -38,12 +40,14 @@ private:
         mfxU32 n_frames;
         mfxStatus sts;
     };
-    int RunTest(const tc_struct (&tcs)[max_num_ctrl], char const * const stream_path);
+    int RunTest(const tc_struct (&tcs)[max_num_ctrl]);
 
     static const tc_struct test_case_nv12[][max_num_ctrl];
     static const tc_struct test_case_p010[][max_num_ctrl];
     static const tc_struct test_case_ayuv[][max_num_ctrl];
     static const tc_struct test_case_y410[][max_num_ctrl];
+    static const tc_struct test_case_p016[][max_num_ctrl];
+    static const tc_struct test_case_y416[][max_num_ctrl];
 };
 
 #define MAKE_TEST_TABLE(FOURCC, L, L_N, M, M_N, S, S_N)                                                                    \
@@ -61,53 +65,55 @@ const TestSuite::tc_struct TestSuite::test_case_##FOURCC[][max_num_ctrl] =      
 };                                                                                                                         \
 const size_t TestSuite::n_cases_##FOURCC = sizeof(TestSuite::test_case_##FOURCC)/sizeof(TestSuite::tc_struct[max_num_ctrl]);
 
-MAKE_TEST_TABLE(nv12, "bbc_704x576_8b420_10.ivf", 10,
-                      "bbc_640x480_8b420_10.ivf", 10,
-                      "bbc_352x288_8b420_10.ivf", 10)
+const int frameCount = 10;
 
-MAKE_TEST_TABLE(p010, "Mobile_ProRes_1280x720_50f_10b420_10.ivf", 10,
-                      "Mobile_ProRes_640x360_50f_10b420_10.ivf", 10,
-                      "Mobile_ProRes_352x288_50f_10b420_10.ivf", 10)
+MAKE_TEST_TABLE(nv12, "forBehaviorTest/dif_resol/vp9/8b420/bbc_704x576_8b420_10.ivf", frameCount,
+                      "forBehaviorTest/dif_resol/vp9/8b420/bbc_640x480_8b420_10.ivf", frameCount,
+                      "forBehaviorTest/dif_resol/vp9/8b420/bbc_352x288_8b420_10.ivf", frameCount)
 
-MAKE_TEST_TABLE(ayuv, "matrix_titles_444_1280x532_15.ivf", 15,
-                      "matrix_titles_444_640x288_15.ivf", 15,
-                      "matrix_titles_444_320x144_15.ivf", 15)
+MAKE_TEST_TABLE(p010, "forBehaviorTest/dif_resol/vp9/10b420/Mobile_ProRes_1280x720_50f_10b420_10.ivf", frameCount,
+                      "forBehaviorTest/dif_resol/vp9/10b420/Mobile_ProRes_640x360_50f_10b420_10.ivf", frameCount,
+                      "forBehaviorTest/dif_resol/vp9/10b420/Mobile_ProRes_352x288_50f_10b420_10.ivf", frameCount)
+
+MAKE_TEST_TABLE(ayuv, "forBehaviorTest/dif_resol/vp9/8b444/matrix_titles_444_1280x532_15.ivf", frameCount,
+                      "forBehaviorTest/dif_resol/vp9/8b444/matrix_titles_444_640x288_15.ivf", frameCount,
+                      "forBehaviorTest/dif_resol/vp9/8b444/matrix_titles_444_320x144_15.ivf", frameCount)
+
+MAKE_TEST_TABLE(y410, "conformance/vp9/SBE/10bit_444/Syntax_VP9_FC2p3ss444_1920x1080_001_intra_basic_1.4.vp9", frameCount,
+                      "conformance/vp9/SBE/10bit_444/Syntax_VP9_FC2p3ss444_432x240_001_intra_basic_1.4.vp9", frameCount,
+                      "conformance/vp9/google/vp93-2-20-10bit-yuv444.ivf", frameCount)
+
+MAKE_TEST_TABLE(p016, "conformance/vp9/SBE/12bit/Syntax_VP9_FC2p2b12_1920x1080_001_intra_basic_1.3.vp9", frameCount,
+                      "conformance/vp9/SBE/12bit/Syntax_VP9_FC2p2b12_432x240_001_intra_basic_1.3.vp9", frameCount,
+                      "conformance/vp9/google/vp92-2-20-12bit-yuv420.ivf", frameCount)
+
+MAKE_TEST_TABLE(y416, "conformance/vp9/google/vpxenc_704x576_20_444_12bit_tiles.ivf", frameCount,
+                      "conformance/vp9/google/vpxenc_352x288_100_444_12bit.ivf", frameCount,
+                      "conformance/vp9/google/vp93-2-20-12bit-yuv444.ivf", frameCount)
 
 #undef MAKE_TEST_TABLE
-
-const char* getStreamPath(const decltype(MFX_FOURCC_YV12)& fourcc)
-{
-    switch (fourcc)
-    {
-    case MFX_FOURCC_NV12: return "forBehaviorTest/dif_resol/vp9/8b420/";
-    case MFX_FOURCC_AYUV: return "forBehaviorTest/dif_resol/vp9/8b444/";
-    case MFX_FOURCC_P010: return "forBehaviorTest/dif_resol/vp9/10b420/";
-    case MFX_FOURCC_Y410: return "forBehaviorTest/dif_resol/vp9/10b444/";
-    default: assert(!"Wrong/unsupported format requested!"); break;
-    }
-    throw tsFAIL;
-    return "";
-}
 
 template<decltype(MFX_FOURCC_YV12) fourcc>
 int TestSuite::RunTest_fourcc(const size_t id)
 {
     switch (fourcc)
     {
-        case MFX_FOURCC_NV12: return RunTest(test_case_nv12[id], getStreamPath(fourcc));
-        case MFX_FOURCC_AYUV: return RunTest(test_case_ayuv[id], getStreamPath(fourcc));
-        case MFX_FOURCC_P010: return RunTest(test_case_p010[id], getStreamPath(fourcc));
-        //case MFX_FOURCC_Y410: return RunTest(test_case_y410[id], getStreamPath(fourcc));
+        case MFX_FOURCC_NV12: return RunTest(test_case_nv12[id]);
+        case MFX_FOURCC_AYUV: return RunTest(test_case_ayuv[id]);
+        case MFX_FOURCC_P010: return RunTest(test_case_p010[id]);
+        case MFX_FOURCC_Y410: return RunTest(test_case_y410[id]);
+        case MFX_FOURCC_P016: return RunTest(test_case_p016[id]);
+        case MFX_FOURCC_Y416: return RunTest(test_case_y416[id]);
         default: assert(!"Wrong/unsupported format requested!"); break;
     }
     throw tsFAIL;
 }
 
-int TestSuite::RunTest(const tc_struct (&tcs)[max_num_ctrl], char const * const stream_path)
+int TestSuite::RunTest(const tc_struct (&tcs)[max_num_ctrl])
 {
     TS_START;
     for (auto& tc : tcs)
-        g_tsStreamPool.Get( tc.stream ? tc.stream : std::string(), stream_path);
+        g_tsStreamPool.Get( tc.stream ? tc.stream : std::string(), "");
 
     g_tsStreamPool.Reg();
     m_use_memid = true;
@@ -154,10 +160,12 @@ int TestSuite::RunTest(const tc_struct (&tcs)[max_num_ctrl], char const * const 
     return 0;
 }
 
-TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9d_8b_420_res_change,  RunTest_fourcc<MFX_FOURCC_NV12>, n_cases_nv12);
-TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9d_10b_420_res_change, RunTest_fourcc<MFX_FOURCC_P010>, n_cases_p010);
-TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9d_8b_444_res_change,  RunTest_fourcc<MFX_FOURCC_AYUV>, n_cases_ayuv);
-//TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9d_10b_444_res_change, RunTest_fourcc<MFX_FOURCC_Y410>, n_cases_y410);
+TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9d_8b_420_nv12_res_change,  RunTest_fourcc<MFX_FOURCC_NV12>, n_cases_nv12);
+TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9d_8b_444_ayuv_res_change,  RunTest_fourcc<MFX_FOURCC_AYUV>, n_cases_ayuv);
+TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9d_10b_420_p010_res_change, RunTest_fourcc<MFX_FOURCC_P010>, n_cases_p010);
+TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9d_10b_444_y410_res_change, RunTest_fourcc<MFX_FOURCC_Y410>, n_cases_y410);
+TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9d_12b_420_p016_res_change, RunTest_fourcc<MFX_FOURCC_P016>, n_cases_p016);
+TS_REG_TEST_SUITE_CLASS_ROUTINE(vp9d_12b_444_y416_res_change, RunTest_fourcc<MFX_FOURCC_Y416>, n_cases_y416);
 
 #undef TEST_NAME
 
