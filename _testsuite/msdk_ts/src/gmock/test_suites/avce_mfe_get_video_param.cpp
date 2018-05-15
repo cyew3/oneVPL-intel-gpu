70bc1bdb498ee6fbe07c72738e047f104828ef2c
Vitaliy Ilichev <vitaliy.ilichev@intel.com>
2018-05-15 03:49:46 +0300

    [Gmock] beh_avce_mfe_init : moved GetVideoParam() functionality
    Moved GetVideoParam() functionality from beh_avce_mfe_init to a new separate
    test beh_avce_mfe_get_video_param
    
    Issues: MDP-36789, MDP-36791
    Tests: beh_avce_mfe_init, beh_avce_mfe_get_video_param
    
    Change-Id: Icbf6e8d6b8d568c9d126a603db48689f1d59b2db

--- _testsuite/msdk_ts/src/gmock/test_suites/avce_mfe_get_video_param.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/avce_mfe_get_video_param.cpp
@@ -17,27 +17,29 @@ Copyright(c) 2018 Intel Corporation. All Rights Reserved.
 /*!
 
 \file avce_mfe_init.cpp
-\brief Gmock test avce_mfe_init
+\brief Gmock test avce_mfe_get_video_param.cpp
 
 Description:
 
-This test checks behavior of the encoding Init() for various values of the MFE
-parameters (fields MFMode and MaxNumFrames) combined with other parameters that
-affect the MFE functionality
+This test checks behavior of encoding GetVideoParam() for various values of the
+MFE parameters (fields MFMode and MaxNumFrames) combined with other parameters
+that affect the MFE functionality
 
 Algorithm:
 - Initializing Media SDK lib
 - Initializing suite and case parameters
 - Run Init()
 - Check Init() status
-- Check that input MFE parameters are not changed
+- Run GetVideoParam()
+- Check GetVideoParam() status
+- Check output MFE parameters
 
 */
 
 namespace avce_mfe
 {
 
-int test_init(unsigned int id)
+int test_get_video_param(unsigned int id)
 {
     TS_START;
     tsVideoEncoder enc(MFX_CODEC_AVC);
@@ -47,9 +49,20 @@ int test_init(unsigned int id)
 
     tsExtBufType<mfxVideoParam> pout;
     mfxExtMultiFrameParam& mfe_out = pout;
+
     set_params(enc, tc, pout);
 
-    // Update some init specific parameters
+    // Update some test specific parameters
+    if ((tc.inMFMode == MFX_MF_AUTO || tc.inMFMode == MFX_MF_MANUAL) &&
+            tc.inMaxNumFrames == 0)
+    {
+        if (g_tsHWtype == MFX_HW_SKL) {
+            tc.expMaxNumFrames = 3;
+        }
+        else {
+            tc.expMaxNumFrames = 1;
+        }
+    }
     if (tc.cOpt & COptMAD) {
         tc.sts = MFX_WRN_INCOMPATIBLE_VIDEO_PARAM;
     }
@@ -57,15 +70,17 @@ int test_init(unsigned int id)
     g_tsStatus.expect(tc.sts);
     enc.Init(enc.m_session, &enc.m_par);
 
-    mfxExtMultiFrameParam& mfe_in = enc.m_par;
-    EXPECT_EQ(mfe_in.MaxNumFrames, tc.inMaxNumFrames);
-    EXPECT_EQ(mfe_in.MFMode, tc.inMFMode);
+    g_tsStatus.expect(MFX_ERR_NONE);
+    enc.GetVideoParam(enc.m_session, &pout);
+
+    EXPECT_EQ(tc.expMaxNumFrames, mfe_out.MaxNumFrames);
+    EXPECT_EQ(tc.expMFMode, mfe_out.MFMode);
 
     TS_END;
     return 0;
 }
 
 
-TS_REG_TEST_SUITE(avce_mfe_init, test_init, test_case_num);
+TS_REG_TEST_SUITE(avce_mfe_get_video_param, test_get_video_param, test_case_num);
 
 };
