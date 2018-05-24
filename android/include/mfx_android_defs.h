4a083fc71832570cdba95a01d163d05c8276520a
Dmitry Brazhkin <dmitry.brazhkin@intel.com>
2018-05-24 13:37:02 +0300

    Update mfx_android_defs.h for the new platforms
    Test: compilation, launch on device
    Issue: MDP-42348
    
    Change-Id: If1ccdac7d37158f5011d74bbcd0563dd2fe8e1ec
    Signed-off-by: Dmitry Brazhkin <dmitry.brazhkin@intel.com>

--- android/include/mfx_android_defs.h
+++ android/include/mfx_android_defs.h
@@ -11,77 +11,87 @@ Copyright(c) 2013-2018 Intel Corporation. All Rights Reserved.
 #ifndef __MFX_ANDROID_DEFS_H__
 #define __MFX_ANDROID_DEFS_H__
 
-#define mfx_generic 1
-#define baytrail    2
-#define merrifield  3
-#define moorefield  4
-#define clovertrail 5
-#define fugu        6
+#define merrifield   1
+#define moorefield   2
+#define fugu         3
+#define clovertrail  4
+#define gmin         5 //CHT
+#define broxton      6
+#define kabylake     7
+#define cannonlake   8
+#define icelakeu     9
+#define tigerlake   10
+#define elkhartlake 11
 
-#if (MFX_ANDROID_PLATFORM == merrifield) || (MFX_ANDROID_PLATFORM == moorefield) || (MFX_ANDROID_PLATFORM == fugu)
 
-  #define MFX_ENABLE_H265_VIDEO_DECODE
+#if (MFX_ANDROID_PLATFORM == merrifield) || \
+    (MFX_ANDROID_PLATFORM == moorefield) || \
+    (MFX_ANDROID_PLATFORM == fugu)
 
-  // H.265 optimization
-  #define MFX_TARGET_OPTIMIZATION_ATOM
-  #define MFX_MAKENAME_ATOM
+    #define MFX_ENABLE_H265_VIDEO_DECODE
+    #define MFX_TARGET_OPTIMIZATION_ATOM
+    #define MFX_MAKENAME_ATOM
 
 #elif (MFX_ANDROID_PLATFORM == clovertrail)
 
-  #define MFX_ENABLE_H265_VIDEO_DECODE
-  #define MFX_TARGET_OPTIMIZATION_SSSE3
-  #define MFX_MAKENAME_SSSE3
-  #define MFX_EMULATE_SSSE3
+    #define MFX_ENABLE_H265_VIDEO_DECODE
+    #define MFX_TARGET_OPTIMIZATION_SSSE3
+    #define MFX_MAKENAME_SSSE3
+    #define MFX_EMULATE_SSSE3
 
-#elif (MFX_ANDROID_PLATFORM == baytrail) || (MFX_ANDROID_PLATFORM == bigcore)
+#elif (MFX_ANDROID_PLATFORM == gmin) || \
+      (MFX_ANDROID_PLATFORM == broxton) || \
+      (MFX_ANDROID_PLATFORM == kabylake) || \
+      (MFX_ANDROID_PLATFORM == cannonlake) || \
+      (MFX_ANDROID_PLATFORM == icelakeu) || \
+      (MFX_ANDROID_PLATFORM == tigerlake) || \
+      (MFX_ANDROID_PLATFORM == elkhartlake)
 
-#define PRE_SI_GEN 9
+    #define PRE_SI_GEN 9
 
-#ifdef MFX_VA
+    #ifdef MFX_VA
 
-  #define MFX_ENABLE_VC1_VIDEO_DECODE
+        #define MFX_ENABLE_VC1_VIDEO_DECODE
 
-  #define MFX_ENABLE_MPEG2_VIDEO_DECODE
+        #define MFX_ENABLE_MPEG2_VIDEO_DECODE
 
-  #define MFX_ENABLE_H265_VIDEO_DECODE
-  #define MFX_TARGET_OPTIMIZATION_ATOM
-  #define MFX_MAKENAME_ATOM
+        #define MFX_ENABLE_H265_VIDEO_DECODE
+        #define MFX_TARGET_OPTIMIZATION_ATOM
+        #define MFX_MAKENAME_ATOM
 
-  #define MFX_ENABLE_H264_VIDEO_DECODE
+        #define MFX_ENABLE_H264_VIDEO_DECODE
 
-  #define MFX_ENABLE_H264_VIDEO_ENCODE
-  #define MFX_ENABLE_H264_VIDEO_BRC
-  #define MFX_ENABLE_H264_VIDEO_ENCODE_HW
-  #define MFX_ENABLE_MVC_VIDEO_ENCODE_HW
-  #define LOWPOWERENCODE_AVC
+        #define MFX_ENABLE_H264_VIDEO_ENCODE
+        #define MFX_ENABLE_H264_VIDEO_BRC
+        #define MFX_ENABLE_H264_VIDEO_ENCODE_HW
+        #define MFX_ENABLE_MVC_VIDEO_ENCODE_HW
+        #define LOWPOWERENCODE_AVC
 
-  #define MFX_ENABLE_MJPEG_VIDEO_DECODE
-  #define MFX_ENABLE_MJPEG_VIDEO_DECODE_HW
+        #define MFX_ENABLE_MJPEG_VIDEO_DECODE
+        #define MFX_ENABLE_MJPEG_VIDEO_DECODE_HW
 
-  #define MFX_ENABLE_MJPEG_VIDEO_ENCODE
+        #define MFX_ENABLE_MJPEG_VIDEO_ENCODE
 
-  #define MFX_ENABLE_VP8_VIDEO_DECODE
-  #define MFX_ENABLE_VP8_VIDEO_DECODE_HW
+        #define MFX_ENABLE_VP8_VIDEO_DECODE
+        #define MFX_ENABLE_VP8_VIDEO_DECODE_HW
 
-  #define MFX_ENABLE_VP9_VIDEO_DECODE
-  #define MFX_ENABLE_VP9_VIDEO_DECODE_HW
+        #define MFX_ENABLE_VP9_VIDEO_DECODE
+        #define MFX_ENABLE_VP9_VIDEO_DECODE_HW
 
-  #define MFX_ENABLE_VPP
+        #define MFX_ENABLE_VPP
 
-  #define MFX_ENABLE_USER_ENCODE
+        #define MFX_ENABLE_USER_ENCODE
 
-#else // MFX_VA
+    #else // MFX_VA
 
-  #define MFX_ENABLE_H265_VIDEO_DECODE
-  #define MFX_TARGET_OPTIMIZATION_ATOM
-  #define MFX_MAKENAME_ATOM
+        #define MFX_ENABLE_H265_VIDEO_DECODE
+        #define MFX_TARGET_OPTIMIZATION_ATOM
+        #define MFX_MAKENAME_ATOM
 
-#endif // MFX_VA
+    #endif // MFX_VA
 
 #else
-
-  #error "undefined platform"
-
+    #error "undefined platform"
 #endif
 
 #endif // #ifndef __MFX_ANDROID_DEFS_H__
