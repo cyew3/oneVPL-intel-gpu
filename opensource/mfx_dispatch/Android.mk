c27f8e074c672089e61178ced6e8c89f71e71956
Dmitry Brazhkin <dmitry.brazhkin@intel.com>
2018-05-24 21:14:46 +0300

    Change mfx_dispatch path to /system/vendor/lib for Android
    Test: compilation, launch on device
    Issue: MDP-42348
    
    Change-Id: Ic2bffb25566f5a4c8df6b4e331b9d38b5c8eb0d9
    Signed-off-by: Dmitry Brazhkin <dmitry.brazhkin@intel.com>

--- opensource/mfx_dispatch/Android.mk
+++ opensource/mfx_dispatch/Android.mk
@@ -28,10 +28,10 @@ LOCAL_C_INCLUDES := $(MFX_C_INCLUDES)
 LOCAL_CFLAGS := $(MFX_CFLAGS_INTERNAL)
 LOCAL_CFLAGS_32 := \
     $(MFX_CFLAGS_INTERNAL_32) \
-    -DMFX_MODULES_DIR=\"/system/lib\"
+    -DMFX_MODULES_DIR=\"/system/vendor/lib\"
 LOCAL_CFLAGS_64 := \
     $(MFX_CFLAGS_INTERNAL_64) \
-    -DMFX_MODULES_DIR=\"/system/lib64\"
+    -DMFX_MODULES_DIR=\"/system/vendor/lib64\"
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libmfx
@@ -69,10 +69,10 @@ LOCAL_CFLAGS := \
     -DDXVA2DEVICE_LOG
 LOCAL_CFLAGS_32 := \
     $(MFX_CFLAGS_INTERNAL_32) \
-    -DMFX_MODULES_DIR=\"/system/lib\"
+    -DMFX_MODULES_DIR=\"/system/vendor/lib\"
 LOCAL_CFLAGS_64 := \
     $(MFX_CFLAGS_INTERNAL_64) \
-    -DMFX_MODULES_DIR=\"/system/lib64\"
+    -DMFX_MODULES_DIR=\"/system/vendor/lib64\"
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libdispatch_trace
