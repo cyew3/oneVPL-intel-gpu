52a0c7ba17665f2b64b0c003a39a0807ee541e04
Dmitry Brazhkin <dmitry.brazhkin@intel.com>
2018-05-24 14:24:42 +0300

    Add Android headers library for MediaSDK  headers
    Test: compilation, launch on device
    Issue: MDP-42348
    
    Change-Id: I50012ffeac57fcafd3e8d5ef73d4fe7109144ceb
    Signed-off-by: Dmitry Brazhkin <dmitry.brazhkin@intel.com>

---
+++ android/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libmfx_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS :=  \
+  $(MFX_HOME)/mdp_msdk-api/include \
+  $(MFX_HOME)/mdp_msdk-lib/android/include
+
+include $(BUILD_HEADER_LIBRARY)
