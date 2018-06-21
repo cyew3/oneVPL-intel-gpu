01781360c2d41a67d0e648515339c14769b831e6
Krivoruchko, Georgy <georgy.krivoruchko@intel.com>
2018-06-21 13:52:08 +0300

    [VS2017] Update compiler version
    Update tools to VS2017 (v141) and Intel C++ Compiler to v18
    
    Issue: MDP-42801
    Test: compile
    
    Change-Id: Ic3536539185d782b69f0a84f93b954f49d0983f0

--- mediasdk_structures/ts_typedef.h
+++ mediasdk_structures/ts_typedef.h
@@ -4,7 +4,7 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2014-2017 Intel Corporation. All Rights Reserved.
+Copyright(c) 2014-2018 Intel Corporation. All Rights Reserved.
 
 \* ****************************************************************************** */
 
@@ -22,7 +22,15 @@ TYPEDEF_MEMBER(mfxExtAVCRefListCtrl,      PreferredRefList[0], mfxExtAVCRefListC
 TYPEDEF_MEMBER(mfxExtPictureTimingSEI,    TimeStamp[0],        mfxExtPictureTimingSEI_TimeStamp)
 TYPEDEF_MEMBER(mfxExtAvcTemporalLayers,   Layer[0],            mfxExtAvcTemporalLayers_Layer)
 TYPEDEF_MEMBER(mfxExtAVCEncodedFrameInfo, UsedRefListL0[0],    mfxExtAVCEncodedFrameInfo_RefList)
+#if _MSC_VER<1914
 TYPEDEF_MEMBER(mfxExtVPPVideoSignalInfo,  In,                  mfxExtVPPVideoSignalInfo_InOut)
+#else
+typedef struct {
+	mfxU16  TransferMatrix;
+	mfxU16  NominalRange;
+	mfxU16  reserved2[6];
+} mfxExtVPPVideoSignalInfo_InOut;
+#endif
 TYPEDEF_MEMBER(mfxExtEncoderROI,          ROI[0],              mfxExtEncoderROI_Entry)
 TYPEDEF_MEMBER(mfxExtDirtyRect,           Rect[0],             mfxExtDirtyRect_Entry)
 TYPEDEF_MEMBER(mfxExtMoveRect,            Rect[0],             mfxExtMoveRect_Entry)
