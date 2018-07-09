c60b627280ca5b35ffdf53dc65a39d027140bfcf
Lobanov, DmitriyX <dmitriyx.lobanov@intel.com>
2018-07-09 14:37:47 +0300

    [umc_h264] GCC 6.3 Warnings fixes [-Werror=misleading-indentation]
    Issue: MDP-43005
    Tests: manual
    
    Change-Id: I5813e95ca7789f216bcfc2348804c905d13e9bff

--- _studio/shared/umc/codec/h264_dec/src/umc_h264_widevine_supplier.cpp
+++ _studio/shared/umc/codec/h264_dec/src/umc_h264_widevine_supplier.cpp
@@ -5,7 +5,7 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2003-2016 Intel Corporation. All Rights Reserved.
+// Copyright(C) 2003-2018 Intel Corporation. All Rights Reserved.
 //
 
 #include "umc_defs.h"
@@ -139,8 +139,8 @@ void POCDecoderWidevine::DecodePictureOrderCount(const H264Slice *slice, Ipp32s 
         m_PicOrderCnt = uAbsFrameNum*2;
         if (sliceHeader->nal_ref_idc == false)
             m_PicOrderCnt--;
-            m_TopFieldPOC = m_PicOrderCnt;
-            m_BottomFieldPOC = m_PicOrderCnt;
+        m_TopFieldPOC = m_PicOrderCnt;
+        m_BottomFieldPOC = m_PicOrderCnt;
     }
 
     if (sliceHeader->nal_ref_idc)
