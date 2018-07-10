d734a5404b55c616de5e905f2f72673df842b76d
Miroslav Goncharenko <miroslav.goncharenko@intel.com>
2018-07-10 18:12:57 +0300

    [sample_hevc_fei_abr] Performance improvements
    Do not parse transform coefficients if not required
    Avoid several copies
    
    Issue: none
    Test: manual run
    
    Change-Id: I16f534279b114349b7b576f0427b4462a1096716

--- samples/sample_hevc_fei_abr/include/pipeline_hevc_fei.h
+++ samples/sample_hevc_fei_abr/include/pipeline_hevc_fei.h
@@ -36,7 +36,7 @@ public:
 
     mfxStatus PreInit(MFXFrameAllocator * mfxAllocator,
                       mfxHDL hdl,
-                      std::shared_ptr<FeiBufferAllocator> bufferAllocator,
+                      std::shared_ptr<FeiBufferAllocator> & bufferAllocator,
                       const sInputParams & params,
                       const mfxFrameInfo & inFrameInfo);
 
@@ -58,7 +58,7 @@ public:
     }
     void UpdateBRCStat(FrameStatData& stat)
     {
-        return m_encoder->UpdateBRCStat(stat);
+        m_encoder->UpdateBRCStat(stat);
     }
     MfxVideoParamsWrapper GetVideoParam()
     {
