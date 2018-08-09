64b4bb45dc85085725951f6286e7112a91604800
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-08-09 07:27:43 -0700

    make few headers self-contained
    Issue: n/a
    Tests: build
    Github: 3aa3be3 ("make few headers self-contained")
    
    Change-Id: I495b18853e09df8c7784985898d35dcca13cc242
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- _studio/shared/asc/include/asc.h
+++ _studio/shared/asc/include/asc.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018 Intel Corporation
+// Copyright (c) 2017-2019 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,8 @@
 #ifndef _ASC_H_
 #define _ASC_H_
 
+#include <list>
+#include <map>
 #include <string>
 #include "asc_structures.h"
 
--- _studio/shared/include/mfx_mfe_adapter.h
+++ _studio/shared/include/mfx_mfe_adapter.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2018 Intel Corporation
+// Copyright (c) 2011-2019 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -24,8 +24,9 @@
 #if defined(MFX_VA_LINUX) && defined(MFX_ENABLE_MFE)
 #include <va/va.h>
 #include <vaapi_ext_interface.h>
-#include <vector>
+#include <map>
 #include <list>
+#include <vector>
 #include <condition_variable>
 #include <mfxstructures.h>
 
