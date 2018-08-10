7a78b1da95c8dd31ff2ad4f7f30b8ceb899ac38f
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-08-10 06:31:53 -0700

    build: add ENABLE_TEXTLOG and ENABLE_STAT trace options
    Issue: n/a
    Tests: build
    Github: 7204408 ("build: add ENABLE_TEXTLOG and ENABLE_STAT trace options")
    
    These trace modes should be activated at runtime via configuration
    file ~/.mfx_trace or global /etc/mfx_trace (user conf file takes preference):
      # cat ~/.mfx_trace
      Output=0x03
      TextLog=/tmp/msdk.log
      Statistic=/tmp/msdk.stat
    Use Ouput=0x01 and Output=0x2 to enable only textlog or stat.
    
    Change-Id: I6713fd35ca30f9eed960d8d5a91091c3024b934f
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- builder/FindTrace.cmake
+++ builder/FindTrace.cmake
@@ -30,12 +30,10 @@ if(__TRACE)
 endif()
 
 # ITT instrumentation is enabled by default to make VTune working out of the box
-if(EXISTS $ENV{MFX_HOME}/mdp_msdk-lib/builder/FindVTune.cmake)
-  set(__ITT TRUE)
-  include ($ENV{MFX_HOME}/mdp_msdk-lib/builder/FindVTune.cmake)
-endif()
+set(__ITT TRUE)
+include ($ENV{MFX_HOME}/mdp_msdk-lib/builder/FindVTune.cmake)
 
 if(__TRACE MATCHES all)
-  append("-DMFX_TRACE_ENABLE_TEXTLOG -DMFX_TRACE_ENABLE_STAT" CMAKE_C_FLAGS)
-  append("-DMFX_TRACE_ENABLE_TEXTLOG -DMFX_TRACE_ENABLE_STAT" CMAKE_CXX_FLAGS)
+  set(ENABLE_TEXTLOG ON)
+  set(ENABLE_STAT ON)
 endif()
