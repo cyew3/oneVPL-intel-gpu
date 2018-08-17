6f65d419ac822b224b59f558336612614cf2c542
Vitaliy Ilichev <vitaliy.ilichev@intel.com>
2018-08-17 17:26:16 +0300

    [github backport] fix -Werror=maybe-uninitialized for gcc8.x
    fix -Werror=maybe-uninitialized caused compilation issues from gcc8.x
    
    Issue: MDP-39538
    Test: build Media SDK
    
    Change-Id: I36503041ca685b32d8b950cbbf211d768151b627

--- opensource/mfx_dispatch/include/mfx_load_plugin.h
+++ opensource/mfx_dispatch/include/mfx_load_plugin.h
@@ -1,6 +1,6 @@
 /* ****************************************************************************** *\
 
-Copyright (C) 2013-2016 Intel Corporation.  All rights reserved.
+Copyright (C) 2013-2018 Intel Corporation.  All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -61,7 +61,7 @@ namespace MFX
             PluginModule module;
             mfxPlugin plugin;
             FactoryRecord ()
-                : plugin()
+                : plgParams(), plugin()
             {}
             FactoryRecord(const mfxPluginParam &plgParams,
                           PluginModule &module,
