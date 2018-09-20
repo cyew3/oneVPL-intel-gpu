62747b5b0799f98c1902f5632977c526fd8489a6
Dmitry Brazhkin <dmitry.brazhkin@intel.com>
2018-09-20 08:25:16 +0300

    Debug additions for Android
    Issue: N/A
    Test: manual build
    
    Change-Id: I5b4c1c4337c8c71282d76addce47837c12bdba3b
    Signed-off-by: Dmitry Brazhkin <dmitry.brazhkin@intel.com>

--- _studio/shared/umc/codec/Android.mk
+++ _studio/shared/umc/codec/Android.mk
@@ -83,6 +83,8 @@ ifeq ($(MFX_IMPL_HW), true)
   LOCAL_CFLAGS_32 := $(MFX_CFLAGS_INTERNAL_32)
   LOCAL_CFLAGS_64 := $(MFX_CFLAGS_INTERNAL_64)
 
+  LOCAL_HEADER_LIBRARIES := liblog_headers
+
   LOCAL_MODULE_TAGS := optional
   LOCAL_MODULE := libumc_codecs_merged_hw
 
@@ -106,6 +108,8 @@ ifeq ($(MFX_IMPL_SW), true)
   LOCAL_CFLAGS := $(MFX_CFLAGS_INTERNAL)
   LOCAL_CFLAGS_32 := $(MFX_CFLAGS_INTERNAL_32)
   LOCAL_CFLAGS_64 := $(MFX_CFLAGS_INTERNAL_64)
+
+  LOCAL_HEADER_LIBRARIES := liblog_headers
 
   LOCAL_MODULE_TAGS := optional
   LOCAL_MODULE := libumc_codecs_merged_sw
