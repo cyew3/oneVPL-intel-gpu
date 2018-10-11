cd498eeaea67eae389014212b676ce3a5294e7ff
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-10-11 08:54:56 -0700

    vm/sys_info: use glibc FILE directly
    This commit removes dependency from vm_file.
    
    Issue: MDP-50971
    Tests: build
    
    Change-Id: I744df4028c9b1ec13d1e9d58f2ce82a3dd42b845
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- _studio/shared/umc/core/vm/src/vm_sys_info_linux32.c
+++ _studio/shared/umc/core/vm/src/vm_sys_info_linux32.c
@@ -11,7 +11,6 @@
 #if defined(LINUX32) || defined(__APPLE__)
 
 #include "vm_sys_info.h"
-#include "vm_file.h"
 #include <time.h>
 #include <sys/utsname.h>
 #include <unistd.h>
@@ -39,7 +38,6 @@ uint32_t osx_sysctl_entry_32u( int ctl_class, int ctl_entry, uint32_t *res ) {
 
 #else
 #include <sys/sysinfo.h>
-
 
 #endif /* __APPLE__ */
 
@@ -77,11 +75,11 @@ void vm_sys_info_get_cpu_name(vm_char *cpu_name)
     if (NULL == cpu_name)
         return;
 
-    pFile = vm_file_fopen(VM_STRING("/proc/cpuinfo"), "r");
+    pFile = fopen("/proc/cpuinfo", "r");
     if (!pFile)
         return;
 
-    while ((vm_file_fgets(buf, _MAX_LEN, pFile)))
+    while (fgets(buf, _MAX_LEN, pFile))
     {
         if (!vm_string_strncmp(buf, VM_STRING("vendor_id"), 9))
         {
@@ -185,11 +183,11 @@ uint32_t vm_sys_info_get_cpu_speed(void)
     FILE *pFile = NULL;
     vm_char buf[PATH_MAX];
 
-    pFile = vm_file_fopen(VM_STRING("/proc/cpuinfo"), "r" );
+    pFile = fopen("/proc/cpuinfo", "r" );
     if (!pFile)
         return 1000;
 
-    while ((vm_file_fgets(buf, PATH_MAX, pFile)))
+    while (fgets(buf, PATH_MAX, pFile))
     {
         if (!vm_string_strncmp(buf, VM_STRING("cpu MHz"), 7))
         {
