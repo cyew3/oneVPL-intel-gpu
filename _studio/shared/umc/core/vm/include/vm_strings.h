13e232a737cb97a1a2e6ee0c4c416e213ee1549f
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-10-11 09:34:32 -0700

    vm/strings: remove declarations of some unused wrappers
    Issue: n/a
    Tests: build
    Github: 8d80648 ("vm/strings: remove declarations of some unused wrappers")
    
    Change-Id: I8ee1b71ed82aefc43796f54b512bf9bf318a6447
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- _studio/shared/umc/core/vm/include/vm_strings.h
+++ _studio/shared/umc/core/vm/include/vm_strings.h
@@ -63,7 +63,6 @@ typedef char vm_char;
 
 #define vm_string_printf    printf
 #define vm_string_sprintf   sprintf
-#define vm_string_sprintf_s sprintf
 #define vm_string_snprintf  snprintf
 
 #define vm_string_vsprintf  vsprintf
@@ -76,42 +75,50 @@ typedef char vm_char;
 #if !defined(ANDROID)
 
 #define vm_string_strcat    strcat
-#define vm_string_strcat_s  strcat_s
-#define vm_string_strncat   strncat_s
 #define vm_string_strcpy    strcpy
-#define vm_string_strcpy_s  strcpy_s
 #define vm_string_strncpy   strncpy
-#define vm_string_strncpy_s strncpy_s
 #define vm_string_strcspn   strcspn
 #define vm_string_strspn    strspn
 
 #define vm_string_strlen    strlen
-#define vm_string_strnlen_s strnlen_s
 #define vm_string_strcmp    strcmp
 #define vm_string_strncmp   strncmp
 #define vm_string_stricmp   strcmp
 #define vm_string_strnicmp  strncmp
 #define vm_string_strrchr   strrchr
 
+#ifndef OPEN_SOURCE
+#define vm_string_sprintf_s sprintf
+#define vm_string_strcat_s  strcat_s
+#define vm_string_strncat   strncat_s
+#define vm_string_strcpy_s  strcpy_s
+#define vm_string_strncpy_s strncpy_s
+#define vm_string_strnlen_s strnlen_s
+#endif
+
 #else // ANDROID
 
 #define vm_string_strcat    strcat
-#define vm_string_strcat_s(dest, size, src)  (strncat((dest), (src), (size)),0)
-#define vm_string_strncat   strncat
 #define vm_string_strcpy    strcpy
-#define vm_string_strcpy_s(dest, size, src)  (strncpy((dest), (src), (size)),0)
 #define vm_string_strncpy   strncpy
-#define vm_string_strncpy_s(dst, dst_size, src, n) (strncpy(dst,src,n))
 #define vm_string_strcspn   strcspn
 #define vm_string_strspn    strspn
 
 #define vm_string_strlen    strlen
-#define vm_string_strnlen_s strnlen_s
 #define vm_string_strcmp    strcmp
 #define vm_string_strncmp   strncmp
 #define vm_string_stricmp   strcasecmp
 #define vm_string_strnicmp  strncasecmp
 #define vm_string_strrchr   strrchr
+
+#ifndef OPEN_SOURCE
+#define vm_string_sprintf_s sprintf
+#define vm_string_strcat_s(dest, size, src)  (strncat((dest), (src), (size)),0)
+#define vm_string_strncat   strncat
+#define vm_string_strcpy_s(dest, size, src)  (strncpy((dest), (src), (size)),0)
+#define vm_string_strncpy_s(dst, dst_size, src, n) (strncpy(dst,src,n))
+#define vm_string_strnlen_s strnlen_s
+#endif
 
 #endif // ANDROID
 
