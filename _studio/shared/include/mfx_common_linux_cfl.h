2f711668d7052f9812ff44a9c4b6bc5253564d4d
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-10-12 08:50:19 -0700

    config: define MFX_ENABLE_HEVCE_ROI in primary config only
    MFX_ENABLE_HEVCE_ROI is defined in primary mfx_config.h,
    there is no need to define it in bxt/bxtmin/cfl configs.
    
    Issue: N/A
    Tests: build
    
    Change-Id: Ic837d092074cab892278c362c0098a15c48c44d2
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- _studio/shared/include/mfx_common_linux_cfl.h
+++ _studio/shared/include/mfx_common_linux_cfl.h
@@ -134,9 +134,6 @@
 //H265 feature
 #undef MFX_ENABLE_HEVCE_HDR_SEI
 
-// H265 features
-#define MFX_ENABLE_HEVCE_ROI
-
 // aac
 #define MFX_ENABLE_AAC_AUDIO_DECODE
 #define MFX_ENABLE_AAC_AUDIO_ENCODE
