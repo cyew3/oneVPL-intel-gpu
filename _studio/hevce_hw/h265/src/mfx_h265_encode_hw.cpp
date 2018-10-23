bf4796355f2cad83708187cbb342fa57e1668aaa
Alexandr Smirnov <alexander.smirnov@intel.com>
2018-10-23 16:09:42 +0300

    [HEVCe] Fix GetRecInfo for 420 10bit in case of ICL
    This commit fixes quality regression, that has
    appeared after changing FourCC from NV12 to P010
    for 420 10 bit in case of ICL.
    To fix the regression, now this check is
    re-targeted for TGL_LP, until figure out what is
    happening on ICL .
    
    Issue: MDP-51162
    Test: manual + Lucas 015.HEVC.VME.MSDK.BRC.REXT
    
    Change-Id: Ie63563c580f6c24c9247997c3ab9cc55cbe3ecfa

--- _studio/hevce_hw/h265/src/mfx_h265_encode_hw.cpp
+++ _studio/hevce_hw/h265/src/mfx_h265_encode_hw.cpp
@@ -124,12 +124,14 @@ bool GetRecInfo(const MfxVideoParam& par, mfxFrameInfo& rec)
     }
     else if (CO3.TargetChromaFormatPlus1 == (1 + MFX_CHROMAFORMAT_YUV420) && CO3.TargetBitDepthLuma == 10)
     {
-        if (par.m_platform >= MFX_HW_ICL)
+#if defined(PRE_SI_TARGET_PLATFORM_GEN12)
+        if (par.m_platform >= MFX_HW_TGL_LP)
         {
             rec.FourCC = MFX_FOURCC_NV12;
             rec.Width = Align(rec.Width, 32) * 2;
         }
         else
+#endif //defined(PRE_SI_TARGET_PLATFORM_GEN12)
         {
             rec.FourCC = MFX_FOURCC_P010;
         }
