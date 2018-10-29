cyew3/oneVPL-intel-gpu

commit 244f3978ec453c8242aa60aaf29b3f8803b39d35

[HEVC FEI] move FEI to library
Moved HEVC FEI back to library

Issue: no
Tests: manual

Signed-off-by: Artem Shaporenko <artem.shaporenko@intel.com>
Change-Id: I09564f65b00f67a18cad5e416b90f00502053aff

_studio/hevc_fei/h265_fei/include/mfx_h265_fei_encode_plugin_hw.h
@@ -1,0 +1,161 @@
+//
+// INTEL CORPORATION PROPRIETARY INFORMATION
+//
+// This software is supplied under the terms of a license agreement or
+// nondisclosure agreement with Intel Corporation and may not be copied
+// or disclosed except in accordance with the terms of that agreement.
+//
+// Copyright(C) 2018 Intel Corporation. All Rights Reserved.
+//
+
+#pragma once
+
+#include "mfx_common.h"
+
+#include "mfxplugin++.h"
+
+namespace MfxHwH265FeiEncode
+{
+
+    static const mfxPluginUID  MFX_PLUGINID_HEVC_FEI_ENCODE = { { 0x54, 0x18, 0xa7, 0x06, 0x66, 0xf9, 0x4d, 0x5c, 0xb4, 0xf7, 0xb1, 0xca, 0xee, 0x86, 0x33, 0x9b } };
+
+    class H265FeiEncodePlugin : public MFXEncoderPlugin
+    {
+    public:
+        static MFXEncoderPlugin* Create()
+        {
+            return new H265FeiEncodePlugin(false);
+        }
+
+        static mfxStatus CreateByDispatcher(mfxPluginUID guid, mfxPlugin* mfxPlg)
+        {
+            if (memcmp(&guid, &MFX_PLUGINID_HEVC_FEI_ENCODE, sizeof(mfxPluginUID)))
+            {
+                return MFX_ERR_NOT_FOUND;
+            }
+
+            H265FeiEncodePlugin* tmp_pplg = nullptr;
+
+            try
+            {
+                tmp_pplg = new H265FeiEncodePlugin(false);
+            }
+            catch (std::bad_alloc&)
+            {
+                return MFX_ERR_MEMORY_ALLOC;
+            }
+            catch (...)
+            {
+                return MFX_ERR_UNKNOWN;
+            }
+
+            *mfxPlg = tmp_pplg->m_adapter;
+            tmp_pplg->m_createdByDispatcher = true;
+
+            return MFX_ERR_NONE;
+        }
+
+        virtual mfxStatus PluginInit(mfxCoreInterface *core) override
+        {
+            MFX_TRACE_INIT();
+            MFX_AUTO_LTRACE(MFX_TRACE_LEVEL_API, "H265FeiEncodePlugin::PluginInit");
+
+            return MFX_ERR_NONE;
+        }
+
+        virtual mfxStatus PluginClose() override
+        {
+            {
+                MFX_AUTO_LTRACE(MFX_TRACE_LEVEL_API, "H265FeiEncodePlugin::PluginClose");
+                if (m_createdByDispatcher)
+                    Release();
+            }
+
+            MFX_TRACE_CLOSE();
+
+            return MFX_ERR_NONE;
+        }
+
+        virtual mfxStatus GetPluginParam(mfxPluginParam *par) override
+        {
+            MFX_CHECK_NULL_PTR1(par);
+
+            par->PluginUID        = MFX_PLUGINID_HEVC_FEI_ENCODE;
+            par->PluginVersion    = 1;
+            par->ThreadPolicy     = MFX_THREADPOLICY_SERIAL;
+            par->MaxThreadNum     = 1;
+            par->APIVersion.Major = MFX_VERSION_MAJOR;
+            par->APIVersion.Minor = MFX_VERSION_MINOR;
+            par->Type             = MFX_PLUGINTYPE_VIDEO_ENCODE;
+            par->CodecId          = MFX_CODEC_HEVC;
+
+            return MFX_ERR_NONE;
+        }
+
+        virtual void Release() override
+        {
+            delete this;
+        }
+
+        virtual mfxStatus Close() override
+        {
+            return MFX_ERR_UNDEFINED_BEHAVIOR;
+        }
+
+        virtual mfxStatus Execute(mfxThreadTask , mfxU32 , mfxU32 ) override
+        {
+            return MFX_ERR_UNDEFINED_BEHAVIOR;
+        }
+
+        virtual mfxStatus FreeResources(mfxThreadTask , mfxStatus) override
+        {
+            return MFX_ERR_UNDEFINED_BEHAVIOR;
+        }
+
+        virtual mfxStatus QueryIOSurf(mfxVideoParam *, mfxFrameAllocRequest *, mfxFrameAllocRequest * ) override
+        {
+            return MFX_ERR_UNDEFINED_BEHAVIOR;
+        }
+
+        virtual mfxStatus Query(mfxVideoParam *, mfxVideoParam *) override
+        {
+            return MFX_ERR_UNDEFINED_BEHAVIOR;
+        }
+
+        virtual mfxStatus Init(mfxVideoParam *) override
+        {
+            return MFX_ERR_UNDEFINED_BEHAVIOR;
+        }
+
+        virtual mfxStatus Reset(mfxVideoParam *) override
+        {
+            return MFX_ERR_UNDEFINED_BEHAVIOR;
+        }
+
+        virtual mfxStatus GetVideoParam(mfxVideoParam *) override
+        {
+            return MFX_ERR_UNDEFINED_BEHAVIOR;
+        }
+
+        virtual mfxStatus EncodeFrameSubmit(mfxEncodeCtrl *, mfxFrameSurface1 *, mfxBitstream *, mfxThreadTask *) override
+        {
+            return MFX_ERR_UNDEFINED_BEHAVIOR;
+        }
+
+        virtual mfxStatus SetAuxParams(void*, int) override
+        {
+            return MFX_ERR_UNSUPPORTED;
+        }
+    protected:
+        explicit H265FeiEncodePlugin(bool CreateByDispatcher)
+            : m_createdByDispatcher(CreateByDispatcher)
+            , m_adapter(this)
+        {}
+
+        ~H265FeiEncodePlugin()
+        {}
+
+        bool                               m_createdByDispatcher;
+        MFXPluginAdapter<MFXEncoderPlugin> m_adapter;
+    };
+}

_studio/hevc_fei/CMakeLists.txt
@@ -46,8 +46,6 @@ if( Linux OR Darwin )
     OUTPUT_STRIP_TRAILING_WHITESPACE
     )
   string( SUBSTRING ${hevc_version} 0 1 ver )
-
-  set(HEVCE_HW_version_defs  "-DMFX_PLUGIN_FILE_VERSION=\"\\\"${ver}${cur_date}\"\\\" -DMFX_PLUGIN_PRODUCT_VERSION=\"\\\"${hevc_version}\"\\\"")
   set(HEVCE_FEI_HW_version_defs  "-DMFX_PLUGIN_FILE_VERSION=\"\\\"${ver}${cur_date}\"\\\" -DMFX_PLUGIN_PRODUCT_VERSION=\"\\\"${hevc_version}\"\\\"")
 endif( )
 
@@ -61,22 +59,11 @@ include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/include )
 include_directories( ${MFX_ROOTDIR}/shared/include )
 include_directories( ${MFX_ROOTDIR}/mfx_lib/plugin/include )
 include_directories( ${MFX_ROOTDIR}/mfx_lib/scheduler/include )
-include_directories( ${MFX_ROOTDIR}/mfx_lib/cmrt_cross_platform/include )
-#include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/genx/include )
-#include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/genx/src )
 
 # =============================================================================
 
 set( sources "" )
 set( sources.plus "" )
-
-include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/../hevce_hw/h265/include )
-
-foreach( dir ${CMAKE_CURRENT_SOURCE_DIR}/../hevce_hw/h265/src )
-  file( GLOB_RECURSE srcs "${dir}/*.c" "${dir}/*.cpp" )
-  list( APPEND sources ${srcs})
-endforeach()
-
 
 set( defs "-DMFX_PLUGIN_PRODUCT_NAME=\"\\\"${HEVCE_HW_PRODUCT_NAME}\"\\\" -DMFX_FILE_DESCRIPTION=\"\\\"${HEVCE_HW_DESCRIPTION}\"\\\" ${HEVCE_HW_version_defs}" )
 
@@ -94,13 +81,9 @@ set( sources.plus "" )
 set( USE_STRICT_NAME TRUE )
 set(MFX_LDFLAGS "${MFX_ORIG_LDFLAGS} -Wl,--version-script=${MFX_ROOTDIR}/mfx_lib/plugin/libmfxsw_plugin.map" )
 
-include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/../hevce_hw/h265/include )
 include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/h265_fei/include )
 
-foreach( dir ${CMAKE_CURRENT_SOURCE_DIR}/h265_fei/src )
-  file( GLOB_RECURSE srcs "${dir}/*.c" "${dir}/*.cpp" )
-  list( APPEND sources ${srcs})
-endforeach()
+  list( APPEND sources ${CMAKE_CURRENT_SOURCE_DIR}/h265_fei/src/export.cpp)
 
   set( LIBS "" )
   list( APPEND LIBS

_studio/hevc_fei/h265_fei/include/mfx_h265_fei_encode_hw.h
@@ -19,8 +19,6 @@
 namespace MfxHwH265FeiEncode
 {
 
-static const mfxPluginUID  MFX_PLUGINID_HEVC_FEI_ENCODE = {{0x54, 0x18, 0xa7, 0x06, 0x66, 0xf9, 0x4d, 0x5c, 0xb4, 0xf7, 0xb1, 0xca, 0xee, 0x86, 0x33, 0x9b}};
-
 class H265FeiEncode_HW : public MfxHwH265Encode::MFXVideoENCODEH265_HW
 {
 public:
@@ -99,131 +97,6 @@ public:
 
         (dynamic_cast<VAAPIh265FeiEncoder*> (m_ddi.get()))->SoftReset(m_vpar);
     }
-};
-
-class H265FeiEncodePlugin : public MFXEncoderPlugin
-{
-public:
-    static MFXEncoderPlugin* Create()
-    {
-        return new H265FeiEncodePlugin(false);
-    }
-
-    static mfxStatus CreateByDispatcher(mfxPluginUID guid, mfxPlugin* mfxPlg)
-    {
-        if (memcmp(&guid, &MFX_PLUGINID_HEVC_FEI_ENCODE, sizeof(mfxPluginUID)))
-        {
-            return MFX_ERR_NOT_FOUND;
-        }
-
-        H265FeiEncodePlugin* tmp_pplg = nullptr;
-
-        try
-        {
-            tmp_pplg = new H265FeiEncodePlugin(false);
-        }
-        catch(std::bad_alloc&)
-        {
-            return MFX_ERR_MEMORY_ALLOC;
-        }
-        catch(...)
-        {
-            return MFX_ERR_UNKNOWN;
-        }
-
-        *mfxPlg = tmp_pplg->m_adapter;
-        tmp_pplg->m_createdByDispatcher = true;
-
-        return MFX_ERR_NONE;
-    }
-
-    virtual mfxStatus PluginInit(mfxCoreInterface *core) override;
-    virtual mfxStatus PluginClose() override;
-
-    virtual mfxStatus GetPluginParam(mfxPluginParam *par) override;
-
-    virtual void Release() override
-    {
-        delete this;
-    }
-
-    virtual mfxStatus Close() override
-    {
-        if (m_pImpl.get())
-            return m_pImpl->Close();
-        else
-            return MFX_ERR_NOT_INITIALIZED;
-    }
-
-    virtual mfxStatus Execute(mfxThreadTask task, mfxU32 uid_p, mfxU32 uid_a) override
-    {
-        if (m_pImpl.get())
-            return H265FeiEncode_HW::Execute((reinterpret_cast<void*>(m_pImpl.get())), task, uid_p, uid_a);
-        else
-            return MFX_ERR_NOT_INITIALIZED;
-    }
-
-    virtual mfxStatus FreeResources(mfxThreadTask /*task*/, mfxStatus /*sts*/) override
-    {
-        return MFX_ERR_NONE;
-    }
-
-    virtual mfxStatus QueryIOSurf(mfxVideoParam *par, mfxFrameAllocRequest *in, mfxFrameAllocRequest * /*out*/) override
-    {
-        return H265FeiEncode_HW::QueryIOSurf(&m_core, par, in);
-    }
-
-    virtual mfxStatus Query(mfxVideoParam *in, mfxVideoParam *out) override
-    {
-        return H265FeiEncode_HW::Query(&m_core, in, out);
-    }
-
-    virtual mfxStatus Init(mfxVideoParam *par) override
-    {
-        mfxStatus sts;
-        if (m_pImpl.get() && (m_pImpl->IsInitialized()))
-            return MFX_ERR_UNDEFINED_BEHAVIOR;
-        m_pImpl.reset(new H265FeiEncode_HW(&m_core, &sts));
-        MFX_CHECK_STS(sts);
-        return m_pImpl->Init(par);
-    }
-
-    virtual mfxStatus Reset(mfxVideoParam *par) override
-    {
-        if (m_pImpl.get())
-            return m_pImpl->Reset(par);
-        else
-            return MFX_ERR_NOT_INITIALIZED;
-    }
-
-    virtual mfxStatus GetVideoParam(mfxVideoParam *par) override
-    {
-        if (m_pImpl.get())
-            return m_pImpl->GetVideoParam(par);
-        else
-            return MFX_ERR_NOT_INITIALIZED;
-    }
-
-    virtual mfxStatus EncodeFrameSubmit(mfxEncodeCtrl *ctrl, mfxFrameSurface1 *surface, mfxBitstream *bs, mfxThreadTask *task) override
-    {
-        if (m_pImpl.get())
-            return m_pImpl->EncodeFrameSubmit(ctrl, surface, bs, task);
-        else
-            return MFX_ERR_NOT_INITIALIZED;
-    }
-
-    virtual mfxStatus SetAuxParams(void*, int) override
-    {
-        return MFX_ERR_UNSUPPORTED;
-    }
-protected:
-    explicit H265FeiEncodePlugin(bool CreateByDispatcher);
-    virtual ~H265FeiEncodePlugin();
-
-    bool m_createdByDispatcher;
-    MFXPluginAdapter<MFXEncoderPlugin> m_adapter;
-    mfxCoreInterface m_core;
-    std::unique_ptr<H265FeiEncode_HW> m_pImpl;
 };
 
 } //MfxHwH265FeiEncode

_studio/hevc_fei/h265_fei/src/export.cpp
@@ -5,12 +5,11 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2014-2017 Intel Corporation. All Rights Reserved.
+// Copyright(C) 2014-2018 Intel Corporation. All Rights Reserved.
 //
 
 #include "mfx_common.h"
-#if defined(MFX_ENABLE_HEVC_VIDEO_FEI_ENCODE)
-#include "mfx_h265_fei_encode_hw.h"
+#include "mfx_h265_fei_encode_plugin_hw.h"
 #include "plugin_version_linux.h"
 
 #if defined(_WIN32)
@@ -30,4 +29,3 @@ MSDK_PLUGIN_API(mfxStatus) CreatePlugin(mfxPluginUID uid, mfxPlugin* plugin)
 {
     return MfxHwH265FeiEncode::H265FeiEncodePlugin::CreateByDispatcher(uid, plugin);
 }
-#endif

_studio/hevc_fei/h265_fei/src/mfx_h265_fei_encode_hw.cpp
@@ -17,54 +17,6 @@ using namespace MfxHwH265Encode;
 namespace MfxHwH265FeiEncode
 {
 
-H265FeiEncodePlugin::H265FeiEncodePlugin(bool CreateByDispatcher)
-    : m_createdByDispatcher(CreateByDispatcher)
-    , m_adapter(this)
-    , m_pImpl(nullptr)
-{}
-
-H265FeiEncodePlugin::~H265FeiEncodePlugin()
-{}
-
-mfxStatus H265FeiEncodePlugin::PluginInit(mfxCoreInterface *core)
-{
-    MFX_TRACE_INIT();
-    MFX_AUTO_LTRACE(MFX_TRACE_LEVEL_API, "H265FeiEncodePlugin::PluginInit");
-
-    MFX_CHECK_NULL_PTR1(core);
-    m_core = *core;
-    return MFX_ERR_NONE;
-}
-
-mfxStatus H265FeiEncodePlugin::PluginClose()
-{
-    {
-        MFX_AUTO_LTRACE(MFX_TRACE_LEVEL_API, "H265FeiEncodePlugin::PluginClose");
-        if (m_createdByDispatcher)
-            Release();
-    }
-
-    MFX_TRACE_CLOSE();
-
-    return MFX_ERR_NONE;
-}
-
-mfxStatus H265FeiEncodePlugin::GetPluginParam(mfxPluginParam *par)
-{
-    MFX_CHECK_NULL_PTR1(par);
-
-    par->PluginUID        = MFX_PLUGINID_HEVC_FEI_ENCODE;
-    par->PluginVersion    = 1;
-    par->ThreadPolicy     = MFX_THREADPOLICY_SERIAL;
-    par->MaxThreadNum     = 1;
-    par->APIVersion.Major = MFX_VERSION_MAJOR;
-    par->APIVersion.Minor = MFX_VERSION_MINOR;
-    par->Type             = MFX_PLUGINTYPE_VIDEO_ENCODE;
-    par->CodecId          = MFX_CODEC_HEVC;
-
-    return MFX_ERR_NONE;
-}
-
 mfxStatus H265FeiEncode_HW::ExtraParametersCheck(mfxEncodeCtrl *ctrl, mfxFrameSurface1 *surface, mfxBitstream *bs)
 {
     if (!surface) return MFX_ERR_NONE; // In case of frames draining in display order

_studio/hevc_fei/mfxplugin_hevc_fei.vcxproj
@@ -265,6 +265,7 @@
     <ClInclude Include="..\mfx_lib\shared\include\mfx_brc_common.h" />
     <ClInclude Include="..\shared\include\auxiliary_device.h" />
     <ClInclude Include="h265_fei\include\mfx_h265_fei_encode_hw.h" />
+    <ClInclude Include="h265_fei\include\mfx_h265_fei_encode_plugin_hw.h" />
     <ClInclude Include="h265_fei\include\mfx_h265_fei_encode_vaapi.h" />
   </ItemGroup>
   <ItemGroup>

_studio/mfx_lib/CMakeLists.txt
@@ -32,6 +32,7 @@ endforeach( )
 
 include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/../hevce_hw/h265/include )
 include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/../vp9e_hw/include )
+include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/../hevc_fei/h265_fei/include )
 
 # =============================================================================
 

_studio/mfx_lib/encode_hw/CMakeLists.txt
@@ -18,6 +18,7 @@ include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/../genx/h264_encode/include )
 include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/../fei/h264_preenc )
 include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/../../hevce_hw/h265/include )
 include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/../../vp9e_hw/include )
+include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/../../hevc_fei/h265_fei/include )
 foreach( dir ${cdirs} )
   include_directories( ${CMAKE_CURRENT_SOURCE_DIR}/../../shared/umc/codec/${dir}/include )
 endforeach( )
@@ -42,7 +43,6 @@ foreach( prefix ${CMAKE_CURRENT_SOURCE_DIR}/../shared/src )
 endforeach()
 
 list( APPEND sources.plus
-    ${CMAKE_CURRENT_SOURCE_DIR}/../../hevce_hw/h265/src/export.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/../../hevce_hw/h265/src/mfx_h265_encode_hw.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/../../hevce_hw/h265/src/mfx_h265_encode_hw_brc.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/../../hevce_hw/h265/src/mfx_h265_encode_hw_bs.cpp
@@ -51,6 +51,8 @@ list( APPEND sources.plus
     ${CMAKE_CURRENT_SOURCE_DIR}/../../hevce_hw/h265/src/mfx_h265_encode_hw_par.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/../../hevce_hw/h265/src/mfx_h265_encode_hw_utils.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/../../hevce_hw/h265/src/mfx_h265_encode_vaapi.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/../../hevc_fei/h265_fei/src/mfx_h265_fei_encode_hw.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/../../hevc_fei/h265_fei/src/mfx_h265_fei_encode_vaapi.cpp
     )
 
 list( APPEND sources.plus

commit 3b984c63927ee236d1cdfa20205ec51201402b35

[github_port][hevce_fei] Fix memleak caused by commit 244f397
Issue: MDP-50639
Test: manual run of hevce_fei_encorder-memleak

Change-Id: Ic7c4bbc2ac45a54362573b7d735af9ff85ac0f8e

_studio/hevc_fei/h265_fei/include/mfx_h265_fei_encode_plugin_hw.h
@@ -24,7 +24,7 @@ namespace MfxHwH265FeiEncode
     public:
         static MFXEncoderPlugin* Create()
         {
-            return new H265FeiEncodePlugin(false);
+            return GetInstance();
         }
 
         static mfxStatus CreateByDispatcher(mfxPluginUID guid, mfxPlugin* mfxPlg)
@@ -38,11 +38,7 @@ namespace MfxHwH265FeiEncode
 
             try
             {
-                tmp_pplg = new H265FeiEncodePlugin(false);
-            }
-            catch (std::bad_alloc&)
-            {
-                return MFX_ERR_MEMORY_ALLOC;
+                tmp_pplg = GetInstance();
             }
             catch (...)
             {
@@ -86,7 +82,7 @@ namespace MfxHwH265FeiEncode
 
         virtual void Release() override
         {
-            delete this;
+            return;
         }
 
         virtual mfxStatus Close() override
@@ -143,6 +139,12 @@ namespace MfxHwH265FeiEncode
             : m_createdByDispatcher(CreateByDispatcher)
             , m_adapter(this)
         {}
+
+        static H265FeiEncodePlugin* GetInstance()
+        {
+            static H265FeiEncodePlugin instance(false);
+            return &instance;
+        }
 
         ~H265FeiEncodePlugin()
         {}