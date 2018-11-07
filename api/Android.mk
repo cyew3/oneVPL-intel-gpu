742f7eb8ca1c820e8d8f3b14264e78b94148272f
Denis Volkov <denis.volkov@intel.com>
2018-11-07 11:50:18 +0300

    Disabled folder api to fix Android build
    Temporary disable folder "api" inside "mdp_msdk-lib"
    until repository "mdp_msdk-api" is alive
    
    Issue: n/a
    Test: building with "mdp_msdk-api"
    
    Change-Id: Ib2be453f27f4a3d5aaf70d5c66d31f03268ed89a
    Signed-off-by: Denis Volkov <denis.volkov@intel.com>

--- api/Android.mk
+++ api/Android.mk
@@ -1,6 +1,6 @@
 ifeq ($(BOARD_HAVE_MEDIASDK_SRC),true)
-  MFX_HOME:= $(call my-dir)/..
+  # MFX_HOME:= $(call my-dir)/..
 
   # Recursively call sub-folder Android.mk
-  include $(call all-subdir-makefiles)
+  # include $(call all-subdir-makefiles)
 endif
