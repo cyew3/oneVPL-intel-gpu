2ad3e8128a9ecf08ddd271967ab5bb2be51e7269
Kokoshnikov, Alexey S <alexey.s.kokoshnikov@intel.com>
2018-11-15 15:48:15 +0300

    h264d: Avoid possible dereferencing nullptr from SetOfSlices::GetSlice()
    -Add check actual number of slices
    
    Issue: MDP-51772
    Test: build
    
    Back Port: "c6b3d1c [h264d/UMC]Avoid possible dereferencing nullptr"
    
    Change-Id: I36877e29c57c71a0a5689a116ce9d6e290fd71d5

--- _studio/shared/umc/codec/h264_dec/src/umc_h264_task_supplier.cpp
+++ _studio/shared/umc/codec/h264_dec/src/umc_h264_task_supplier.cpp
@@ -3649,6 +3649,8 @@ Status TaskSupplier::AddSlice(H264Slice * pSlice, bool force)
             {
                 SetOfSlices * setOfSlices = m_accessUnit.GetLayer(i);
                 H264Slice * slice = setOfSlices->GetSlice(0);
+                if (slice == nullptr)
+                    continue;
 
                 AllocateAndInitializeView(slice);
 
@@ -4092,7 +4094,8 @@ void TaskSupplier::DBPUpdate(H264DecoderFrame * pFrame, int32_t field)
 {
     H264DecoderFrameInfo *slicesInfo = pFrame->GetAU(field);
 
-    for (uint32_t i = 0; i < slicesInfo->GetSliceCount(); i++)
+    uint32_t count = slicesInfo->GetSliceCount();
+    for (uint32_t i = 0; i < count; i++)
     {
         H264Slice * slice = slicesInfo->GetSlice(i);
         if (!slice->IsReference())
