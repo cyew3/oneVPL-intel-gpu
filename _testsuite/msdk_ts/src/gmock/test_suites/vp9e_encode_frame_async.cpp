353c7b3dd66de7e94a8f72758c31c927a6968da1
Sokolova,Anastasiia <anastasiia.sokolova@intel.com>
2018-11-21 12:20:26 +0300

    [Test][VP9e] Fixed y410 case
    Issue: MDP-52041
    Test: vp9e_10b_444_y410_encode_frame_async
    
    Change-Id: I6d381962a2795a23b145e08e96c8dfb8bbaa4a04

--- _testsuite/msdk_ts/src/gmock/test_suites/vp9e_encode_frame_async.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/vp9e_encode_frame_async.cpp
@@ -4,7 +4,7 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2016-2017 Intel Corporation. All Rights Reserved.
+Copyright(c) 2016-2018 Intel Corporation. All Rights Reserved.
 
 \* ****************************************************************************** */
 
@@ -333,6 +333,12 @@ namespace vp9e_encode_frame_async
             }
 
             SETPARS(m_pSurf, MFX_SURF);
+            if (m_par.mfx.FrameInfo.FourCC == MFX_FOURCC_Y410 &&
+                m_par.IOPattern == MFX_IOPATTERN_IN_SYSTEM_MEMORY &&
+                m_pSurf->Data.Y == 0)
+            {
+                m_pSurf->Data.Y410 = NULL;
+            }
         }
 
         if (tc.type == CLOSED)
