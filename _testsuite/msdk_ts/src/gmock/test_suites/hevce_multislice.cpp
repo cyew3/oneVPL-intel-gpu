6b55d608de636c764d20fc1f69885d39e494065c
Alexandr Smirnov <alexander.smirnov@intel.com>
2019-01-16 13:31:12 +0300

    [gmock][HEVCe] extended beh_hevce_multislice with REXT formats
    P010/YUY2/Y210/AYUV/Y410 formats were enabled
    
    Issue: MDP-52804, MDP-41735, MDP-41493
    Test: beh_hevce_multislice
    
    Change-Id: Ic339bfb3dfe4b4ca8453acaf17e3fd5a866a7b46

--- _testsuite/msdk_ts/src/gmock/test_suites/hevce_multislice.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/hevce_multislice.cpp
@@ -4,7 +4,7 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2014-2017 Intel Corporation. All Rights Reserved.
+Copyright(c) 2014-2019 Intel Corporation. All Rights Reserved.
 
 \* ****************************************************************************** */
 #include "ts_encoder.h"
@@ -49,7 +49,11 @@ namespace hevce_multislice
         {
         }
         ~TestSuite() { }
-        int RunTest(unsigned int id);
+
+        template<mfxU32 fourcc>
+        int RunTest_Subtype(const unsigned int id);
+        int RunTest(tc_struct tc, unsigned int fourcc_id);
+
         static const unsigned int n_cases;
         static const tc_struct test_case[];
     };
@@ -346,9 +350,14 @@ namespace hevce_multislice
 
     const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case) / sizeof(TestSuite::test_case[0]);
 
-    int TestSuite::RunTest(unsigned int id) {
+    template<mfxU32 fourcc>
+    int TestSuite::RunTest_Subtype(const unsigned int id) {
+        const tc_struct& tc = test_case[id];
+        return RunTest(tc, fourcc);
+    }
+
+    int TestSuite::RunTest(tc_struct tc, unsigned int fourcc_id) {
         TS_START;
-        auto& tc = test_case[id];
 
         if (g_tsOSFamily != MFX_OS_FAMILY_WINDOWS) {
             g_tsLog << "[ SKIPPED ] This test is only for windows platform\n";
@@ -356,7 +365,7 @@ namespace hevce_multislice
         }
 
         if (g_tsImpl == MFX_IMPL_HARDWARE) {
-            if (g_tsHWtype < MFX_HW_CNL || m_par.mfx.LowPower != MFX_CODINGOPTION_ON) {
+            if (g_tsHWtype < MFX_HW_CNL) {
                 g_tsLog << "SKIPPED for this platform\n";
                 return 0;
             }
@@ -371,6 +380,54 @@ namespace hevce_multislice
         SETPARS(&co2, EXT_COD2)
 
         Load();
+
+        if (fourcc_id == MFX_FOURCC_NV12)
+        {
+            m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_NV12;
+            m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV420;
+            m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 8;
+        }
+        else if (fourcc_id == MFX_FOURCC_P010)
+        {
+            m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_P010;
+            m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV420;
+            m_par.mfx.FrameInfo.Shift = 1;
+            m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 10;
+            m_par.mfx.CodecProfile = MFX_PROFILE_HEVC_MAIN10;
+        }
+        else if (fourcc_id == MFX_FOURCC_AYUV)
+        {
+            m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_AYUV;
+            m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV444;
+            m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 8;
+            m_par.mfx.CodecProfile = MFX_PROFILE_HEVC_REXT;
+        }
+        else if (fourcc_id == MFX_FOURCC_Y410)
+        {
+            m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_Y410;
+            m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV444;
+            m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 10;
+            m_par.mfx.CodecProfile = MFX_PROFILE_HEVC_REXT;
+        }
+        else if (fourcc_id == MFX_FOURCC_YUY2)
+        {
+            m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_YUY2;
+            m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV422;
+            m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 8;
+            m_par.mfx.CodecProfile = MFX_PROFILE_HEVC_REXT;
+        }
+        else if (fourcc_id == MFX_FOURCC_Y210)
+        {
+            m_par.mfx.FrameInfo.FourCC = MFX_FOURCC_Y210;
+            m_par.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV422;
+            m_par.mfx.FrameInfo.BitDepthLuma = m_par.mfx.FrameInfo.BitDepthChroma = 10;
+            m_par.mfx.CodecProfile = MFX_PROFILE_HEVC_REXT;
+        }
+        else
+        {
+            g_tsLog << "WARNING: invalid fourcc_id parameter: " << fourcc_id << "\n";
+            return 0;
+        }
 
         if (tc.type & QUERY) {
             SETPARS(m_par, MFX_PAR);
@@ -408,5 +465,10 @@ namespace hevce_multislice
         return 0;
     }
 
-    TS_REG_TEST_SUITE_CLASS(hevce_multislice);
+    TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_8b_420_nv12_multislice,  RunTest_Subtype<MFX_FOURCC_NV12>, n_cases);
+    TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_10b_420_p010_multislice, RunTest_Subtype<MFX_FOURCC_P010>, n_cases);
+    TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_8b_444_ayuv_multislice,  RunTest_Subtype<MFX_FOURCC_AYUV>, n_cases);
+    TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_10b_444_y410_multislice, RunTest_Subtype<MFX_FOURCC_Y410>, n_cases);
+    TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_8b_422_yuy2_multislice,  RunTest_Subtype<MFX_FOURCC_YUY2>, n_cases);
+    TS_REG_TEST_SUITE_CLASS_ROUTINE(hevce_10b_422_y210_multislice, RunTest_Subtype<MFX_FOURCC_Y210>, n_cases);
 }
