cyew3/oneVPL-intel-gpu

commit ac8ebfd1bf87e4f69d790a7ab5c7eabc734f29ed

[av1d] Request 2x video surfaces in case of FilmGrain
Issue: VSMGWL-12015
Test: manual

In case of VIDEO memory at input app will get request for 2x frames.
In case of SYSTEM memory at input app will get request for 1x frames and
decoder will get internal request for 2x VIDEO surfaces during initialization.

2x output surfaces is required for case when FilmGrain is applied. This
multiplier concerns both numbers of (1) reference frames and (2) async
frames. 2x for reference frames is required because we need to keep
frame with applied film grain until respective reconstruct is removed
from reference frames because output of "filmgrained" frame is required
in case of "show_existing_frame".

Change-Id: I062ebff332a312234c54df3b747a2d899fa425d4
Signed-off-by: Stanislav Efremov <stanislav.efremov@intel.com>

_studio/mfx_lib/shared/src/mfx_vpx_dec_common.cpp
@@ -452,6 +452,10 @@ namespace MFX_VPX_Utility
         p_request->NumFrameMin = p_params->mfx.CodecId == MFX_CODEC_VP8 ? mfxU16(4) : mfxU16(8);
 
         p_request->NumFrameMin += p_params->AsyncDepth ? p_params->AsyncDepth : MFX_AUTO_ASYNC_DEPTH_VALUE;
+
+        if (MFX_CODEC_AV1 && p_params->mfx.FilmGrain)
+            p_request->NumFrameMin = 2 * p_request->NumFrameMin; // we need two output surfaces for each frame when film_grain is applied
+
         p_request->NumFrameSuggested = p_request->NumFrameMin;
 
         if (p_params->IOPattern & MFX_IOPATTERN_OUT_SYSTEM_MEMORY)

commit a36588bd02cc8d12f440e9782cb2a733742391b4

av1d: fix typo in FilmGrain path
Issue: MDP-53014
Tests: build
Fixes: ac8ebfd1 ("[av1d] Request 2x video surfaces in case of FilmGrain")

Change-Id: Icbef0081a9b8927f722ad18892164489057ebf23
Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

_studio/mfx_lib/shared/src/mfx_vpx_dec_common.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018 Intel Corporation
+// Copyright (c) 2017-2019 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -439,7 +439,7 @@ namespace MFX_VPX_Utility
         p_request->NumFrameMin += p_params->AsyncDepth ? p_params->AsyncDepth : MFX_AUTO_ASYNC_DEPTH_VALUE;
 
 #if (MFX_VERSION >= MFX_VERSION_NEXT)
-        if (MFX_CODEC_AV1 && p_params->mfx.FilmGrain)
+        if ((p_params->mfx.CodecId == MFX_CODEC_AV1) && p_params->mfx.FilmGrain)
             p_request->NumFrameMin = 2 * p_request->NumFrameMin; // we need two output surfaces for each frame when film_grain is applied
 #endif
         p_request->NumFrameSuggested = p_request->NumFrameMin;