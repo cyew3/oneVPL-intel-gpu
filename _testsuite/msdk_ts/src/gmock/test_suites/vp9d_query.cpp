e8a76bfe16c04dd9939c243bb5ec8d697ce6abf5
Vladimir Sidorov <vladimirx.sidorov@intel.com>
2019-01-31 15:07:18 +0300

    [vp9d] Skip protected cases
    Protected field has no support
    in vp9 linux decoder
    
    Issue: n/a
    Test: manual run
    
    Change-Id: Id3407b684be67a5a0e3de437113975ba171bc387

--- _testsuite/msdk_ts/src/gmock/test_suites/vp9d_query.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/vp9d_query.cpp
@@ -4,7 +4,7 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2016-2018 Intel Corporation. All Rights Reserved.
+Copyright(c) 2016-2019 Intel Corporation. All Rights Reserved.
 
 \* ****************************************************************************** */
 
@@ -178,9 +178,9 @@ const TestSuite::tc_struct TestSuite::test_case[] =
     {/*27*/ MFX_ERR_UNSUPPORTED, 0, {{FourCC, MFX_FOURCC_YUY2, INVALID} }},
     {/*28*/ MFX_ERR_UNSUPPORTED, 0, {{FourCC, MFX_FOURCC_NV16, INVALID} }},
     {/*29*/ MFX_ERR_UNSUPPORTED, 0, {{FourCC, MFX_FOURCC_ARGB16, INVALID} }},
-
     {/*30*/ MFX_ERR_UNSUPPORTED, 0, {{&tsStruct::mfxVideoParam.Protected, MFX_PROTECTION_PAVP} }, },
     {/*31*/ MFX_ERR_UNSUPPORTED, 0, {{&tsStruct::mfxVideoParam.Protected, MFX_PROTECTION_GPUCP_PAVP} }, },
+
 };
 const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case)/sizeof(TestSuite::tc_struct);
 
@@ -317,6 +317,11 @@ int TestSuite::RunTest_fourcc(const unsigned int id)
 int TestSuite::RunTest(const tc_struct& tc)
 {
     TS_START;
+
+    if (g_tsOSFamily == MFX_OS_FAMILY_LINUX && tc.set_par->f == &tsStruct::mfxVideoParam.Protected) {
+        g_tsLog << "[ SKIPPED ] Protected is not supported on Linux\n";
+        throw tsSKIP;
+    }
 
     MFXInit();
     if(m_uid && PLUGIN_NOT_LOADED != tc.mode)
