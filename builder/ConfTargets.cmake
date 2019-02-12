cyew3/oneVPL-intel-gpu

commit fcba50435e7c5680785702edeb2779cb2290b165

Optional build and use the asc lib
Issue: n/a
Tests: build
Github: 657080b ("Optional build and use the asc lib")

Change-Id: I0795995f5bc1dfa995009daeecf1490088c58acb

builder/ConfTargets.cmake
@@ -107,7 +107,11 @@ cmake_dependent_option(
 
 option( MFX_ENABLE_VP9_VIDEO_ENCODE "Enable VP9 encoder?" ON)
 
-option( MFX_ENABLE_MCTF "Build with MCTF support?"  ${MFX_1_25_OPTIONS_ALLOWED} )
+option( MFX_ENABLE_ASC "Enable ASC support?"  ON )
+
+cmake_dependent_option(
+  MFX_ENABLE_MCTF "Build with MCTF support?"  ${MFX_1_25_OPTIONS_ALLOWED}
+  "MFX_ENABE_ASC;MFX_ENABE_KERNELS" OFF)
 
 # Now we will include config file which may overwrite default values of the
 # options and options which user provided in a command line.

_studio/shared/include/mfx_config.h
@@ -464,6 +464,10 @@
 
 #endif//#if defined(_WIN32) || defined(_WIN64)
 
+#if defined(MFX_ENABLE_ASC)
+    #define MFX_ENABLE_SCENE_CHANGE_DETECTION_VPP
+#endif
+
 #if (MFX_VERSION >= MFX_VERSION_NEXT) && defined(MFX_ENABLE_MCTF)
     #define MFX_ENABLE_MCTF_EXT // extended MCTF interface
 #endif

builder/mfxconfig.h.in
@@ -20,6 +20,7 @@
 
 #cmakedefine MFX_ENABLE_KERNELS
 #cmakedefine MFX_ENABLE_MCTF
+#cmakedefine MFX_ENABLE_ASC
 
 #cmakedefine MFX_ENABLE_H264_VIDEO_ENCODE
 #cmakedefine MFX_ENABLE_H264_VIDEO_FEI_ENCODE

builder/open_source_symbols.list
@@ -575,6 +575,7 @@ CM_BUFFER
 MFX_ENABLE_HEVC_VIDEO_FEI_ENCODE
 DISALLOW_COPY_AND_ASSIGN
 FALSE
+MFX_ENABLE_ASC
 MFX_ENABLE_MCTF
 MFX_ENABLE_MCTF_EXT
 ENABLE_MCTF

builder/profiles/fastboot.cmake
@@ -29,6 +29,7 @@ set( BUILD_DISPATCHER OFF )
 
 set( MFX_ENABLE_KERNELS OFF )
 set( MFX_ENABLE_MCTF OFF )
+set( MFX_ENABLE_ASC OFF )
 
 set( MFX_ENABLE_H264_VIDEO_ENCODE OFF )
 set( MFX_ENABLE_H264_VIDEO_FEI_ENCODE OFF )

commit 34f6b459e50cd623858a61f6c6cb2bc9ec096532

MCTF build update
Issue: n/a
Tests: build
Github: 3e7ca86 ("Optional MCTF build update")
Github: 40f419b ("build: fix usage of MFX_ENABLE_ASC and MFX_ENABLE_MCTF")
Fixes: fcba504 ("Optional build and use the asc lib")

Change-Id: I40bd9b90e818492807b857668d6cbed1a5e0af8c

_studio/mfx_lib/CMakeLists.txt
@@ -311,7 +311,7 @@ list( APPEND sources ${srcs})
 set( USE_STRICT_NAME TRUE )
 set(MFX_LDFLAGS "${MFX_ORIG_LDFLAGS} -Wl,--version-script=${CMAKE_CURRENT_SOURCE_DIR}/libmfxaudio.map" )
 
-if( API_VERSION VERSION_GREATER 1.25 )
+if( MFX_ENABLE_KERNELS )
   set(MCTF_LIB "mctf_hw")
 else()
   set(MCTF_LIB "")

_studio/mfx_lib/genx/CMakeLists.txt
@@ -79,7 +79,7 @@ if(MFX_ENABLE_KERNELS)
     ${prefix}/genx_skl_simple_me_isa.cpp
   )
 
-  if( API_VERSION VERSION_GREATER 1.25 )
+  if( MFX_ENABLE_KERNELS )
     set( prefix ${MSDK_GENX_ROOT}/mctf/isa )
     list( APPEND sources
       ${prefix}/mctf_mc_gen8_isa.cpp

_studio/mfx_lib/mctf_package/CMakeLists.txt
@@ -1,4 +1,4 @@
-if( API_VERSION VERSION_GREATER 1.25 )
+if( MFX_ENABLE_KERNELS )
     set(MFX_ORIG_LDFLAGS "${MFX_LDFLAGS}" )
 
     mfx_include_dirs( )

builder/ConfTargets.cmake
@@ -113,7 +113,7 @@ option( MFX_ENABLE_ASC "Enable ASC support?"  ON )
 
 cmake_dependent_option(
   MFX_ENABLE_MCTF "Build with MCTF support?"  ${MFX_1_25_OPTIONS_ALLOWED}
-  "MFX_ENABE_ASC;MFX_ENABE_KERNELS" OFF)
+  "MFX_ENABE_ASC;MFX_ENABLE_KERNELS" OFF)
 
 # Now we will include config file which may overwrite default values of the
 # options and options which user provided in a command line.