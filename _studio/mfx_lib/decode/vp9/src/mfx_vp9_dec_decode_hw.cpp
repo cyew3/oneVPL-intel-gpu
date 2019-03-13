c80ca3a050ad76eae9894c6f18ff0d5282f40489
Mikhail Aksenov <mikhailx.aksenov@intel.com>
2019-03-13 14:37:41 +0300

    [vp9d] Clear mfx.Shift for packed format
    DecodeHeader and UpdateVideoParam functions
    set Shift to one for planar formats
    but didn't clear it for packed formats (like Y410).
    
    Github: cdb0662b68e1dc00b426dc75fcbe4a2f2a19645f
    Issue: vsmgwl-10697
    Test: vp9d_*_format_change./*
    
    Change-Id: Iad192c714a65b8531340dfa5e5521ab4972bab95

--- _studio/mfx_lib/decode/vp9/src/mfx_vp9_dec_decode_hw.cpp
+++ _studio/mfx_lib/decode/vp9/src/mfx_vp9_dec_decode_hw.cpp
@@ -701,19 +701,6 @@ mfxStatus VideoDECODEVP9_HW::DecodeHeader(VideoCORE* core, mfxBitstream* bs, mfx
     mfxStatus sts = MFX_VP9_Utility::DecodeHeader(core, bs, par);
     MFX_CHECK_STS(sts);
 
-    if (   par->mfx.FrameInfo.FourCC == MFX_FOURCC_P010
-#if defined(PRE_SI_TARGET_PLATFORM_GEN12)
-        || par->mfx.FrameInfo.FourCC == MFX_FOURCC_P016
-        || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y416
-#endif //PRE_SI_TARGET_PLATFORM_GEN12
-        )
-    {
-        par->mfx.FrameInfo.Shift = 1;
-    }
-    else
-    {
-        par->mfx.FrameInfo.Shift = 0;
-    }
     return sts;
 }
 
@@ -881,21 +868,6 @@ mfxStatus VideoDECODEVP9_HW::GetVideoParam(mfxVideoParam *par)
     par->mfx.FrameInfo.FrameRateExtN = m_vInitPar.mfx.FrameInfo.FrameRateExtN;
 
     return MFX_ERR_NONE;
-}
-
-void VideoDECODEVP9_HW::UpdateVideoParam(mfxVideoParam *par, VP9DecoderFrame const & frameInfo)
-{
-    VM_ASSERT(par);
-
-    MFX_VP9_Utility::FillVideoParam(m_core, frameInfo, par);
-
-    if (   par->mfx.FrameInfo.FourCC == MFX_FOURCC_P010
-#if defined(PRE_SI_TARGET_PLATFORM_GEN12)
-        || par->mfx.FrameInfo.FourCC == MFX_FOURCC_P016
-        || par->mfx.FrameInfo.FourCC == MFX_FOURCC_Y416
-#endif //PRE_SI_TARGET_PLATFORM_GEN12
-        )
-        par->mfx.FrameInfo.Shift = 1;
 }
 
 mfxStatus VideoDECODEVP9_HW::GetDecodeStat(mfxDecodeStat *pStat)
@@ -1269,7 +1241,7 @@ mfxStatus VideoDECODEVP9_HW::DecodeFrameCheck(mfxBitstream *bs, mfxFrameSurface1
     sts = DecodeFrameHeader(bs, frameInfo);
     MFX_CHECK_STS(sts);
 
-    UpdateVideoParam(&m_vPar, frameInfo);
+    MFX_VP9_Utility::FillVideoParam(m_core->GetPlatformType(), frameInfo, m_vPar);
 
     // check resize
     if (m_vPar.mfx.FrameInfo.Width > surface_work->Info.Width || m_vPar.mfx.FrameInfo.Height > surface_work->Info.Height)
