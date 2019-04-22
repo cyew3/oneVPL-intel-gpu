05fb786e28fd8508f39efb363fe4be722e766338
Miroslav Goncharenko <miroslav.goncharenko@intel.com>
2019-04-22 15:08:36 +0300

    encode: align returned statuses with documentation
    Issue: none
    Test: manual
    GitHub: a96a2fd
    
    Change-Id: I2c3000a57e7b7ff32cb20ef86ee44ad47bd88de8

--- _studio/mfx_lib/shared/src/libmfxsw_encode.cpp
+++ _studio/mfx_lib/shared/src/libmfxsw_encode.cpp
@@ -713,7 +713,7 @@ mfxStatus MFXVideoENCODE_QueryIOSurf(mfxSession session, mfxVideoParam *par, mfx
             handler = codecId2Handlers.find(CodecKey(par->mfx.CodecId, fei));
         }
 
-        mfxRes = handler == codecId2Handlers.end() ? MFX_ERR_UNSUPPORTED
+        mfxRes = handler == codecId2Handlers.end() ? MFX_ERR_INVALID_VIDEO_PARAM
             : (handler->second.primary.queryIOSurf)(session, par, request);
 
         if (MFX_WRN_PARTIAL_ACCELERATION == mfxRes)
@@ -725,7 +725,7 @@ mfxStatus MFXVideoENCODE_QueryIOSurf(mfxSession session, mfxVideoParam *par, mfx
             if (!session->m_plgEnc.get())
 #endif
             {
-                mfxRes = !handler->second.fallback.query ? MFX_ERR_UNSUPPORTED
+                mfxRes = !handler->second.fallback.query ? MFX_ERR_INVALID_VIDEO_PARAM
                     : (handler->second.fallback.queryIOSurf)(session, par, request);
             }
         }
@@ -737,7 +737,7 @@ mfxStatus MFXVideoENCODE_QueryIOSurf(mfxSession session, mfxVideoParam *par, mfx
     // handle error(s)
     catch(...)
     {
-        mfxRes = MFX_ERR_NULL_PTR;
+        mfxRes = MFX_ERR_UNKNOWN;
     }
 
     if (MFX_PLATFORM_HARDWARE == session->m_currentPlatform &&
@@ -747,7 +747,7 @@ mfxStatus MFXVideoENCODE_QueryIOSurf(mfxSession session, mfxVideoParam *par, mfx
 #ifndef OPEN_SOURCE
         mfxRes = MFX_WRN_PARTIAL_ACCELERATION;
 #else // OPEN_SOURCE
-        mfxRes = MFX_ERR_UNSUPPORTED;
+        mfxRes = MFX_ERR_INVALID_VIDEO_PARAM;
 #endif // OPEN_SOURCE
     }
 
@@ -825,8 +825,9 @@ mfxStatus MFXVideoENCODE_Close(mfxSession session)
 
     MFX_AUTO_LTRACE_FUNC(MFX_TRACE_LEVEL_API);
 
-    MFX_CHECK(session, MFX_ERR_INVALID_HANDLE);
+    MFX_CHECK(session,               MFX_ERR_INVALID_HANDLE);
     MFX_CHECK(session->m_pScheduler, MFX_ERR_NOT_INITIALIZED);
+    MFX_CHECK(session->m_pENCODE,    MFX_ERR_NOT_INITIALIZED);
 
     try
     {
