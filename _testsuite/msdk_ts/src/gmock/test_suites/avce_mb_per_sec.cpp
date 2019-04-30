14cf7529a1d5845cff78332cf9c1e501a1fbebac
Emelianova Svetlana <svetlana.emelianova@intel.com>
2019-04-30 11:18:28 +0300

    [Gmock] [AVCe] Deleted preprocessing definition, corrected check D3D11
    Deleted preprocessing definition to avoid mistakes when gmock was built on one
    machine but is used on other with different version libva. Also corrected
    checking D3D11 implementation, it is needed only for Windows.
    
    Test: manual testing avce_mb_per_sec
    Issue: MDP-54440, MDP-54449
    
    Change-Id: I7a3a191e06d71eac4f94403ef6eed4837c09d84b

--- _testsuite/msdk_ts/src/gmock/test_suites/avce_mb_per_sec.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/avce_mb_per_sec.cpp
@@ -91,20 +91,10 @@ namespace avce_mb_per_sec{
                 { MFX_PAR, &tsStruct::mfxVideoParam.mfx.RateControlMethod, MFX_RATECONTROL_CQP }}},
         {/*03*/ MFX_ERR_NONE, {{MFX_PAR, &tsStruct::mfxExtEncoderCapability.MBPerSec, 0xFFFFFFFF},
                 { MFX_PAR, &tsStruct::mfxVideoParam.mfx.RateControlMethod, MFX_RATECONTROL_ICQ }}},
- #if VA_CHECK_VERSION(1,3,0)
-        {/*04*/ MFX_ERR_NONE, {{MFX_PAR, &tsStruct::mfxExtEncoderCapability.MBPerSec, 0xFFFFFFFF},
+        {/*04*/ MFX_ERR_NONE, {{MFX_PAR, &tsStruct::mfxExtEncoderCapability.MBPerSec, 0xFFFFFFFF}, // supported with va_version >= 1.3.0
                 { MFX_PAR, &tsStruct::mfxVideoParam.mfx.RateControlMethod, MFX_RATECONTROL_AVBR }}},
-#else
-        {/*04*/ MFX_ERR_UNSUPPORTED, {{MFX_PAR, &tsStruct::mfxExtEncoderCapability.MBPerSec, 0xFFFFFFFF},
-                { MFX_PAR, &tsStruct::mfxVideoParam.mfx.RateControlMethod, MFX_RATECONTROL_AVBR }}},
-#endif
-#ifdef MFX_ENABLE_QVBR
-        {/*05*/ MFX_ERR_NONE, {{MFX_PAR, &tsStruct::mfxExtEncoderCapability.MBPerSec, 0xFFFFFFFF},
+        {/*05*/ MFX_ERR_NONE, {{MFX_PAR, &tsStruct::mfxExtEncoderCapability.MBPerSec, 0xFFFFFFFF}, // supported with va_version >= 1.3.0
                 { MFX_PAR, &tsStruct::mfxVideoParam.mfx.RateControlMethod, MFX_RATECONTROL_QVBR }}},
-#else
-        {/*05*/ MFX_ERR_UNSUPPORTED, {{MFX_PAR, &tsStruct::mfxExtEncoderCapability.MBPerSec, 0xFFFFFFFF},
-                { MFX_PAR, &tsStruct::mfxVideoParam.mfx.RateControlMethod, MFX_RATECONTROL_QVBR }}},
-#endif
         {/*06*/ MFX_ERR_UNSUPPORTED, {{MFX_PAR, &tsStruct::mfxExtEncoderCapability.MBPerSec, 0xFFFFFFFF},
                 { MFX_PAR, &tsStruct::mfxVideoParam.mfx.RateControlMethod, MFX_RATECONTROL_LA }}},
         {/*07*/ MFX_ERR_UNSUPPORTED, {{MFX_PAR, &tsStruct::mfxExtEncoderCapability.MBPerSec, 0xFFFFFFFF},
@@ -130,10 +120,13 @@ namespace avce_mb_per_sec{
         mfxExtEncoderCapability *m_ec = reinterpret_cast <mfxExtEncoderCapability*> (m_par.GetExtBuffer(MFX_EXTBUFF_ENCODER_CAPABILITY));
 
         mfxStatus exp = tc.sts;
-        if (g_tsImpl & MFX_IMPL_VIA_D3D9)
-            exp = MFX_ERR_UNSUPPORTED;
-        if (g_tsImpl & MFX_IMPL_VIA_D3D11)
-            exp = MFX_ERR_NONE;
+        if (g_tsOSFamily == MFX_OS_FAMILY_WINDOWS)
+        {
+            if (!(g_tsImpl & MFX_IMPL_VIA_D3D11))
+                exp = MFX_ERR_UNSUPPORTED;
+            else
+                exp = MFX_ERR_NONE;
+        }
         g_tsStatus.expect(exp);
 
         Query();
