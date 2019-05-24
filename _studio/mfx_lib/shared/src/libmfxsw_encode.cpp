9fa0aa6ea656de5f8a4a6c5d796fd99be72ceaa0
Alexandr Smirnov <alexander.smirnov@intel.com>
2019-05-24 17:11:58 +0300

    [HEVCe] Disable mfx_reflection for HEVCe plugins
    This is to fix ICC build issues with hevce plugins
    
    Test: manual
    Issue: -
    
    Change-Id: I5472229629d42dd7589de2f84cfa8ee8f63050d1

--- _studio/mfx_lib/shared/src/libmfxsw_encode.cpp
+++ _studio/mfx_lib/shared/src/libmfxsw_encode.cpp
@@ -654,6 +654,7 @@ mfxStatus MFXVideoENCODE_Query(mfxSession session, mfxVideoParam *in, mfxVideoPa
     }
 
 #if (MFX_VERSION >= 1025)
+#if !defined(AS_HEVCE_PLUGIN)
     if (mfxRes == MFX_WRN_INCOMPATIBLE_VIDEO_PARAM || mfxRes == MFX_ERR_INCOMPATIBLE_VIDEO_PARAM)
     {
         try
@@ -674,6 +675,7 @@ mfxStatus MFXVideoENCODE_Query(mfxSession session, mfxVideoParam *in, mfxVideoPa
             MFX_LTRACE_MSG(MFX_TRACE_LEVEL_INTERNAL, "Unknown exception was caught while comparing In and Out VideoParams.");
         }
     }
+#endif
 #endif
 
     MFX_LTRACE_BUFFER(MFX_TRACE_LEVEL_API, out);
