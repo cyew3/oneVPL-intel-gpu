45decd8230933f7a810e6634ee640749132a5cbc
Emelianova Svetlana <svetlana.emelianova@intel.com>
2019-10-01 13:12:32 +0300

    [Gmock] [AVCe] Refactoring of avce_mb_disable_skip_map beh
    Fixed encode process by reading real stream. Corrected check for unexpected
    skipped macroblocks. Encode all frames and then check count of unexpected
    skipped MB. If m_unexpected_skipped_MB > 0 return failure.
    
    Test: manual testing beh_avce_mb_disable_skip_map,
    beh_avce_mb_disable_skip_map-lowpower on skl, icl
    Issue: MDP-52292
    
    Change-Id: Idb28b21afe0f1c9f58f2e629110faae0ec618250

--- _testsuite/msdk_ts/src/gmock/test_suites/avce_mb_disable_skip_map.cpp
+++ _testsuite/msdk_ts/src/gmock/test_suites/avce_mb_disable_skip_map.cpp
@@ -1,3 +1,16 @@
+﻿/* ****************************************************************************** *\
+
+INTEL CORPORATION PROPRIETARY INFORMATION
+This software is supplied under the terms of a license agreement or nondisclosure
+agreement with Intel Corporation and may not be copied or disclosed except in
+accordance with the terms of that agreement
+Copyright(c) 2019 Intel Corporation. All Rights Reserved.
+
+File Name: avce_mb_disable_skip_map.cpp
+
+\* ****************************************************************************** */
+
+
 #include "ts_encoder.h"
 #include "ts_struct.h"
 #include "bs_parser++.h"
@@ -7,26 +20,34 @@ namespace avce_mb_disable_skip_map
 
 #define IS_MB_SKIPPED(mbtype)\
   ((mbtype == BS_AVC2::P_Skip || mbtype == BS_AVC2::B_Skip) ? true : false)
+#define WIDTH 352
+#define HEIGHT 288
+#define N_FRAMES 10
 
-class TestSuite : public tsVideoEncoder, public tsBitstreamProcessor, public BS_AVC2_parser
+class TestSuite : public tsVideoEncoder, public tsSurfaceProcessor, public tsBitstreamProcessor, public BS_AVC2_parser
 {
-    std::vector<mfxExtMBDisableSkipMap> m_skip_map;
+    std::vector<mfxExtMBDisableSkipMap> m_non_skip_map;
+    tsRawReader* m_reader;
 public:
     TestSuite()
         : tsVideoEncoder(MFX_CODEC_AVC)
         , BS_AVC2_parser(BS_AVC2::INIT_MODE_PARSE_SD)
+        , m_reader(nullptr)
     {
+        m_filler = this;
         m_bs_processor = this;
     }
     ~TestSuite()
     {
-        for (mfxU32 i = 0; i < m_skip_map.size(); i++) {
-            if (m_skip_map[i].Map) {
-                delete[] m_skip_map[i].Map;
-                m_skip_map[i].Map = 0;
+        for (mfxU32 i = 0; i < m_non_skip_map.size(); i++) {
+            if (m_non_skip_map[i].Map) {
+                delete[] m_non_skip_map[i].Map;
+                m_non_skip_map[i].Map = nullptr;
             }
         }
-        m_skip_map.clear();
+        m_non_skip_map.clear();
+        if (m_reader)
+            delete m_reader;
     }
 
     int RunTest(unsigned int id);
@@ -83,24 +104,18 @@ public:
             frm_map.Map = new mfxU8[frm_map.MapSize];
             memset(frm_map.Map, 0, sizeof(mfxU8) * frm_map.MapSize);
 
-            mfxU32 non_skip = 0;
-
             for (Bs32u i = 0; i < au->NumUnits; i++)
             {
-                if (au->nalu[i]->nal_unit_type == BS_AVC2::SLICE_NONIDR ||
-                    au->nalu[i]->nal_unit_type == BS_AVC2::SLICE_IDR)
+                if (au->nalu[i]->nal_unit_type == BS_AVC2::SLICE_NONIDR)
                 {
                     for (Bs32u nmb = 0; nmb < au->nalu[i]->slice->NumMB; nmb++)
                     {
-                        if (IS_MB_SKIPPED(au->nalu[i]->slice->mb[nmb].MbType))
-                        {
+                        if (!IS_MB_SKIPPED(au->nalu[i]->slice->mb[nmb].MbType))
                             frm_map.Map[nmb] = 1;
-                            non_skip++;
-                        }
                     }
                 }
             }
-            m_skip_map.push_back(frm_map);
+            m_non_skip_map.push_back(frm_map);
         }
 
         bs.DataOffset = 0;
@@ -108,15 +123,23 @@ public:
 
         return MFX_ERR_NONE;
     }
+
+    mfxStatus ProcessSurface(mfxFrameSurface1& s)
+    {
+        m_reader->ProcessSurface(s);
+        return MFX_ERR_NONE;
+    }
 };
 
 class TestEncoder : public tsVideoEncoder, public tsSurfaceProcessor, public tsBitstreamProcessor, public BS_AVC2_parser
 {
-    std::vector<mfxExtMBDisableSkipMap> m_skip_map;
+    std::vector<mfxExtMBDisableSkipMap> m_non_skip_map;
     std::vector<mfxEncodeCtrl> m_ctrls;
     mfxU32 m_curr_in_frame;
     mfxU32 m_curr_out_frame;
 public:
+    mfxU32 m_unexpected_skipped_MB;
+    tsRawReader* m_reader;
     mfxStatus Init()
     {
         return tsVideoEncoder::Init(this->m_session, this->m_pPar);
@@ -125,7 +148,9 @@ public:
     TestEncoder(std::vector<mfxExtMBDisableSkipMap> skip_map)
         : tsVideoEncoder(MFX_CODEC_AVC)
         , BS_AVC2_parser(BS_AVC2::INIT_MODE_PARSE_SD)
-        , m_skip_map(skip_map)
+        , m_non_skip_map(skip_map)
+        , m_reader(nullptr)
+        , m_unexpected_skipped_MB(0)
     {
         m_curr_in_frame = 0;
         m_curr_out_frame = 0;
@@ -133,7 +158,10 @@ public:
         m_bs_processor = this;
     }
     ~TestEncoder()
-    {}
+    {
+        if (m_reader)
+            delete m_reader;
+    }
 
     mfxStatus ProcessBitstream(mfxBitstream& bs, mfxU32 nFrames)
     {
@@ -153,16 +181,17 @@ public:
                     {
                         if (!IS_MB_SKIPPED(au->nalu[i]->slice->mb[nmb].MbType))
                             real_nonskip++;
-                        if (m_skip_map[m_curr_out_frame].Map[nmb] == 1)
+
+                        if (m_non_skip_map[m_curr_out_frame].Map[nmb] == 1)
                             exp_nonskip++;
 
-                        if (!IS_MB_SKIPPED(au->nalu[i]->slice->mb[nmb].MbType) &&
-                            (m_skip_map[m_curr_out_frame].Map[nmb] == 1))
+                        if (IS_MB_SKIPPED(au->nalu[i]->slice->mb[nmb].MbType) &&
+                            (m_non_skip_map[m_curr_out_frame].Map[nmb] == 1))
                         {
                             g_tsLog << "ERROR: unexpected skipped MB" << "\n"
                                     << "frame#" << m_curr_out_frame << " MB#" << nmb
                                     << " (MbType=" << au->nalu[i]->slice->mb[nmb].MbType << ")\n";
-                            g_tsStatus.check(MFX_ERR_ABORTED);
+                            m_unexpected_skipped_MB++;
                         }
                     }
                 }
@@ -170,7 +199,6 @@ public:
             g_tsLog << "Frame#" << m_curr_out_frame << ": " << real_nonskip << "/" << exp_nonskip << "\n";
             m_curr_out_frame++;
         }
-
         bs.DataOffset = 0;
         bs.DataLength = 0;
 
@@ -179,7 +207,7 @@ public:
 
     mfxStatus ProcessSurface(mfxFrameSurface1& s)
     {
-        if (m_curr_in_frame >= m_skip_map.size())
+        if (m_curr_in_frame >= m_non_skip_map.size())
             return MFX_ERR_NONE;
 
         m_ctrls.push_back(m_ctrl);
@@ -190,8 +218,8 @@ public:
         if (!m_pCtrl->ExtParam)
             m_pCtrl->ExtParam = new mfxExtBuffer*[1];
 
-        m_pCtrl->ExtParam[0] = (mfxExtBuffer*)&m_skip_map[m_curr_in_frame++];
-
+        m_pCtrl->ExtParam[0] = (mfxExtBuffer*)&m_non_skip_map[m_curr_in_frame++];
+        m_reader->ProcessSurface(s);
         return MFX_ERR_NONE;
     }
 };
@@ -199,14 +227,14 @@ public:
 const TestSuite::tc_struct TestSuite::test_case[] =
 {
     {/*00*/ MFX_ERR_NONE, DEFAULT, {}},
-    {/*00*/ MFX_ERR_NONE, EACH_OTHER_FRAME, {}},
-    {/*00*/ MFX_ERR_NONE, EACH_WO_BORDERS, {}},
-    {/*00*/ MFX_ERR_NONE, EACH_OTHER_MB, {}},
+    {/*01*/ MFX_ERR_NONE, EACH_OTHER_FRAME, {}},
+    {/*02*/ MFX_ERR_NONE, EACH_WO_BORDERS, {}},
+    {/*03*/ MFX_ERR_NONE, EACH_OTHER_MB, {}},
 
-    {/*00*/ MFX_ERR_NONE, DEFAULT, {{MFXPAR, &tsStruct::mfxVideoParam.mfx.NumSlice, 2}}},
-    {/*00*/ MFX_ERR_NONE, EACH_OTHER_FRAME, {{MFXPAR, &tsStruct::mfxVideoParam.mfx.NumSlice, 2}}},
-    {/*00*/ MFX_ERR_NONE, EACH_WO_BORDERS, {{MFXPAR, &tsStruct::mfxVideoParam.mfx.NumSlice, 2}}},
-    {/*00*/ MFX_ERR_NONE, EACH_OTHER_MB, {{MFXPAR, &tsStruct::mfxVideoParam.mfx.NumSlice, 2}}}
+    {/*04*/ MFX_ERR_NONE, DEFAULT, {{MFXPAR, &tsStruct::mfxVideoParam.mfx.NumSlice, 2}}},
+    {/*05*/ MFX_ERR_NONE, EACH_OTHER_FRAME, {{MFXPAR, &tsStruct::mfxVideoParam.mfx.NumSlice, 2}}},
+    {/*06*/ MFX_ERR_NONE, EACH_WO_BORDERS, {{MFXPAR, &tsStruct::mfxVideoParam.mfx.NumSlice, 2}}},
+    {/*07*/ MFX_ERR_NONE, EACH_OTHER_MB, {{MFXPAR, &tsStruct::mfxVideoParam.mfx.NumSlice, 2}}}
 };
 
 const unsigned int TestSuite::n_cases = sizeof(TestSuite::test_case)/sizeof(TestSuite::tc_struct);
@@ -220,31 +248,39 @@ int TestSuite::RunTest(unsigned int id)
 
     AllocSurfaces();
 
-    // m_par.AsyncDepth = 1;
-    mfxU32 nframes = 10;
-
     SETPARS(m_pPar, MFXPAR);
 
+    m_par.AsyncDepth = 1;
+    m_par.mfx.GopRefDist = 1;
+    m_par.mfx.NumRefFrame = 1;
+    m_par.mfx.FrameInfo.PicStruct = MFX_PICSTRUCT_PROGRESSIVE;
+    m_par.mfx.FrameInfo.Width = WIDTH;
+    m_par.mfx.FrameInfo.Height = HEIGHT;
+    m_par.mfx.FrameInfo.CropW = WIDTH;
+    m_par.mfx.FrameInfo.CropH = HEIGHT;
+
+    m_reader = (new tsRawReader(g_tsStreamPool.Get("forBehaviorTest/foreman_cif_10.yuv"), m_par.mfx.FrameInfo, N_FRAMES));
+    g_tsStreamPool.Reg();
     // run encoder with default parameters and collect skipped MB indexes per-frame
     Init();
-    EncodeFrames(nframes, true);
+    EncodeFrames(N_FRAMES, true);
     Close();
     MFXClose();
 
     if (tc.mode == EACH_OTHER_FRAME) {
-        for (mfxU32 i = 1; i < m_skip_map.size(); i++) {    // skip first I
-            memset(m_skip_map[i].Map, i%2, sizeof(mfxU8)*m_skip_map[i].MapSize);
+        for (mfxU32 i = 1; i < m_non_skip_map.size(); i++) {    // skip first I
+            memset(m_non_skip_map[i].Map, i%2, sizeof(mfxU8)*m_non_skip_map[i].MapSize);
         }
     } else if (tc.mode == EACH_WO_BORDERS) {
-        for (mfxU32 i = 1; i < m_skip_map.size(); i++) {    // skip first I
-            memset(m_skip_map[i].Map, 0, sizeof(mfxU8)*m_skip_map[i].MapSize);
-            m_skip_map[i].Map[0] = 1;
-            m_skip_map[i].Map[m_skip_map[i].MapSize-1] = 1;
+        for (mfxU32 i = 1; i < m_non_skip_map.size(); i++) {    // skip first I
+            memset(m_non_skip_map[i].Map, 0, sizeof(mfxU8)*m_non_skip_map[i].MapSize);
+            m_non_skip_map[i].Map[0] = 1;
+            m_non_skip_map[i].Map[m_non_skip_map[i].MapSize-1] = 1;
         }
     } else if (tc.mode == EACH_OTHER_MB) {
-        for (mfxU32 i = 1; i < m_skip_map.size(); i++) {    // skip first I
-            for (mfxU32 n = 0; n < m_skip_map[i].MapSize; n++)
-                m_skip_map[i].Map[n] = n%2;
+        for (mfxU32 i = 1; i < m_non_skip_map.size(); i++) {    // skip first I
+            for (mfxU32 n = 0; n < m_non_skip_map[i].MapSize; n++)
+                m_non_skip_map[i].Map[n] = n%2;
         }
     }
 
@@ -252,16 +288,26 @@ int TestSuite::RunTest(unsigned int id)
     AllocSurfaces();
 
     // configure pipeline to use MBDisableSkipMap obtained on prev step
-    TestEncoder test_encoder(m_skip_map);
+    TestEncoder test_encoder(m_non_skip_map);
     test_encoder.MFXInit();
     test_encoder.AllocSurfaces();
     mfxExtCodingOption3& co3 = test_encoder.m_par;
     co3.MBDisableSkipMap = MFX_CODINGOPTION_ON;
 
+    test_encoder.m_par = m_par;
+    test_encoder.m_reader = (new tsRawReader(g_tsStreamPool.Get("forBehaviorTest/foreman_cif_10.yuv"), test_encoder.m_par.mfx.FrameInfo, N_FRAMES));
+    g_tsStreamPool.Reg();
+
     // run test pipeline
     g_tsStatus.expect(tc.sts);
     test_encoder.Init();
-    test_encoder.EncodeFrames(nframes, true);
+    test_encoder.EncodeFrames(N_FRAMES, true);
+
+    if (test_encoder.m_unexpected_skipped_MB > 0)
+    {
+        g_tsLog << "ERROR: unexpected skipped MBs: " << test_encoder.m_unexpected_skipped_MB << '\n';
+        g_tsStatus.check(MFX_ERR_ABORTED);
+    }
 
     TS_END;
     return 0;
