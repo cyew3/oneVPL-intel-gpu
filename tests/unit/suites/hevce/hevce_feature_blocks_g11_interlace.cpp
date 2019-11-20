fb32a98a74a63cf3b38341ab2118ec036d56a2ef
Dmitry Gurulev <dmitry.gurulev@intel.com>
2019-11-20 15:29:33 +0300

    [tests/hevce]: add UTs for 'Interlace' feature
    Issue: MDP-60487
    Test: FeatureBlocksInterlace
    
    Change-Id: I213c6e6a00213d21ce598f45d07fd74868c7f5c4

---
+++ tests/unit/suites/hevce/hevce_feature_blocks_g11_interlace.cpp
@@ -0,0 +1,105 @@
+// Copyright (c) 2019 Intel Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "hevcehw_base.h"
+#include "g11/hevcehw_g11_legacy.h"
+#include "g11/hevcehw_g11_interlace.h"
+
+namespace hevce { namespace tests
+{
+    struct FeatureBlocksInterlace
+        : testing::Test
+    {
+        HEVCEHW::FeatureBlocks    blocks{};
+        HEVCEHW::Gen11::Interlace interlace;
+        HEVCEHW::Gen11::Legacy    legacy;
+        HEVCEHW::StorageRW        storage;
+        mfxFrameAllocRequest      request{};
+
+        FeatureBlocksInterlace()
+            : legacy(HEVCEHW::Gen11::FEATURE_LEGACY)
+            , interlace(HEVCEHW::Gen11::FEATURE_INTERLACE)
+        {}
+
+        void SetUp() override
+        {
+            legacy.Init(HEVCEHW::QUERY_IO_SURF, blocks);
+            interlace.Init(HEVCEHW::QUERY_IO_SURF, blocks);
+
+            auto& vp = HEVCEHW::Gen11::Glob::VideoParam::GetOrConstruct(storage);
+            vp.AsyncDepth = 3;
+            vp.IOPattern = MFX_IOPATTERN_IN_VIDEO_MEMORY;
+            vp.mfx.FrameInfo.FourCC = MFX_FOURCC_NV12;
+            vp.mfx.GopRefDist = 16;
+
+            auto& queueQIS = HEVCEHW::FeatureBlocks::BQ<HEVCEHW::FeatureBlocks::BQ_QueryIOSurf>::Get(blocks);
+            auto block = HEVCEHW::FeatureBlocks::Get(queueQIS, { HEVCEHW::Gen11::FEATURE_LEGACY, HEVCEHW::Gen11::Legacy::BLK_SetFrameAllocRequest });
+
+            ASSERT_EQ(
+                block->Call(vp, request, storage),
+                MFX_ERR_NONE
+            );
+        }
+    };
+
+    TEST_F(FeatureBlocksInterlace, QueryIOSurfNoInterlace)
+    {
+        auto& queueQIS = HEVCEHW::FeatureBlocks::BQ<HEVCEHW::FeatureBlocks::BQ_QueryIOSurf>::Get(blocks);
+        auto block = HEVCEHW::FeatureBlocks::Get(queueQIS, { HEVCEHW::Gen11::FEATURE_INTERLACE, HEVCEHW::Gen11::Interlace::BLK_QueryIOSurf });
+
+        mfxFrameAllocRequest result{};
+        result.NumFrameMin = request.NumFrameMin;
+
+        ASSERT_EQ(
+            block->Call(mfxVideoParam{}, result, storage),
+            MFX_ERR_NONE
+        );
+
+        //Shouldn't change if PicStruct != MFX_PICSTRUCT_FIELD
+        EXPECT_EQ(
+            result.NumFrameMin, request.NumFrameMin
+        );
+    }
+
+    TEST_F(FeatureBlocksInterlace, QueryIOSurf)
+    {
+        auto& queueQIS = HEVCEHW::FeatureBlocks::BQ<HEVCEHW::FeatureBlocks::BQ_QueryIOSurf>::Get(blocks);
+        auto block = HEVCEHW::FeatureBlocks::Get(queueQIS, { HEVCEHW::Gen11::FEATURE_INTERLACE, HEVCEHW::Gen11::Interlace::BLK_QueryIOSurf });
+
+        mfxFrameAllocRequest result{};
+        result.NumFrameMin = request.NumFrameMin;
+
+        auto& vp = HEVCEHW::Gen11::Glob::VideoParam::Get(storage);
+        vp.mfx.FrameInfo.PicStruct = MFX_PICSTRUCT_FIELD_SINGLE;
+
+        ASSERT_EQ(
+            block->Call(mfxVideoParam{}, result, storage),
+            MFX_ERR_NONE
+        );
+
+        EXPECT_EQ(
+            result.NumFrameMin, request.NumFrameMin + (vp.mfx.GopRefDist - 1)
+        );
+    }
+
+} }
