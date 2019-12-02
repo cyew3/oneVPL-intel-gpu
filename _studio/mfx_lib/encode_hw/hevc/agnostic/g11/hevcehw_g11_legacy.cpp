cyew3/oneVPL-intel-gpu

commit af5b6202229b11772454f9dc907c7d47a9d1abdf

[HEVCe] Fix STRPS at Reset (use empty DPB instead of current)
Issue: n/a
Test: -

Change-Id: I475b07e4034fa97e44d8b898a9ef0a5ec61ed483

_studio/mfx_lib/encode_hw/hevc/agnostic/g11/hevcehw_g11_legacy.cpp
@@ -940,13 +940,20 @@ void Legacy::InitInternal(const FeatureBlocks& /*blocks*/, TPushII Push)
         auto& par = Glob::VideoParam::Get(strg);
 
         auto pReorderer = make_storable<Reorderer>();
-        pReorderer->Push(
-            [&](Reorderer::TExt, TTaskIt begin, TTaskIt end, bool bFlush)
-        {
-            return ReorderWrap(par, begin, end, bFlush);
-        });
+
         pReorderer->BufferSize = par.mfx.GopRefDist - 1;
-        pReorderer->DPB = &m_prevTask.DPB.After;
+        pReorderer->DPB        = &m_prevTask.DPB.After;
+
+        pReorderer->Push(
+            [&](Reorderer::TExt, const DpbArray& DPB, TTaskIt begin, TTaskIt end, bool bFlush)
+        {
+            auto IsIdrFrame = [](const TItWrap::reference fi) { return IsIdr(fi.FrameType); };
+            auto newEnd = std::find_if(TItWrap(begin), TItWrap(end), IsIdrFrame);
+
+            bFlush = newEnd != begin && newEnd != end;
+
+            return Reorder(par, DPB, TItWrap(begin), newEnd, bFlush).it;
+        });
 
         strg.Insert(Glob::Reorder::Key, std::move(pReorderer));
 
@@ -2390,10 +2397,9 @@ mfxU32 Legacy::GetBiFrameLocation(mfxU32 i, mfxU32 num, bool &ref, mfxU32 &level
     return GetEncodingOrder(i, 0, num, level, 0, ref);
 }
 
-template <class T>
-static T BPyrReorder(T begin, T end)
+Legacy::TItWrapIt Legacy::BPyrReorder(TItWrapIt begin, TItWrapIt end)
 {
-    typedef typename std::iterator_traits<T>::reference TRef;
+    using TRef = std::iterator_traits<TItWrapIt>::reference;
 
     mfxU32 num = mfxU32(std::distance(begin, end));
     bool bSetOrder = num && (*begin)->BPyramidOrder == mfxU32(MFX_FRAMEORDER_UNKNOWN);
@@ -2413,22 +2419,21 @@ static T BPyrReorder(T begin, T end)
         , [](TRef a, TRef b) { return a->BPyramidOrder < b->BPyramidOrder; });
 }
 
-template<class T>
-static T Reorder(
+Legacy::TItWrap Legacy::Reorder(
     ExtBuffer::Param<mfxVideoParam> const & par
     , DpbArray const & dpb
-    , T begin
-    , T end
+    , TItWrap begin
+    , TItWrap end
     , bool flush)
 {
-    typedef typename std::iterator_traits<T>::reference TRef;
+    using TRef = TItWrap::reference;
 
     const mfxExtCodingOption2& CO2 = ExtBuffer::Get(par);
     bool isBPyramid = (CO2.BRefType == MFX_B_REF_PYRAMID);
-    T top = begin;
-    std::list<T> brefs;
+    TItWrap top = begin;
+    std::list<TItWrap> brefs;
     auto IsB = [](TRef f) { return HEVCEHW::IsB(f.FrameType); };
-    auto NoL1 = [&](T& f) { return !CountL1(dpb, f->POC); };
+    auto NoL1 = [&](TItWrap& f) { return !CountL1(dpb, f->POC); };
 
     std::generate_n(
         std::back_inserter(brefs)
@@ -2441,11 +2446,11 @@ static T Reorder(
     if (bNoPyramidB)
     {
         auto B0POC = brefs.front()->POC;
-        auto RefB = [B0POC](T& f)
+        auto RefB = [B0POC](TItWrap& f)
         {
             return IsRef(f->FrameType) && (f->POC - B0POC < 2);
         };
-        typename std::list<T>::iterator BCand[2] =
+        TItWrapIt BCand[2] =
         {
             std::find_if(brefs.begin() , brefs.end(), RefB)
             , brefs.begin()
@@ -2465,28 +2470,6 @@ static T Reorder(
     }
 
     return top;
-}
-
-TTaskIt Legacy::ReorderWrap(
-    ExtBuffer::Param<mfxVideoParam> const & par
-    , TTaskIt begin
-    , TTaskIt end
-    , bool flush)
-{
-    typedef TaskItWrap<FrameBaseInfo, Task::Common::Key> TItWrap;
-    TItWrap newEnd(begin);
-
-    while (newEnd != end)
-    {
-        if (newEnd != begin && IsIdr(newEnd->FrameType))
-        {
-            flush = true;
-            break;
-        }
-        newEnd++;
-    }
-
-    return Reorder(par, m_prevTask.DPB.After, TItWrap(begin), newEnd, flush).it;
 }
 
 std::tuple<mfxStatus, mfxU16, mfxU16>
@@ -3157,13 +3140,17 @@ void Legacy::SetSTRPS(
     STRPS     sets[65]   = {};
     auto      pSetsBegin = sets;
     auto      pSetsEnd   = pSetsBegin;
-    DpbArray& dpb        = *reorder.DPB;
     bool      bTL        = dflts.base.GetNumTemporalLayers(dflts) > 1;
     mfxI32    nGops      = par.mfx.IdrInterval + !par.mfx.IdrInterval * 4;
     mfxI32    stDist     = std::min<mfxI32>(par.mfx.GopPicSize * nGops, 128);
     mfxI32    lastIPoc   = 0;
     bool      bDone      = false;
     mfxI32    i          = 0;
+    Reorderer localReorder;
+    DpbArray  dpb;
+
+    localReorder        = reorder;
+    localReorder.DPB    = &dpb; //use own DPB
 
     do
     {
@@ -3176,7 +3163,7 @@ void Legacy::SetSTRPS(
             frames.back().Insert(Task::Common::Key, new FrameBaseInfo(fi));
         }
 
-        auto frIt  = reorder(frames.begin(), frames.end(), false);
+        auto frIt  = localReorder(frames.begin(), frames.end(), false);
         bool bNext = frIt == frames.end();
 
         FrameBaseInfo* cur = nullptr;

_studio/mfx_lib/encode_hw/hevc/agnostic/g11/hevcehw_g11_data.h
@@ -1029,10 +1029,17 @@ namespace Gen11
     };
 
     struct Reorderer
-        : CallChain<TTaskIt, TTaskIt, TTaskIt, bool>
+        : CallChain<TTaskIt, const DpbArray&, TTaskIt, TTaskIt, bool>
     {
         mfxU16 BufferSize = 0;
         NotNull<DpbArray*> DPB;
+
+        using TBaseCC = CallChain<TTaskIt, const DpbArray&, TTaskIt, TTaskIt, bool>;
+
+        TTaskIt operator()(TTaskIt itBegin, TTaskIt itEnd, bool bFlush)
+        {
+            return TBaseCC::operator()((const DpbArray&)*DPB, itBegin, itEnd, bFlush);
+        }
     };
 
     class EncodeCapsHevc

_studio/mfx_lib/encode_hw/hevc/agnostic/g11/hevcehw_g11_interlace.cpp
@@ -446,9 +446,9 @@ void Interlace::InitInternal(const FeatureBlocks&, TPushII Push)
 
         auto& rdr = Glob::Reorder::Get(strg);
         rdr.BufferSize += par.mfx.GopRefDist - 1;
-        rdr.Push([&](Reorderer::TExt, TTaskIt begin, TTaskIt end, bool bFlush)
+        rdr.Push([&](Reorderer::TExt, const DpbArray& DPB, TTaskIt begin, TTaskIt end, bool bFlush)
         {
-            return IntReorder(par, *rdr.DPB, begin, end, bFlush).it;
+            return IntReorder(par, DPB, begin, end, bFlush).it;
         });
 
         return MFX_ERR_NONE;

_studio/mfx_lib/encode_hw/hevc/agnostic/g11/hevcehw_g11_legacy.h
@@ -230,7 +230,6 @@ namespace Gen11
             , const SPS& sps
             , const PPS& pps
             , Slice & s);
-        TTaskIt ReorderWrap(const ExtBuffer::Param<mfxVideoParam> & par, TTaskIt begin, TTaskIt end, bool flush);
         static mfxU32 GetRawBytes(mfxU16 w, mfxU16 h, mfxU16 ChromaFormat, mfxU16 BitDepth);
         static bool IsSWBRC(mfxVideoParam const & par, const mfxExtCodingOption2* pCO2);
         static bool IsInVideoMem(const mfxVideoParam & par, const mfxExtOpaqueSurfaceAlloc* pOSA);
@@ -323,6 +322,18 @@ namespace Gen11
                 , m_hw
                 , Glob::Defaults::Get(strg));
         }
+
+        using TItWrap = TaskItWrap<FrameBaseInfo, Task::Common::Key>;
+        using TItWrapIt = std::list<TItWrap>::iterator;
+
+        static TItWrapIt BPyrReorder(TItWrapIt begin, TItWrapIt end);
+        static TItWrap Reorder(
+            ExtBuffer::Param<mfxVideoParam> const & par
+            , DpbArray const & dpb
+            , TItWrap begin
+            , TItWrap end
+            , bool flush);
+
     };
 
 } //Gen11

_studio/mfx_lib/encode_hw/hevc/agnostic/g11/hevcehw_g11_legacy_defaults.cpp
@@ -652,8 +652,8 @@ public:
             + !bGen9 * nRefs[bLP][1][tu]);
 
         return std::make_tuple(
-            std::min<mfxU16>(nRefL0, par.caps.MaxNum_Reference0)
-            , std::min<mfxU16>(nRefL1, par.caps.MaxNum_Reference1));
+            std::min<mfxU16>({ nRefL0, par.caps.MaxNum_Reference0, par.mvp.mfx.NumRefFrame })
+            , std::min<mfxU16>({ nRefL1, par.caps.MaxNum_Reference1, par.mvp.mfx.NumRefFrame }));
     }
 
     static mfxU16 RateControlMethod(

commit 4f36a368a9c3047557313ceda5af9135c77e307c

[HEVCe] Fix reordering flush
Fix Regression introduced by af5b6202229b11772454f9dc907c7d47a9d1abdf
(UNDEFINED_BEHAVIOR at the end of stream)

Issue: n/a
Test: hevce_func/0001

Change-Id: I13f3ef4920bde54d23f6d57d834f8811189b5de5

_studio/mfx_lib/encode_hw/hevc/agnostic/g11/hevcehw_g11_legacy.cpp
@@ -950,7 +950,7 @@ void Legacy::InitInternal(const FeatureBlocks& /*blocks*/, TPushII Push)
             auto IsIdrFrame = [](const TItWrap::reference fi) { return IsIdr(fi.FrameType); };
             auto newEnd = std::find_if(TItWrap(begin), TItWrap(end), IsIdrFrame);
 
-            bFlush = newEnd != begin && newEnd != end;
+            bFlush |= (newEnd != begin && newEnd != end);
 
             return Reorder(par, DPB, TItWrap(begin), newEnd, bFlush).it;
         });