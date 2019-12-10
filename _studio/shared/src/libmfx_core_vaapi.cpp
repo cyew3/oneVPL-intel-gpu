b5d1df977f0ecb62f6ea393bd034f64c51ad9cd9
Dmitry Ermilov <dmitry.ermilov@intel.com>
2019-12-10 04:46:07 -0500

    [core] Removed wrong JSL device ID
    Fixed: #1820
    Issue: n/a
    Tests: build
    Github: 4e98b9c("[core] Removed wrong JSL device ID")
    
    Change-Id: Ie8e1b80ee76b8203d3c43f749a4563630e31d4ff
    Signed-off-by: Dmitry Ermilov <dmitry.ermilov@intel.com>

--- _studio/shared/src/libmfx_core_vaapi.cpp
+++ _studio/shared/src/libmfx_core_vaapi.cpp
@@ -332,7 +332,6 @@ typedef struct {
     { 0x8A71, MFX_HW_ICL_LP, MFX_GT1 },
 
     /* JSL */
-    { 0x4500, MFX_HW_JSL, MFX_GT2 },
     { 0x4E51, MFX_HW_JSL, MFX_GT2 },
     { 0x4E61, MFX_HW_JSL, MFX_GT2 },
     { 0x4E71, MFX_HW_JSL, MFX_GT2 },
