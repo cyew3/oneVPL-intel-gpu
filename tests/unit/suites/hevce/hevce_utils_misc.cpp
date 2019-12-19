7203e7eec4330581aabbe940b6bf2915131837f1
Starov, Evgeniy <evgeniy.starov@intel.com>
2019-12-19 17:37:20 +0300

    [HEVCe] Fix GUIDs compare
    Issue: n/a
    Test: unit hevce UtilsGUID.GUIDLess
    
    Change-Id: I4e4310a1f855b0b169e39eea1fa6803500e5be7a

---
+++ tests/unit/suites/hevce/hevce_utils_misc.cpp
@@ -0,0 +1,57 @@
+// Copyright (c) 2019 Intel Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "g9/hevcehw_g9_data.h"
+
+namespace hevce { namespace tests
+{
+    TEST(UtilsGUID, GUIDLess)
+    {
+        const GUID GuidsAsc[] =
+        {
+              {0, 0, 0, {0, 0, 0, 0, 0, 0, 0, 0}}
+            , {0, 0, 0, {0, 0, 0, 0, 0, 0, 0, 1}}
+            , {0, 0, 0, {0, 0, 0, 0, 0, 0, 1, 0}}
+            , {0, 0, 0, {0, 0, 0, 0, 0, 1, 0, 0}}
+            , {0, 0, 0, {0, 0, 0, 0, 1, 0, 0, 0}}
+            , {0, 0, 0, {0, 0, 0, 1, 0, 0, 0, 0}}
+            , {0, 0, 0, {0, 0, 1, 0, 0, 0, 0, 0}}
+            , {0, 0, 0, {0, 1, 0, 0, 0, 0, 0, 0}}
+            , {0, 0, 0, {1, 0, 0, 0, 0, 0, 0, 0}}
+            , {0, 0, 1, {0, 0, 0, 0, 0, 0, 0, 0}}
+            , {0, 1, 0, {0, 0, 0, 0, 0, 0, 0, 0}}
+            , {1, 0, 0, {0, 0, 0, 0, 0, 0, 0, 0}}
+        };
+
+        const size_t nGUIDS = std::extent<decltype(GuidsAsc)>::value;
+
+        for (size_t lIdx = 0; lIdx < nGUIDS; ++lIdx)
+        {
+            for (size_t rIdx = 0; rIdx < nGUIDS; ++rIdx)
+            {
+                EXPECT_EQ(HEVCEHW::Gen9::LessGUID()(GuidsAsc[lIdx], GuidsAsc[rIdx]), lIdx < rIdx);
+                EXPECT_EQ(HEVCEHW::Gen9::LessGUID()(GuidsAsc[rIdx], GuidsAsc[lIdx]), rIdx < lIdx);
+            }
+        }
+    }
+} }
