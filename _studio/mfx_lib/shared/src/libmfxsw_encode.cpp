3047493abde052b42bdc6bfecfd195bdb6c2d5dd
Starov, Evgeniy <evgeniy.starov@intel.com>
2019-12-25 15:55:45 +0300

    [HEVCe] Prepare FEI refactored code path for enabling
    Issue: n/a
    Test: manually
    
    Change-Id: I91b8b4f15968ce498d79bb6af5b33e7923b1cd8f

--- _studio/mfx_lib/shared/src/libmfxsw_encode.cpp
+++ _studio/mfx_lib/shared/src/libmfxsw_encode.cpp
@@ -448,6 +448,22 @@ static const CodecId2Handlers codecId2Handlers =
         {
             // .primary =
             {
+#if defined(MFX_ENABLE_HEVCEHW_REFACTORING_FEI)
+                // .ctor =
+                [](VideoCORE* core, mfxU16 /*codecProfile*/, mfxStatus *mfxRes)
+                -> VideoENCODE*
+                {
+                    if (core && mfxRes)
+                        return HEVCEHW::Create(*core, *mfxRes, true);
+                    return nullptr;
+                },
+                // .query =
+                [](mfxSession session, mfxVideoParam *in, mfxVideoParam *out)
+                { return HEVCEHW::Query(session->m_pCORE.get(), in, out, true); },
+                // .queryIOSurf =
+                [](mfxSession session, mfxVideoParam *par, mfxFrameAllocRequest *request)
+                { return HEVCEHW::QueryIOSurf(session->m_pCORE.get(), par, request, true); }
+#else
                 // .ctor =
                 [](VideoCORE* core, mfxU16 /*codecProfile*/, mfxStatus *mfxRes)
                 -> VideoENCODE*
@@ -460,6 +476,7 @@ static const CodecId2Handlers codecId2Handlers =
                 // .queryIOSurf =
                 [](mfxSession session, mfxVideoParam *par, mfxFrameAllocRequest *request)
                 { return MfxHwH265FeiEncode::H265FeiEncode_HW::QueryIOSurf(session->m_pCORE.get(), par, request); }
+#endif //defined(MFX_ENABLE_HEVCEHW_REFACTORING_FEI)
             }
         }
     },
