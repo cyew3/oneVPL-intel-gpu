00ab431e78700b505d416226a757777ecc855d58
Starov, Evgeniy <evgeniy.starov@intel.com>
2020-01-13 17:18:23 +0300

    [HEVCe] Move ExtBuffer utilities to mfx_lib shared
    Issue: n/a
    Test: manually
    
    Change-Id: I955a08ebe7333f199c756387318a75d2ed5880a1

--- tests/unit/suites/hevce/hevce_feature_blocks_g11_win_qmatrix.cpp
+++ tests/unit/suites/hevce/hevce_feature_blocks_g11_win_qmatrix.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2019-2020 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,6 @@
 #include "mfxvideo++.h"
 #include "mfx_session.h"
 
-#include "hevcehw_extbuf.h"
 #include "g9/hevcehw_g9_legacy.h"
 #include "g9/hevcehw_g9_qmatrix_win.h"
 
