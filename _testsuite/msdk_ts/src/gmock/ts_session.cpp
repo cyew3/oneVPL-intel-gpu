4c6c715edd0b2f56df900a404c848a1cd695e2ed
Dmitry Gurulev <dmitry.gurulev@intel.com>
2020-02-13 11:02:41 +0300

    [tests]: fix 'VAHandle' memory leak on Windows
    Issue: N/A
    Test: manual
    
    Change-Id: Ibe12dfa5e5d614d0dc0fb8f9aff8686465d0150e

--- _testsuite/msdk_ts/src/gmock/ts_session.cpp
+++ _testsuite/msdk_ts/src/gmock/ts_session.cpp
@@ -4,7 +4,7 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2014-2018 Intel Corporation. All Rights Reserved.
+Copyright(c) 2014-2020 Intel Corporation. All Rights Reserved.
 
 \* ****************************************************************************** */
 
@@ -32,12 +32,8 @@ tsSession::~tsSession()
     {
         MFXClose();
     }
-#if (defined(LINUX32) || defined(LINUX64))
-    if (m_pVAHandle)
-    {
-        delete m_pVAHandle;
-    }
-#endif
+
+    delete m_pVAHandle;
 }
 
 mfxStatus tsSession::MFXInit()
