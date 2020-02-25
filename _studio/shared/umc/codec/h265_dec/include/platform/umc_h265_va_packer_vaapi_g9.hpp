c0cf863245972dcad17b2d905f14f41fc1aca2a1
Yan Wang <yan.wang@linux.intel.com>
2020-02-25 07:40:26 +0000

    [HEVCd] Fix HEVC SCC self reference
    It should be set by long reference flag and calcualted in
    the count.
    
    Issue: MDP-61004
    Test: hevcd_func
    
    Change-Id: Ifb4885651a3e16d1ef8c7913f7b0edeebd5164ea
    Signed-off-by: Yan Wang <yan.wang@linux.intel.com>

--- _studio/shared/umc/codec/h265_dec/include/platform/umc_h265_va_packer_vaapi_g9.hpp
+++ _studio/shared/umc/codec/h265_dec/include/platform/umc_h265_va_packer_vaapi_g9.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2020 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -78,8 +78,8 @@ namespace UMC_HEVC_DECODER
             {
                     pp->ReferenceFrames[count].pic_order_cnt = frame->m_PicOrderCnt;
                     pp->ReferenceFrames[count].picture_id = va->GetSurfaceID(frame->GetFrameMID());
-                    pp->ReferenceFrames[count].flags = SHORT_TERM_REFERENCE | VA_PICTURE_HEVC_RPS_ST_CURR_BEFORE;
-                    numRefPicSetStCurr++;
+                    pp->ReferenceFrames[count].flags = VA_PICTURE_HEVC_LONG_TERM_REFERENCE;
+                    count++;
             }
 
             for (size_t n = count+numRefPicSetStCurr; n < sizeof(pp->ReferenceFrames) / sizeof(pp->ReferenceFrames[0]); n++)
