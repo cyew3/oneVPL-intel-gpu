03c529dd15689e5d35ce4173e480f8b959ef2a12
egorovdanil <danil.egorov@intel.com>
2020-02-25 21:13:05 +0300

    [Tools][Tracer] Remove unused event (CS0067 warnin (auto backmerge)
    Signed-off-by: egorovdanil <danil.egorov@intel.com>
    Issue: -
    Test: -
    Github: 94e0301 ([Tools][Tracer] Remove unused event (CS0067 warning))
    
    Change-Id: Id4fabd8cd49ab498b3cd20486148e7f0324415a1

--- tools/tracer/gui/EtlToTextConverter.cs
+++ tools/tracer/gui/EtlToTextConverter.cs
@@ -4,7 +4,7 @@ INTEL CORPORATION PROPRIETARY INFORMATION
 This software is supplied under the terms of a license agreement or nondisclosure
 agreement with Intel Corporation and may not be copied or disclosed except in
 accordance with the terms of that agreement
-Copyright(c) 2020Intel Corporation. All Rights Reserved.
+Copyright(c) 2020 Intel Corporation. All Rights Reserved.
 
 File Name: 
 
@@ -32,7 +32,6 @@ namespace msdk_analyzer
     class EtlToTextConverter
     {
         uint m_percentage;
-        public event ConversionStatusUpdated ConversionUpdated;
         ConversionStatus m_status;
         string m_etlfilename;
         string m_textfilename;
