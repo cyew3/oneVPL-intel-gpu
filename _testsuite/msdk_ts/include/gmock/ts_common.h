967fa0226fdc3b5502a05ef1271d0812d51cdcfa
Vitaliy Ilichev <vitaliy.ilichev@intel.com>
2020-02-27 12:22:36 +0300

    [HEVCe][refactored] Switched on refactored code path for TGL Linux
    Issue: MDP-61803
    Test:  build
    
    Change-Id: Ia0f30dbf20af3afc0a94ad3df905bf554e474e91

--- _testsuite/msdk_ts/include/gmock/ts_common.h
+++ _testsuite/msdk_ts/include/gmock/ts_common.h
@@ -29,7 +29,8 @@
                                                                     MFX_HW_TGL, MFX_HW_DG1, MFX_HW_ATS, MFX_HW_DG2}; // Platforms where refactored HEVCe used on Windows
 #else
     static HWType platforms_with_refactored_hevce[] = { MFX_HW_SKL, MFX_HW_KBL, MFX_HW_GLK, MFX_HW_CFL, MFX_HW_APL,
-                                                                    MFX_HW_ICL, MFX_HW_LKF, MFX_HW_JSL}; // Platforms where refactored HEVCe used on Linux
+                                                                    MFX_HW_ICL, MFX_HW_LKF, MFX_HW_JSL,
+                                                                    MFX_HW_TGL}; // Platforms where refactored HEVCe used on Linux
 #endif
 
 static bool use_refactored_hevce(HWType current_platform)
