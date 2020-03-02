cdc2d589e2474b4cdc921dd59c59fc2eb8044de3
AlexandrX Konovalov <alexandrx.konovalov@intel.com>
2020-03-02 19:05:39 +0300

    [vp9e] Remove unused private field
    Issue: N/A
    Test: build
    
    Change-Id: I7deedf0a3744b5ffda32615d71434153ce890015

--- _studio/mfx_lib/encode_hw/vp9/include/mfx_vp9_encode_hw_vaapi.h
+++ _studio/mfx_lib/encode_hw/vp9/include/mfx_vp9_encode_hw_vaapi.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2019 Intel Corporation
+// Copyright (c) 2012-2020 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -135,7 +135,6 @@ namespace MfxHwVP9Encode
         // encode params (extended structures)
         VAEncSequenceParameterBufferVP9             m_sps;
         VAEncPictureParameterBufferVP9              m_pps;
-        VAEncMiscParameterTemporalLayerStructure    m_tempLayers;
         VAEncMiscParameterTypeVP9PerSegmantParam    m_segPar;
         VAEncMiscParameterRateControl               m_vaBrcPar;
         VAEncMiscParameterFrameRate                 m_vaFrameRate;
