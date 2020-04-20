cyew3/oneVPL-intel-gpu

commit e830768dacf5ffc73a29ba76dea935ad9eb413d6

build: enable AV1 decode on Linux
Issue: n/a
Tests: build

Change-Id: Id72ffa5fbac18ed5bf56a35d8f5a8f1806913bbb
Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

builder/ConfTargets.cmake
@@ -99,6 +99,7 @@ option( MFX_ENABLE_USER_ENCODE "Enabled user encode plugins?" ON)
 option( MFX_ENABLE_USER_ENC "Enabled user ENC plugins?" ON)
 option( MFX_ENABLE_USER_VPP "Enabled user VPP plugins?" ON)
 
+option( MFX_ENABLE_AV1_VIDEO_DECODE "Enabled AV1 decoder?" ON)
 option( MFX_ENABLE_VP8_VIDEO_DECODE "Enabled VP8 decoder?" ON)
 option( MFX_ENABLE_VP9_VIDEO_DECODE "Enabled VP9 decoder?" ON)
 option( MFX_ENABLE_H264_VIDEO_DECODE "Enabled AVC decoder?" ON)

_studio/shared/include/mfxvideo++int.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2007-2019 Intel Corporation
+// Copyright (c) 2007-2020 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -120,7 +120,7 @@ DEFINE_GUID_VA(DXVA2_Intel_Encode_MPEG2,                     VAProfileMPEG2Main,
 
 /* AV1 */
 #if defined(MFX_ENABLE_AV1_VIDEO_DECODE)
-DEFINE_GUID_VA(DXVA_Intel_ModeAV1_VLD,                       VAProfileAV1,    VAEntrypointVLD);
+DEFINE_GUID_VA(DXVA_Intel_ModeAV1_VLD,                       VAProfileAV1Profile0,   VAEntrypointVLD);
 #endif
 
 #else //if defined(MFX_VA_LINUX)

_studio/shared/umc/io/umc_va/src/umc_va_linux.cpp
@@ -185,7 +185,7 @@ VAProfile g_VP910BitsProfiles[] =
 #if defined(MFX_ENABLE_AV1_VIDEO_DECODE)
 VAProfile g_AV1Profiles[] =
 {
-    VAProfileAV1
+    VAProfileAV1Profile0
 };
 #endif
 

api/include/mfxstructures.h
@@ -517,7 +517,7 @@ enum {
     MFX_PROFILE_VP9_2                       = 3,
     MFX_PROFILE_VP9_3                       = 4,
 
-#if (MFX_VERSION >= MFX_VERSION_NEXT) && !defined(OPEN_SOURCE)
+#if (MFX_VERSION >= MFX_VERSION_NEXT)
     /* AV1 Profiles */
     MFX_PROFILE_AV1_MAIN                    = 1,
     MFX_PROFILE_AV1_HIGH                    = 2,

builder/mfxconfig.h.in
@@ -28,6 +28,7 @@
 #cmakedefine MFX_ENABLE_USER_ENC
 #cmakedefine MFX_ENABLE_USER_VPP
 
+#cmakedefine MFX_ENABLE_AV1_VIDEO_DECODE
 #cmakedefine MFX_ENABLE_H264_VIDEO_ENCODE
 #cmakedefine MFX_ENABLE_H264_VIDEO_FEI_ENCODE
 #cmakedefine MFX_ENABLE_H265_VIDEO_ENCODE

builder/open_source_symbols_undefine.list
@@ -113,7 +113,6 @@ VC1_DEBUG_ON
 __TIMING__
 UMC_ENABLE_VC1_SPLITTER
 UMC_ENABLE_VC1_VIDEO_ENCODER
-UMC_ENABLE_AV1_VIDEO_DECODER
 UMC_ENABLE_VPP_RESIZE
 MOD_ENC
 MOD_ENC_CREATE_PIPELINE

builder/profiles/fastboot.cmake
@@ -36,6 +36,7 @@ set ( MFX_ENABLE_USER_ENCODE OFF)
 set ( MFX_ENABLE_USER_ENC OFF)
 set ( MFX_ENABLE_USER_VPP OFF)
 
+set( MFX_ENABLE_AV1_VIDEO_DECODE OFF )
 set( MFX_ENABLE_H264_VIDEO_ENCODE OFF )
 set( MFX_ENABLE_H264_VIDEO_FEI_ENCODE OFF )
 set( MFX_ENABLE_H265_VIDEO_ENCODE OFF )

commit 28dfd8bad718962a4f0134e228797a8d8ef7db1a

av1d, build: fix Agama build w default config
Fix a build done with 'cmake ..' configuration.

Issue: n/a
Tests: build
Fixes: e830768 ("build: enable AV1 decode on Linux")

Change-Id: I55c3327adeabc6687135348f22a70f14e4bda700
Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

builder/ConfTargets.cmake
@@ -99,7 +99,7 @@ option( MFX_ENABLE_USER_ENCODE "Enabled user encode plugins?" ON)
 option( MFX_ENABLE_USER_ENC "Enabled user ENC plugins?" ON)
 option( MFX_ENABLE_USER_VPP "Enabled user VPP plugins?" ON)
 
-option( MFX_ENABLE_AV1_VIDEO_DECODE "Enabled AV1 decoder?" ON)
+option( MFX_ENABLE_AV1_VIDEO_DECODE "Enabled AV1 decoder?" ${API_USE_LATEST})
 option( MFX_ENABLE_VP8_VIDEO_DECODE "Enabled VP8 decoder?" ON)
 option( MFX_ENABLE_VP9_VIDEO_DECODE "Enabled VP9 decoder?" ON)
 option( MFX_ENABLE_H264_VIDEO_DECODE "Enabled AVC decoder?" ON)