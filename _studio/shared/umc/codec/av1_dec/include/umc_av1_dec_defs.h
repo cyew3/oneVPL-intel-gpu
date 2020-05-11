c5d70783ee030efaba65c46fb0b389a6358d9209
minghui1 <minghui.yang@intel.com>
2020-05-11 11:04:23 +0800

    [AV1D] Fix AV1 bitstream parsing issue for max tile numbers
    MAX_TILE_COLS/MAX_TILE_ROWS defination in MSDK is incorrect.In AV1 Spec,
    these two values are 64, while in MSDK, they are 1024. 
    Changing back to 64, 2 failed clips can pass.
    
    Issue: MDP-62700
    Test: Allegro Rev1.1 clip Main_8bits_471_HighRes_8192x4352_r5234.av1
    
    Change-Id: I374033d6bc918a327e62cadb31eef025918111e0

--- _studio/shared/umc/codec/av1_dec/include/umc_av1_dec_defs.h
+++ _studio/shared/umc/codec/av1_dec/include/umc_av1_dec_defs.h
@@ -74,8 +74,8 @@ namespace UMC_AV1_DECODER
 
     const uint32_t MAX_TILE_WIDTH = 4096;        // Max Tile width in pixels
     const uint32_t MAX_TILE_AREA  = 4096 * 2304;  // Maximum tile area in pixels
-    const uint32_t MAX_TILE_ROWS  = 1024;
-    const uint32_t MAX_TILE_COLS  = 1024;
+    const uint32_t MAX_TILE_ROWS  = 64;
+    const uint32_t MAX_TILE_COLS  = 64;
 
     const uint8_t FRAME_CONTEXTS_LOG2           = 3;
     const uint8_t MAX_MODE_LF_DELTAS            = 2;
