204f23ea4fe087456dea400d7e7e395d3ca10bc8
Yang, Ning <ning.yang@intel.com>
2020-06-02 11:52:04 +0800

    [AVCd] fix avc interlaced decode corruption issue
    For some avc interlaced clip, Frame(Top->Bottom) -> Frame(Bottome->Top).
    Current driver logic IsFieldOfOneFrame() couldn't handle this situation.. 
    
    Issue: MDP-62171
    Test: avc_mts_completeframe_07
    
    Change-Id: I29f8b1c55dbde749e51198efa4d036601432f4c6

--- _studio/shared/umc/io/media_buffers/src/umc_frame_constructor.cpp
+++ _studio/shared/umc/io/media_buffers/src/umc_frame_constructor.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2005-2019 Intel Corporation
+// Copyright (c) 2005-2020 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -2826,8 +2826,8 @@ bool H264ParsingCore::IsFieldOfOneFrame(const H264ParsingCore::Slice& prev, cons
     if (prev.field_pic_flag != last.field_pic_flag)
         return false;
 
-//    if (pSlice1->GetSliceHeader()->frame_num != pSlice2->GetSliceHeader()->frame_num)
-  //      return false;
+    if (prev.frame_num != last.frame_num)
+        return false;
 
     if (prev.bottom_field_flag == last.bottom_field_flag)
         return false;
