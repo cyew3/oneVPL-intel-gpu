cyew3/oneVPL-intel-gpu

commit bfa32f594b4580919385194732f38b635c17a211

Added nNalHrdConformance/nVuiNalHrdParameters cmd opt
GitHub: c30b5e2
Issue: no
Test: build only

Change-Id: I969ee438b0e4ead63410bc3596e1ffe222bebf49
Signed-off-by: Dmitry Ermilov <dmitry.ermilov@intel.com>

samples/sample_encode/src/pipeline_encode.cpp
@@ -623,6 +623,14 @@ mfxStatus CEncodingPipeline::InitMfxEncParams(sInputParams *pInParams)
         m_CodingOption.ViewOutput = MFX_CODINGOPTION_ON;
         bCodingOption = true;
     }
+
+    if (pInParams->nPicTimingSEI || pInParams->nNalHrdConformance || pInParams->nVuiNalHrdParameters)
+    {
+        m_CodingOption.PicTimingSEI        = pInParams->nPicTimingSEI;
+        m_CodingOption.NalHrdConformance   = pInParams->nNalHrdConformance;
+        m_CodingOption.VuiNalHrdParameters = pInParams->nVuiNalHrdParameters;
+    }
+
     if (bCodingOption)
     {
         m_EncExtParams.push_back((mfxExtBuffer *)&m_CodingOption);

samples/sample_encode/include/pipeline_encode.h
@@ -149,6 +149,9 @@ struct sInputParams
     mfxU16 nQPI;
     mfxU16 nQPP;
     mfxU16 nQPB;
+    mfxU16 nPicTimingSEI;
+    mfxU16 nNalHrdConformance;
+    mfxU16 nVuiNalHrdParameters;
 
     mfxU16 nGPB;
     ExtBRCType nExtBRC;

samples/sample_encode/src/sample_encode.cpp
@@ -137,6 +137,9 @@ void PrintHelp(msdk_char *strAppName, const msdk_char *strErrorMessage, ...)
     msdk_printf(MSDK_STRING("   [-ir_qp_delta]           - QP difference for inserted intra MBs. This is signed value in [-51, 51] range\n"));
     msdk_printf(MSDK_STRING("   [-ir_cycle_dist]         - Distance between the beginnings of the intra-refresh cycles in frames\n"));
     msdk_printf(MSDK_STRING("   [-gpb:<on,off>]          - Turn this option OFF to make HEVC encoder use regular P-frames instead of GPB\n"));
+    msdk_printf(MSDK_STRING("  -PicTimingSEI:<on,off>                    Enables or disables picture timing SEI\n"));
+    msdk_printf(MSDK_STRING("  -NalHrdConformance:<on,off>               Enables or disables picture HRD conformance\n"));
+    msdk_printf(MSDK_STRING("  -VuiNalHrdParameters:<on,off>             Enables or disables NAL HRD parameters in VUI header\n"));
     msdk_printf(MSDK_STRING("   [-ppyr:<on,off>]         - Turn this option ON to enable P-pyramid (by default the decision is made by library)\n"));
     msdk_printf(MSDK_STRING("   [-num_slice]             - number of slices in each video frame. 0 by default.\n"));
     msdk_printf(MSDK_STRING("                              If num_slice equals zero, the encoder may choose any slice partitioning allowed by the codec standard.\n"));
@@ -226,6 +229,40 @@ void PrintHelp(msdk_char *strAppName, const msdk_char *strErrorMessage, ...)
 
     msdk_printf(MSDK_STRING("\n"));
 }
+
+mfxStatus ParseAdditionalParams(msdk_char *strInput[], mfxU8 /*nArgNum*/, mfxU8& i, sInputParams* pParams)
+{
+    if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-PicTimingSEI:on")))
+    {
+        pParams->nPicTimingSEI = MFX_CODINGOPTION_ON;
+    }
+    else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-PicTimingSEI:off")))
+    {
+        pParams->nPicTimingSEI = MFX_CODINGOPTION_OFF;
+    }
+    else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-NalHrdConformance:on")))
+    {
+        pParams->nNalHrdConformance = MFX_CODINGOPTION_ON;
+    }
+    else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-NalHrdConformance:off")))
+    {
+        pParams->nNalHrdConformance = MFX_CODINGOPTION_OFF;
+    }
+    else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-VuiNalHrdParameters:on")))
+    {
+        pParams->nVuiNalHrdParameters = MFX_CODINGOPTION_ON;
+    }
+    else if (0 == msdk_strcmp(strInput[i], MSDK_STRING("-VuiNalHrdParameters:off")))
+    {
+        pParams->nVuiNalHrdParameters = MFX_CODINGOPTION_OFF;
+    }
+    else
+    {
+        return MFX_ERR_NOT_FOUND;
+    }
+    return MFX_ERR_NONE;
+}
+
 
 mfxStatus ParseInputString(msdk_char* strInput[], mfxU8 nArgNum, sInputParams* pParams)
 {
@@ -1209,9 +1246,13 @@ mfxStatus ParseInputString(msdk_char* strInput[], mfxU8 nArgNum, sInputParams* p
         }
         else
         {
-            msdk_printf(MSDK_STRING("Unknown option: %s\n"), strInput[i]);
-            PrintHelp(strInput[0], NULL);
-            return MFX_ERR_UNSUPPORTED;
+            mfxStatus sts = ParseAdditionalParams(strInput, nArgNum, i, pParams);
+            if (sts < MFX_ERR_NONE)
+            {
+                msdk_printf(MSDK_STRING("Unknown option: %s\n"), strInput[i]);
+                PrintHelp(strInput[0], NULL);
+                return MFX_ERR_UNSUPPORTED;
+            }
         }
     }
 

samples/sample_multi_transcode/include/pipeline_transcode.h
@@ -291,6 +291,9 @@ namespace TranscodingSample
         mfxU16 nQPP;
         mfxU16 nQPB;
         bool bDisableQPOffset;
+        mfxU16 nPicTimingSEI;
+        mfxU16 nNalHrdConformance;
+        mfxU16 nVuiNalHrdParameters;
 
         bool bOpenCL;
         mfxU16 reserved[4];
@@ -833,6 +836,9 @@ namespace TranscodingSample
 #endif //MFX_VERSION >= 1022
 
         mfxExtLAControl          m_ExtLAControl;
+
+        mfxExtCodingOption       m_CodingOption;
+
         // for setting MaxSliceSize
         mfxExtCodingOption2      m_CodingOption2;
 

samples/sample_multi_transcode/src/pipeline_transcode.cpp
@@ -176,6 +176,7 @@ CTranscodingPipeline::CTranscodingPipeline():
     MSDK_ZERO_MEMORY(m_PluginOpaqueAlloc);
     MSDK_ZERO_MEMORY(m_PreEncOpaqueAlloc);
     MSDK_ZERO_MEMORY(m_ExtLAControl);
+    MSDK_ZERO_MEMORY(m_CodingOption);
     MSDK_ZERO_MEMORY(m_CodingOption2);
     MSDK_ZERO_MEMORY(m_CodingOption3);
     MSDK_ZERO_MEMORY(m_ExtHEVCParam);
@@ -247,6 +248,9 @@ CTranscodingPipeline::CTranscodingPipeline():
 #if MFX_VERSION >= 1024
     m_VppCompParams.NumTiles = 0;
 #endif
+    m_CodingOption.Header.BufferId = MFX_EXTBUFF_CODING_OPTION;
+    m_CodingOption.Header.BufferSz = sizeof(m_CodingOption);
+
     m_CodingOption2.Header.BufferId = MFX_EXTBUFF_CODING_OPTION2;
     m_CodingOption2.Header.BufferSz = sizeof(m_CodingOption2);
 
@@ -2559,6 +2563,16 @@ MFX_IOPATTERN_IN_VIDEO_MEMORY : MFX_IOPATTERN_IN_SYSTEM_MEMORY);
         }
 #endif
     }
+
+
+    if (pInParams->nPicTimingSEI || pInParams->nNalHrdConformance || pInParams->nVuiNalHrdParameters)
+    {
+        m_CodingOption.PicTimingSEI = pInParams->nPicTimingSEI;
+        m_CodingOption.NalHrdConformance = pInParams->nNalHrdConformance;
+        m_CodingOption.VuiNalHrdParameters = pInParams->nVuiNalHrdParameters;
+        m_EncExtParams.push_back((mfxExtBuffer*)&m_CodingOption);
+    }
+
 
     // we don't specify profile and level and let the encoder choose those basing on parameters
     // we must specify profile only for MVC codec

samples/sample_multi_transcode/src/transcode_utils.cpp
@@ -278,6 +278,10 @@ void TranscodingSample::PrintHelp()
     msdk_printf(MSDK_STRING("  -roi_qpmap    Use QP map to emulate ROI for CQP mode\n"));
     msdk_printf(MSDK_STRING("  -extmbqp      Use external MBQP map\n"));
 #endif //MFX_VERSION >= 1022
+    msdk_printf(MSDK_STRING("  -PicTimingSEI:<on,off>                    Enables or disables picture timing SEI\n"));
+    msdk_printf(MSDK_STRING("  -NalHrdConformance:<on,off>               Enables or disables picture HRD conformance\n"));
+    msdk_printf(MSDK_STRING("  -VuiNalHrdParameters:<on,off>             Enables or disables NAL HRD parameters in VUI header\n"));
+
     msdk_printf(MSDK_STRING("\n"));
     msdk_printf(MSDK_STRING("Pipeline description (vpp options):\n"));
     msdk_printf(MSDK_STRING("  -deinterlace             Forces VPP to deinterlace input stream\n"));
@@ -1070,6 +1074,30 @@ mfxStatus ParseAdditionalParams(msdk_char *argv[], mfxU32 /*argc*/, mfxU32& i, T
     else if (0 == msdk_strcmp(argv[i], MSDK_STRING("-lowpower:off")))
     {
         InputParams.enableQSVFF = false;
+    }
+    else if (0 == msdk_strcmp(argv[i], MSDK_STRING("-PicTimingSEI:on")))
+    {
+        InputParams.nPicTimingSEI = MFX_CODINGOPTION_ON;
+    }
+    else if (0 == msdk_strcmp(argv[i], MSDK_STRING("-PicTimingSEI:off")))
+    {
+        InputParams.nPicTimingSEI = MFX_CODINGOPTION_OFF;
+    }
+    else if (0 == msdk_strcmp(argv[i], MSDK_STRING("-NalHrdConformance:on")))
+    {
+        InputParams.nNalHrdConformance = MFX_CODINGOPTION_ON;
+    }
+    else if (0 == msdk_strcmp(argv[i], MSDK_STRING("-NalHrdConformance:off")))
+    {
+        InputParams.nNalHrdConformance = MFX_CODINGOPTION_OFF;
+    }
+    else if (0 == msdk_strcmp(argv[i], MSDK_STRING("-VuiNalHrdParameters:on")))
+    {
+        InputParams.nVuiNalHrdParameters = MFX_CODINGOPTION_ON;
+    }
+    else if (0 == msdk_strcmp(argv[i], MSDK_STRING("-VuiNalHrdParameters:off")))
+    {
+        InputParams.nVuiNalHrdParameters = MFX_CODINGOPTION_OFF;
     }
     else if (0 == msdk_strcmp(argv[i], MSDK_STRING("-BitrateLimit:on")))
     {

commit 1ad8cda997609a9e014d590f2ffa2b71d9d51722

sample_encode: fix attach of codingOption
fixes: bfa32f5("Added nNalHrdConformance/nVuiNalHrdParameters cmd opt")

Issue: VSMGWL-28473
Test: manual runs & compare os/cs behavior

Change-Id: I9380ce16b6b6e09161ceafedae7bc55c7439e8dc

samples/sample_encode/src/pipeline_encode.cpp
@@ -629,6 +629,7 @@ mfxStatus CEncodingPipeline::InitMfxEncParams(sInputParams *pInParams)
         m_CodingOption.PicTimingSEI        = pInParams->nPicTimingSEI;
         m_CodingOption.NalHrdConformance   = pInParams->nNalHrdConformance;
         m_CodingOption.VuiNalHrdParameters = pInParams->nVuiNalHrdParameters;
+        bCodingOption = true;
     }
 
     if (bCodingOption)