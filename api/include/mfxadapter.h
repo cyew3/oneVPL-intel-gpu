dcdb62993ab1ea1f43c3adbd6f05ea8b1e45e86a
Miroslav Goncharenko <miroslav.goncharenko@intel.com>
2020-06-23 18:19:18 +0300

    api: fix cdecl for adapters API
    Issue: -
    Test: -
    Github: 3c9cb5d (api: fix cdecl for adapters API)
    
    Change-Id: Ic4cc2b2ae6d0d285c8fe5b13cb8122ce3cc87cbf

--- api/include/mfxadapter.h
+++ api/include/mfxadapter.h
@@ -1,6 +1,6 @@
 /******************************************************************************* *\
 
-Copyright (C) 2007-2019 Intel Corporation.  All rights reserved.
+Copyright (C) 2007-2020 Intel Corporation.  All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -38,9 +38,9 @@ File Name: mfxadapter.h
 extern "C"
 {
 #endif
-mfxStatus MFXQueryAdapters(mfxComponentInfo* input_info, mfxAdaptersInfo* adapters);
-mfxStatus MFXQueryAdaptersDecode(mfxBitstream* bitstream, mfxU32 codec_id, mfxAdaptersInfo* adapters);
-mfxStatus MFXQueryAdaptersNumber(mfxU32* num_adapters);
+mfxStatus MFX_CDECL MFXQueryAdapters(mfxComponentInfo* input_info, mfxAdaptersInfo* adapters);
+mfxStatus MFX_CDECL MFXQueryAdaptersDecode(mfxBitstream* bitstream, mfxU32 codec_id, mfxAdaptersInfo* adapters);
+mfxStatus MFX_CDECL MFXQueryAdaptersNumber(mfxU32* num_adapters);
 #ifdef __cplusplus
 } // extern "C"
 #endif
