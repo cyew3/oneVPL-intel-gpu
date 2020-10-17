cyew3/oneVPL-intel-gpu

commit 3ca505e8e25fceb0ee98a2250ad24ff28b72f477

[h264e] Return UNSUPPORTED, if sliding window is not supported by caps
Issue: MDP-62514
Test: h264e_sliding_window
Github: 9188b15 ([h264e] Return UNSUPPORTED, if sliding window is not supported by caps)

Change-Id: Ia4c2aa8d993aec272bf94f03a8923b78b0833b45

_studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -3515,6 +3515,12 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
         }
     }
 
+    if (extOpt3->WinBRCSize && !hwCaps.ddi_caps.FrameSizeTolerance)
+    {
+        unsupported = true;
+        extOpt3->WinBRCSize = 0;
+    }
+
     if (extOpt2->BRefType > 2)
     {
         changed = true;

_studio/mfx_lib/shared/src/mfx_h264_encode_vaapi.cpp
@@ -225,13 +225,12 @@ mfxStatus SetRateControl(
         rate_param->target_percentage = par.mfx.Accuracy;
     }
 
-#if defined(LINUX_TARGET_PLATFORM_BXTMIN) || defined(LINUX_TARGET_PLATFORM_BXT) || defined(LINUX_TARGET_PLATFORM_CFL)
+    rate_param->rc_flags.bits.frame_tolerance_mode = ConvertLowDelayBRCMfx2Ddi(extOpt3.LowDelayBRC, par.calcParam.TCBRCTargetFrameSize);
     // Activate frame tolerance sliding window mode
-    mfxExtCodingOption3 const & extOpt3 = GetExtBufferRef(par);
-    if (extOpt3.WinBRCSize) {
-        rate_param->rc_flags.bits.frame_tolerance_mode = 1;
+    if (extOpt3.WinBRCSize)
+    {
+        rate_param->rc_flags.bits.frame_tolerance_mode = eFrameSizeTolerance_Low;
     }
-#endif  // defined(LINUX_TARGET_PLATFORM_BXTMIN) || defined(LINUX_TARGET_PLATFORM_BXT) || defined(LINUX_TARGET_PLATFORM_CFL)
 
     //  MBBRC control
     // Control VA_RC_MB 0: default, 1: enable, 2: disable, other: reserved
@@ -1513,18 +1512,21 @@ mfxStatus VAAPIEncoder::CreateAuxilliaryDevice(
 {
     m_core = core;
 
+    m_caps = {};
+
     VAAPIVideoCORE * hwcore = dynamic_cast<VAAPIVideoCORE *>(m_core);
     MFX_CHECK_WITH_ASSERT(hwcore != 0, MFX_ERR_DEVICE_FAILED);
     if(hwcore)
     {
         mfxStatus mfxSts = hwcore->GetVAService(&m_vaDisplay);
         MFX_CHECK_STS(mfxSts);
+        eMFXHWType platform = hwcore->GetHWType();
+        if (MFX_HW_APL == platform || MFX_HW_CFL == platform)
+            m_caps.ddi_caps.FrameSizeTolerance = 1;
     }
 
     m_width  = width;
     m_height = height;
-
-    m_caps = {};
 
     m_caps.ddi_caps.BRCReset        = 1; // no bitrate resolution control
     m_caps.ddi_caps.HeaderInsertion = 0; // we will provide headers (SPS, PPS) in binary format to the driver

commit c1d09bcbc76b758d04c36401688e02759d67f3c4

[h264e] Fix sliding window for MFX_RATECONTROL_LA
1) Sliding window for MFX_RATECONTROL_LA* doesn't require driver
support. It was broken in 3ca505e (Github 9188b153c).
2) Sliding window for VBR and QVBR requires driver support along with
CBR.
3) WinBRCSize==0 better matches unsupported, not changed, as the
functionality is not supported.

Issue: MDP-62514, VSMGWL-29997
Test: -
Github: 7beb403 ([h264e] Fix sliding window for MFX_RATECONTROL_LA*)

Change-Id: I404d5cc55910f706635140464ea6c517c5ea9455

_studio/mfx_lib/shared/include/encoder_ddi.hpp
@@ -610,7 +610,7 @@ typedef struct tagENCODE_CAPS
             UINT    HMEOffsetSupport             : 1;
             UINT    DirtyRectSupport             : 1;
             UINT    MoveRectSupport              : 1;
-            UINT    FrameSizeTolerance           : 1;
+            UINT    FrameSizeToleranceSupport    : 1;
             UINT    HWCounterAutoIncrement       : 2;
             UINT    MBControlSupport             : 1;
             UINT    ForceRepartitionCheckSupport : 1;

_studio/mfx_lib/shared/include/mfx_h264_encode_struct_vaapi.h
@@ -312,14 +312,14 @@ typedef struct tagENCODE_CAPS
             UINT    HMEOffsetSupport        : 1;
             UINT    DirtyRectSupport        : 1;
             UINT    MoveRectSupport         : 1;
-            UINT    FrameSizeTolerance      : 1;
-            UINT    HWCounterAutoIncrement  : 2;
-            UINT    MBControlSupport        : 1;
+            UINT    FrameSizeToleranceSupport    : 1; // eFrameSizeTolerance_Low (Sliding window) supported
+            UINT    HWCounterAutoIncrement       : 2;
+            UINT    MBControlSupport             : 1;
             UINT    ForceRepartitionCheckSupport : 1;
-            UINT    CustomRoundingControl   : 1;
+            UINT    CustomRoundingControl        : 1;
             UINT    LLCStreamingBufferSupport    : 1;
             UINT    DDRStreamingBufferSupport    : 1;
-            UINT    LowDelayBRCSupport           : 1;
+            UINT    LowDelayBRCSupport           : 1; // eFrameSizeTolerance_ExtremelyLow (Low delay) supported
             UINT    MaxNumDeltaQPMinus1          : 4;
             UINT    TCBRCSupport                 : 1;
             UINT    HRDConformanceSupport        : 1;

_studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -3560,12 +3560,6 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
         }
     }
 
-    if (extOpt3->WinBRCSize && !hwCaps.ddi_caps.FrameSizeTolerance)
-    {
-        unsupported = true;
-        extOpt3->WinBRCSize = 0;
-    }
-
     if (extOpt2->BRefType > 2)
     {
         changed = true;
@@ -4968,17 +4962,17 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
         changed = true;
     }
 
-    bool mfxRateControlHwCbr =
-        (platform == MFX_HW_APL || platform == MFX_HW_CFL) &&
-        (par.mfx.RateControlMethod == MFX_RATECONTROL_CBR);
+    bool mfxRateControlHwSupport =
+        hwCaps.ddi_caps.FrameSizeToleranceSupport &&
+        (par.mfx.RateControlMethod == MFX_RATECONTROL_CBR ||
+         par.mfx.RateControlMethod == MFX_RATECONTROL_VBR ||
+         par.mfx.RateControlMethod == MFX_RATECONTROL_QVBR);
 
     bool slidingWindowSupported  =
             par.mfx.RateControlMethod == MFX_RATECONTROL_LA  ||
             par.mfx.RateControlMethod == MFX_RATECONTROL_LA_HRD ||
             par.mfx.RateControlMethod == MFX_RATECONTROL_LA_EXT ||
-            par.mfx.RateControlMethod == MFX_RATECONTROL_VBR ||
-            par.mfx.RateControlMethod == MFX_RATECONTROL_QVBR ||
-            (mfxRateControlHwCbr && !IsOn(extOpt2->ExtBRC));
+            (mfxRateControlHwSupport && !IsOn(extOpt2->ExtBRC));
 
      if (extOpt3->WinBRCMaxAvgKbps || extOpt3->WinBRCSize)
      {
@@ -4987,16 +4981,13 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
              extOpt3->WinBRCMaxAvgKbps = 0;
              extOpt3->WinBRCSize = 0;
              par.calcParam.WinBRCMaxAvgKbps = 0;
-             changed = true;
+             unsupported = true;
          }
          else if (extOpt3->WinBRCSize==0)
          {
              warning = true;
          }
-         else if ((par.mfx.RateControlMethod == MFX_RATECONTROL_VBR ||
-             par.mfx.RateControlMethod == MFX_RATECONTROL_QVBR ||
-             mfxRateControlHwCbr) &&
-             !IsOn(extOpt2->ExtBRC))
+         else if (mfxRateControlHwSupport && !IsOn(extOpt2->ExtBRC))
          {
              if (par.mfx.FrameInfo.FrameRateExtN != 0 && par.mfx.FrameInfo.FrameRateExtD != 0)
              {
@@ -5022,7 +5013,7 @@ mfxStatus MfxHwH264Encode::CheckVideoParamQueryLike(
              }
              else if (par.calcParam.WinBRCMaxAvgKbps)
              {
-                 if (mfxRateControlHwCbr &&
+                 if (par.mfx.RateControlMethod == MFX_RATECONTROL_CBR &&
                      par.calcParam.targetKbps &&
                      par.calcParam.WinBRCMaxAvgKbps != par.calcParam.targetKbps)
                  {

_studio/mfx_lib/shared/src/mfx_h264_encode_vaapi.cpp
@@ -1522,7 +1522,7 @@ mfxStatus VAAPIEncoder::CreateAuxilliaryDevice(
         MFX_CHECK_STS(mfxSts);
         eMFXHWType platform = hwcore->GetHWType();
         if (MFX_HW_APL == platform || MFX_HW_CFL == platform)
-            m_caps.ddi_caps.FrameSizeTolerance = 1;
+            m_caps.ddi_caps.FrameSizeToleranceSupport = 1;
     }
 
     m_width  = width;