3f15aa3eacebf4f6e690c56f14610c5fcdb50856
Gurulev, Dmitry <dmitry.gurulev@intel.com>
2020-11-30 15:34:58 +0300

    [build]: fix av1/mpeg2 decoders (#161)
    - some decoder's related files
       were compiled w/o MFX_VA defined
    
    Issue: MDP-65168
    Test: CI, nightly
          MSDK_Decode_AV1_Allegro_PreSi_MTS/
          MSDK_Decode_MPEG2_MTS

--- _studio/mfx_lib/shared/sources_ext.cmake
+++ _studio/mfx_lib/shared/sources_ext.cmake
@@ -9,17 +9,44 @@ set_property(TARGET mfx_common_sw PROPERTY FOLDER "sw_fallback")
 
 target_sources(mfx_common_sw
   PRIVATE
+    include/mfx_brc_common.h
+    include/mfx_common_decode_int.h
+    include/mfx_common_int.h
     include/mfx_critical_error_handler.h
+    include/mfx_enc_common.h
+    include/mfx_h264_enc_common.h
+    include/mfx_interface.h
+    include/mfx_mpeg2_dec_common.h
+    include/mfx_mpeg2_enc_common.h
+    include/mfx_session.h
+    include/mfx_user_plugin.h
+    include/mfx_vc1_dec_common.h
+    include/mfx_vpx_dec_common.h
+
+    src/mfx_brc_common.cpp
+    src/mfx_common_decode_int.cpp
+    src/mfx_common_int.cpp
+    src/mfx_enc_common.cpp
+    src/mfx_mpeg2_dec_common.cpp
+    src/mfx_h264_enc_common.cpp
+    src/mfx_vc1_dec_common.cpp
     src/mfx_critical_error_handler.cpp
+    src/mfx_vpx_dec_common.cpp
   )
 
-target_compile_definitions(mfx_common_sw PRIVATE MFX_RT MFX_NO_VA)
-target_include_directories(mfx_common_sw PUBLIC include)
+target_compile_definitions(mfx_common_sw PRIVATE MFX_NO_VA)
 
-target_link_libraries(mfx_common_sw PUBLIC mfx_common umc_io)
+target_include_directories(mfx_common_sw
+  PUBLIC
+    include
+    ${MSDK_UMC_ROOT}/codec/brc/include
+    ${MSDK_UMC_ROOT}/codec/h264_enc/include
+    ${MSDK_UMC_ROOT}/codec/vc1_common/include
+    ${MSDK_UMC_ROOT}/codec/vc1_dec/include
+)
+
+target_link_libraries(mfx_common_sw PUBLIC mfx_static_lib umc_io umc)
 
 # ====================================== mfx_common_hw ======================================
-target_link_libraries(mfx_common_hw
-  PRIVATE
-    $<$<BOOL:${MFX_ENABLE_LP_LOOKAHEAD}>:lpla>
-)
+
+target_link_libraries(mfx_common_hw PUBLIC mfx_common_sw lpla)
