7e9e6a2d9ec4e701978103b03070875c7c18674c
Sebastian Possos <sebastian.possos@intel.com>
2020-12-10 16:11:08 -0800

    MCTF: ATS re-update kernels (#123)
    Recompilation of Gen12 (ATS) MCTF kernels as they got overwritten
    with older version.
    
    Issue: HSD1508270875
    Test:  vpp_mctf_denoise

--- _studio/mfx_lib/genx/mctf/isa/mctf_mc_gen12_isa.h
+++ _studio/mfx_lib/genx/mctf/isa/mctf_mc_gen12_isa.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2020 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -19,5 +19,5 @@
 // SOFTWARE.
 #ifndef __mctf_mc_gen12_isa__
 #define __mctf_mc_gen12_isa__
-extern const unsigned char mctf_mc_gen12_isa[129846];
+extern const unsigned char mctf_mc_gen12_isa[128654];
 #endif
--- _studio/mfx_lib/genx/mctf/isa/mctf_sd_gen12_isa.h
+++ _studio/mfx_lib/genx/mctf/isa/mctf_sd_gen12_isa.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Intel Corporation
+// Copyright (c) 2020 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -19,5 +19,5 @@
 // SOFTWARE.
 #ifndef __mctf_sd_gen12_isa__
 #define __mctf_sd_gen12_isa__
-extern const unsigned char mctf_sd_gen12_isa[103284];
+extern const unsigned char mctf_sd_gen12_isa[115923];
 #endif
