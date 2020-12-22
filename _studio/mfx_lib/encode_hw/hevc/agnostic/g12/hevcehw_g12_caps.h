cyew3/oneVPL-intel-gpu

commit 5d61efaa31ea7c199fc3c8ed899f26ea68512612

[HEVCe] Disable RAB with speed mode for Gen12 windows (#271)
For Gen12 HEVC VDEnc, RAB frame has no restriction with all target usage on
Linux, yet resticted with speed mode on Windows.

Issue: VSMGWL-34439
Test: cases with TU7 and GopRefDist bigger than 1

Change-Id: I50076eb9b7ae21332b8d3c9b2fa02c828c55b07e

_studio/mfx_lib/encode_hw/hevc/embargo/windows/g12/hevcehw_g12_caps_win.h
@@ -1,0 +1,67 @@
+// Copyright (c) 2020 Intel Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include "mfx_common.h"
+#if defined(MFX_ENABLE_H265_VIDEO_ENCODE)
+
+#include "hevcehw_base.h"
+#include "hevcehw_g12_data.h"
+#include "hevcehw_g12_caps.h"
+
+namespace HEVCEHW
+{
+namespace Windows
+{
+namespace Gen12
+{
+
+    using namespace HEVCEHW::Gen12;
+
+    class Caps
+        : public HEVCEHW::Gen12::Caps
+    {
+    public:
+        Caps(mfxU32 FeatureId)
+            : HEVCEHW::Gen12::Caps(FeatureId)
+        {}
+
+    protected:
+        virtual void Query1WithCaps(const FeatureBlocks& blocks, TPushQ1 Push) override
+        {
+            HEVCEHW::Gen12::Caps::Query1WithCaps(blocks, Push);
+
+            Push(BLK_HardcodeCaps
+                , [this](const mfxVideoParam&, mfxVideoParam& par, StorageRW& strg)->mfxStatus
+            {
+                auto& caps = Glob::EncodeCaps::Get(strg);
+                caps.SliceIPOnly = IsOn(par.mfx.LowPower) && (par.mfx.TargetUsage == 6 || par.mfx.TargetUsage == 7 || par.mfx.CodecProfile == MFX_PROFILE_HEVC_SCC);
+
+                return MFX_ERR_NONE;
+            });
+        }
+    };
+
+} //Gen12
+} //Windows
+} //namespace HEVCEHW
+
+#endif

_studio/mfx_lib/encode_hw/hevc/embargo/windows/g12/hevcehw_g12_win.cpp
@@ -25,7 +25,7 @@
 #include "hevcehw_g12_win.h"
 #include "hevcehw_g12_rext_win.h"
 #include "hevcehw_g12_scc_win.h"
-#include "hevcehw_g12_caps.h"
+#include "hevcehw_g12_caps_win.h"
 #if defined(MFX_ENABLE_HW_BLOCKING_TASK_SYNC)
 #include "hevcehw_base_blocking_sync_win.h"
 #endif

_studio/mfx_lib/encode_hw/encode_hw.vcxproj
@@ -269,6 +269,7 @@
     <ClInclude Include="hevc\embargo\windows\g12dg2\hevcehw_g12dg2_win.h" />
     <ClInclude Include="hevc\embargo\windows\g12\hevcehw_g12_qp_modulation_win.h" />
     <ClInclude Include="hevc\embargo\windows\g12\hevcehw_g12_rext_win.h" />
+    <ClInclude Include="hevc\embargo\windows\g12\hevcehw_g12_caps_win.h" />
     <ClInclude Include="hevc\embargo\windows\g12\hevcehw_g12_scc_win.h" />
     <ClInclude Include="hevc\embargo\windows\g12\hevcehw_g12_win.h" />
     <ClInclude Include="hevc\embargo\windows\hevcehw_ddi_trace.h" />

_studio/mfx_lib/encode_hw/encode_hw.vcxproj.filters
@@ -969,6 +969,9 @@
     <ClInclude Include="hevc\embargo\windows\g12\hevcehw_g12_rext_win.h">
       <Filter>hevc\embargo\windows\g12</Filter>
     </ClInclude>
+    <ClInclude Include="hevc\embargo\windows\g12\hevcehw_g12_caps_win.h">
+      <Filter>hevc\embargo\windows\g12</Filter>
+    </ClInclude>
     <ClInclude Include="hevc\agnostic\base\hevcehw_base_recon_info.h">
       <Filter>hevc\open\agnostic\base</Filter>
     </ClInclude>

commit 4b175329bb97a0457b07bdf11447672a96549191

[HEVCe] Fix TU7 RAB issue on DG2 (#322)
It fixes the issue introduced by #271 for TU7 cases with GopRefDist set as 0

Issue: VSMGWL-34439
Test: cases with TU7 and various GopRefDist setting

Change-Id: I9ec4916e5c6d48997800c1dae53812b71b501c6e

_studio/mfx_lib/encode_hw/hevc/agnostic/g12/hevcehw_g12_caps.h
@@ -36,7 +36,8 @@ class Caps
 public:
 #define DECL_BLOCK_LIST\
     DECL_BLOCK(SetDefaultsCallChain)\
-    DECL_BLOCK(HardcodeCaps)
+    DECL_BLOCK(HardcodeCaps)\
+    DECL_BLOCK(HardcodeCapsExt)
 #define DECL_FEATURE_NAME "G12_Caps"
 #include "hevcehw_decl_blocks.h"
 

_studio/mfx_lib/encode_hw/hevc/embargo/windows/g12/hevcehw_g12_caps_win.h
@@ -49,7 +49,7 @@ namespace Gen12
         {
             HEVCEHW::Gen12::Caps::Query1WithCaps(blocks, Push);
 
-            Push(BLK_HardcodeCaps
+            Push(BLK_HardcodeCapsExt
                 , [this](const mfxVideoParam&, mfxVideoParam& par, StorageRW& strg)->mfxStatus
             {
                 auto& caps = Glob::EncodeCaps::Get(strg);

_studio/mfx_lib/encode_hw/hevc/embargo/windows/g12/hevcehw_g12_win.cpp
@@ -120,6 +120,10 @@ MFXVideoENCODEH265_HW::MFXVideoENCODEH265_HW(
             , { Base::FEATURE_DDI_PACKER, Base::IDDIPacker::BLK_HardcodeCaps }
             , { FEATURE_CAPS, Caps::BLK_HardcodeCaps }
             , PLACE_AFTER);
+        Reorder(
+            qwc
+            , { Base::FEATURE_LEGACY, Base::Legacy::BLK_CheckHeaders }
+            , { FEATURE_CAPS, Caps::BLK_HardcodeCapsExt });
     }
 
     if (mode & INIT)

_studio/mfx_lib/encode_hw/hevc/embargo/windows/g12dg2/hevcehw_g12dg2_win.cpp
@@ -26,7 +26,7 @@
 #include "hevcehw_g12dg2_caps.h"
 #include "hevcehw_base_legacy.h"
 #include "hevcehw_base_blocking_sync_win.h"
-#include "hevcehw_g12_caps.h"
+#include "hevcehw_g12_caps_win.h"
 
 namespace HEVCEHW
 {
@@ -69,7 +69,7 @@ MFXVideoENCODEH265_HW::MFXVideoENCODEH265_HW(
         auto& qwc = BQ<BQ_Query1WithCaps>::Get(*this);
         Reorder(
             qwc
-            , { HEVCEHW::Gen12::FEATURE_CAPS, HEVCEHW::Gen12::Caps::BLK_HardcodeCaps }
+            , { HEVCEHW::Gen12::FEATURE_CAPS, HEVCEHW::Windows::Gen12::Caps::BLK_HardcodeCapsExt }
             , { FEATURE_CAPS, Caps::BLK_HardcodeCaps }
         , PLACE_AFTER);
     }