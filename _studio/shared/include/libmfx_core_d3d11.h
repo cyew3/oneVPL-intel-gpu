cyew3/oneVPL-intel-gpu

commit 42a8d1fef0fdb0e82a1d1bbd1e95f6dd25f62632

Introduce DX9ON11 support (#488)
* Initial commit for DX9on11
* Updated SetHandle and SetFrameAllocator to support DX9on11
* Fixed behavior of internal allocator
* Partially working decoders (green frames on output)
* Introduced DX9SharedSurfaces classes
* Moved DX9SharedSurfaces under define
* Implemented copy process
* Create D3D9ON11 video core (workable jpeg encoder/decoder)
* Implement D3D9ON11 support for VPP
* Implement D3D9ON11 support for decoders
* D3D9ON11 support for h264 encoder\transcoder
* Add D3D9ON11 support for JPEG transcode
* Implement D3D9ON11 support for mpeg2 encoder
* Implement D3D9ON11 support for hevcr encoder
* Implement D3D9ON11 support for vp9 encoder
* Add support to all fourcc
* Add D3D9ON11 core to the cmake build system
* h264: Move wrap logic to DDI part
* mjpege: Move wrap logic to DDI part
* mpeg2: Move almost all wrap logic to DDI part
* hevc: Move wrap logic to DDI part
* vp9: Move wrap logic to DDI part
* vpp: move all wrapper logic to DDI part
* Decode: move wrapper logic to DDI part
* common: add checks to improve debuggability
* dx9on11: Add static mutex field to control access to shared DX9 resourse
* Decode: move unwrap to the corresponding place
* Add RT platform checks for dx9/dx9on11 core
* dx9on11 core: Remove deadcode & reference
* Make dynamic_cast once
* Fix win cmake build
* remove MFX_DX9ON11 define from code
* Rename unwrap method & create common alloc method for encoders
* Add missing error checks & remove dead code
* Change copyright & remove unused header

Issue: MDP-64835, MDP-64840, MDP-65020, MDP-64837, MDP-64836, MDP-64838, MDP-64839, MDP-64841, MDP-64847, MDP-65511
Test: manual runs, CSC, denoise & another simple filters. Also verified pipelines with reset:
sample_vpp.exe -i hevc.yuv -sw 1920 -sh 1080 -dw 720 -dh 480 -scc nv12 -dcc nv12 -o 720x480_vpp.yuv -d3d \
-iopattern d3d_to_d3d  -reset_start 50 -dw 720 -dh 480 -denoise 32 -reset_end

_studio/shared/include/libmfx_core_d3d11.h
@@ -152,7 +152,8 @@ public:
 
     virtual bool         IsCompatibleForOpaq() override;
 
-private:
+protected:
+
     D3D11VideoCORE_T(const mfxU32 adapterNum, const mfxU32 numThreadsAvailable, const mfxSession session = nullptr);
 
     mfxStatus InternalInit();
@@ -162,7 +163,7 @@ private:
     std::unique_ptr<D3D11Adapter> m_pid3d11Adapter;
 
     mfxStatus InitializeDevice(bool isTemporal = false);
-    mfxStatus InternalCreateDevice();
+    virtual mfxStatus InternalCreateDevice();
     virtual mfxStatus DefaultAllocFrames(mfxFrameAllocRequest *request, mfxFrameAllocResponse *response)                      override;
     mfxStatus ProcessRenderTargets(mfxFrameAllocRequest *request, mfxFrameAllocResponse *response, mfxBaseWideFrameAllocator* pAlloc);
     // this function should not be virtual

commit 77add26dbbd171a6dd3c68338681cac33ff7d99c

Remove unnecessary virtual specifier from InternalCreateDevice (#566)
Fixes: 42a8d1f ("Introduce DX9ON11 support (#488)")
Issue: -
Test: manual

_studio/shared/include/libmfx_core_d3d11.h
@@ -163,7 +163,7 @@ protected:
     std::unique_ptr<D3D11Adapter> m_pid3d11Adapter;
 
     mfxStatus InitializeDevice(bool isTemporal = false);
-    virtual mfxStatus InternalCreateDevice();
+    mfxStatus InternalCreateDevice();
     virtual mfxStatus DefaultAllocFrames(mfxFrameAllocRequest *request, mfxFrameAllocResponse *response)                      override;
     mfxStatus ProcessRenderTargets(mfxFrameAllocRequest *request, mfxFrameAllocResponse *response, mfxBaseWideFrameAllocator* pAlloc);
     // this function should not be virtual