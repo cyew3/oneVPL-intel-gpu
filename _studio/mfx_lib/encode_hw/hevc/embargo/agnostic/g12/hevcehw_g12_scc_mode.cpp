64e4a78a1a1a08f317d408fee6583285d721fc88
AleksMikhaylov <aleksandr.mikhaylov@intel.com>
2021-05-21 23:47:25 +0300

    Revert "[HEVCe] Correct default value for IBC and Palette (#1580)" (#1619)
    This reverts commit d07d56cde302b1ce98bea61ce5c5e15b78190dea.

--- _studio/mfx_lib/encode_hw/hevc/embargo/agnostic/g12/hevcehw_g12_scc_mode.cpp
+++ _studio/mfx_lib/encode_hw/hevc/embargo/agnostic/g12/hevcehw_g12_scc_mode.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2021 Intel Corporation
+// Copyright (c) 2020 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -88,8 +88,8 @@ void SCCMode::SetDefaults(const FeatureBlocks& /*blocks*/, TPushSD Push)
         mfxExtCodingOptionDDI* pCODDI = ExtBuffer::Get(par);
         MFX_CHECK(pCODDI, MFX_ERR_NONE);
 
-        SetDefault(pCODDI->IBC, IsOn(par.mfx.LowPower)? (mfxU16)MFX_CODINGOPTION_OFF : (mfxU16)MFX_CODINGOPTION_UNKNOWN);
-        SetDefault(pCODDI->Palette, IsOn(par.mfx.LowPower)? (mfxU16)MFX_CODINGOPTION_OFF : (mfxU16)MFX_CODINGOPTION_UNKNOWN);
+        SetDefault(pCODDI->IBC, (mfxU16)MFX_CODINGOPTION_ON);
+        SetDefault(pCODDI->Palette, (mfxU16)MFX_CODINGOPTION_ON);
 
         return MFX_ERR_NONE;
     });
