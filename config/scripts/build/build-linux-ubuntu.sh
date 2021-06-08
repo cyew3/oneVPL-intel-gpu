4af37b4a91f7eed2238f7142d87ddd63754c4849
Sergey Fomin <sergey.fomin@intel.com>
2021-06-08 21:16:43 +0300

    Added RHEL build (#1775)
    Issue: -
    Test: manual runs

--- config/scripts/build/build-linux-ubuntu.sh
+++ config/scripts/build/build-linux-ubuntu.sh
@@ -2,7 +2,9 @@
 set -ex
 
 mkdir -p $MFX_HOME/build && cd $MFX_HOME/build
-/opt/tools/cmake-latest/bin/cmake \
+# Workaround to get latest cmake on system
+if [ -f /opt/tools/cmake-latest/bin/cmake ]; then CMAKE=/opt/tools/cmake-latest/bin/cmake; else CMAKE=cmake;fi
+$CMAKE \
   -DCMAKE_INSTALL_PREFIX=/opt/intel/mediasdk \
   -DCMAKE_C_FLAGS_RELEASE="-O2 -D_FORTIFY_SOURCE=2 -fstack-protector-strong -DNDEBUG" \
   -DCMAKE_CXX_FLAGS_RELEASE="-O2 -D_FORTIFY_SOURCE=2 -fstack-protector-strong -DNDEBUG" \
