1ce458e6a8dcbeeaa4b83c6842933d5c495bbb79
Wang, Yuchen <yuchen.wang@intel.com>
2021-06-08 11:57:18 +0800

    [Decode] Cmake update for alignment of OS and CS (#1809)
    Split embargo only source_ext and update CmakeList to align with OS.
    
    Issue: VSMGWL-38855
    Test: -

--- _studio/mfx_lib/decode/sources_ext.cmake
+++ _studio/mfx_lib/decode/sources_ext.cmake
@@ -1,50 +1,22 @@
-if(MFX_DISABLE_SW_FALLBACK)
+if (OPEN_SOURCE)
   return()
 endif()
 
-list( APPEND vdirs
-  h264 h265 mjpeg mpeg2 vc1 vp8 vp9 av1
+list(APPEND sources
+    ${UMC_CODECS}/h265_dec/src/umc_h265_va_packer_dxva.cpp
+    ${UMC_CODECS}/mpeg2_dec/src/umc_mpeg2_dxva_packer.cpp
+    ${UMC_CODECS}/av1_dec/src/umc_av1_va_packer_dxva.cpp
+    ${UMC_CODECS}/av1_dec/src/umc_av1_va_packer_msft.cpp
+    ${MSDK_UMC_ROOT}/core/umc/src/umc_default_frame_allocator.cpp
 )
 
-list( APPEND cdirs
-  h265_dec h264_dec mpeg2_dec vp9_dec av1_dec vc1_dec jpeg_dec vc1_common color_space_converter jpeg_common
+list(APPEND sources_hw
+    ${CMAKE_CURRENT_SOURCE_DIR}/vp8/src/mfx_vp8_dec_decode_hw_dxva.cpp
 )
 
-foreach( dir ${vdirs} )
-  list (APPEND include ${CMAKE_CURRENT_SOURCE_DIR}/${dir}/include )
-endforeach()
-
-list (APPEND include ${MSDK_LIB_ROOT}/vpp/include )
-
-foreach( dir ${cdirs} )
-  list (APPEND include ${MSDK_UMC_ROOT}/codec/${dir}/include )
-endforeach( )
-
-set(UMC_CODECS ${MSDK_UMC_ROOT}/codec)
-
-list(APPEND source
-  ${UMC_CODECS}/jpeg_dec/src/jpegdec.cpp
-)
-
-# ================================ decode_ext_hw ================================
-
-target_include_directories(decode_hw PRIVATE ${include})
-
-target_sources(decode_hw PRIVATE ${source})
-
-target_link_libraries(decode_hw
-  PUBLIC
-    h264_dispatcher # optimized SW-path
-  PRIVATE 
-    media_buffers
-  )
-
-target_compile_definitions(decode_hw PRIVATE $<$<PLATFORM_ID:Windows>:MFX_DX9ON11>)
-
-# ================================ decode_ext_hw ================================
-
-target_include_directories(decode_sw PRIVATE ${include})
-
-target_sources(decode_sw PRIVATE ${source})
-target_link_libraries(decode_sw PRIVATE h264_dispatcher umc umc_io vm vm_plus media_buffers) 
-
+set(IPP_LIBS "")
+if (MFX_BUNDLED_IPP)
+  set(IPP_LIBS ipp)
+else()
+  set(IPP_LIBS IPP::msdk IPP::j IPP::vc IPP::cc IPP::cv IPP::i IPP::s IPP::core)
+endif()
