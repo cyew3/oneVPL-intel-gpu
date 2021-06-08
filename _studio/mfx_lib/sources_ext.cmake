dded335cedc192431bb86db2782601b5503d4160
Oleg Nabiullin <oleg.nabiullin@intel.com>
2021-06-08 12:20:48 -0700

    Restore exported symbol list on Windows (#1849)
    Fixup: https://github.com/intel-innersource/drivers.gpu.media.sdk.lib/commit/b8e1916e459842980d38ed2e1c1afeed2ea8cd29
    
    Issue: empty export list
    Test: CI

--- _studio/mfx_lib/sources_ext.cmake
+++ _studio/mfx_lib/sources_ext.cmake
@@ -1,3 +1,7 @@
+if (OPEN_SOURCE)
+  return()
+endif()
+
 if (CMAKE_SYSTEM_NAME MATCHES Windows)
   set(HWA_LIBS av1e_core_hwa av1e_fei_hwa av1_pp_avx2 av1_pp_ssse3 av1_pp_px genx_av1_encode_embeded)
   target_link_libraries(${mfxlibname}
@@ -6,14 +10,13 @@ if (CMAKE_SYSTEM_NAME MATCHES Windows)
   )
 endif()
 
-list(APPEND sources_common
+target_sources(${mfxlibname}
+  PRIVATE
     shared/src/mfx_check_hardware_support.cpp
     ${MSDK_STUDIO_ROOT}/shared/src/mfx_timing.cpp
     $<$<PLATFORM_ID:Windows>:libvpl.def>
     $<$<PLATFORM_ID:Windows>:libmfx-gen.rc>
-)
 
-list(APPEND sources_hw
     ${MSDK_STUDIO_ROOT}/shared/src/fast_compositing_ddi.cpp
     ${MSDK_STUDIO_ROOT}/shared/src/libmfx_allocator_d3d9.cpp
     ${MSDK_STUDIO_ROOT}/shared/src/libmfx_allocator_d3d11.cpp
@@ -25,9 +28,4 @@ list(APPEND sources_hw
     ${MSDK_STUDIO_ROOT}/shared/src/d3d11_decode_accelerator.cpp
     ${MSDK_STUDIO_ROOT}/shared/src/d3d11_video_processor.cpp
     ${MSDK_STUDIO_ROOT}/shared/src/mfx_dxva2_device.cpp
-  )
-
-return()
-
-#include(sources_libaudio.cmake)
-#include(sources_librt.cmake)
+)
