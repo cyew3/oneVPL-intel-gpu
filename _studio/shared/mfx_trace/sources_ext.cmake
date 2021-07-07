861e597b55407f70663a6aa08c646a764af8a65c
Oleg Nabiullin <oleg.nabiullin@intel.com>
2021-07-07 00:44:02 -0700

    [cmake] Fix Rebuild on VS (#2227)
    VS unexpectedly deleted media_sdk_etq.man during rebuild
    
    Issue: failed rebuilds
    Test: CI, local check

--- _studio/shared/mfx_trace/sources_ext.cmake
+++ _studio/shared/mfx_trace/sources_ext.cmake
@@ -6,7 +6,7 @@ target_include_directories(mfx_trace PRIVATE ${CMAKE_BINARY_DIR})
 
 add_custom_command(
     OUTPUT ${CMAKE_BINARY_DIR}/media_sdk_etw.h ${CMAKE_BINARY_DIR}/media_sdk_etw.rc
-    SOURCES ${MSDK_STUDIO_ROOT}/shared/mfx_trace/media_sdk_etw.man
+    MAIN_DEPENDENCY ${MSDK_STUDIO_ROOT}/shared/mfx_trace/media_sdk_etw.man
     COMMENT Running Windows Message Compiler to compile Media SDK ETW manifest
     COMMAND mc ARGS -h "${CMAKE_BINARY_DIR}" -r "${CMAKE_BINARY_DIR}" -z media_sdk_etw -um "${MSDK_STUDIO_ROOT}/shared/mfx_trace/media_sdk_etw.man"
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
