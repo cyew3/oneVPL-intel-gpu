1e7e31d953aa3713bcf2d72c24399d5e46af1ea2
Gurulev, Dmitry <dmitry.gurulev@intel.com>
2021-07-07 20:04:20 +0300

    [test]: amend HEVCE disp tests to use 'mocks::core' (#2178)
    - Use 'mocks::core' instead of handmade 'hevce_coremock.h :: CoreMock'
    
    Issue: MDP-66967
    Test: mfx_hevce_test

--- tests/unit/suites/hevce/hevce_disp.cpp
+++ tests/unit/suites/hevce/hevce_disp.cpp
@@ -36,24 +36,24 @@
 #include "hevcehw_g12xehp_lin.h"
 #include "hevcehw_g12dg2_lin.h"
 #endif
-#include "hevce_coremock.h"
+
+#include "mocks/include/mfx/core.h"
 
 namespace hevce { namespace tests
 {
-using Core = CoreMock;
 #if defined(MFX_VA_WIN)
-using TBase     = HEVCEHW::Windows::Base::MFXVideoENCODEH265_HW;
-using TGen11LKF = HEVCEHW::Windows::Gen11LKF::MFXVideoENCODEH265_HW;
-using TGen12    = HEVCEHW::Windows::Gen12::MFXVideoENCODEH265_HW;
-using TGen12XEHP = HEVCEHW::Windows::Gen12XEHP::MFXVideoENCODEH265_HW;
-using TGen12DG2 = HEVCEHW::Windows::Gen12DG2::MFXVideoENCODEH265_HW;
-#elif defined(MFX_VA_LINUX)
-using TBase     = HEVCEHW::Linux::Base::MFXVideoENCODEH265_HW;
-using TGen11LKF = HEVCEHW::Linux::Gen11LKF::MFXVideoENCODEH265_HW;
-using TGen12    = HEVCEHW::Linux::Gen12::MFXVideoENCODEH265_HW;
-using TGen12XEHP = HEVCEHW::Linux::Gen12XEHP::MFXVideoENCODEH265_HW;
-using TGen12DG2 = HEVCEHW::Linux::Gen12DG2::MFXVideoENCODEH265_HW;
+namespace HEVCERoot = HEVCEHW::Windows;
+#else
+#if defined(MFX_VA_LINUX)
+namespace HEVCERoot = HEVCEHW::Linux;
 #endif
+#endif
+
+using TBase      = HEVCERoot::Base::MFXVideoENCODEH265_HW;
+using TGen11LKF  = HEVCERoot::Gen11LKF::MFXVideoENCODEH265_HW;
+using TGen12     = HEVCERoot::Gen12::MFXVideoENCODEH265_HW;
+using TGen12XEHP = HEVCERoot::Gen12XEHP::MFXVideoENCODEH265_HW;
+using TGen12DG2  = HEVCERoot::Gen12DG2::MFXVideoENCODEH265_HW;
 
 template<typename T>
 inline bool IsGen(VideoENCODE*) { return false; }
@@ -99,12 +99,27 @@ inline bool IsGen<TGen12DG2>(VideoENCODE* p)
         && !dynamic_cast<TGen12XEHP*>(p);
 }
 
-TEST(Disp, Unsupported)
+struct Disp
+    : testing::TestWithParam<eMFXHWType>
 {
-    Core vcore;
-    mfxStatus sts = MFX_ERR_NONE;
-    eMFXHWType hwSet[] = {
-        MFX_HW_UNKNOWN
+    std::unique_ptr<mocks::mfx::core> core;
+
+    void SetUp() override
+    {
+        core = mocks::mfx::make_core(
+            GetParam(),
+#if defined(MFX_VA_WIN)
+            MFX_HW_D3D11
+#elif defined(MFX_VA_LINUX)
+            MFX_HW_VAAPI
+#endif
+        );
+    }
+};
+
+INSTANTIATE_TEST_SUITE_P(Unsupported, Disp,
+    testing::Values(
+          MFX_HW_UNKNOWN
         , MFX_HW_SNB
         , MFX_HW_IVB
         , MFX_HW_HSW
@@ -112,23 +127,17 @@ TEST(Disp, Unsupported)
         , MFX_HW_VLV
         , MFX_HW_BDW
         , MFX_HW_CHT
-    };
+    )
+);
 
-    for (auto hw : hwSet)
-    {
-        vcore.m_hw = hw;
+struct BaseDisp
+    : Disp
+    , testing::WithParamInterface<eMFXHWType>
+{};
 
-        EXPECT_EQ(std::unique_ptr<VideoENCODE>(HEVCEHW::Create((VideoCORE&)vcore, sts)).get(), (VideoENCODE*)nullptr);
-        EXPECT_EQ(sts, MFX_ERR_UNSUPPORTED);
-    }
-}
-
-TEST(Disp, Base)
-{
-    Core vcore;
-    mfxStatus sts = MFX_ERR_NONE;
-    eMFXHWType hwSet[] = {
-        MFX_HW_SCL
+INSTANTIATE_TEST_SUITE_P(Supported, BaseDisp,
+    testing::Values(
+          MFX_HW_SCL
         , MFX_HW_APL
         , MFX_HW_KBL
         , MFX_HW_GLK
@@ -138,101 +147,116 @@ TEST(Disp, Base)
         , MFX_HW_ICL_LP
         , MFX_HW_CNX_G
         , MFX_HW_EHL
-    };
+    )
+);
 
-    for (auto hw : hwSet)
-    {
-        vcore.m_hw = hw;
-
-        std::unique_ptr<VideoENCODE> pHEVC(HEVCEHW::Create((VideoCORE&)vcore, sts));
-        EXPECT_NE(pHEVC.get(), (VideoENCODE*)nullptr);
-        EXPECT_EQ(sts, MFX_ERR_NONE);
-        EXPECT_TRUE(IsGen<TBase>(pHEVC.get()));
-    }
+TEST_P(Disp, Create)
+{
+    mfxStatus sts = MFX_ERR_NONE;
+    EXPECT_EQ(std::unique_ptr<VideoENCODE>(HEVCEHW::Create(*core, sts)).get(), nullptr);
+    EXPECT_EQ(sts, MFX_ERR_UNSUPPORTED);
 }
 
-TEST(Disp, Gen11LKF)
+TEST_P(BaseDisp, Create)
 {
-    Core vcore;
     mfxStatus sts = MFX_ERR_NONE;
-    eMFXHWType hwSet[] = {
-        MFX_HW_LKF
+
+    std::unique_ptr<VideoENCODE> pHEVC(HEVCEHW::Create(*core, sts));
+    EXPECT_NE(pHEVC.get(), nullptr);
+    EXPECT_EQ(sts, MFX_ERR_NONE);
+    EXPECT_TRUE(IsGen<TBase>(pHEVC.get()));
+}
+
+struct Gen11LKF
+    : Disp
+    , testing::WithParamInterface<eMFXHWType>
+{};
+
+INSTANTIATE_TEST_SUITE_P(Supported, Gen11LKF,
+    testing::Values(
+          MFX_HW_LKF
         , MFX_HW_JSL
-    };
+    )
+);
 
-    for (auto hw : hwSet)
-    {
-        vcore.m_hw = hw;
+TEST_P(Gen11LKF, Create)
+{
+    mfxStatus sts = MFX_ERR_NONE;
 
-        std::unique_ptr<VideoENCODE> pHEVC(HEVCEHW::Create((VideoCORE&)vcore, sts));
-        EXPECT_NE(pHEVC.get(), (VideoENCODE*)nullptr);
-        EXPECT_EQ(sts, MFX_ERR_NONE);
-        EXPECT_TRUE(IsGen<TGen11LKF>(pHEVC.get()));
-    }
+    std::unique_ptr<VideoENCODE> pHEVC(HEVCEHW::Create(*core, sts));
+    EXPECT_NE(pHEVC.get(), nullptr);
+    EXPECT_EQ(sts, MFX_ERR_NONE);
+    EXPECT_TRUE(IsGen<TGen11LKF>(pHEVC.get()));
 }
 
-TEST(Disp, Gen12)
-{
-    Core vcore;
-    mfxStatus sts = MFX_ERR_NONE;
-    eMFXHWType hwSet[] = {
-        MFX_HW_TGL_LP
+struct Gen12
+    : Disp
+    , testing::WithParamInterface<eMFXHWType>
+{};
+
+INSTANTIATE_TEST_SUITE_P(Supported, Gen12,
+    testing::Values(
+          MFX_HW_TGL_LP
         , MFX_HW_RYF
         , MFX_HW_RKL
         , MFX_HW_DG1
         , MFX_HW_ADL_S
         , MFX_HW_ADL_P
-    };
+    )
+);
 
-    for (auto hw : hwSet)
-    {
-        vcore.m_hw = hw;
+TEST_P(Gen12, Create)
+{
+    mfxStatus sts = MFX_ERR_NONE;
 
-        std::unique_ptr<VideoENCODE> pHEVC(HEVCEHW::Create((VideoCORE&)vcore, sts));
-        EXPECT_NE(pHEVC.get(), (VideoENCODE*)nullptr);
-        EXPECT_EQ(sts, MFX_ERR_NONE);
-        EXPECT_TRUE(IsGen<TGen12>(pHEVC.get()));
-    }
+    std::unique_ptr<VideoENCODE> pHEVC(HEVCEHW::Create(*core, sts));
+    EXPECT_NE(pHEVC.get(), nullptr);
+    EXPECT_EQ(sts, MFX_ERR_NONE);
+    EXPECT_TRUE(IsGen<TGen12>(pHEVC.get()));
 }
 
-TEST(Disp, Gen12XEHP)
+struct Gen12XEHP
+    : Disp
+    , testing::WithParamInterface<eMFXHWType>
+{};
+
+INSTANTIATE_TEST_SUITE_P(Supported, Gen12XEHP,
+    testing::Values(
+          MFX_HW_XE_HP_SDV
+    )
+);
+
+TEST_P(Gen12XEHP, Create)
 {
-    Core vcore;
     mfxStatus sts = MFX_ERR_NONE;
-    eMFXHWType hwSet[] = {
-        MFX_HW_XE_HP_SDV
-    };
 
-    for (auto hw : hwSet)
-    {
-        vcore.m_hw = hw;
-
-        std::unique_ptr<VideoENCODE> pHEVC(HEVCEHW::Create((VideoCORE&)vcore, sts));
-        EXPECT_NE(pHEVC.get(), (VideoENCODE*)nullptr);
-        EXPECT_EQ(sts, MFX_ERR_NONE);
-        EXPECT_TRUE(IsGen<TGen12XEHP>(pHEVC.get()));
-    }
+    std::unique_ptr<VideoENCODE> pHEVC(HEVCEHW::Create(*core, sts));
+    EXPECT_NE(pHEVC.get(), nullptr);
+    EXPECT_EQ(sts, MFX_ERR_NONE);
+    EXPECT_TRUE(IsGen<TGen12XEHP>(pHEVC.get()));
 }
 
-TEST(Disp, Gen12DG2)
-{
-    Core vcore;
-    mfxStatus sts = MFX_ERR_NONE;
-    eMFXHWType hwSet[] = {
-        MFX_HW_DG2
+struct Gen12DG2
+    : Disp
+    , testing::WithParamInterface<eMFXHWType>
+{};
+
+INSTANTIATE_TEST_SUITE_P(Supported, Gen12DG2,
+    testing::Values(
+          MFX_HW_DG2
         , MFX_HW_PVC
         , eMFXHWType(0xfffffff)
-    };
+    )
+);
 
-    for (auto hw : hwSet)
-    {
-        vcore.m_hw = hw;
+TEST_P(Gen12DG2, Create)
+{
+    mfxStatus sts = MFX_ERR_NONE;
 
-        std::unique_ptr<VideoENCODE> pHEVC(HEVCEHW::Create((VideoCORE&)vcore, sts));
-        EXPECT_NE(pHEVC.get(), (VideoENCODE*)nullptr);
-        EXPECT_EQ(sts, MFX_ERR_NONE);
-        EXPECT_TRUE(IsGen<TGen12DG2>(pHEVC.get()));
-    }
+    std::unique_ptr<VideoENCODE> pHEVC(HEVCEHW::Create(*core, sts));
+    EXPECT_NE(pHEVC.get(), nullptr);
+    EXPECT_EQ(sts, MFX_ERR_NONE);
+    EXPECT_TRUE(IsGen<TGen12DG2>(pHEVC.get()));
 }
 
 }}
